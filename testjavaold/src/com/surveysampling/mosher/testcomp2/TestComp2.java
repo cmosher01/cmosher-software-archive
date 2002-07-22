9319a51338d36b788f68940cbd573184aa69299f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-07-22 16:34:29 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@16 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/testcomp2/TestComp2.java
@@ -0,0 +1,13 @@
+package com.surveysampling.mosher.testcomp2;
+
+import com.surveysampling.mosher.testcomp.TestComp;
+
+public class TestComp2 extends TestComp
+{
+	public TestComp2() {}
+	public static void main(String[] rArg)
+	{
+		TestComp2 a = new TestComp2();
+		a.x();
+	}
+}
