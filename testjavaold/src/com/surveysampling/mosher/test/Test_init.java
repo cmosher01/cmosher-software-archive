53213228d86ea9f2b7dce05bb938cd4d76794963
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-07-31 18:23:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@19 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/test/Test_init.java
@@ -0,0 +1,25 @@
+// Decompiled by DJ v3.0.0.63 Copyright 2002 Atanas Neshkov  Date: 6/24/2002 12:04:26 PM
+// Home Page : http://members.fortunecity.com/neshkov/dj.html  - Check often for new version!
+// Decompiler options: packimports(3) disassembler 
+// Source File Name:   Test.java
+
+package com.surveysampling.mosher.test;
+
+
+public class Test_init
+{
+
+    Test_init()
+    {
+    //    0    0:aload_0         
+    //    1    1:invokespecial   #5   <Method void Object()>
+    //    2    4:aconst_null     
+    //    3    5:astore_1        
+    //    4    6:new             #4   <Class String>
+    //    5    9:dup             
+    //    6   10:ldc1            #1   <String "foo">
+    //    7   12:invokespecial   #6   <Method void String(String)>
+    //    8   15:astore_1        
+    //    9   16:return          
+    }
+}
