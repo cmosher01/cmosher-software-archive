10e932fa4bea49d261795485a10b166fa86601ae
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-08-15 23:23:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@127 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- a2diskedit/src/nu/mosher/mine/a2diskedit/CatEntryDos33.java
+++ a2diskedit/src/nu/mosher/mine/a2diskedit/CatEntryDos33.java
@@ -3,4 +3,13 @@ package nu.mosher.mine.a2diskedit;
 public class CatEntryDos33 extends CatEntry
 {
 
+	/**
+	 * Constructor for CatEntryDos33.
+	 * @param rb
+	 */
+	public CatEntryDos33(byte[] rb)
+	{
+		super(rb);
+	}
+
 }
