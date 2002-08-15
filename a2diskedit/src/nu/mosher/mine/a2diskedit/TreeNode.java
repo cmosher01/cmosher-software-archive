75df52364539a00da9a94e63747a3d50e0d55270
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-08-15 23:37:25 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@138 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- a2diskedit/src/nu/mosher/mine/a2diskedit/TreeNode.java
+++ a2diskedit/src/nu/mosher/mine/a2diskedit/TreeNode.java
@@ -2,10 +2,12 @@ package nu.mosher.mine.a2diskedit;
 
 import java.awt.Component;
 import javax.swing.Icon;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public interface TreeNode
 {
 	public String toString();
 	public Icon getIcon();
 	public Component getRightPane();
+	public void setNode(DefaultMutableTreeNode n);
 }
