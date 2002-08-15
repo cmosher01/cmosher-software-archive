a467aa7693a8707966c20519d7fb0537da89d799
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-08-15 01:01:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@72 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- a2diskedit/src/nu/mosher/mine/a2diskedit/A2DiskImage.java
+++ a2diskedit/src/nu/mosher/mine/a2diskedit/A2DiskImage.java
@@ -12,6 +12,7 @@ public class A2DiskImage
 	public A2DiskImage(byte[] image)
 	{
 		img = image;
+		parseImage();
 	}
 
 	public byte getByte(int track, int sector, int offset)
@@ -24,5 +25,59 @@ public class A2DiskImage
 	{
 		int i = block*BYTES_PER_BLOCK + offset;
 		return img[i];
+	}
+
+	private void parseImage()
+	{
+		fixOrder();
+	}
+
+	private void fixOrder()
+	{
+		if (
+			getByte(0,0xB,0)==0 &&
+			getByte(0,0xB,1)==0 &&
+			getByte(0,0xB,2)==3 &&
+			getByte(0,0xB,3)==0 &&
+			getByte(0,0xB,4)>>4==0xF)
+		{
+			m_osType = osProdos;
+			// Prodos disk in track-sector order
+			// switch to block order
+			SwitchTSBlock();
+		}
+		else if (
+			getByte([2][0]==0 &&
+			getByte([2][1]==0 &&
+			getByte([2][2]==3 &&
+			getByte([2][3]==0 &&
+			getByte([2][4]>>4==0xF)
+		{
+			m_osType = osProdos;
+			// Prodos disk in block order
+			// OK
+		}
+		else if (
+			getByte([0x11][0xE][1]==0x11 &&
+			getByte([0x11][0xE][2]==0x0D)
+		{
+			m_osType = osDos33;
+			// DOS 3.3 disk in track-sector order
+			// OK
+		}
+		else if (
+			getByte([0x88][0x101]==0x11 &&
+			getByte([0x88][0x102]==0x0D)
+		{
+			m_osType = osDos33;
+			// DOS 3.3 disk in block order
+			// switch to track-sector order
+			SwitchTSBlock();
+		}
+		else
+		{
+			AfxMessageBox("Cannot determine type of disk.");
+			AfxThrowUserException();
+		}
 	}
 }
