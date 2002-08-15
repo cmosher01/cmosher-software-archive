0f9ffdab56c22301ae40565ee7f6c8818162c9a6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2002-08-15 00:56:37 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@69 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- a2diskedit/src/nu/mosher/mine/a2diskedit/A2DiskImage.java
+++ a2diskedit/src/nu/mosher/mine/a2diskedit/A2DiskImage.java
@@ -3,8 +3,24 @@ package nu.mosher.mine.a2diskedit;
 public class A2DiskImage
 {
 	private final byte[] img;
+
+	private final int BYTES_PER_BLOCK = 0x200;
+
+	private final int BYTES_PER_SECTOR = 0x100;
+	private final int SECTORS_PER_TRACK = 0x10;
+
 	public A2DiskImage(byte[] image)
 	{
 		img = image;
+	}
+
+	public byte getByte(int track, int sector, int offset)
+	{
+		return 0;
+	}
+
+	public byte getByte(int block, int offset)
+	{
+		return 0;
 	}
 }
