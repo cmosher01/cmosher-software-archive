6677be5a6893b0b3f4f0c3f5d289325cf02e0405
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-03-20 21:29:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@207 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testpool/foo/FooDefault.java
@@ -0,0 +1,21 @@
+package foo;
+
+class FooDefault implements Foo
+{
+    private static int count = 0;
+    private int id = count++;
+
+    FooDefault()
+    {
+    }
+
+    public void foo()
+    {
+        System.out.println("Foo::foo says: " + this);
+    }
+
+    public String toString()
+    {
+        return "foo: " + id;
+    }
+}
