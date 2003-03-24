0af0d7631cb85a38233e17bf55d78f87d76d76be
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-03-24 19:55:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@231 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testpool/pool4/PassThrough.java
@@ -0,0 +1,19 @@
+package pool4;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class PassThrough implements InvocationHandler
+{
+    private final Object obj;
+
+    public PassThrough(Object obj)
+    {
+        this.obj = obj;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        return method.invoke(obj,args);
+    }
+}
