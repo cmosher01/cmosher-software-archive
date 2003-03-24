cc63af307ba3b6f54cc84de32609ee0e3608c400
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-03-24 18:43:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@219 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testpool/pool2/FooProxy.java
@@ -0,0 +1,21 @@
+package pool2;
+
+class FooProxy implements Foo
+{
+    private Foo delegate;
+
+    FooProxy(Foo f)
+    {
+        delegate = f;
+    }
+
+    public void foo()
+    {
+        delegate.foo();
+    }
+
+    public String toString()
+    {
+        return delegate.toString();
+    }
+}
