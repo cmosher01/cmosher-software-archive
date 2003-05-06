66a01804e0cc6447bec8f2f5eb0e09ebaa26f3a2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-05-06 23:27:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@244 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ groapplet/src/to/go/gro/groapplet/FamilyChart.java
@@ -0,0 +1,59 @@
+import java.awt.AWTEvent;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+
+public class FamilyChart extends JPanel
+{
+    private final IndiSet mIndis;
+    private final FamiSet mFamis;
+    private boolean mInitialized;
+
+    public FamilyChart(IndiSet indis, FamiSet famis)
+    {
+        mIndis = indis;
+        mFamis = famis;
+    }
+
+    public void paint(Graphics g)
+    {
+        if (!mInitialized)
+            init(g);
+
+        Rectangle clip = g.getClipBounds();
+        g.clearRect(clip.x,clip.y,clip.width,clip.height);
+
+        mFamis.paint(g);
+        mIndis.paint(g);
+    }
+
+    protected void init(Graphics g)
+    {
+        Rectangle bounds = mIndis.init(g);
+        mFamis.init(g);
+
+        Dimension dimBounds = new Dimension(bounds.width,bounds.height);
+        setSize(dimBounds);
+        setPreferredSize(dimBounds);
+
+        enableEvents(AWTEvent.MOUSE_MOTION_EVENT_MASK);
+
+        mInitialized = true;
+    }
+
+    public boolean isOnIndi(Point point)
+    {
+        return mIndis.isOnIndi(point);
+    }
+
+    protected void processMouseMotionEvent(MouseEvent e)
+    {
+//        if (isOnIndi(e.getPoint()))
+//            setCursor();
+        super.processMouseMotionEvent(e);
+    }
+}
