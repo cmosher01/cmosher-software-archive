5c6f3d8e96f6a5948a4ca7114fa44d0967b2dab4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-05-11 03:03:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@261 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ groapplet/src/to/go/gro/groapplet/GROMouseListener.java
@@ -0,0 +1,20 @@
+/*
+ * Created on May 10, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package to.go.gro.groapplet;
+
+import javax.swing.event.MouseInputAdapter;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class GROMouseListener extends MouseInputAdapter
+{
+
+}
