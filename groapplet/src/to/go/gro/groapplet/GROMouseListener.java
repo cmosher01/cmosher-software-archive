c8a9f7a26f6b9f1a6d2c41c5bec82f13540d716a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-05-11 03:20:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@279 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- groapplet/src/to/go/gro/groapplet/GROMouseListener.java
+++ groapplet/src/to/go/gro/groapplet/GROMouseListener.java
@@ -1,26 +1,36 @@
 package to.go.gro.groapplet;
 
+import java.awt.Component;
 import java.awt.event.MouseEvent;
 
 import javax.swing.event.MouseInputAdapter;
 
 public class GROMouseListener extends MouseInputAdapter
 {
-    public void mousePressed(MouseEvent arg0)
+	private Component mLastHit;
+	private final FamilyChart fc;
+
+	public GROMouseListener(FamilyChart fc)
+	{
+		this.fc = fc;
+	}
+
+    public void mousePressed(MouseEvent evt)
     {
-        // TODO Auto-generated method stub
-        super.mousePressed(arg0);
+		mLastHit = evt.getComponent();
+		evt.getPoint();
+        super.mousePressed(evt);
     }
 
-    public void mouseReleased(MouseEvent arg0)
+    public void mouseReleased(MouseEvent evt)
     {
         // TODO Auto-generated method stub
-        super.mouseReleased(arg0);
+        super.mouseReleased(evt);
     }
 
-    public void mouseDragged(MouseEvent arg0)
+    public void mouseDragged(MouseEvent evt)
     {
         // TODO Auto-generated method stub
-        super.mouseDragged(arg0);
+        super.mouseDragged(evt);
     }
 }
