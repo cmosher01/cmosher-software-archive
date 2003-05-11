a5946faed9f4fdbdb7fab1d826643b2d3ca0bcae
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-05-11 03:08:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@267 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- groapplet/src/to/go/gro/groapplet/GROMouseListener.java
+++ groapplet/src/to/go/gro/groapplet/GROMouseListener.java
@@ -6,31 +6,22 @@ import javax.swing.event.MouseInputAdapter;
 
 public class GROMouseListener extends MouseInputAdapter
 {
-    /* (non-Javadoc)
-     * @see java.awt.event.MouseMotionListener#mouseMoved(java.awt.event.MouseEvent)
-     */
-    public void mouseMoved(MouseEvent arg0)
-    {
-        // TODO Auto-generated method stub
-        super.mouseMoved(arg0);
-    }
-
-    /* (non-Javadoc)
-     * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
-     */
     public void mousePressed(MouseEvent arg0)
     {
         // TODO Auto-generated method stub
         super.mousePressed(arg0);
     }
 
-    /* (non-Javadoc)
-     * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
-     */
     public void mouseReleased(MouseEvent arg0)
     {
         // TODO Auto-generated method stub
         super.mouseReleased(arg0);
+    }
+
+    public void mouseDragged(MouseEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        super.mouseDragged(arg0);
     }
 
 }
