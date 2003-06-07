6aceb9033e667721348b67dbbac752f1b689fa64
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-07 16:35:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@353 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ play/src/Player.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Jun 7, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Player
+{
+
+    /**
+     * 
+     */
+    public Player()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+    }
+}
