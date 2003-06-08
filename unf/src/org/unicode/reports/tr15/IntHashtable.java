cc1d8c410c54d81e052ca418740649f1c2b8baa4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-08 22:37:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@425 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/src/org/unicode/reports/tr15/IntHashtable.java
+++ unf/src/org/unicode/reports/tr15/IntHashtable.java
@@ -5,28 +5,36 @@ import java.util.Hashtable;
  * Integer hash table. Uses Java Hashtable for now.
  * @author Mark Davis
  */
- 
-public class IntHashtable {
+
+public class IntHashtable
+{
     static final String copyright = "Copyright © 1998-1999 Unicode, Inc.";
-    
-    public IntHashtable (int defaultValue) {
+
+    public IntHashtable(int defaultValue)
+    {
         this.defaultValue = defaultValue;
     }
-    
-    public void put(int key, int value) {
-        if (value == defaultValue) {
+
+    public void put(int key, int value)
+    {
+        if (value == defaultValue)
+        {
             table.remove(new Integer(key));
-        } else {
+        }
+        else
+        {
             table.put(new Integer(key), new Integer(value));
         }
     }
-    
-    public int get(int key) {
+
+    public int get(int key)
+    {
         Object value = table.get(new Integer(key));
-        if (value == null) return defaultValue;
+        if (value == null)
+            return defaultValue;
         return ((Integer)value).intValue();
     }
-    
+
     private int defaultValue;
     private Hashtable table = new Hashtable();
 }
