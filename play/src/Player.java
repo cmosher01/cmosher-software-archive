a7b209cfe8dfe7bae77e6dfa81fc39275ab8eeec
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-08 00:20:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@357 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- play/src/Player.java
+++ play/src/Player.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.logging.Logger;
 
 import nu.mine.mosher.core.Immutable;
@@ -28,11 +32,33 @@ public class Player implements Immutable
         super();
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
     	LoggingInitializer.init();
-    	Player p = new Player();
-    	p.play();
+        BufferedReader in;
+
+        if (args.length > 0)
+        {
+            // open the named file
+            in = new BufferedReader(new FileReader(args[0]));
+        }
+        else
+        {
+            // wrap a BufferedReader around stdin
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        PrintStream out = new PrintStream(System.out,false,"x-gedcom-ansel");
+
+        String s = in.readLine();
+        while (s != null)
+        {
+            out.println(s);
+			s = in.readLine();
+        }
+
+        out.flush();
+        out.close();
     }
 
     /**
