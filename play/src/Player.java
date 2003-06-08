a49260aeb8da3690f2a95b6cace31d55cdfd8cbe
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-08 01:14:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@376 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- play/src/Player.java
+++ play/src/Player.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.nio.charset.spi.CharsetProvider;
 import java.util.logging.Logger;
 
 import nu.mine.mosher.core.Immutable;
