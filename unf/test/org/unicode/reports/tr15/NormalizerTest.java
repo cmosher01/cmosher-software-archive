e1e5d673e3002a4644ee423d8bff17673cce1cce
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-08 22:07:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@423 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Jun 8, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.unicode.reports.tr15;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class NormalizerTest extends TestCase
+{
+
+    /**
+     * Constructor for NormalizerTest.
+     * @param name
+     */
+    public NormalizerTest(String name)
+    {
+        super(name);
+    }
+
+    /*
+     * Test for StringBuffer normalize(String, StringBuffer)
+     */
+    public void testNormalizeStringStringBuffer()
+    {
+    }
+
+}
