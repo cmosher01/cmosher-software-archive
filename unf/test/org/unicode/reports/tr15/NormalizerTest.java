1cbb03b3acca0abd04df940f3efa298df1984ee5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-09 22:43:46 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@437 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/test/org/unicode/reports/tr15/NormalizerTest.java
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -15,5 +15,22 @@ public class NormalizerTest extends TestCase
     public void testNormalizeStringStringBuffer()
     {
     	Normalizer n = new Normalizer();
+    	String s = "\u0041\u030a";
+    	String sn = n.normalize(s);
+    	dumphex(sn);
     }
+
+	private static void dumphex(String s)
+	{
+		char[] rc = s.toCharArray();
+		for (int i = 0; i < rc.length; ++i)
+		{
+			char c = rc[i];
+			String h = Integer.toHexString(c);
+			System.out.print(h);
+			System.out.print(" ");
+		}
+		System.out.println();
+	}
+
 }
