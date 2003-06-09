abb200c05f7d253271cd555157a0dc74b0cebf49
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-09 23:48:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@450 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/test/org/unicode/reports/tr15/NormalizerTest.java
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -28,41 +28,14 @@ public class NormalizerTest extends TestCase
 		assertEquals(n.normalize(pre),comp);
 	}
 
-	private static void dumphex(String s)
-	{
-		char[] rc = s.toCharArray();
-		for (int i = 0; i < rc.length; ++i)
-		{
-			char c = rc[i];
-			String h = Integer.toHexString(c);
-			System.out.print(h);
-			System.out.print(" ");
-		}
-		System.out.println();
-	}
-
-	public static void main(String[] rArg) throws Throwable
-	{
-		Normalizer n = new Normalizer();
-		String s = "\u212bChris";
-		dumphex(s);
-		String sn = n.normalize(s);
-		dumphex(sn);
-	}
-
     protected void setUp() throws Exception
     {
         super.setUp();
         n = new Normalizer();
     }
 
-    /* (non-Javadoc)
-     * @see junit.framework.TestCase#tearDown()
-     */
     protected void tearDown() throws Exception
     {
-        // TODO Auto-generated method stub
         super.tearDown();
     }
-
 }
