fad479c2740a52cfa219f4a85dc3d2415678903a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-09 23:49:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@453 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/test/org/unicode/reports/tr15/NormalizerTest.java
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -21,6 +21,7 @@ public class NormalizerTest extends TestCase
 		assertNormalize("Chris","Chris");
 		assertNormalize("\u0041\u030a","\u00c5");
 		assertNormalize("\u212b","\u00c5");
+		assertNormalize("Henry \u2163","Henry \u2163");
     }
 
 	protected void assertNormalize(String pre, String comp)
