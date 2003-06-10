ded0bf1b7b10561a059d6b0f650faa533d436fbf
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-10 00:48:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@459 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/test/org/unicode/reports/tr15/NormalizerTest.java
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -3,6 +3,7 @@ package org.unicode.reports.tr15;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import junit.framework.TestCase;
