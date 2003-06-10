63f21715f8e149bb0717c0e7db347ab9ccdad256
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-10 00:47:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@457 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- unf/test/org/unicode/reports/tr15/NormalizerTest.java
+++ unf/test/org/unicode/reports/tr15/NormalizerTest.java
@@ -1,5 +1,10 @@
 package org.unicode.reports.tr15;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import junit.framework.TestCase;
 
 public class NormalizerTest extends TestCase
@@ -23,6 +28,14 @@ public class NormalizerTest extends TestCase
 		assertNormalize("\u212b","\u00c5");
 		assertNormalize("Henry \u2163","Henry \u2163");
     }
+
+	/*
+	 * Unicode Normalization Test Suite (see http://www.unicode.org/Public/UNIDATA/UCD.html re NormalizationTest)
+	 */
+	public void testNormalizationTestTxt()
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File("NormalizationTest.txt"))));
+	}
 
 	protected void assertNormalize(String pre, String comp)
 	{
