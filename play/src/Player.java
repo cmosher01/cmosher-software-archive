0f34812da793150f61ac05ffd99deab7bd774b34
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-19 00:46:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@516 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- play/src/Player.java
+++ play/src/Player.java
@@ -13,19 +13,6 @@ import java.util.logging.Logger;
 import nu.mine.mosher.core.Immutable;
 import nu.mine.mosher.logging.LoggingInitializer;
 
-/*
- * Created on Jun 7, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
-
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class Player implements Immutable
 {
 	/**
