d2571681073e56a2766e9d1adfc1663ec31430ee
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-29 14:12:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@530 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb/src/nu/mine/mosher/gedcomold/Gedcom.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jun 29, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.gedcom;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Gedcom
+{
+
+}
