857378383343d742be5131d85e9fb4724aa34a33
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-06-29 14:16:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@531 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/Gedcom.java
+++ grodb/src/nu/mine/mosher/gedcomold/Gedcom.java
@@ -1,18 +1,14 @@
-/*
- * Created on Jun 29, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.gedcom;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import java.io.InputStream;
+
 public class Gedcom
 {
+	private Gedcom()
+	{
+	}
 
+	public String guessCharset(InputStream in)
+	{
+	}
 }
