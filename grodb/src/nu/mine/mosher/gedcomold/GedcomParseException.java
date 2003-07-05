ba27a49ffaaf984d8cb01179c384566c228d5052
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-05 03:20:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@565 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb/src/nu/mine/mosher/gedcomold/GedcomParseException.java
@@ -0,0 +1,19 @@
+package nu.mine.mosher.gedcom;
+
+public class GedcomParseException extends Exception
+{
+	private String rawGedcomLine;
+	private GedcomLine parsedGedcomLine;
+	public GedcomParseException(String message, String rawGedcomLine, GedcomLine parsedGedcomLine)
+	{
+		super(message);
+		this.rawGedcomLine = rawGedcomLine;
+		this.parsedGedcomLine = parsedGedcomLine;
+	}
+	public GedcomParseException(String message, Exception cause)
+	{
+		super(message,cause);
+		this.rawGedcomLine = "";
+		this.parsedGedcomLine = null;
+	}
+}
