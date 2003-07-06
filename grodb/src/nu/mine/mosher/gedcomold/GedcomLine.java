b831545cf5a9a36e28036537ae1b050ec3e07e0b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-06 17:35:02 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@576 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb/src/nu/mine/mosher/gedcomold/GedcomLine.java
@@ -0,0 +1,80 @@
+package nu.mine.mosher.gedcom;
+
+public class GedcomLine
+{
+	private final int level;
+	private final String id;
+	private final String tag;
+	private final String value;
+	private final String pointer;
+	public GedcomLine(int level, String id, String tag, String value)
+	{
+		this.level = level;
+		this.id = getPointer(id);
+		this.tag = tag;
+		String v = getPointer(value);
+		if (v.length() > 0)
+		{
+			this.pointer = v;
+			this.value = "";
+		}
+		else
+		{
+			this.value = replaceAts(value);
+			this.pointer = "";
+		}
+	}
+	public static String getPointer(String s)
+	{
+		if (!s.startsWith("@") || !s.endsWith("@") || s.length() < 3)
+		{
+			return "";
+		}
+
+		String pointer = s.substring(1,s.length()-1);
+		if (pointer.indexOf('@') >= 0)
+		{
+			return "";
+		}
+		return pointer;
+	}
+	public static String replaceAts(String s)
+	{
+		return s.replaceAll("@@","@");
+	}
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer(256);
+		sb.append(level);
+		sb.append(",");
+		if (hasID())
+		{
+			sb.append("id=");
+			sb.append(id);
+			sb.append(",");
+		}
+		sb.append("tag=");
+		sb.append(tag);
+		sb.append(",");
+		if (isPointer())
+		{
+			sb.append("pointer=");
+			sb.append(pointer);
+		}
+		else
+		{
+			sb.append("value=\"");
+			sb.append(value);
+			sb.append("\"");
+		}
+		return sb.toString();
+	}
+	public boolean hasID()
+	{
+		return id.length() > 0;
+	}
+	public boolean isPointer()
+	{
+		return pointer.length() > 0;
+	}
+}
