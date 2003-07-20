8740a7cca17d15721effd33611ac9e58e7b90fed
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-20 03:38:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@591 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/GedcomTree.java
+++ grodb/src/nu/mine/mosher/gedcomold/GedcomTree.java
@@ -1,18 +1,14 @@
-/*
- * Created on Jul 19, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.gedcom;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class GedcomTree
 {
+	private int prevLevel = -1;
 
+	public void appendLine(GedcomLine line)
+	{
+		if (prevLevel+1 < line.getLevel())
+		{
+			throw new IllegalLevel(line);
+		}
+	}
 }
