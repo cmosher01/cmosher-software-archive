8945c2d14b3d702cbe1608601ed43ec619104e2e
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-20 03:40:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@600 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/InvalidLevel.java
+++ grodb/src/nu/mine/mosher/gedcomold/InvalidLevel.java
@@ -12,9 +12,9 @@ package nu.mine.mosher.gedcom;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class IllegalLevelChange extends Exception
+public class InvalidLevel extends Exception
 {
-    public IllegalLevelChange(GedcomLine line)
+    public InvalidLevel(GedcomLine line)
     {
         super("GEDCOM line has illegal level number at line: "+line);
     }
