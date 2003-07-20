c4cd2718f34af369124d0081f887f2357c5da2f9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-20 03:39:58 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@598 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/IllegalLevel.java
+++ grodb/src/nu/mine/mosher/gedcomold/IllegalLevel.java
@@ -1,21 +1,9 @@
-/*
- * Created on Jul 19, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.gedcom;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
-public class IllegalLevel extends Exception
+public class IllegalLevel extends GedcomParseException
 {
-    public IllegalLevel(GedcomLine line)
-    {
-        super("GEDCOM line has illegal level number at line: "+line);
-    }
+	public IllegalLevel(String rawGedcomLine, GedcomLine parsedGedcomLine)
+	{
+		super("GEDCOM line has an invalid level number",rawGedcomLine,parsedGedcomLine);
+	}
 }
