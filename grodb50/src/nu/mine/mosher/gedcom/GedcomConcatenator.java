96e7582a25ba9f37bc8d09ad5bbc9e1c8062389c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-23 03:56:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@649 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/src/nu/mine/mosher/gedcom/GedcomConcatenator.java
@@ -0,0 +1,47 @@
+package nu.mine.mosher.gedcom;
+
+import java.util.Iterator;
+
+import nu.mine.mosher.util.TreeNode;
+
+public class GedcomConcatenator
+{
+	private final GedcomTree tree;
+
+	public GedcomConcatenator(GedcomTree tree)
+	{
+		this.tree = tree;
+	}
+
+	public void concatenate()
+	{
+		concatenateHelper(tree.getRoot());
+	}
+
+	private void concatenateHelper(TreeNode parent)
+	{
+		GedcomLine parentLine = (GedcomLine)parent.getObject();
+		for (Iterator i = parent.children(); i.hasNext();)
+        {
+            TreeNode child = (TreeNode)i.next();
+            GedcomLine line = (GedcomLine)child.getObject();
+            String tag = line.getTag();
+            boolean cont = tag.equalsIgnoreCase("cont");
+			boolean conc = tag.equalsIgnoreCase("conc");
+            if (cont)
+            {
+            	parentLine.contValue(line.getValue());
+            	i.remove();
+            }
+            else if (conc)
+            {
+				parentLine.concValue(line.getValue());
+				i.remove();
+            }
+            else
+            {
+				concatenateHelper(child);
+            }
+        }
+	}
+}
