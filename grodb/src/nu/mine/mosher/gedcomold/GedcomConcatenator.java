9cb4bb71723c5b923fc61b93ea14691cce7e9de7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-23 02:49:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@640 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/GedcomConcatenator.java
+++ grodb/src/nu/mine/mosher/gedcomold/GedcomConcatenator.java
@@ -1,18 +1,18 @@
-/*
- * Created on Jul 22, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.gedcom;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import nu.mine.mosher.util.TreeNode;
+
 public class GedcomConcatenator
 {
+	private final GedcomTree tree;
 
+	public GedcomConcatenator(GedcomTree tree)
+	{
+		this.tree = tree;
+	}
+
+	public void concatenate()
+	{
+		TreeNode node = tree.getRoot();
+	}
 }
