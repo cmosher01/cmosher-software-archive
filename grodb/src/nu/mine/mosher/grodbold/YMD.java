4d8c6ceaf20f427cac40be5142794b6aeac8d049
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-25 22:05:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@654 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb/src/nu/mine/mosher/grodbold/YMD.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jul 25, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.grodb;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class YMD
+{
+
+}
