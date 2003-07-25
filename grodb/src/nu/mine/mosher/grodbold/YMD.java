9714370ba2239d1571b4f59f670be6f6f4f4fe5f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-25 22:17:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@661 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/YMD.java
+++ grodb/src/nu/mine/mosher/grodbold/YMD.java
@@ -6,15 +6,18 @@ import nu.mine.mosher.core.Immutable;
 
 public class YMD implements Immutable, Serializable
 {
-	private int year;
-	private int month;
-	private int day;
+	private final int year;
+	private final int month;
+	private final int day;
+	private final int hash;
 
 	public YMD(int year, int month, int day)
 	{
 		this.year = year;
 		this.month = month;
 		this.day = day;
+
+		computeHash();
 	}
 
     public int getDay()
@@ -48,13 +51,17 @@ public class YMD implements Immutable, Serializable
 
     public int hashCode()
     {
-    	int hash = 17;
+    	return hash;
+    }
+
+    private void computeHash()
+    {
+		hash = 17;
 		hash *= 37;
 		hash += year;
 		hash *= 37;
 		hash += month;
 		hash *= 37;
 		hash += day;
-		return hash;
     }
 }
