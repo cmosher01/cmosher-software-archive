d6125411cf0a4ccba7795a919c63dd4415eed4a8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-25 22:06:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@656 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/YMD.java
+++ grodb/src/nu/mine/mosher/grodbold/YMD.java
@@ -2,7 +2,29 @@ package nu.mine.mosher.grodb;
 
 public class YMD
 {
-	int year;
-	int month;
-	int day;
+	private int year;
+	private int month;
+	private int day;
+
+	public YMD(int year, int month, int day)
+	{
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
 }
