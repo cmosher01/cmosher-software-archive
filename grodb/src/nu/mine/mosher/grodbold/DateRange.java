35f86ab8be1b44940c0d8e975d6c255f6f138475
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-25 22:08:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@658 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/DateRange.java
+++ grodb/src/nu/mine/mosher/grodbold/DateRange.java
@@ -1,18 +1,13 @@
-/*
- * Created on Jul 25, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.grodb;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class DateRange
 {
-
+	YMD earliest;
+	YMD latest;
+	int hour;
+	int minute;
+	int hourOffGMT;
+	int minuteOffGMT;
+	boolean circa;
+	boolean julian;
 }
