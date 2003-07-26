afb61511cc71ccd3fe87a3f4f817cc4b1eb3187c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-26 04:42:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@710 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/RelationType.java
+++ grodb/src/nu/mine/mosher/grodbold/RelationType.java
@@ -1,5 +1,18 @@
 package nu.mine.mosher.grodb;
 
-public interface RelationType
+import java.io.Serializable;
+
+public class RelationType implements Serializable
 {
+	private final String name;
+
+	protected RelationType(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
 }
