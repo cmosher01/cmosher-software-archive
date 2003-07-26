abed869ffa10ce901191e9fb5c1fb59f21b201b1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-26 01:23:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@667 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/DateRange.java
+++ grodb/src/nu/mine/mosher/grodbold/DateRange.java
@@ -5,17 +5,53 @@ import java.util.TimeZone;
 
 public class DateRange
 {
-	YMD earliest;
-	YMD latest;
-	int hour;
-	int minute;
-	int hourOffGMT;
-	int minuteOffGMT;
-	boolean circa;
-	TimeZone tz;
+	private final YMD earliest;
+	private final YMD latest;
+	private final boolean julian; // true==Julian, false==Gregorian
+	private final int hour;
+	private final int minute;
+	private final int hourOffGMT;
+	private final int minuteOffGMT;
+	private final boolean circa;
+	private final TimeZone timezone;
+    /**
+     * @return
+     */
+    public boolean isJulian()
+    {
+        return julian;
+    }
 
-	void DateRange()
-	{
-		Calendar c;
-	}
+    /**
+     * @return
+     */
+    public YMD getLatest()
+    {
+        return latest;
+    }
+
+    /**
+     * @return
+     */
+    public int getMinute()
+    {
+        return minute;
+    }
+
+    /**
+     * @return
+     */
+    public int getMinuteOffGMT()
+    {
+        return minuteOffGMT;
+    }
+
+    /**
+     * @return
+     */
+    public TimeZone getTimeZone()
+    {
+        return timezone;
+    }
+
 }
