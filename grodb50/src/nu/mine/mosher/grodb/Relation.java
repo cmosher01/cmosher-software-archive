2736bcc8ec8ccc886adb825f573439959babe3f9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-26 05:37:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@715 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/src/nu/mine/mosher/grodb/Relation.java
@@ -0,0 +1,29 @@
+package nu.mine.mosher.grodb;
+
+import java.io.Serializable;
+
+public class Relation implements Serializable
+{
+	private final Object a;
+	private final Object b;
+	private final String description;
+	private final RelationType type;
+
+	public Relation(Object a, Object b, RelationType type)
+	{
+		this(a,b,"",type);
+	}
+
+	public Relation(Object a, Object b, String description)
+	{
+		this(a,b,description,null);
+	}
+
+	public Relation(Object a, Object b, String description, RelationType type)
+	{
+		this.a = a;
+		this.b = b;
+		this.description = description;
+		this.type = type;
+	}
+}
