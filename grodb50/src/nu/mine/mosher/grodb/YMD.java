260608421daa75fbac2db9c68d97c8079f886273
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-26 04:33:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@708 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/src/nu/mine/mosher/grodb/YMD.java
@@ -0,0 +1,102 @@
+package nu.mine.mosher.grodb;
+
+import java.io.Serializable;
+
+import nu.mine.mosher.core.Immutable;
+
+public class YMD implements Immutable, Serializable, Comparable
+{
+	/*
+	 * One-based year, month, and day.
+	 * Gregorian calendar is assumed.
+	 * Zero indicates that field is "unknown"
+	 */
+	private final int year;
+	private final int month;
+	private final int day;
+
+	private final int hash;
+
+	public YMD(int year)
+	{
+		this(year,0,0);
+	}
+
+	public YMD(int year, int month, int day)
+	{
+		this.year = year;
+		this.month = month;
+		this.day = day;
+
+		this.hash = getHash();
+	}
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof YMD))
+		{
+			return false;
+		}
+
+		YMD that = (YMD)o;
+		return
+			this.year == that.year &&
+			this.month == that.month &&
+			this.day == that.day;
+	}
+
+    public int hashCode()
+    {
+    	return hash;
+    }
+
+    private int getHash()
+    {
+		int h = 17;
+
+		h *= 37;
+		h += year;
+		h *= 37;
+		h += month;
+		h *= 37;
+		h += day;
+
+		return h;
+    }
+
+    public int compareTo(Object o)
+    {
+    	YMD that = (YMD)o;
+    	int d = 0;
+
+		if (d==0)
+		{
+	    	d = this.year-that.year;
+		}
+    	if (d==0)
+    	{
+    		d = this.month-that.month;
+    	}
+		if (d==0)
+		{
+			d = this.day-that.day;
+		}
+
+		return d;
+    }
+}
