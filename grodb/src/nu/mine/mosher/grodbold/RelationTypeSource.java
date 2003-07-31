6e886868bc33db283a2018e71a6c5e2a6e968885
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-31 14:02:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@739 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/RelationTypeSource.java
+++ grodb/src/nu/mine/mosher/grodbold/RelationTypeSource.java
@@ -28,8 +28,8 @@ public class RelationTypeSource implements Serializable
 	public static final RelationTypeSource CITES = new RelationTypeSource("cites");
 	public static final RelationTypeSource CONTAINS = new RelationTypeSource("contains");
 
-	private Object readResolve() throws ObjectStreamException
-	{
-		return PRIVATE_VALUES.get(id);
-	}
+//	private Object readResolve() throws ObjectStreamException
+//	{
+//		return PRIVATE_VALUES.get(id);
+//	}
 }
