8a32ecc2591f99863a175cbfd079a016d841d99d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-31 14:48:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@744 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/src/nu/mine/mosher/grodb/Source.java
@@ -0,0 +1,29 @@
+package nu.mine.mosher.grodb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Source
+{
+	private String title;
+	private String author;
+	private DateRange dateWritten;
+	private Place placeWritten;
+	private String publication;
+	private DateRange dateTopicStart;
+	private DateRange dateTopicEnd;
+	private Place placeTopic;
+
+	private List rRel = new ArrayList();
+	private List rRep = new ArrayList();
+	private List rSearch = new ArrayList();
+
+	public Source()
+	{
+	}
+
+	public void addRel(Source that, RelationTypeSource relThisToThat)
+	{
+//		rRel.add(new RelationSource(that,relThisToThat));
+	}
+}
