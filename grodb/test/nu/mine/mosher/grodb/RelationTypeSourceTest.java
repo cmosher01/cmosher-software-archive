605f2759d4a0d63ae014d084a8f8e5b5214a581d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-31 13:51:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@734 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/test/nu/mine/mosher/grodb/RelationTypeSourceTest.java
+++ grodb/test/nu/mine/mosher/grodb/RelationTypeSourceTest.java
@@ -1,26 +1,9 @@
-/*
- * Created on Jul 31, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.grodb;
 
 import junit.framework.TestCase;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class RelationTypeSourceTest extends TestCase
 {
-
-    /**
-     * Constructor for RelationTypeSourceTest.
-     * @param name
-     */
     public RelationTypeSourceTest(String name)
     {
         super(name);
@@ -33,6 +16,6 @@ public class RelationTypeSourceTest extends TestCase
 
     public void testRelationTypeSource()
     {
+    	assertEquals("cites",RelationTypeSource.CITES);
     }
-
 }
