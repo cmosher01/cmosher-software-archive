59b6a4624885fe4ebbb6c13ea8040423b3dc901c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-31 15:02:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@747 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/test/nu/mine/mosher/grodb/RelationTypeSourceTest.java
@@ -0,0 +1,40 @@
+package nu.mine.mosher.grodb;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import junit.framework.TestCase;
+
+public class RelationTypeSourceTest extends TestCase
+{
+    public RelationTypeSourceTest(String name)
+    {
+        super(name);
+    }
+
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(RelationTypeSourceTest.class);
+    }
+
+    public void testRelationTypeSource() throws IOException, ClassNotFoundException
+    {
+    	assertEquals("cites",RelationTypeSource.CITES.toString());
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(256);
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		oos.writeObject(RelationTypeSource.CITES);
+		byte[] rb = baos.toByteArray();
+		oos.close();
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(rb);
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		Object obj = ois.readObject();
+		assertTrue(obj instanceof RelationTypeSource);
+		RelationTypeSource srel = (RelationTypeSource)obj;
+		assertEquals(RelationTypeSource.CITES,srel);
+    }
+}
