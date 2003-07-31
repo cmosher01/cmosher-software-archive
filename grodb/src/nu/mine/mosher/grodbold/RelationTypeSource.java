af91d2c0f0f78bdf80e68de1ccf0b1ada73d8999
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-07-31 13:43:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@727 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/grodbold/RelationTypeSource.java
+++ grodb/src/nu/mine/mosher/grodbold/RelationTypeSource.java
@@ -1,18 +1,31 @@
-/*
- * Created on Jul 31, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package nu.mine.mosher.grodb;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import java.io.ObjectStreamException;
+
 public class RelationTypeSource
 {
+	private static int idNext = 0;
+	private final int id = idNext++;
 
+	private final String name;
+
+	protected RelationTypeSource(String name)
+	{
+		this.name = name;
+	}
+
+	public String toString()
+	{
+		return name;
+	}
+
+	public static final RelationTypeSource CITES = new RelationTypeSource("cites");
+	public static final RelationTypeSource CONTAINS = new RelationTypeSource("contains");
+
+	private static final RelationTypeSource[] PRIVATE_VALUES = { CITES, CONTAINS };
+
+	private Object readResolve() throws ObjectStreamException
+	{
+		return PRIVATE_VALUES[id];
+	}
 }
