6b84b1550c1e06cf60bb515956fcd7d1e4a7dfcf
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-12 01:47:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@781 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- resinaccess/src/nu/mine/mosher/com/caucho/http/log/SimpleAccessLog.java
+++ resinaccess/src/nu/mine/mosher/com/caucho/http/log/SimpleAccessLog.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.caucho.http.log.AccessLog;
+import com.caucho.server.connection.AbstractHttpResponse;
 
 /**
  * @author Chris Mosher
@@ -21,9 +22,15 @@ public class SimpleAccessLog extends AccessLog
     public void log(HttpServletRequest request, HttpServletResponse response, ServletContext context) throws IOException
     {
     	super.log(request,response,context);
+    	String status = "";
+    	if (response instanceof AbstractHttpResponse)
+    	{
+    		status = ", status "+((AbstractHttpResponse)response).getStatusCode();
+    	}
     	System.err.println("--------------------> IP:"+request.getRemoteAddr()+
 			", method:"+request.getMethod()+
 			", URI:"+request.getRequestURI()+
+			status+
 			", browser:"+request.getHeader("User-Agent")+
 			", referer:"+request.getHeader("Referer"));
     }
