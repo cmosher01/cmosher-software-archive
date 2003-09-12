db97d884b48f8b76b7157747ab9d390bbc0d47cb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-12 00:14:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@773 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- resinaccess/src/nu/mine/mosher/com/caucho/http/log/SimpleAccessLog.java
+++ resinaccess/src/nu/mine/mosher/com/caucho/http/log/SimpleAccessLog.java
@@ -9,30 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import com.caucho.http.log.AbstractAccessLog;
 
 /**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author Chris Mosher
  */
 public class SimpleAccessLog extends AbstractAccessLog
 {
-
-    /**
-     * 
-     */
     public SimpleAccessLog()
     {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /* (non-Javadoc)
-     * @see com.caucho.http.log.AbstractAccessLog#log(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.ServletContext)
-     */
-    public void log(HttpServletRequest arg0, HttpServletResponse arg1, ServletContext arg2) throws IOException
+    public void log(HttpServletRequest request, HttpServletResponse response, ServletContext context) throws IOException
     {
-        // TODO Auto-generated method stub
-
+    	super.log(request,response,context);
     }
-
 }
