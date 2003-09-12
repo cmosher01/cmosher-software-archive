a9fa2c714a0503ee432a641d010db1db459e604e
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-12 00:11:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@771 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ resinaccess/src/nu/mine/mosher/com/caucho/http/log/SimpleAccessLog.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Sep 11, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.com.caucho.http.log;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.caucho.http.log.AbstractAccessLog;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SimpleAccessLog extends AbstractAccessLog
+{
+
+    /**
+     * 
+     */
+    public SimpleAccessLog()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see com.caucho.http.log.AbstractAccessLog#log(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.ServletContext)
+     */
+    public void log(HttpServletRequest arg0, HttpServletResponse arg1, ServletContext arg2) throws IOException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
