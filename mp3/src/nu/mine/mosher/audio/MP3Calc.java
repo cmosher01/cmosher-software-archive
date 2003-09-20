243a1721d90f1f3efc6c6da8195cec3922870768
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 14:31:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@806 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- mp3/src/nu/mine/mosher/audio/MP3Calc.java
+++ mp3/src/nu/mine/mosher/audio/MP3Calc.java
@@ -3,17 +3,18 @@
  */
 package nu.mine.mosher.mp3;
 
-import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
 
 /**
  * @author Chris Mosher
  */
 public class MP3Calc
 {
+    private static int[] layer2bps = {0,32,48,56,64,80,96,112,128,160,192,224,256,320,384,0};
+    }
+
     public static void main(String[] rArg) throws Throwable
     {
     	if (rArg.length == 0)
@@ -59,6 +60,7 @@ public class MP3Calc
 
 		// bitrate
 		int bps = calcBitRate(h & 15,mpeg,layer);
+		h >>= 4;
     }
 
     private static int calcBitRate(int key, int mpeg, int layer)
@@ -67,6 +69,13 @@ public class MP3Calc
     	if (layer == 1)
     	{
     		bps = key*32;
+    	}
+    	else if (layer == 2)
+    	{
+    		bps = layer2bps[key];
+    	}
+    	else if (layer == 3)
+    	{
     	}
     	return 1000*bps;
     }
