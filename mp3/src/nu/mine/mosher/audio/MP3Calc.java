0a96132a917e07bc74d4bdd91bcc88f02ef3bab7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 14:05:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@794 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ mp3/src/nu/mine/mosher/audio/MP3Calc.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Sep 20, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.mp3;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class MP3Calc
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
