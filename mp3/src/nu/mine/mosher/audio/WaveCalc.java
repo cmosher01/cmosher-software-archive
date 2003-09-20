093e33dd91065a64ee00a0de881d68571f1358e0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 17:30:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@839 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- mp3/src/nu/mine/mosher/audio/WaveCalc.java
+++ mp3/src/nu/mine/mosher/audio/WaveCalc.java
@@ -19,39 +19,42 @@ public class WaveCalc
 {
     public static void main(String[] rArg) throws Throwable
     {
-		if (rArg.length > 0)
-		{
-			calc(rArg[0]);
-		}
+        if (rArg.length > 0)
+        {
+            calc(rArg[0]);
+        }
 
-		if (System.in.available() > 0)
-		{
-			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(FileDescriptor.in)));
-			for (String s = in.readLine(); s != null; s = in.readLine())
-			{
-				calc(s);
-			}
-			in.close();
-		}
-	}
+        if (System.in.available() > 0)
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(FileDescriptor.in)));
+            for (String s = in.readLine(); s != null; s = in.readLine())
+            {
+            	try
+            	{
+					calc(s);
+            	}
+            	catch (Exception e)
+            	{
+            		// keep processing any remaining files
+            	}
+            }
+            in.close();
+        }
+    }
 
-	private static void calc(String filename) throws IOException, FileNotFoundException
-	{
-		File fin = new File(filename);
-		if (!fin.canRead())
-		{
-			System.err.println("Cannot access file "+fin.getCanonicalPath());
-			System.exit(1);
-		}
-        
-		long flen = fin.length();
-		System.out.print(flen);
-		System.out.print(",");
-        
-		DataInputStream in = new DataInputStream(new FileInputStream(fin));
-        
-		final int h = in.readInt();
-		System.out.print(Integer.toHexString(h));
-		System.out.print(",");
+    private static void calc(String filename) throws Exception
+    {
+        File fin = new File(filename);
+        if (!fin.canRead())
+        {
+            System.err.println("Cannot access file " + fin.getCanonicalPath());
+            System.exit(1);
+        }
+
+        DataInputStream in = new DataInputStream(new FileInputStream(fin));
+        if (in.readInt() != 0x46464952)
+        {
+        	throw new Exception("File does not start with RIFF.");
+        }
     }
 }
