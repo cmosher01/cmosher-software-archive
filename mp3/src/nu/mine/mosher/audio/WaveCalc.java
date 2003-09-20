797871c7961754b04668afc15d06f99a672b2801
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 17:25:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@837 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ mp3/src/nu/mine/mosher/audio/WaveCalc.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Sep 20, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.audio;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class WaveCalc
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
