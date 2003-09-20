cee1a5ef63c2b45b1f638f86744d26b1ca3e083c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 14:28:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@805 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- mp3/src/nu/mine/mosher/audio/MP3Calc.java
+++ mp3/src/nu/mine/mosher/audio/MP3Calc.java
@@ -4,6 +4,7 @@
 package nu.mine.mosher.mp3;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -30,18 +31,43 @@ public class MP3Calc
 
 		long flen = fin.length();
 
-		InputStream in = new FileInputStream(fin);
+		DataInputStream in = new DataInputStream(new FileInputStream(fin));
 
-		BufferedInputStream bufin = null;
-    	if (in instanceof BufferedInputStream)
-    	{
-			bufin = (BufferedInputStream)in;
-    	}
-    	else
-    	{
-    		bufin = new BufferedInputStream(in);
-    	}
+		int h = in.readInt();
 
-		bufin.readInt();
+		// ignore synch word
+		h >>= 12;
+
+		// MPEG1 or MPEG2
+		int mpeg;
+		if ((h & 1) == 0)
+		{
+			mpeg = 2;
+		}
+		else
+		{
+			mpeg = 1;
+		}
+		h >>= 1;
+
+		// layer 1, 2, or 3 (4 means undefined)
+		int layer = 4-(h&3);
+		h >>= 2;
+
+		// ignore protection
+		h >>= 1;
+
+		// bitrate
+		int bps = calcBitRate(h & 15,mpeg,layer);
+    }
+
+    private static int calcBitRate(int key, int mpeg, int layer)
+    {
+    	int bps = 0;
+    	if (layer == 1)
+    	{
+    		bps = key*32;
+    	}
+    	return 1000*bps;
     }
 }
