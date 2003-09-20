1e4c1cf9f1c07f1253152e88612fafffb3ce6eeb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 18:10:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@850 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- mp3/src/nu/mine/mosher/audio/MP3Calc.java
+++ mp3/src/nu/mine/mosher/audio/MP3Calc.java
@@ -10,6 +10,7 @@ import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -22,27 +23,27 @@ public class MP3Calc
     private static int[] M1L3 = { 0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0 };
     private static int[] M2L1 = { 0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0 };
     private static int[] M2L2 = { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 };
-	private static int[] M2L3 = { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 };
-	private static int[] M1f = { 44100, 48000, 32000, 0 };
-	private static int[] M2f = { 22050, 24000, 16000, 0 };
-	private static int[] M25f = { 11025, 12000, 8000, 0 };
+    private static int[] M2L3 = { 0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0 };
+    private static int[] M1f = { 44100, 48000, 32000, 0 };
+    private static int[] M2f = { 22050, 24000, 16000, 0 };
+    private static int[] M25f = { 11025, 12000, 8000, 0 };
 
     public static void main(String[] rArg) throws Throwable
     {
-    	if (rArg.length > 0)
-    	{
-			calc(rArg[0]);
-    	}
+        if (rArg.length > 0)
+        {
+            calc(rArg[0]);
+        }
 
-    	if (System.in.available() > 0)
-    	{
-    		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(FileDescriptor.in)));
-    		for (String s = in.readLine(); s != null; s = in.readLine())
-    		{
-    			calc(s);
-    		}
-    		in.close();
-    	}
+        if (System.in.available() > 0)
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(FileDescriptor.in)));
+            for (String s = in.readLine(); s != null; s = in.readLine())
+            {
+                calc(s);
+            }
+            in.close();
+        }
     }
 
     private static void calc(String filename) throws IOException, FileNotFoundException
@@ -50,142 +51,150 @@ public class MP3Calc
         File fin = new File(filename);
         if (!fin.canRead())
         {
-        	System.err.println("Cannot access file "+fin.getCanonicalPath());
-        	System.exit(1);
+            System.err.println("Cannot access file " + fin.getCanonicalPath());
+            System.exit(1);
         }
-        
+
         long flen = fin.length();
         System.out.print(flen);
         System.out.print(",");
-        
-        DataInputStream in = new DataInputStream(new FileInputStream(fin));
-        
-        final int h = in.readInt();
+
+        InputStream in = new FileInputStream(fin);
+
+		byte[] rb = new byte[4];
+        if (in.read(rb,0,4) != 4)
+        {
+			System.err.println("Cannot read first 4 bytes of file " + fin.getCanonicalPath());
+			System.exit(1);
+        }
+
+		final int h = (rb[3]<<24)+(rb[2]<<16)+(rb[1]<<8)+rb[0];
+
         System.out.print(Integer.toHexString(h));
         System.out.print(",");
-        
+
         // synch word
-        int synch = getbits(h,31,21);
+        int synch = getbits(h, 31, 21);
         System.out.print(Integer.toHexString(synch));
         System.out.print(",");
-        
+
         // MPEG1 or MPEG2 or MPEG 2.5
         int mpeg = 0;
-        switch (getbits(h,20,19))
+        switch (getbits(h, 20, 19))
         {
-        	case 0:
-        		mpeg = 25;
-        		System.out.print("mpeg2.5,");
-        	break;
-        	case 2:
-        		mpeg = 2;
-        		System.out.print("mpeg2,");
-        	break;
-        	case 3:
-        		mpeg = 1;
-        		System.out.print("mpeg1,");
-        	break;
+            case 0 :
+                mpeg = 25;
+                System.out.print("mpeg2.5,");
+                break;
+            case 2 :
+                mpeg = 2;
+                System.out.print("mpeg2,");
+                break;
+            case 3 :
+                mpeg = 1;
+                System.out.print("mpeg1,");
+                break;
         }
-        
+
         // layer 1, 2, or 3 (4 means undefined)
-        int layer = 4-(getbits(h,18,17));
+        int layer = 4 - (getbits(h, 18, 17));
         System.out.print("layer");
         System.out.print(layer);
         System.out.print(",");
-        
+
         // protection
-        boolean crc = (getbits(h,16,16) > 0);
+        boolean crc = (getbits(h, 16, 16) > 0);
         if (!crc)
         {
-        	System.out.print("no");
+            System.out.print("no");
         }
         System.out.print("crc");
         System.out.print(",");
-        
+
         // bitrate
-        int bps = calcBitRate(getbits(h,15,12),mpeg,layer);
+        int bps = calcBitRate(getbits(h, 15, 12), mpeg, layer);
         System.out.print(bps);
         System.out.print(",");
-        
+
         int freq = 0;
-        int key = getbits(h,11,10);
+        int key = getbits(h, 11, 10);
         if (mpeg == 1)
         {
-        	freq = M1f[key];
+            freq = M1f[key];
         }
         else if (mpeg == 2)
         {
-        	freq = M2f[key];
+            freq = M2f[key];
         }
         else if (mpeg == 25)
         {
-        	freq = M25f[key];
+            freq = M25f[key];
         }
         System.out.print(freq);
         System.out.print(",");
-        
+
         // padding
-        boolean padding = (getbits(h,9,9) > 0);
+        boolean padding = (getbits(h, 9, 9) > 0);
         if (!padding)
         {
-        	System.out.print("no");
+            System.out.print("no");
         }
         System.out.print("padding");
         System.out.print(",");
-        
+
         // private bit
-        int priv = getbits(h,8,8);
+        int priv = getbits(h, 8, 8);
         System.out.print(priv);
         System.out.print(",");
-        
+
         // channel mode
-        int mode = getbits(h,7,6);
+        int mode = getbits(h, 7, 6);
         switch (mode)
         {
-        	case 0:
-        		System.out.print("stereo,");
-        	break;
-        	case 1:
-        		System.out.print("jstereo,");
-        	break;
-        	case 2:
-        		System.out.print("2mono,");
-        	break;
-        	case 3:
-        		System.out.print("mono,");
-        	break;
+            case 0 :
+                System.out.print("stereo,");
+                break;
+            case 1 :
+                System.out.print("jstereo,");
+                break;
+            case 2 :
+                System.out.print("2mono,");
+                break;
+            case 3 :
+                System.out.print("mono,");
+                break;
         }
-        
-        int jointstereo = getbits(h,5,4);
+
+        int jointstereo = getbits(h, 5, 4);
         System.out.print(jointstereo);
         System.out.print(",");
-        
-        boolean copyright = (getbits(h,3,3) > 0);
+
+        boolean copyright = (getbits(h, 3, 3) > 0);
         if (!copyright)
         {
-        	System.out.print("no");
+            System.out.print("no");
         }
         System.out.print("copyright");
         System.out.print(",");
-        
-        boolean original = (getbits(h,2,2) > 0);
+
+        boolean original = (getbits(h, 2, 2) > 0);
         System.out.print(original ? "original," : "copy,");
-        
-        int emphasis = getbits(h,1,0);
+
+        int emphasis = getbits(h, 1, 0);
         switch (emphasis)
         {
-        	case 0:
-        		System.out.print("noemphasis");
-        	break;
-        	case 1:
-        		System.out.print("50/15 ms");
-        	break;
-        	case 2:
-        		System.out.print("[invalid]");
-        	break;
-        	case 3:
-        		System.out.print("CCITT J.17");
-        	break;
+            case 0 :
+                System.out.print("noemphasis");
+                break;
+            case 1 :
+                System.out.print("50/15 ms");
+                break;
+            case 2 :
+                System.out.print("[invalid]");
+                break;
+            case 3 :
+                System.out.print("CCITT J.17");
+                break;
         }
         System.out.println();
     }
@@ -198,61 +207,61 @@ public class MP3Calc
      */
     private static int getbits(int src, int hi, int lo)
     {
-    	src >>= lo;
-    	int c = hi-lo+1;
-    	int m = 0;
-    	for (int i = 0; i < c; ++i)
+        src >>= lo;
+        int c = hi - lo + 1;
+        int m = 0;
+        for (int i = 0; i < c; ++i)
         {
-        	m <<= 1;
-        	m |= 1;
+            m <<= 1;
+            m |= 1;
         }
-    	return src & m;
+        return src & m;
     }
 
     private static int calcBitRate(int key, int mpeg, int layer)
     {
-    	int bps = 0;
+        int bps = 0;
 
-    	if (layer == 1)
-    	{
-    		switch (mpeg)
+        if (layer == 1)
+        {
+            switch (mpeg)
             {
-                case 1:
-                	bps = M1L1[key];
-                break;
-				case 2:
-				case 25:
-					bps = M2L1[key];
-				break;
+                case 1 :
+                    bps = M1L1[key];
+                    break;
+                case 2 :
+                case 25 :
+                    bps = M2L1[key];
+                    break;
             }
-    	}
-		else if (layer == 2)
-		{
-			switch (mpeg)
-			{
-				case 1:
-					bps = M1L2[key];
-				break;
-				case 2:
-				case 25:
-					bps = M2L2[key];
-				break;
-			}
-		}
-		else if (layer == 3)
-		{
-			switch (mpeg)
-			{
-				case 1:
-					bps = M1L3[key];
-				break;
-				case 2:
-				case 25:
-					bps = M2L3[key];
-				break;
-			}
-		}
+        }
+        else if (layer == 2)
+        {
+            switch (mpeg)
+            {
+                case 1 :
+                    bps = M1L2[key];
+                    break;
+                case 2 :
+                case 25 :
+                    bps = M2L2[key];
+                    break;
+            }
+        }
+        else if (layer == 3)
+        {
+            switch (mpeg)
+            {
+                case 1 :
+                    bps = M1L3[key];
+                    break;
+                case 2 :
+                case 25 :
+                    bps = M2L3[key];
+                    break;
+            }
+        }
 
-    	return 1000*bps;
+        return 1000 * bps;
     }
 }
