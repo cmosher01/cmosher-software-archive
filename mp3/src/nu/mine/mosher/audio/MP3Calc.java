2447f24ffd5c0dfd814b426cc547acbdebb772f1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-09-20 14:07:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@795 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- mp3/src/nu/mine/mosher/audio/MP3Calc.java
+++ mp3/src/nu/mine/mosher/audio/MP3Calc.java
@@ -1,21 +1,25 @@
 /*
  * Created on Sep 20, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package nu.mine.mosher.mp3;
 
+import java.io.InputStream;
+
 /**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author Chris Mosher
  */
 public class MP3Calc
 {
-
-    public static void main(String[] args)
+    public static void main(String[] rArg) throws Throwable
     {
+    	InputStream in;
+    	if (System.in.available() > 0)
+    	{
+    		in = System.in;
+    	}
+    	else if (rArg.length > 0)
+    	{
+    		in = new FileInputStream(new File(rArg[0]));
+    	}
     }
 }
