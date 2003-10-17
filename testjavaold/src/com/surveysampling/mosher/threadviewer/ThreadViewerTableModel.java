f446c8cd98e9a8e12c8daa9e4b20cc8596d0ffc3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-10-17 18:25:49 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@917 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/threadviewer/ThreadViewerTableModel.java
+++ testjavaold/src/com/surveysampling/mosher/threadviewer/ThreadViewerTableModel.java
@@ -5,107 +5,129 @@ import java.lang.reflect.InvocationTargetException;
 import javax.swing.SwingUtilities;
 import javax.swing.table.AbstractTableModel;
 
-public class ThreadViewerTableModel extends AbstractTableModel {
-	private Object dataLock; 
-	private int rowCount;
-	private Object[][] cellData;
-	private Object[][] pendingCellData;
+public class ThreadViewerTableModel extends AbstractTableModel
+{
+    private Object dataLock;
+    private int rowCount;
+    private Object[][] cellData;
+    private Object[][] pendingCellData;
 
-	// the column information remains constant
-	private final int columnCount;
-	private final String[] columnName;
-	private final Class[] columnClass;
+    // the column information remains constant
+    private final int columnCount;
+    private final String[] columnName;
+    private final Class[] columnClass;
 
-	// self-running object control variables
-	private Thread internalThread;
-	private volatile boolean noStopRequested;
+    // self-running object control variables
+    private Thread internalThread;
+    private volatile boolean noStopRequested;
 
-	public ThreadViewerTableModel() {
-		rowCount = 0;
-		cellData = new Object[0][0];
+    public ThreadViewerTableModel()
+    {
+        rowCount = 0;
+        cellData = new Object[0][0];
 
-		// JTable uses this information for the column headers
-		String[] names = { 
-			"Priority", "Alive", 
-			"Daemon", "Interrupted", 
-			"Name of ThreadGroup that Thread is from", "ParentGroup", "Thread Name" };
-		columnName = names;							
-						
-		// JTable uses this information for cell rendering
-		Class[] classes = { 
-			Integer.class, Boolean.class, 
-			Boolean.class, Boolean.class, 
-			String.class, String.class, String.class };
-		columnClass = classes;
+        // JTable uses this information for the column headers
+        String[] names =
+            {
+                "Priority",
+                "Alive",
+                "Daemon",
+                "Interrupted",
+                "Name of ThreadGroup that Thread is from",
+                "ParentGroup",
+                "Thread Name" };
+        columnName = names;
 
-		columnCount = columnName.length;
+        // JTable uses this information for cell rendering
+        Class[] classes = { Integer.class, Boolean.class, Boolean.class, Boolean.class, String.class, String.class, String.class };
+        columnClass = classes;
 
-		// used to control concurrent access
-		dataLock = new Object(); 
+        columnCount = columnName.length;
 
-		noStopRequested = true;
-		Runnable r = new Runnable() {
-				public void run() {
-					try {
-						runWork();
-					} catch ( Exception x ) {
-						// in case ANY exception slips through
-						x.printStackTrace(); 
-					}
-				}
-			};
+        // used to control concurrent access
+        dataLock = new Object();
 
-		internalThread = new Thread(r, "ThreadViewer");
-		internalThread.setPriority(Thread.MAX_PRIORITY - 2);
-		internalThread.setDaemon(true);
-		internalThread.start();
-	}
+        noStopRequested = true;
+        Runnable r = new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    runWork();
+                }
+                catch (Exception x)
+                {
+                    // in case ANY exception slips through
+                    x.printStackTrace();
+                }
+            }
+        };
 
-	private void runWork() {
+        internalThread = new Thread(r, "ThreadViewer");
+        internalThread.setPriority(Thread.MAX_PRIORITY - 2);
+        internalThread.setDaemon(true);
+        internalThread.start();
+    }
 
-		// The run() method of transferPending is called by 
-		// the event handling thread for safe concurrency.
-		Runnable transferPending = new Runnable() {
-				public void run() {
-					transferPendingCellData();
+    private void runWork()
+    {
 
-					// Method of AbstractTableModel that 
-					// causes the table to be updated.
-					fireTableDataChanged(); 
-				}
-			};
+        // The run() method of transferPending is called by 
+        // the event handling thread for safe concurrency.
+        Runnable transferPending = new Runnable()
+        {
+            public void run()
+            {
+                transferPendingCellData();
 
-		while ( noStopRequested ) {
-			try {
-				createPendingCellData();
-				SwingUtilities.invokeAndWait(transferPending);
-				Thread.sleep(5000);
-			} catch ( InvocationTargetException tx ) {
-				tx.printStackTrace();
-				stopRequest();
-			} catch ( InterruptedException x ) {
-				Thread.currentThread().interrupt(); 
-			}
-		}
-	}
+                // Method of AbstractTableModel that 
+                // causes the table to be updated.
+                fireTableDataChanged();
+            }
+        };
 
-	public void stopRequest() {
-		noStopRequested = false;
-		internalThread.interrupt();
-	}
+        while (noStopRequested)
+        {
+            try
+            {
+                createPendingCellData();
+                SwingUtilities.invokeAndWait(transferPending);
+                Thread.sleep(5000);
+            }
+            catch (InvocationTargetException tx)
+            {
+                tx.printStackTrace();
+                stopRequest();
+            }
+            catch (InterruptedException x)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 
-	public boolean isAlive() {
-		return internalThread.isAlive();
-	}
+    public void stopRequest()
+    {
+        noStopRequested = false;
+        internalThread.interrupt();
+    }
 
-	private void createPendingCellData() {
-		// this method is called by the internal thread
-		Thread[] thread = findAllThreads();
-		Object[][] cell = new Object[thread.length][columnCount];
+    public boolean isAlive()
+    {
+        return internalThread.isAlive();
+    }
 
-		for ( int i = 0; i < thread.length; i++ ) {
-			Thread t = thread[i];
-			Object[] rowCell = cell[i];
+    private void createPendingCellData()
+    {
+        // this method is called by the internal thread
+        Thread[] thread = findAllThreads();
+        Object[][] cell = new Object[thread.length][columnCount];
+
+        for (int i = 0; i < thread.length; i++)
+        {
+            Thread t = thread[i];
+            Object[] rowCell = cell[i];
 
             ThreadGroup pg = t.getThreadGroup().getParent();
             String spg;
@@ -113,77 +135,86 @@ public class ThreadViewerTableModel extends AbstractTableModel {
                 spg = "null";
             else
                 spg = pg.getName();
-			rowCell[0] = new Integer(t.getPriority());
-			rowCell[1] = new Boolean(t.isAlive());
-			rowCell[2] = new Boolean(t.isDaemon());
-			rowCell[3] = new Boolean(t.isInterrupted());
-			rowCell[4] = t.getThreadGroup().getName();
-			rowCell[5] = spg;
-			rowCell[6] = t.getName();
-		}
+            rowCell[0] = new Integer(t.getPriority());
+            rowCell[1] = new Boolean(t.isAlive());
+            rowCell[2] = new Boolean(t.isDaemon());
+            rowCell[3] = new Boolean(t.isInterrupted());
+            rowCell[4] = t.getThreadGroup().getName();
+            rowCell[5] = spg;
+            rowCell[6] = t.getName();
+        }
 
-		synchronized ( dataLock ) {
-			pendingCellData = cell;
-		}
-	}
+        synchronized (dataLock)
+        {
+            pendingCellData = cell;
+        }
+    }
 
-	private void transferPendingCellData() {
-		// this method is called by the event thread
-		synchronized ( dataLock ) {
-			cellData = pendingCellData;
-			rowCount = cellData.length;
-		}
-	}
+    private void transferPendingCellData()
+    {
+        // this method is called by the event thread
+        synchronized (dataLock)
+        {
+            cellData = pendingCellData;
+            rowCount = cellData.length;
+        }
+    }
 
-	public int getRowCount() {
-		// this method is called by the event thread
-		return rowCount;
-	}
-	
-	public Object getValueAt(int row, int col) {
-		// this method is called by the event thread
-		return cellData[row][col];
-	}
+    public int getRowCount()
+    {
+        // this method is called by the event thread
+        return rowCount;
+    }
 
-	public int getColumnCount() {
-		return columnCount;
-	}
+    public Object getValueAt(int row, int col)
+    {
+        // this method is called by the event thread
+        return cellData[row][col];
+    }
 
-	public Class getColumnClass(int columnIdx) {
-		return columnClass[columnIdx];
-	}
+    public int getColumnCount()
+    {
+        return columnCount;
+    }
 
-	public String getColumnName(int columnIdx) {
-		return columnName[columnIdx];
-	}
+    public Class getColumnClass(int columnIdx)
+    {
+        return columnClass[columnIdx];
+    }
 
-	public static Thread[] findAllThreads() {
-		ThreadGroup group = 
-			Thread.currentThread().getThreadGroup();
+    public String getColumnName(int columnIdx)
+    {
+        return columnName[columnIdx];
+    }
 
-		ThreadGroup topGroup = group;
+    public static Thread[] findAllThreads()
+    {
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
 
-		// traverse the ThreadGroup tree to the top
-		while ( group != null ) {
-			topGroup = group;
-			group = group.getParent();
-		}
+        ThreadGroup topGroup = group;
 
-		// Create a destination array that is about
-		// twice as big as needed to be very confident
-		// that none are clipped.
-		int estimatedSize = topGroup.activeCount() * 2;
-		Thread[] slackList = new Thread[estimatedSize];
+        // traverse the ThreadGroup tree to the top
+        while (group != null)
+        {
+            topGroup = group;
+            group = group.getParent();
+        }
 
-		// Load the thread references into the oversized
-		// array. The actual number of threads loaded 
-		// is returned.
-		int actualSize = topGroup.enumerate(slackList);
+        // Create a destination array that is about
+        // twice as big as needed to be very confident
+        // that none are clipped.
+        int estimatedSize = topGroup.activeCount() * 2;
+        Thread[] slackList = new Thread[estimatedSize];
 
-		// copy into a list that is the exact size
-		Thread[] list = new Thread[actualSize];
-		System.arraycopy(slackList, 0, list, 0, actualSize);
+        // Load the thread references into the oversized
+        // array. The actual number of threads loaded 
+        // is returned.
+        int actualSize = topGroup.enumerate(slackList);
 
-		return list;
-	}
+        // copy into a list that is the exact size
+        Thread[] list = new Thread[actualSize];
+        System.arraycopy(slackList, 0, list, 0, actualSize);
+
+        return list;
+    }
 }
