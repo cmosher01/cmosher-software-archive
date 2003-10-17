40669d76a3bbdff473fa60fc7944365fb3b049c9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-10-17 18:06:13 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@887 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Closer.java
+++ testjava/src/nu/mine/mosher/util/Closer.java
@@ -4,6 +4,20 @@ public class UniversalCloser
      * Calls the given object's "close()" method, if it has one.
      * Any exceptions are ignored.
      * 
+     * For example:
+     * <pre>
+     * OutputStream os = null;
+     * try
+     * {
+     *     os = new FileOutputStream(new File("test.txt"));
+     *     os.write(65);
+     * }
+     * finally
+     * {
+     *     UniversalCloser.close(os);
+     * }
+     * </pre>
+     * 
      * @param obj the Object whose close() method is to be called.
      */
     public static void close(Object obj)
