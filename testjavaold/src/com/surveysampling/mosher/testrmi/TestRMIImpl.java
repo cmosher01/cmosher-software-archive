1b74a797989e1258e06953e8cdaf317720586a8b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-10-17 18:13:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@891 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/testrmi/TestRMIImpl.java
@@ -0,0 +1,59 @@
+package com.surveysampling.mosher.testrmi;
+
+import java.rmi.RemoteException;
+import java.rmi.activation.Activatable;
+import java.rmi.activation.ActivationID;
+import java.rmi.MarshalledObject;
+
+public class TestRMIImpl extends Activatable implements TestRMI
+{
+    public TestRMIImpl(ActivationID id, MarshalledObject data) throws RemoteException
+    {
+        MarshalledObject unused = data; data = unused;
+        super(id,0);
+    }
+
+    public String getMyString() throws RemoteException
+    {
+/*		System.out.println("Threads:");
+		Thread thread[] = findAllThreads();
+		for (int i = 0; i < thread.length; ++i)
+		{
+			Thread t = thread[i];
+			System.out.println(t.getName());
+		}
+*/
+        return "My string.";
+    }
+
+	public static Thread[] findAllThreads()
+	{
+		ThreadGroup group = Thread.currentThread().getThreadGroup();
+
+		ThreadGroup topGroup = group;
+
+		// traverse the ThreadGroup tree to the top
+		while (group != null)
+		{
+			topGroup = group;
+			group = group.getParent();
+		}
+
+		// Create a destination array that is about
+		// twice as big as needed to be very confident
+		// that none are clipped.
+		int estimatedSize = topGroup.activeCount() * 2;
+		Thread[] slackList = new Thread[estimatedSize];
+
+		// Load the thread references into the oversized
+		// array. The actual number of threads loaded 
+		// is returned.
+		int actualSize = topGroup.enumerate(slackList);
+
+		// copy into a list that is the exact size
+		Thread[] list = new Thread[actualSize];
+		System.arraycopy(slackList, 0, list, 0, actualSize);
+
+		return list;
+	}
+}
