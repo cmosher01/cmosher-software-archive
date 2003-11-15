6fc968ef9f474e58f1c70ef5ecf2db8f00f3b2e6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-15 06:08:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@970 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixAppleDisasm_v1/src/FixAppleDisasm.java
+++ FixAppleDisasm_v1/src/FixAppleDisasm.java
@@ -206,6 +206,7 @@ public class FixAppleDisasm
 								Line lnm = new Line();
 								lines.put(new Integer(++lineNumber),lnm);
 								lnm.addr = i;
+								ln.instr = "DB";
 								lnm.oper = sval;
 							}
 						}
