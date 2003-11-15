e25ab86c118f373858965d49b32beabfb7d62bc2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-15 05:10:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@948 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixAppleDisasm_v1/src/FixAppleDisasm.java
+++ FixAppleDisasm_v1/src/FixAppleDisasm.java
@@ -1,20 +1,82 @@
-/*
- * Created on Nov 15, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.io.Writer;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class FixAppleDisasm
 {
-
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
+        if (args.length > 0)
+        {
+            System.err.println("arguments ignored.");
+        }
+
+        Reader in = null;
+        OutputStream out = null;
+        try
+        {
+            in = new InputStreamReader(new FileInputStream(FileDescriptor.in));
+            out = new FileOutputStream(FileDescriptor.out);
+
+            PipedReader pr = new PipedReader();
+            PipedWriter pw = new PipedWriter();
+            pr.connect(pw);
+
+            Dis2Dump(in,pw);
+            Dump2Bin(pr,out);
+        }
+        finally
+        {
+            close(out);
+            close(in);
+        }
+    }
+
+    private static void Dump2Bin(Reader in, OutputStream outbin)
+    {
+        BufferedReader inbuf = null;
+        if (in instanceof BufferedReader)
+        {
+            inbuf = (BufferedReader)in;
+        }
+        else
+        {
+            inbuf = new BufferedReader(in);
+        }
+    }
+
+    private static void Dis2Dump(Reader in, Writer out) throws IOException
+    {
+        BufferedReader inbuf = null;
+        if (in instanceof BufferedReader)
+        {
+            inbuf = (BufferedReader)in;
+        }
+        else
+        {
+            inbuf = new BufferedReader(in);
+        }
+        for (String s = inbuf.readLine(); s != null; s = inbuf.readLine())
+        {
+        }
+    }
+
+    private static void close(Object obj)
+    {
+        try
+        {
+            obj.getClass().getMethod("close",null).invoke(obj,null);
+        }
+        catch (Throwable ignore)
+        {
+        }
     }
 }
