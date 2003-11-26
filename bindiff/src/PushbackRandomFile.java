3dc6674609b96ee97ad5fda38c4002b83a65ce26
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 02:51:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1141 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- bindiff/src/PushbackRandomFile.java
+++ bindiff/src/PushbackRandomFile.java
@@ -3,7 +3,7 @@ import java.io.RandomAccessFile;
 
 public class PushbackRandomFile
 {
-	private RandomAccessFile f;
+	private final RandomAccessFile f;
 	private int unread = -1;
 
 	public PushbackRandomFile(RandomAccessFile f)
@@ -13,19 +13,19 @@ public class PushbackRandomFile
 
     public int read() throws IOException
     {
-    	int x;
+    	int c;
 
     	if (unread != -1)
     	{
-    		x = unread;
+    		c = unread;
     		unread = -1;
     	}
     	else
     	{
-			x = f.read();
+			c = f.read();
     	}
 
-    	return x;
+    	return c;
     }
 
     public void unread(int c)
