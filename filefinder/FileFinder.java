ee523aa7dd457b913fe97650e64c209da497d98b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 18:30:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1160 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- filefinder/FileFinder.java
+++ filefinder/FileFinder.java
@@ -1,16 +1,27 @@
-/*
- * TODO
- *
- * Created on Nov 26, 2003
- */
+import java.io.File;
+import java.io.FileFilter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**
- * TODO
- */
 public class FileFinder
 {
-
-    public static void main(String[] args)
+    public static void main(String[] rArg) throws Throwable
     {
+        if (rArg.length != 2)
+        {
+            System.err.println("Usage: java FileFinder dir file-reg-exp");
+            System.exit(1);
+        }
+
+        final File dir = new File(rArg[0]);
+        final Pattern pat = Pattern.compile(rArg[1]);
+        dir.listFiles(new FileFilter()
+        {
+            public boolean accept(File pathname)
+            {
+                Matcher mat = pat.matcher(pathname.getName());
+                return mat.matches();
+            }
+        });
     }
 }
