d83179de64df6df187c4268a08ca9b440b1ada05
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 01:53:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1100 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- bindiff/src/PushbackRandomFile.java
+++ bindiff/src/PushbackRandomFile.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class PushbackRandomFile
@@ -9,4 +10,8 @@ public class PushbackRandomFile
 		this.f = f;
 	}
 
+    public int read() throws IOException
+    {
+    	return f.read();
+    }
 }
