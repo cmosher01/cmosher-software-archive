95f080c75a836ce3e42bc46415e1572390d03673
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 01:51:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1098 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- bindiff/src/PushbackRandomFile.java
+++ bindiff/src/PushbackRandomFile.java
@@ -1,17 +1,12 @@
-/*
- * Created on Nov 25, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import java.io.RandomAccessFile;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class PushbackRandomFile
 {
+	private RandomAccessFile f;
+
+	public PushbackRandomFile(RandomAccessFile f)
+	{
+		this.f = f;
+	}
 
 }
