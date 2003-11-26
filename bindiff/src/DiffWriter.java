1f1cb3d85a7c74da44382c2f37cb9e6c5edac1b8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 02:37:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1128 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- bindiff/src/DiffWriter.java
+++ bindiff/src/DiffWriter.java
@@ -1,17 +1,14 @@
-/*
- * Created on Nov 25, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
-
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class DiffWriter
 {
+	public DiffWriter()
+	{
+	}
 
+	public void outTag(String type, int c)
+	{
+	}
+
+	public void outByte(int b)
+	{
+	}
 }
