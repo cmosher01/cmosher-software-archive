1391000a327fe3ca4970cdb20f2474b4f269cdad
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 02:36:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1127 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ bindiff/src/DiffWriter.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Nov 25, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class DiffWriter
+{
+
+}
