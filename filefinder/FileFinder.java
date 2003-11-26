97f4dc7464088c4d547f8d7012e5268f1a668848
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-26 18:35:21 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1161 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- filefinder/FileFinder.java
+++ filefinder/FileFinder.java
@@ -7,15 +7,22 @@ public class FileFinder
 {
     public static void main(String[] rArg) throws Throwable
     {
-        if (rArg.length != 2)
+        if (rArg.length < 2 || 3 < rArg.length)
         {
-            System.err.println("Usage: java FileFinder dir file-reg-exp");
+            System.err.println("Usage: java FileFinder dir file-reg-exp [subdir-reg-exp]");
             System.exit(1);
         }
 
         final File dir = new File(rArg[0]);
-        final Pattern pat = Pattern.compile(rArg[1]);
-        dir.listFiles(new FileFilter()
+        final Pattern patFile = Pattern.compile(rArg[1]);
+        String sub = "";
+        if (rArg.length > 2)
+        {
+            sub = rArg[2];
+        }
+        final Pattern patSubdir = Pattern.compile(sub);
+
+        File[] rm = dir.listFiles(new FileFilter()
         {
             public boolean accept(File pathname)
             {
