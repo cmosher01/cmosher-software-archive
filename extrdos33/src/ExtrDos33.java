e937e28e9f9c619d42e36172666c01e5c586d5af
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-27 01:42:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1179 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- extrdos33/src/ExtrDos33.java
+++ extrdos33/src/ExtrDos33.java
@@ -1,20 +1,81 @@
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /*
  * Created on Nov 26, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 
 /**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author Chris Mosher
  */
 public class ExtrDos33
 {
-
-    public static void main(String[] args)
+    public static void main(String[] rArg) throws Throwable
     {
+        if (rArg.length < 2 || 3 < rArg.length)
+        {
+            System.err.println("Usage: java FileFinder dir file-reg-exp [subdir-reg-exp]");
+            System.exit(1);
+        }
+
+        String sDir = rArg[0];
+        System.err.println("dir: " + sDir);
+
+        String regexpFile = rArg[1];
+        System.err.println("file-reg-exp: " + regexpFile);
+
+        String regexpDir = "";
+        if (rArg.length > 2)
+        {
+            regexpDir = rArg[2];
+            System.err.println("subdir-reg-exp: " + regexpDir);
+        }
+
+        File[] rf = listRegFiles(sDir, regexpFile, regexpDir);
+        for (int i = 0; i < rf.length; ++i)
+        {
+            File file = rf[i];
+            System.out.println(file.getPath());
+        }
+    }
+
+    public static File[] listRegFiles(String sDir, String regexpFile, String regexpDir) throws IOException
+    {
+        final Pattern patFile = Pattern.compile(regexpFile);
+        final Pattern patSubdir = Pattern.compile(regexpDir);
+
+        final List dirs = new ArrayList();
+        dirs.add(new File(sDir));
+
+        List results = new ArrayList();
+
+        while (dirs.size() > 0)
+        {
+            File d = (File)dirs.remove(0);
+            File[] rm = d.listFiles(new FileFilter()
+            {
+                public boolean accept(File pathname)
+                {
+                    Matcher matDir = patSubdir.matcher(pathname.getName());
+                    if (matDir.matches())
+                    {
+                        dirs.add(pathname);
+                    }
+                    Matcher matFile = patFile.matcher(pathname.getName());
+                    return matFile.matches();
+                }
+            });
+            for (int i = 0; rm != null && i < rm.length; ++i)
+            {
+                File file = rm[i];
+                results.add(file.getCanonicalFile().getAbsoluteFile());
+            }
+        }
+        return (File[])results.toArray(new File[results.size()]);
     }
 }
