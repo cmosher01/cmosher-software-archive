a5593b325bc9704dca0d165f35842e2dc8849202
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-11-27 04:18:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1196 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- extrdos33/src/ExtrDos33.java
+++ extrdos33/src/ExtrDos33.java
@@ -4,6 +4,7 @@ import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -90,14 +91,7 @@ public class ExtrDos33
         		return false;
         	}
         	DosImage that = (DosImage)obj;
-        	for (int i= 0; i < this.rbc.length; ++i)
-            {
-                if (this.rbc[i] != that.rbc[i])
-                {
-                	return false;
-                }
-            }
-            return true;
+			return Arrays.equals(this.rbc,that.rbc);
         }
         public int hashCode()
         {
