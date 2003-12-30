01cae10ee278519d4b41451fc8aca841d537e860
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 21:51:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1306 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/Flag.java
+++ testjavaold/src/com/surveysampling/mosher/Flag.java
@@ -1,14 +1,79 @@
-/*
- * TODO
- *
- * Created on Dec 30, 2003
- */
 package com.surveysampling.mosher;
 
 /**
- * TODO
+ * A thread-safe boolean flag. Provides methods
+ * to wait for the flag to be in a specified state.
+ * 
+ * @author Chris Mosher
  */
 public class Flag
 {
+    private boolean mFlag;
 
+
+
+    /**
+     * Construct a flag with an initial state of false.
+     */
+    public Flag()
+    {
+        this(false);
+    }
+
+    /**
+     * Construct a flag with the specified initial state.
+     */
+    public Flag(boolean bInitialState)
+    {
+        mFlag = bInitialState;
+    }
+
+
+
+    public synchronized void set(boolean bNewState)
+    {
+        if (mFlag != bNewState)
+        {
+            mFlag = bNewState;
+            notifyAll();
+        }
+    }
+
+    public synchronized void waitToSetTrue() throws InterruptedException
+    {
+        waitUntilFalse();
+        set(true);
+    }
+
+    public synchronized void waitToSetFalse() throws InterruptedException
+    {
+        waitUntilTrue();
+        set(false);
+    }
+
+    public synchronized boolean isTrue()
+    {
+        return mFlag;
+    }
+
+    public synchronized boolean isFalse()
+    {
+        return !mFlag;
+    }
+
+    public synchronized void waitUntilTrue() throws InterruptedException
+    {
+        waitUntilStateIs(true);
+    }
+
+    public synchronized void waitUntilFalse() throws InterruptedException
+    {
+        waitUntilStateIs(false);
+    }
+
+    public synchronized void waitUntilStateIs(boolean bState) throws InterruptedException
+    {
+        while (mFlag != bState)
+            wait();
+    }
 }
