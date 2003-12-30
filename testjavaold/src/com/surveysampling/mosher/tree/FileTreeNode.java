ad26f7a43be4f50b365e8332e98fd374885e2646
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 20:52:31 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1246 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/tree/FileTreeNode.java
@@ -0,0 +1,16 @@
+/*
+ * TODO
+ *
+ * Created on Dec 30, 2003
+ */
+package com.surveysampling.mosher.tree;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * TODO
+ */
+public class FileTreeNode extends DefaultMutableTreeNode
+{
+
+}
