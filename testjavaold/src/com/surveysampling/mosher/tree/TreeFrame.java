bda43c8a672f77eb578cb73715bbbf93c71a08e4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 21:45:11 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1293 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/tree/TreeFrame.java
+++ testjavaold/src/com/surveysampling/mosher/tree/TreeFrame.java
@@ -71,12 +71,6 @@ public class TreeFrame extends JPanel
         frame.setVisible(true);
     }
 
-    public static void main(String[] args)
-    {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-    }
-
     public static void main(String argv[])
     {
         String dir = System.getProperty("user.dir");
