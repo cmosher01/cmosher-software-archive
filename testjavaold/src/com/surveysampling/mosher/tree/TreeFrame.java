5fcc2cafe07c742acc0abec2955f8f3df72a1bce
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 20:54:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1253 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/tree/TreeFrame.java
@@ -0,0 +1,17 @@
+/*
+ * TODO
+ *
+ * Created on Dec 30, 2003
+ */
+package com.surveysampling.mosher.tree;
+
+import java.awt.Frame;
+import java.awt.event.WindowListener;
+
+/**
+ * TODO
+ */
+public class TreeFrame extends Frame implements WindowListener
+{
+
+}
