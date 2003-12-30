dd33738dcdb50b91093992f1e962b3237d2eec3b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 20:53:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1250 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/tree/HierarchyBrowserUpdater.java
+++ testjavaold/src/com/surveysampling/mosher/tree/HierarchyBrowserUpdater.java
@@ -5,12 +5,69 @@
  */
 package com.surveysampling.mosher.tree;
 
+import java.awt.Cursor;
+import java.awt.Frame;
+import java.util.Enumeration;
+
+import javax.swing.JTree;
+import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 /**
  * TODO
  */
 public class HierarchyBrowserUpdater implements TreeExpansionListener
 {
+    FileTreeNode root;
+    JTree jtree;
+    DefaultTreeModel treemodel;
+    Frame frame;
 
+    public HierarchyBrowserUpdater(JTree tree, FileTreeNode node, DefaultTreeModel tmodel, Frame theFrame)
+    {
+        root = node;
+        jtree = tree;
+        treemodel = tmodel;
+        frame = theFrame;
+    }
+
+    public void treeExpanded(TreeExpansionEvent event)
+    {
+
+        TreePath path = event.getPath();
+
+        System.out.print("Received expansion event on ");
+        System.out.println(path);
+
+        FileTreeNode node = (FileTreeNode)path.getLastPathComponent();
+        System.out.println("Node level: " + node.getLevel());
+        System.out.println("Children " + node.getChildCount());
+        System.out.println("Reading subtree " + node.toString());
+
+        frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        frame.setEnabled(false);
+
+        if (node.readTree())
+        {
+            System.out.println("Calling nodesWereInserted");
+            int childrenIdx[] = new int[node.getChildCount()];
+            int i = 0;
+            for (Enumeration e = node.children(); e.hasMoreElements();)
+            {
+                Object obj = e.nextElement();
+                childrenIdx[i] = node.getIndex((TreeNode)obj);
+                i++;
+            }
+            treemodel.nodesWereInserted(node, childrenIdx);
+        }
+        frame.setEnabled(true);
+        frame.setCursor(Cursor.getDefaultCursor());
+    }
+
+    public void treeCollapsed(TreeExpansionEvent event)
+    {
+    }
 }
