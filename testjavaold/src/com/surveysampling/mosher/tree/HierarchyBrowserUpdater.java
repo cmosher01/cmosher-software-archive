e9c14f877496f396e9cb72ac1a91cd70feec04db
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 20:53:29 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1249 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjavaold/src/com/surveysampling/mosher/tree/HierarchyBrowserUpdater.java
@@ -0,0 +1,16 @@
+/*
+ * TODO
+ *
+ * Created on Dec 30, 2003
+ */
+package com.surveysampling.mosher.tree;
+
+import javax.swing.event.TreeExpansionListener;
+
+/**
+ * TODO
+ */
+public class HierarchyBrowserUpdater implements TreeExpansionListener
+{
+
+}
