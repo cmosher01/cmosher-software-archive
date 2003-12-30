414732817b67c7b1f910a83134cf483c71c3d747
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2003-12-30 20:52:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1247 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/tree/FileTreeNode.java
+++ testjavaold/src/com/surveysampling/mosher/tree/FileTreeNode.java
@@ -1,16 +1,56 @@
-/*
- * TODO
- *
- * Created on Dec 30, 2003
- */
 package com.surveysampling.mosher.tree;
+
+import java.io.File;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 
-/**
- * TODO
- */
 public class FileTreeNode extends DefaultMutableTreeNode
 {
+    boolean hasbeenread;
 
+    public FileTreeNode(File file)
+    {
+        super(file);
+        hasbeenread = false;
+    }
+
+    public boolean readTree()
+    {
+        return readTree(false);
+    }
+
+    public boolean readTree(boolean b)
+    {
+        if (hasbeenread)
+            return false;
+
+        String list[] = getFileObject().list();
+        if (list != null)
+        {
+            for (int i = 0; i < list.length; i++)
+            {
+                FileTreeNode subnode = new FileTreeNode(new File(getFileObject(), list[i]));
+                add(subnode);
+                if (b)
+                    subnode.readTree(b);
+            }
+        }
+        hasbeenread = true;
+        return true;
+    }
+
+    public File getFileObject()
+    {
+        return (File)getUserObject();
+    }
+
+    public String toString()
+    {
+        return getFileObject().getName();
+    }
+
+    public boolean isLeaf()
+    {
+        return ((File)userObject).isFile();
+    }
 }
