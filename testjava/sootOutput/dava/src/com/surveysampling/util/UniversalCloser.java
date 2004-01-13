b04c1d8584d5941777f06238488ee8bd07ef425f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-13 20:22:13 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1404 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/sootOutput/dava/src/com/surveysampling/util/UniversalCloser.java
@@ -0,0 +1,31 @@
+package com.surveysampling.util;
+
+import java.lang.reflect.*;
+
+public class UniversalCloser
+{
+
+    public static void close(java.lang.Object obj)
+    {
+        label_0:
+        {
+            try
+            {
+                obj.getClass().getMethod("close", null).invoke(obj, null);
+            }
+            catch (Throwable ignore)
+            {
+                break label_0;
+            }
+        }
+
+        return;
+    }
+
+    public UniversalCloser()
+    {
+        super();
+
+        return;
+    }
+}
