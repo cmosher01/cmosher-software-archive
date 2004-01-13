6a1d0b74a1cf52d478d5a2232f5ab1886768b828
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-13 18:58:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1390 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/sootOutput/dava/src/A.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public abstract class A
+{
+
+    public A()
+    {
+        super();
+
+        System.out.println("A constructor");
+        return;
+    }
+
+    public void c()
+    {
+        System.out.println("A c");
+        this.s();
+        return;
+    }
+
+    public abstract void s();
+}
