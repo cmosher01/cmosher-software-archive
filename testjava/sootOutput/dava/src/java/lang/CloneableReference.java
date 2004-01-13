a8ed2a208abe18ca46c7ab1ff684d15f05d75282
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-13 19:03:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1399 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/sootOutput/dava/src/java/lang/CloneableReference.java
@@ -0,0 +1,35 @@
+package java.lang;
+
+public class CloneableReference implements java.lang.Cloneable
+{
+    private java.lang.Object referent;
+
+    public CloneableReference(java.lang.Object referent)
+    {
+        super();
+
+        referent = referent;
+        return;
+    }
+
+    public java.lang.Object clone()
+    {
+        java.lang.Object c;
+
+        c = null;
+
+        label_0:
+        {
+            try
+            {
+                c = referent.clone();
+            }
+            catch (CloneNotSupportedException e)
+            {
+                break label_0;
+            }
+        }
+
+        return c;
+    }
+}
