1621e55b77793da2fefea2d03131a7b235a00900
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-14 15:31:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1413 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Cloner.java
+++ testjava/src/nu/mine/mosher/util/Cloner.java
@@ -1,5 +1,10 @@
 package java.lang;
 
+/**
+ * Provides a static method that will clone any Cloneable Object.
+ * This class needs to be in the java.lang package in order to be
+ * able to access the protected clone method of Object.
+ */
 public class Cloner
 {
     private Cloner()
