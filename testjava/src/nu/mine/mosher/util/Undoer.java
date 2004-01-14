b1a3a2469c6dbb36382ca20af5db65e7685797fd
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-14 21:50:49 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1497 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Undoer.java
+++ testjava/src/nu/mine/mosher/util/Undoer.java
@@ -7,23 +7,6 @@ public class Undoer
     private final LinkedList mrUndo = new LinkedList();
     private final LinkedList mrRedo = new LinkedList();
 
-//    public void saveForUndo(Cloneable state) throws CloneNotSupportedException
-//    {
-//        mrUndo.addLast(new ImmutableReference(state));
-//        mrRedo.clear();
-//    }
-//
-//    public Cloneable undo(Cloneable state) throws CloneNotSupportedException
-//    {
-//        mrRedo.addFirst(new ImmutableReference(state));
-//        return ((ImmutableReference)mrUndo.removeLast()).object();
-//    }
-//
-//    public Cloneable redo(Cloneable state) throws CloneNotSupportedException
-//    {
-//        mrUndo.addLast(new ImmutableReference(state));
-//        return ((ImmutableReference)mrRedo.removeFirst()).object();
-//    }
     public void saveForUndo(Cloneable state) throws CloneNotSupportedException
     {
         mrUndo.addLast(Cloner.cloneObject(state));
