3de3c201d2de9d87b36fe618070d495d3cb56684
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-14 15:38:37 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1416 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/ImmutableReference.java
+++ testjava/src/nu/mine/mosher/util/ImmutableReference.java
@@ -1,13 +1,112 @@
-/*
- * TODO
- *
- * Created on Jan 14, 2004
- */
-
-/**
- * TODO
- */
 public class ImmutableReference
 {
+    private Cloneable a;
 
+    public ImmutableReference(Cloneable a, Cloneable b) throws CloneNotSupportedException
+    {
+        this.a = Cloner.cloneObject(a);
+        this.b = Cloner.cloneObject(b);
+    }
+
+    public Cloneable a() throws CloneNotSupportedException
+    {
+        return Cloner.cloneObject(this.a);
+    }
+
+    public Cloneable b() throws CloneNotSupportedException
+    {
+        return Cloner.cloneObject(this.b);
+    }
+
+    public String toString()
+    {
+        return "("+a+","+b+")";
+    }
+
+    public Object clone()
+    {
+        Pair clon = null;
+        try
+        {
+            clon = (Pair)super.clone();
+            clon.a = a();
+            clon.b = b();
+        }
+        catch (CloneNotSupportedException cantHappen)
+        {
+        }
+        return clon;
+    }
+
+    public boolean equals(Object o)
+    {
+        if (!(o instanceof Pair))
+        {
+            return false;
+        }
+        Pair that = (Pair)o;
+
+        return eq(this.a,that.a) && eq(this.b,that.b);
+    }
+
+    private boolean eq(Object x, Object y)
+    {
+        if (x == null && y == null)
+        {
+            return true;
+        }
+        if (x == null)
+        {
+            return false;
+        }
+        if (y == null)
+        {
+            return false;
+        }
+        return x.equals(y);
+    }
+
+    public int compareTo(Object o)
+    {
+        Pair that = (Pair)o;
+
+        int c;
+        c = cmp(this.a,that.a);
+        if (c == 0)
+        {
+            c = cmp(this.b,that.b);
+        }
+
+        return c;
+    }
+
+    private int cmp(Object x, Object y)
+    {
+        if (x == null && y == null)
+        {
+            return 0;
+        }
+        if (x == null)
+        {
+            return -1;
+        }
+        if (y == null)
+        {
+            return +1;
+        }
+        return ((Comparable)x).compareTo(y);
+    }
+
+    public int hashCode()
+    {
+        int h = 17;
+
+        h *= 37;
+        h += a==null ? 0 : a.hashCode();
+
+        h *= 37;
+        h += b==null ? 0 : b.hashCode();
+
+        return h;
+    }
 }
