4b481a8e355bbc9aacb96ff31feb12874da4223d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-14 21:53:23 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1503 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/UndoableReference.java
+++ testjava/src/nu/mine/mosher/util/UndoableReference.java
@@ -3,4 +3,14 @@ package com.surveysampling.util;
 public class UndoableReference
 {
     private Cloneable state;
+
+    public UndoableReference(Cloneable object) throws CloneNotSupportedException
+    {
+        if (object == null)
+        {
+            throw new IllegalArgumentException();
+        }
+        state = object;
+    }
+
 }
