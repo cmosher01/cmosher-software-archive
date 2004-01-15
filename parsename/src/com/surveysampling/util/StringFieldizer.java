deba24df34d47c2f04c609d698545968c91037a4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-15 18:02:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1519 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ parsename/src/com/surveysampling/util/StringFieldizer.java
@@ -0,0 +1,40 @@
+public class StringFieldizer
+{
+    private final String s;
+    private final char delim;
+    private int pos;
+
+    public StringFieldizer(String s)
+    {
+        this(s,',');
+    }
+
+    public StringFieldizer(String s, char delim)
+    {
+        this.s = s;
+        this.delim = delim;
+    }
+
+    public boolean hasMoreTokens()
+    {
+        return pos <= s.length();
+    }
+
+    public String nextToken()
+    {
+        int i = nextPos();
+        String tok = s.substring(pos,i);
+        pos = i+1;
+        return tok;
+    }
+
+    protected int nextPos()
+    {
+        int i = s.indexOf(delim,pos);
+        if (i == -1)
+        {
+            i = s.length();
+        }
+        return i;
+    }
+}
