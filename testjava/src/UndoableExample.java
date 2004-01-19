80d74510b3ce3f168cae54f0bdefbdea5a265fb8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-19 20:30:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1547 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/UndoableExample.java
+++ testjava/src/UndoableExample.java
@@ -1,13 +1,60 @@
-/*
- * TODO
- *
- * Created on Jan 19, 2004
- */
+import com.surveysampling.util.UndoableReference;
 
-/**
- * TODO
- */
-public class UndoableExample
+public class UndoableExample implements Cloneable
 {
+    private int x;
+    private UndoableReference u;
 
+    public UndoableExample(int a)
+    {
+        x = a;
+        try
+        {
+            u = new UndoableReference(this);
+        }
+        catch (CloneNotSupportedException cantHappen)
+        {
+        }
+    }
+    public Object clone()
+    {
+        UndoableExample clon = null;
+        try
+        {
+            clon = (UndoableExample)super.clone();
+        }
+        catch (CloneNotSupportedException cantHappen)
+        {
+        }
+        return clon;
+    }
+    private UndoableExample ref()
+    {
+        return (UndoableExample)u.state();
+    }
+    public String toString()
+    {
+        return ""+ref().x;
+    }
+
+    public void set(int x)
+    {
+        try
+        {
+            u.save();
+        }
+        catch (CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+        }
+        this.x = x;
+    }
+    public void undo()
+    {
+        u.undo();
+    }
+    public void redo()
+    {
+        u.redo();
+    }
 }
