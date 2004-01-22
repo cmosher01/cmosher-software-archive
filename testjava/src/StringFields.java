e8a27a4c3cb85420317904b8ce425aefeb65dd9a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-22 16:05:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1561 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/StringFields.java
+++ testjava/src/StringFields.java
@@ -1,13 +1,15 @@
-/*
- * TODO
- *
- * Created on Jan 22, 2004
- */
+import java.util.Iterator;
 
-/**
- * TODO
- */
 public class SF
 {
+    private final String s;
 
+    public SF(String s)
+    {
+        this.s = s;
+    }
+
+    public Iterator iterator()
+    {
+    }
 }
