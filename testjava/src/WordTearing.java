728478d39d3955e1cbc9d997c35c384582499dc9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-22 20:02:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1601 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/WordTearing.java
+++ testjava/src/WordTearing.java
@@ -1,13 +1,30 @@
-/*
- * TODO
- *
- * Created on Jan 22, 2004
- */
-
-/**
- * TODO
- */
-public class WordTearing
+public class WordTearing extends Thread
 {
+    static final int LENGTH = 8;
+    static final int ITERS = 10000;
+    static byte[] counts = new byte[LENGTH];
+    static Thread[] threads = new Thread[LENGTH];
+    final int id;
 
+
+    public WordTearing(int i)
+    {
+        id = i;
+    }
+
+    public void run()
+    {
+        for (; counts[id] < ITERS; counts[id]++);
+        if (counts[id] != ITERS)
+        {
+            System.err.println("Word-Tearing found: " + "counts[" + id + "] = " + counts[id]);
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < LENGTH; ++i)
+             (threads[i] = new WordTearing(i)).start();
+    }
 }
