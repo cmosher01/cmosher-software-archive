790d2bfe9d771d2dd742e950ab5438f8991b81ff
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-23 18:54:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1605 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/ReaderLines.java
+++ testjava/src/ReaderLines.java
@@ -1,13 +1,74 @@
-/*
- * TODO
- *
- * Created on Jan 23, 2004
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-/**
- * TODO
- */
-public class ReaderLines
+public class ReaderLines /* TODO implements Iterable */
 {
+    private final Iterator i;
+    
+    public ReaderLines(Reader r) throws IOException
+    {
+        i = new Iter(r);
+    }
 
+    public Iterator iterator() /* TODO SimpleIterator */
+    {
+        return i;
+    }
+
+    private static class Iter implements Iterator /* TODO SimpleIterator */
+    {
+        private final BufferedReader r;
+        private String nextLine;
+
+        private Iter(Reader r) throws IOException
+        {
+            if (this.r != null)
+            {
+                throw new UnsupportedOperationException();
+            }
+            if (r instanceof BufferedReader)
+            {
+                this.r = (BufferedReader)r;
+            }
+            else
+            {
+                this.r = new BufferedReader(r);
+            }
+            nextLine();
+        }
+
+        private void nextLine() throws IOException
+        {
+            nextLine = this.r.readLine();
+        }
+
+        public boolean hasNext()
+        {
+            return nextLine != null;
+        }
+
+        public Object next()
+        {
+            if (!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
+            int i = s.indexOf(',',pos);
+            if (i < 0)
+            {
+                i = s.length();
+            }
+            String tok = s.substring(pos,i);
+            pos = i+1;
+            return tok;
+        }
+
+        public void remove() /* TODO for SimpleIterator, don't provide this method */
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
