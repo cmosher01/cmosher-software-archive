5d26f4e151777c602842ad7dc584008d32838728
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-23 19:32:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1625 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -69,7 +69,7 @@ public class Test
 //or under 1.5:
 //        Set setLine = Util.set(new ReaderLines(r));
 
-        System.out.println(listLine);
+        System.out.println(setLine);
 
 
 //        String x = "a,b,,d";
