fc0049e0a4a7fdf7bcfbdc99229d347ff449b235
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-26 23:54:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1650 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ mysqltest/src/MySQLTest.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Jan 26, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class MySQLTest
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
