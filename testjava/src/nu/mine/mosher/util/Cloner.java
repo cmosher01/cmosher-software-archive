d8a6ba8deecdfee98fa9d69d9c45166ed5e5d6b5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-28 21:00:25 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1779 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Cloner.java
+++ testjava/src/nu/mine/mosher/util/Cloner.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 /**
  * Provides a static method that will clone any Cloneable Object.
- * This class needs to be in the java.lang package in order to be
+ * This class reflection in order to be
  * able to access the protected clone method of Object.
  */
 public final class Cloner
