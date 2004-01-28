4b98235bd6983d93a4d88e999ad81cd962bb9fe4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-28 20:31:25 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1752 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -50,7 +50,7 @@ public class Test
 
         X x = new X();
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000; ++i)
+        for (int i = 0; i < 1000000; ++i)
         {
             UniversalCloser.close(x);
         }
