b2c016b6ff6f38b2290535b5e122779fa5d26295
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-01-29 01:22:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1794 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- TestServletApp/src/HelloJSP.java
+++ TestServletApp/src/HelloJSP.java
@@ -1,20 +1,9 @@
-/*
- * Created on Jan 28, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+import servletapp.ServletApp;
 
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class HelloJSP
 {
-
-    public static void main(String[] args)
+    public static void main(String[] rArg) throws Throwable
     {
+    	ServletApp.startBrowser("/hello.jsp");
     }
 }
