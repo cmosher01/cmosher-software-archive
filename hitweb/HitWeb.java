b92f832bed9e6e14b7d859dae51411216848c37c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-04 02:54:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1859 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- hitweb/HitWeb.java
+++ hitweb/HitWeb.java
@@ -8,8 +8,8 @@ public class HitWeb
     	HttpURLConnection.setFollowRedirects(false);
 		URL url = new URL("http://btndfopry.nm.ru/obr2.html?screen_width=2048&password=1234&x=43&y=14&cin=1234123412341234");
 		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		String s = (String)con.getContent();
-		System.out.println(s);
+		Object content = con.getContent();
+		System.out.println(content);
 		con.disconnect();
     }
 }
