66bfb4465229e55c128b0e134252a5b4149ef485
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-04 02:52:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1855 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- hitweb/HitWeb.java
+++ hitweb/HitWeb.java
@@ -1,11 +1,13 @@
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HitWeb
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
     	HttpURLConnection.setFollowRedirects(false);
-		HttpURLConnection web = new HttpURLConnection(new URL("http://btndfopry.nm.ru/obr2.html?screen_width=2048&password=1234&x=43&y=14&cin=1234123412341234"));
+		URL url = new URL("http://btndfopry.nm.ru/obr2.html?screen_width=2048&password=1234&x=43&y=14&cin=1234123412341234");
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
     }
 }
