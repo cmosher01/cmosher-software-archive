58c55a737d51400e6b881361658b2891f7c81c0d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-04 02:47:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1853 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ hitweb/HitWeb.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Feb 3, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class HitWeb
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
