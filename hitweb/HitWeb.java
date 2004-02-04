034c21f92aa54a7e6c9527f83daac60bbdd0e1f1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-04 02:58:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1864 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- hitweb/HitWeb.java
+++ hitweb/HitWeb.java
@@ -8,14 +8,16 @@ public class HitWeb
     public static void main(String[] args) throws Throwable
     {
     	HttpURLConnection.setFollowRedirects(false);
-		URL url = new URL("http://btndfopry.nm.ru/obr2.html?screen_width=2048&password=1234&x=43&y=14&cin=1234123412341234");
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		con.setUseCaches(false);
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		for (String s = in.readLine(); s != null; s = in.readLine())
-		{
-			System.out.println(s);
-		}
-		con.disconnect();
+    	for (int i = 0; i < TIMES; ++i)
+    	{
+			URL url = new URL("http://btndfopry.nm.ru/obr2.html?screen_width=2048&password=1234&x=43&y=14&cin=1234123412341234");
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			for (String s = in.readLine(); s != null; s = in.readLine())
+			{
+			}
+			con.disconnect();
+    	}
     }
 }
