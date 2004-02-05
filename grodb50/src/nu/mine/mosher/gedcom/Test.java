72407fd0251bd666cacac02494d359e332a8c80c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-05 03:37:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1875 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/src/nu/mine/mosher/gedcom/Test.java
@@ -0,0 +1,33 @@
+package nu.mine.mosher.gedcom;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+
+public class Test
+{
+	public static void main(String[] rArg) throws Throwable
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("tgc55c.ged"))));
+		GedcomParser gp = new GedcomParser(br);
+
+		GedcomTree gt = new GedcomTree();
+		for (GedcomLine gl = gp.nextLine(); gl != null; gl = gp.nextLine())
+		{
+			gt.appendLine(gl);
+		}
+
+		br.close();
+
+		GedcomConcatenator gcat = new GedcomConcatenator(gt);
+		gcat.concatenate();
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("parsed.txt"))));
+		bw.write(gt.toString());
+		bw.close();
+	}
+}
