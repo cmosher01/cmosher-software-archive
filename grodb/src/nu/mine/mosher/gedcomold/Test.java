96c8d17f26f29a7a94401d8ac6228b2d47f238e2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-05 02:37:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1869 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb/src/nu/mine/mosher/gedcomold/Test.java
+++ grodb/src/nu/mine/mosher/gedcomold/Test.java
@@ -13,31 +13,21 @@ public class Test
 	public static void main(String[] rArg) throws Throwable
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("tgc55c.ged"))));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("parsed.txt"))));
 		GedcomParser gp = new GedcomParser(br);
-		GedcomLine gl = null;
-		GedcomTree gt = new GedcomTree();
 
-//		try
-//		{
-			gl = gp.nextLine();
-//		}
-//		catch (GedcomParseException e)
-//		{
-//			throw e;
-//		}
-		while (gl != null)
+		GedcomTree gt = new GedcomTree();
+		for (GedcomLine gl = gp.nextLine(); gl != null; gl = gp.nextLine())
 		{
 			gt.appendLine(gl);
-			gl = gp.nextLine();
 		}
 
-		GedcomConcatenator gc = new GedcomConcatenator(gt);
-		gc.concatenate();
-
-		bw.write(gt.toString());
-
-		bw.close();
 		br.close();
+
+		GedcomConcatenator gcat = new GedcomConcatenator(gt);
+		gcat.concatenate();
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("parsed.txt"))));
+		bw.write(gt.toString());
+		bw.close();
 	}
 }
