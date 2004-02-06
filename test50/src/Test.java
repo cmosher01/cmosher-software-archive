7eeb3e7f030aae797d4f4ed6d8fbf110495829e1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-06 02:28:34 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1904 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- test50/src/Test.java
+++ test50/src/Test.java
@@ -1,18 +1,9 @@
-/*
- * Created on Feb 5, 2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
-
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
-public class Test {
-
-	public static void main(String[] args) {
+public class Test
+{
+	public static void main(String[] args)
+	{
+		String test = "this,is,a,test";
+		StringFieldizer sf = new StringFieldizer(test);
+		SimpleIterator<String> i = sf.iterator();
 	}
 }
