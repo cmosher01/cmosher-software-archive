38f95df6e4b075c6ef58a96e3de69b578ec37e85
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-06 02:42:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1918 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/StringFieldizer.java
+++ core50/src/nu/mine/mosher/core/StringFieldizer.java
@@ -1,7 +1,7 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class StringFieldizer implements Iterable, Immutable
+public class StringFieldizer implements Iterable<String>, Immutable
 {
 	private final String s;
 
@@ -10,42 +10,44 @@ public class StringFieldizer implements Iterable, Immutable
 		this.s = s;
 	}
 
-	public SimpleIterator iterator()
+	public SimpleIterator<String> iterator()
 	{
-		return new SimpleIterator(s)
+		return new Iter(s);
+	}
+
+	private static class Iter implements SimpleIterator
+	{
+		private final String s;
+		private int pos;
+
+		private Iter(String s)
 		{
-			private final String s;
-			private int pos;
-	
-			private Iter(String s)
+			if (this.s != null)
 			{
-				if (this.s != null)
-				{
-					throw new UnsupportedOperationException();
-				}
-				this.s = s;
+				throw new UnsupportedOperationException();
 			}
-	
-			public boolean hasNext()
+			this.s = s;
+		}
+
+		public boolean hasNext()
+		{
+			return pos <= s.length();
+		}
+
+		public Object next() throws NoSuchElementException
+		{
+			if (!hasNext())
 			{
-				return pos <= s.length();
+				throw new NoSuchElementException();
 			}
-	
-			public Object next() throws NoSuchElementException
+			int i = s.indexOf(',',pos);
+			if (i < 0)
 			{
-				if (!hasNext())
-				{
-					throw new NoSuchElementException();
-				}
-				int i = s.indexOf(',',pos);
-				if (i < 0)
-				{
-					i = s.length();
-				}
-				String tok = s.substring(pos,i);
-				pos = i+1;
-				return tok;
+				i = s.length();
 			}
-		};
+			String tok = s.substring(pos,i);
+			pos = i+1;
+			return tok;
+		}
 	}
 }
