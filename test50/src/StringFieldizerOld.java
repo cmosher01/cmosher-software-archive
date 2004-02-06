26e2a43c4dafc0638cc83a93cce9584c0b11c858
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-06 02:26:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1899 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- test50/src/StringFieldizerOld.java
+++ test50/src/StringFieldizerOld.java
@@ -12,42 +12,40 @@ public class StringFieldizer implements Iterable<String>, Immutable
 
 	public SimpleIterator<String> iterator()
 	{
-		return new Iter(s);
-	}
-
-	private static class Iter implements SimpleIterator<String>
-	{
-		private final String s;
-		private int pos;
-
-		private Iter(String s)
+		return new SimpleIterator<String>()
 		{
-			if (this.s != null)
+			private final String s;
+			private int pos;
+	
+			private Iter(String s)
 			{
-				throw new UnsupportedOperationException();
+				if (this.s != null)
+				{
+					throw new UnsupportedOperationException();
+				}
+				this.s = s;
 			}
-			this.s = s;
-		}
-
-		public boolean hasNext()
-		{
-			return pos <= s.length();
-		}
-
-		public Object next() throws NoSuchElementException
-		{
-			if (!hasNext())
+	
+			public boolean hasNext()
 			{
-				throw new NoSuchElementException();
+				return pos <= s.length();
 			}
-			int i = s.indexOf(',',pos);
-			if (i < 0)
+	
+			public Object next() throws NoSuchElementException
 			{
-				i = s.length();
+				if (!hasNext())
+				{
+					throw new NoSuchElementException();
+				}
+				int i = s.indexOf(',',pos);
+				if (i < 0)
+				{
+					i = s.length();
+				}
+				String tok = s.substring(pos,i);
+				pos = i+1;
+				return tok;
 			}
-			String tok = s.substring(pos,i);
-			pos = i+1;
-			return tok;
 		}
-	}
+//	}
 }
