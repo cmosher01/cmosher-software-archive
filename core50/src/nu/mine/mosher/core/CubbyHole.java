ff75d9766705e77f550c1acb999a2ec552b54b8a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 20:39:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2057 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/CubbyHole.java
+++ core50/src/nu/mine/mosher/core/CubbyHole.java
@@ -1,9 +1,59 @@
+package nu.mine.mosher.core;
 
-
-/**
- * @author Chris Mosher
- * Created: Feb 7, 2004
- */
-public class CubbyHole
+public class CubbyHole<T>
 {
+	private T mContents;
+	private boolean mHasContents;
+
+	/**
+	 * Waits for another thread to put and object into this
+	 * cubbyhole, then (this thread) removes the object from
+	 * this cubbyhole and returns it.
+	 * @return the Object; may be null;
+	 */
+	public synchronized T get()
+	{
+		T contents = null;
+		try
+		{
+			while (!mHasContents)
+			{
+				wait();
+			}
+			contents = mContents;
+			mHasContents = false;
+			notifyAll();
+		}
+		catch (InterruptedException e)
+		{
+			Thread.currentThread().interrupt();
+		}
+		mContents = null;
+
+		return contents;
+	}
+
+	/**
+	 * Puts the specified object into this cubbyhole, and
+	 * nofities threads waiting to get from this cubbyhole.
+	 * 
+	 * @param value the Object to put into this cubbyhole; can be null
+	 */
+	public synchronized void put(T value)
+	{
+		try
+		{
+			while (mHasContents)
+			{
+				wait();
+			}
+			mContents = value;
+			mHasContents = true;
+			notifyAll();
+		}
+		catch (InterruptedException e)
+		{
+			Thread.currentThread().interrupt();
+		}
+	}
 }
