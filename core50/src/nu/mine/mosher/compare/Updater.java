f09a8ab595b02b200218590b5b0d228393d27dd2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 19:10:46 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2038 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/compare/Updater.java
+++ core50/src/nu/mine/mosher/compare/Updater.java
@@ -1,9 +1,12 @@
-
+package nu.mine.mosher.core;
 
 /**
  * @author Chris Mosher
  * Created: Feb 7, 2004
  */
-public interface Updater
+public interface Updater<T>
 {
+	void insert(T objNew) throws UpdateException;
+	void update(T objOld, Object objNew) throws UpdateException;
+	void delete(T objOld) throws UpdateException;
 }
