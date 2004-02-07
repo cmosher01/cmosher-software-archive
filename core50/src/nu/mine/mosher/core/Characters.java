12fdc16a270c36667f189a5391b68d280b83d2ea
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 04:07:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1960 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ core50/src/nu/mine/mosher/core/Characters.java
@@ -0,0 +1,22 @@
+package nu.mine.mosher.core;
+
+/**
+ * This class consists exclusively of static methods that
+ * deal with characters.
+ *
+ * @author Chris Mosher
+ */
+public final class Characters
+{
+	private Characters()
+	{
+	}
+
+    public static boolean isHexDigit(char c)
+    {
+    	return
+    		('0' <= c && c <= '9') ||
+    		('a' <= c && c <= 'f') ||
+    		('A' <= c && c <= 'F');
+    }
+}
