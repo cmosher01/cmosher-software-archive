f992c9bd0592d55bce3411a131c8c82772af3305
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 04:05:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1959 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ core50/src/nu/mine/mosher/core/Immutable.java
@@ -0,0 +1,12 @@
+package nu.mine.mosher.core;
+
+/**
+ * This class is immutable.
+ * The Immutable interface is used only as a marker on an implementing
+ * class to indicate that objects of that class are (intended to be) immutable.
+ * 
+ * @author Chris Mosher
+ */
+public interface Immutable
+{
+}
