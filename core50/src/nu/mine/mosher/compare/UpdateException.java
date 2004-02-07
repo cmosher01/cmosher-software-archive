63ef33e61e711b2e061f17de831ba7857244e13f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 20:18:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2040 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/compare/UpdateException.java
+++ core50/src/nu/mine/mosher/compare/UpdateException.java
@@ -1,45 +1,24 @@
+package nu.mine.mosher.core;
 
-
-/**
- * @author Chris Mosher
- * Created: Feb 7, 2004
- */
 public class UpdateException extends Exception
 {
-	/**
-	 * 
-	 */
 	public UpdateException()
 	{
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param message
-	 */
 	public UpdateException(String message)
 	{
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
 	public UpdateException(String message, Throwable cause)
 	{
 		super(message,cause);
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param cause
-	 */
 	public UpdateException(Throwable cause)
 	{
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 }
