70c90e4c266ee89136a588ab346160eac5104847
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 20:23:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2042 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Closer.java
+++ core50/src/nu/mine/mosher/core/Closer.java
@@ -1,9 +1,58 @@
+package nu.mine.mosher.core;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * @author Chris Mosher
- * Created: Feb 7, 2004
- */
-public class Closer
+public final class Closer
 {
+	private Closer()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Calls the given object's "close()" method, if it has one.
+	 * Any exceptions are ignored.
+	 * 
+	 * For example:
+	 * <pre>
+	 * OutputStream os = null;
+	 * try
+	 * {
+	 *     os = new FileOutputStream(new File("test.txt"));
+	 *     os.write(65);
+	 * }
+	 * finally
+	 * {
+	 *     UniversalCloser.close(os);
+	 * }
+	 * </pre>
+	 * 
+	 * Performance considerations: on an Intel-based Windows PC
+	 * with Java 1.4.2, where calling close on an object took
+	 * about 4 nanoseconds, calling UniversalCloser2.close on
+	 * that object took about 384 nanoseconds.
+	 * 
+	 * @param obj the Object whose close() method is to be called.
+	 */
+	private static final Map<Class,Method> mClasses = new HashMap<Class,Method>();
+	public static void close(Object obj)
+	{
+		try
+		{
+			Class cl = obj.getClass();
+			Method methodClose = mClasses.get(cl);
+			if (methodClose == null)
+			{
+				methodClose = cl.getMethod("close",null);
+				mClasses.put(cl,methodClose);
+			}
+			methodClose.invoke(obj,null);
+		}
+		catch (Throwable ignore)
+		{
+			ignore.printStackTrace();
+		}
+	}
 }
