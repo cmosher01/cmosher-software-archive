45806b2a9467cb910ad11449d8bca738e33da5b8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 20:26:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2044 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Undoer.java
+++ core50/src/nu/mine/mosher/core/Undoer.java
@@ -1,9 +1,39 @@
+package nu.mine.mosher.core;
 
+import java.util.LinkedList;
 
-/**
- * @author Chris Mosher
- * Created: Feb 7, 2004
- */
-public class Undoer
+import static nu.mine.mosher.Cloner.cloneObject;
+
+public final class Undoer<T>
 {
+	private final LinkedList<T> mrUndo = new LinkedList<T>();
+	private final LinkedList<T> mrRedo = new LinkedList<T>();
+
+	public void save(T state) throws CloneNotSupportedException
+	{
+		mrUndo.addLast(cloneObject(state));
+		mrRedo.clear();
+	}
+
+	public T undo(T state)
+	{
+		mrRedo.addFirst(state);
+		return mrUndo.removeLast();
+	}
+
+	public T redo(T state)
+	{
+		mrUndo.addLast(state);
+		return mrRedo.removeFirst();
+	}
+
+	public boolean canUndo()
+	{
+		return !mrUndo.isEmpty();
+	}
+
+	public boolean canRedo()
+	{
+		return !mrRedo.isEmpty();
+	}
 }
