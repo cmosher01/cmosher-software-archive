03487e75faff497e682e50ff3ef705e63c3bc2d6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 05:19:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2001 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Pair.java
+++ core50/src/nu/mine/mosher/core/Pair.java
@@ -1,9 +1,60 @@
+package nu.mine.mosher.core;
 
-
-/**
- * @author Chris Mosher
- * Created: Feb 7, 2004
- */
-public class Pair
+public final class Pair<T,U> implements Cloneable, Comparable<T>, Serializable,	Immutable
 {
+	private final ImmutableReference<T> a;
+	private final ImmutableReference<U> b;
+
+	public Pair(T a, U b) throws CloneNotSupportedException
+	{
+		this.a = new ImmutableReference<T>(a);
+		this.b = new ImmutableReference<U>(b);
+	}
+
+	public T a() throws CloneNotSupportedException
+	{
+		return this.a.object();
+	}
+
+	public U b() throws CloneNotSupportedException
+	{
+		return this.b.object();
+	}
+
+	public Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+
+	public String toString()
+	{
+		return "(" + a + "," + b + ")";
+	}
+
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof Pair))
+		{
+			return false;
+		}
+		Pair that = (Pair)o;
+		return this.a.equals(that.a) && this.b.equals(that.b);
+	}
+
+	public int hashCode()
+	{
+		return a.hashCode() ^ b.hashCode();
+	}
+
+	public int compareTo(T o)
+	{
+		Pair that = (Pair)o;
+		int c;
+		c = this.a.compareTo(that.a);
+		if (c == 0)
+		{
+			c = this.b.compareTo(that.b);
+		}
+		return c;
+	}
 }
