b3041c1ab08df68c8a5b9a56bc34c9cd6205e836
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 20:27:26 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2047 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Undoer.java
+++ core50/src/nu/mine/mosher/core/Undoer.java
@@ -11,7 +11,7 @@ public final class Undoer<T>
 
 	public void save(T state) throws CloneNotSupportedException
 	{
-		mrUndo.addLast(cloneObject<T>(state));
+		mrUndo.addLast(cloneObject(state));
 		mrRedo.clear();
 	}
 
