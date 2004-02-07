02f0c4192fea65d0afee3813045665f5b27b2bae
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-07 04:19:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@1968 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/CloneFactory.java
+++ core50/src/nu/mine/mosher/core/CloneFactory.java
@@ -1,9 +1,50 @@
+package nu.mine.mosher.core;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * @author Chris Mosher
- * Created: Feb 6, 2004
+ * Provides a static method that will clone any Cloneable Object.
+ * This class reflection in order to be
+ * able to access the protected clone method of Object.
  */
-public class Cloner
+public final class Cloner
 {
+	private static final Map<Class,Method> mClasses = new HashMap();
+
+	private Cloner()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public static Cloneable cloneObject(Cloneable cloneableObject) throws CloneNotSupportedException
+	{
+		try
+		{
+			Class cl = cloneableObject.getClass();
+			Method methodClone = mClasses.get(cl);
+			if (methodClone == null)
+			{
+				methodClone = cl.getMethod("clone",null);
+				methodClone.setAccessible(true);
+				mClasses.put(cl,methodClone);
+			}
+			return (Cloneable)methodClone.invoke(cloneableObject,null);
+		}
+		catch (Throwable cause)
+		{
+			CloneNotSupportedException ex;
+			if (cause instanceof CloneNotSupportedException)
+			{
+				ex = (CloneNotSupportedException)cause;
+			}
+			else
+			{
+				ex = new CloneNotSupportedException();
+				ex.initCause(cause);
+			}
+			throw ex;
+		}
+	}
 }
