8cf44131ffc77744da84e125b64609c6d2581075
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-08 04:39:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2127 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/grodb/EventType.java
+++ grodb50/src/nu/mine/mosher/grodb/EventType.java
@@ -5,6 +5,12 @@ package nu.mine.mosher.grodb;
  * @author Chris Mosher
  * Created: Feb 7, 2004
  */
-public class EventType
+public enum EventType
 {
+	birth,
+	death,
+	wedding,
+	baptism,
+	relocation,
+	census
 }
