7ceeb96df3f9f5164ea9f1167e9546e0bdb907a8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-08 05:40:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2175 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/grodb/ItemType.java
+++ grodb50/src/nu/mine/mosher/grodb/ItemType.java
@@ -15,7 +15,7 @@ public class ItemType<T extends Enum<T>>
 	}
 	public ItemType(String other)
 	{
-		Enum.valueOf(Class<T>.getClass());
+		Enum.valueOf(Class<T>.getClass(),other);
 		this.other = other;
 	}
 
