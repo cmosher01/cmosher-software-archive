321c145d46ae75d8dbf1737890da08a67abc3c69
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-08 02:17:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2089 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/grodb/Search.java
+++ grodb50/src/nu/mine/mosher/grodb/Search.java
@@ -1,5 +1,8 @@
 package nu.mine.mosher.grodb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Chris Mosher
  * Created: Feb 7, 2004
@@ -7,6 +10,8 @@ package nu.mine.mosher.grodb;
 public class Search
 {
 	private String description;
+
+	private List<Relation<Source,Search>> rSource = new ArrayList<Relation<Source,Search>>();
 
 	public Search()
 	{
