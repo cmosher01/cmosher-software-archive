d859045d987e7ba6e8177ab04ec879c87fa258c5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-08 18:52:52 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2200 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/grodb/Hypothesis.java
+++ grodb50/src/nu/mine/mosher/grodb/Hypothesis.java
@@ -9,11 +9,15 @@ import java.util.List;
  */
 public class Hypothesis
 {
+	private boolean affirmed;
 	private String rationale;
 	private Source source;
 
+	private Role role;
+
 	private List<Relation<Hypothesis,Hypothesis,HypothesisRelType>> rRelUp = new ArrayList<Relation<Hypothesis,Hypothesis,HypothesisRelType>>();
 	private List<Relation<Hypothesis,Hypothesis,HypothesisRelType>> rRelDown = new ArrayList<Relation<Hypothesis,Hypothesis,HypothesisRelType>>();
+	private List<Relation<Hypothesis,Search,HypoSearchRelType>> rSearch = new ArrayList<Hypothesis,Search,HypoSearchRelType>>();
 
 	public Hypothesis()
 	{
