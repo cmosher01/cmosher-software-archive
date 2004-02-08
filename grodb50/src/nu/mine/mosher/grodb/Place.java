9938997c3b1a832e6259185e88660add82ec7c55
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-08 02:34:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2090 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/grodb/Place.java
+++ grodb50/src/nu/mine/mosher/grodb/Place.java
@@ -2,5 +2,12 @@ package nu.mine.mosher.grodb;
 
 public class Place
 {
+	private String nameDisplay;
+	private String nameFull;
 
+	private List<Relation<Place,Place>> rPlace = new ArrayList<Relation<Place,Place>>();
+
+	public Place()
+	{
+	}
 }
