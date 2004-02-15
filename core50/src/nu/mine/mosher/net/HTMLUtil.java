ce88dc6ef109e011d07cf4da183ae93d51092708
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-15 23:36:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2273 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/net/HTMLUtil.java
+++ core50/src/nu/mine/mosher/net/HTMLUtil.java
@@ -1,9 +1,22 @@
-
+package nu.mine.mosher.net;
 
 /**
  * @author Chris Mosher
  * Created: Feb 15, 2004
  */
-public class HTMLUtil
+public final class HTMLUtil
 {
+	private HTMLUtil()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public String fixHTML(String s)
+	{
+		return s
+			.replaceAll("&","&amp;")
+			.replaceAll("<","&lt;")
+			.replaceAll(">","&gt;")
+			.replaceAll("\"","&quot;");
+	}
 }
