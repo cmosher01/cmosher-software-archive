edd40099330fab31ed5a7c3fb2a4c88906dfe586
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-19 01:42:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2302 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest50/src/nu/mine/mosher/jdotest/PersonPersist.java
+++ jdotest50/src/nu/mine/mosher/jdotest/PersonPersist.java
@@ -1,4 +1,8 @@
+import java.util.Properties;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
 
 /**
  * @author Chris Mosher
@@ -6,4 +10,28 @@
  */
 public class PersonPersist
 {
+	private final static int SIZE = 3;
+	private PersistenceManagerFactory pmf;
+	private PersistenceManager pm;
+	private Transaction transaction;
+
+	private Person[] people;
+
+	private List<Integer> rid = new ArrayList<Integer>(SIZE);
+
+	public PersonPersist()
+	{
+		try
+		{
+			Properties props = new Properties();
+			props.setProperty("javax.jdo.PersistenceManagerFactoryClass","com.prismt.j2ee.jdo.PersistenceManagerFactoryImpl");
+			pmf = JDOHelper.getPersistenceManagerFactory(props);
+			pmf.setConnectionFactory(createConnectionFactory());
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
 }
