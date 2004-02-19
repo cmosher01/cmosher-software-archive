e3b66025acfc74528fc919ead0e15bc686a7c9e5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-19 20:52:54 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2311 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testcrypt/TestCrypt.java
+++ testcrypt/TestCrypt.java
@@ -1,9 +1,43 @@
+import java.security.Provider;
 import java.security.SecureRandom;
+import java.security.Security;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class TestCrypt
 {
     public static void main(String[] rArg) throws Throwable
     {
         SecureRandom rand = new SecureRandom();
+    }
+
+    public static String[] getCryptoImpls(String serviceType)
+    {
+        Set result = new HashSet();
+
+        // All all providers
+        Provider[] providers = Security.getProviders();
+        for (int i = 0; i < providers.length; i++)
+        {
+            // Get services provided by each provider
+            Set keys = providers[i].keySet();
+            for (Iterator it = keys.iterator(); it.hasNext();)
+            {
+                String key = (String)it.next();
+                key = key.split(" ")[0];
+
+                if (key.startsWith(serviceType + "."))
+                {
+                    result.add(key.substring(serviceType.length() + 1));
+                }
+                else if (key.startsWith("Alg.Alias." + serviceType + "."))
+                {
+                    // This is an alias
+                    result.add(key.substring(serviceType.length() + 11));
+                }
+            }
+        }
+        return (String[])result.toArray(new String[result.size()]);
     }
 }
