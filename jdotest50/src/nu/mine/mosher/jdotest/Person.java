fc5dbf2d05f7346a18ce28d87a63b583fd5b56b0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-19 01:38:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2299 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest50/src/nu/mine/mosher/jdotest/Person.java
+++ jdotest50/src/nu/mine/mosher/jdotest/Person.java
@@ -1,9 +1,27 @@
-
-
 /**
  * @author Chris Mosher
  * Created: Feb 18, 2004
  */
 public class Person
 {
+	private String name;
+
+	public Person()
+	{
+	}
+
+	public Person(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
