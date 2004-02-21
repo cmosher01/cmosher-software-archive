cb5f5f5cbcca2fa458a33900a6de263a01dba314
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-02-21 01:18:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2388 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest50/enhance.bat
+++ jdotest50/enhance.bat
@@ -1,1 +1,1 @@
-java -cp lib\jdo.jar;lib\jdori.jar;lib\jpox-1.0.0-beta-1.jar;lib\log4j-1.2.8.jar;bin org.jpox.enhance.SunReferenceEnhancer -v -s bin;lib\jdo.jar bin\package.jdo
+java -cp lib\jdo.jar;lib\jdori.jar;lib\jpox-1.0.0-beta-1.jar;lib\log4j-1.2.8.jar;bin org.jpox.enhance.SunReferenceEnhancer -v -s bin;lib\jdo.jar bin\package.jdo -d bin
