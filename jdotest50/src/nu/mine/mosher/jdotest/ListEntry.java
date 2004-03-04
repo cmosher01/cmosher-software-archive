afadce35d2199016f2a05d7cbff248fa9b1fca41
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-04 03:11:26 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2430 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest50/src/nu/mine/mosher/jdotest/ListEntry.java
+++ jdotest50/src/nu/mine/mosher/jdotest/ListEntry.java
@@ -20,7 +20,7 @@ public class ListEntry
 		return this.s;
 	}
 
-	public static String formatListEntry(List<ListEntry> r)
+	public static <ListEntry> String formatListEntry(List<ListEntry> r)
 	{
 		return tr(r,"td");
 	}
