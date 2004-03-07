62a7df3bdab387a56141f36bc3acdcb4985e4d48
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-07 04:18:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2481 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/Perm.java
+++ jdotest/src/nu/mine/mosher/jdotest/Perm.java
@@ -1,8 +1,5 @@
 /*
  * Created on Mar 6, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package nu.mine.mosher.jdotest;
 
@@ -12,10 +9,7 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
 /**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author Chris Mosher
  */
 public class Perm
 {
