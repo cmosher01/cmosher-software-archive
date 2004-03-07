c69170205ee92363bfe5bd7a0bf8a0bae79ceccb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-07 02:43:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2475 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/build/enhance.bat
+++ jdotest/build/enhance.bat
@@ -1,3 +1,4 @@
+set JDO=%BIN%\nu\mine\mosher\jdotest\package.jdo
 set W=..\web\WEB-INF
 set LIB=%W%\lib
 set BIN=%W%\classes
@@ -8,5 +9,4 @@ set CLASSPATH=%CLASSPATH%;%LIB%\jdori.jar
 set CLASSPATH=%CLASSPATH%;%LIB%\log4j-1.2.8.jar
 set SRC=%BIN%
 set SRC=%SRC%;%LIB%\jdo.jar
-set JDO=%BIN%\nu\mine\mosher\jdotest\package.jdo
 C:\Progra~1\Java\j2sdk1.4.2\bin\java -cp %CLASSPATH% org.jpox.enhance.SunReferenceEnhancer -v -s %SRC% %JDO% -d %BIN%
