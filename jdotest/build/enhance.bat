74cc19f714ef5a65cfc55d7bb66862e5ea1af4c6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-07 02:36:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2467 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/build/enhance.bat
+++ jdotest/build/enhance.bat
@@ -1,1 +1,12 @@
-"C:\Program Files\Java\j2sdk1.4.2\bin\java" -cp ..\web\WEB-INF\lib\jdo.jar;..\web\WEB-INF\lib\jdori.jar;..\web\WEB-INF\lib\jpox-1.0.0-beta-2.jar;..\web\WEB-INF\lib\log4j-1.2.8.jar;..\web\WEB-INF\classes org.jpox.enhance.SunReferenceEnhancer -v -s ..\web\WEB-INF\classes;..\web\WEB-INF\lib\jdo.jar ..\web\WEB-INF\classes\nu\mine\mosher\jdotest\package.jdo -d ..\web\WEB-INF\classes
+set W=..\web\WEB-INF
+set LIB=%W%\lib
+set BIN=%W%\classes
+set CLASSPATH=%LIB%\jdo.jar
+set CLASSPATH=%CLASSPATH%;%LIB%\jdori.jar
+set CLASSPATH=%CLASSPATH%;%LIB%\jpox-1.0.0-beta-2.jar
+set CLASSPATH=%CLASSPATH%;%LIB%\log4j-1.2.8.jar
+set CLASSPATH=%CLASSPATH%;%BIN%
+set SRC=%BIN%
+set SRC=%SRC%;%LIB%\jdo.jar
+set JDO=%BIN%\nu\mine\mosher\jdotest\package.jdo
+"C:\Program Files\Java\j2sdk1.4.2\bin\java" -cp %CLASSPATH% org.jpox.enhance.SunReferenceEnhancer -v -s %SRC% %JDO% -d %BIN%
