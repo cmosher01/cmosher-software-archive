d6001e8ceac5669712652e4d7a272b7b7c03f2c1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-07 04:07:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2476 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ jdotest/src/nu/mine/mosher/jdotest/Perm.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Mar 6, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.jdotest;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Perm
+{
+
+}
