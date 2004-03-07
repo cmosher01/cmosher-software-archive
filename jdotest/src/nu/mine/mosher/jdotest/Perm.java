4ba97626c4fad237d4f5406189c1eb355fb2f304
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-07 06:53:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2551 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/Perm.java
+++ jdotest/src/nu/mine/mosher/jdotest/Perm.java
@@ -3,6 +3,11 @@
  */
 package nu.mine.mosher.jdotest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 import javax.jdo.JDOHelper;
@@ -15,22 +20,25 @@ import javax.naming.NamingException;
  */
 public class Perm
 {
-	private static final Properties props = new Properties();
-	static
-	{
-		props.setProperty("javax.jdo.PersistenceManagerFactoryClass","org.jpox.PersistenceManagerFactoryImpl");
-		props.setProperty("javax.jdo.option.ConnectionDriverName","org.jpox.driver.JPOXDriver");
-		props.setProperty("javax.jdo.option.ConnectionURL","jpox:java:comp/env/jdbc/jdotest");
-		props.setProperty("javax.jdo.option.NontransactionalRead","true");
-		props.setProperty("org.jpox.autoCreateTables","true");
-		props.setProperty("org.jpox.validateTables","false");
-		props.setProperty("org.jpox.validateConstraints","false");
-	}
+//	private static final Properties props = new Properties();
+//	static
+//	{
+//		props.setProperty("javax.jdo.PersistenceManagerFactoryClass","org.jpox.PersistenceManagerFactoryImpl");
+//		props.setProperty("javax.jdo.option.ConnectionDriverName","org.jpox.driver.JPOXDriver");
+//		props.setProperty("javax.jdo.option.ConnectionURL","jpox:java:comp/env/jdbc/jdotest");
+//		props.setProperty("javax.jdo.option.NontransactionalRead","true");
+//		props.setProperty("org.jpox.autoCreateTables","true");
+//		props.setProperty("org.jpox.validateTables","false");
+//		props.setProperty("org.jpox.validateConstraints","false");
+//	}
 
 	private final PersistenceManager pm;
 
-	public Perm() throws NamingException
+	public Perm() throws NamingException, FileNotFoundException, IOException
 	{
+		URL urlProps = Perm.class.getClassLoader().getResource("nu/mine/mosher/jdotest/jdo.properties");
+		Properties props = new Properties();
+		props.load(new FileInputStream(new File(urlProps.toString())));
 		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(props);
 		this.pm = pmf.getPersistenceManager();
 	}
