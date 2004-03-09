9c44979f410d38cb9bea520fe6cf3d68f3f0846b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-09 02:48:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2639 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/Thing.java
+++ jdotest/src/nu/mine/mosher/jdotest/Thing.java
@@ -14,7 +14,7 @@ import java.util.List;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class Thing
+public class Thing implements Named
 {
 	private String name;
 	private List rmmItem;
