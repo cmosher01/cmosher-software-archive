6dd4a9056f33fa837c1d8572054c32ce831883aa
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-09 02:53:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2643 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/ListNamed.java
+++ jdotest/src/nu/mine/mosher/jdotest/ListNamed.java
@@ -6,6 +6,10 @@
  */
 package nu.mine.mosher.jdotest;
 
+import java.util.Iterator;
+
+import javax.jdo.PersistenceManager;
+
 /**
  * @author Chris
  *
@@ -20,5 +24,25 @@ public class ListNamed
 		{
 			return "Error";
 		}
+
+		StringBuffer sb = new StringBuffer(2048);
+		sb.append("<table>\n");
+
+		PersistenceManager pm = Perm.pm();
+		for (Iterator i = pm.getExtent(Item.class,true).iterator(); i.hasNext(); )
+		{
+			Item item = (Item)i.next();
+			String id = pm.getObjectId(item).toString();
+			sb.append("<tr><td><a href=\"edit.jsp?id=");
+			sb.append("\">");
+			sb.append(item.getName());
+			sb.append("</a></td></tr>");
+		}
+
+		sb.append("<tr><td>&lt;<a href=\"add.jsp\">add</a>&gt;</td></tr>");
+
+		sb.append("</table>");
+
+		return sb.toString();
 	}
 }
