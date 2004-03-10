d2d90cd672152c8728ef54a92283755e57db4b8d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-10 19:57:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2660 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testexec/src/TestExec.java
+++ testexec/src/TestExec.java
@@ -1,7 +1,28 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestExec
 {
     public static void main(String[] args)
     {
-        System.out.println("parent process starting...");
+        System.out.println("parent process starting");
+    }
+
+    protected void execDumpOutput() throws InterruptedException, IOException
+    {
+        System.out.println("starting subprocess...");
+        Process p = Runtime.getRuntime().exec("lsloop");
+
+        LineDumper procout = new LineDumper(new InputStreamReader(p.getInputStream()));
+        LineDumper procerr = new LineDumper(new InputStreamReader(p.getErrorStream()));
+
+        p.waitFor();
+        System.out.println("subprocess done");
+
+        procout.waitFor();
+        procerr.waitFor();
+
+        int exitValue = p.exitValue();
+        System.out.println("exit value: "+exitValue);
     }
 }
