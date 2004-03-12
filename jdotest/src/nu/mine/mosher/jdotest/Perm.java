4d5430eb4b3db67fa898a0a3e9d4cf683584e908
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-12 02:46:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2708 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/Perm.java
+++ jdotest/src/nu/mine/mosher/jdotest/Perm.java
@@ -3,11 +3,6 @@
  */
 package nu.mine.mosher.jdotest;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.URL;
-import java.util.Properties;
-
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 
@@ -16,23 +11,8 @@ import javax.jdo.PersistenceManager;
  */
 public class Perm
 {
-	private static final Properties props = new Properties();
-
-	static
-	{
-		try
-        {
-			URL urlProps = Perm.class.getClassLoader().getResource("nu/mine/mosher/jdotest/jdo.properties");
-            props.load(new FileInputStream(new File(urlProps.getFile())));
-        }
-        catch (Throwable e)
-        {
-            e.printStackTrace();
-        }
-	}
-
 	public static PersistenceManager pm()
 	{
-		return JDOHelper.getPersistenceManagerFactory(props).getPersistenceManager();
+		return JDOHelper.getPersistenceManagerFactory(AppInit.getJDOProperties()).getPersistenceManager();
 	}
 }
