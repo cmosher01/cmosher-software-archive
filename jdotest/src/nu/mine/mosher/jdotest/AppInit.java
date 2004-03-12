02f407cce81628236a0ec7c225c7257ca431af1e
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-12 02:29:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2695 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/AppInit.java
+++ jdotest/src/nu/mine/mosher/jdotest/AppInit.java
@@ -6,6 +6,12 @@
  */
 package nu.mine.mosher.jdotest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 /**
@@ -14,7 +20,24 @@ import javax.servlet.ServletContextListener;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class AppInit extends ServletContextListener
+public class AppInit implements ServletContextListener
 {
+	private static final Properties props = new Properties();
 
+    public void contextInitialized(ServletContextEvent arg0)
+    {
+		try
+		{
+			URL urlProps = Perm.class.getClassLoader().getResource("nu/mine/mosher/jdotest/jdo.properties");
+			props.load(new FileInputStream(new File(urlProps.getFile())));
+		}
+		catch (Throwable e)
+		{
+			e.printStackTrace();
+		}
+    }
+
+    public void contextDestroyed(ServletContextEvent arg0)
+    {
+    }
 }
