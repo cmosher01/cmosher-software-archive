ac4663c5a448041a773f24897df569d5b377d735
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-14 22:48:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2844 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- jdotest/src/nu/mine/mosher/jdotest/NamedDefault.java
+++ jdotest/src/nu/mine/mosher/jdotest/NamedDefault.java
@@ -3,24 +3,17 @@
  */
 package nu.mine.mosher.jdotest;
 
-import javax.jdo.PersistenceManager;
+import javax.jdo.JDOHelper;
 
 /**
  * @author Chris
  */
 public abstract class NamedDefault implements Named
 {
-	private transient PersistenceManager pm;
-
     public String getId()
     {
-        return this.pm.getObjectId(this).toString();
+        return JDOHelper.getObjectId(this).toString();
     }
-
-	public void setPerm(PersistenceManager pm)
-	{
-		this.pm = pm;
-	}
 
 	public abstract String getName();
 }
