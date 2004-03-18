d4da71612223f70abf8b6e919c992ba49cda2bd6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-03-18 20:53:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2876 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -43,7 +43,7 @@ public class Test
 	{
         StringBuffer code = new StringBuffer(20);
 
-        String base = "thisfqawsed";
+        String base = "thisfqawse";
         if (base.length() > 11)
         {
             base = base.substring(0, 11);
