d856acf3b8a53fbcc063e9e13e1a902aafdc857c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-01 19:32:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2917 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/src/nu/mine/mosher/util/EnumIter.java
@@ -0,0 +1,52 @@
+/*
+ * TODO
+ *
+ * Created on Apr 1, 2004
+ */
+package com.surveysampling.util;
+
+import java.util.Iterator;
+
+/**
+ * TODO
+ */
+public class EnumIter extends Object implements Iterator
+{
+
+    /**
+     * 
+     */
+    public EnumIter()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#hasNext()
+     */
+    public boolean hasNext()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#next()
+     */
+    public Object next()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#remove()
+     */
+    public void remove()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
