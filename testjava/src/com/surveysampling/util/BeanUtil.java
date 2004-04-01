5cfde0ddf5b2fa44000e3705761456a62d52a4e7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-01 19:54:13 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2929 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/util/BeanUtil.java
+++ testjava/src/com/surveysampling/util/BeanUtil.java
@@ -1,14 +1,79 @@
-/*
- * TODO
- *
- * Created on Apr 1, 2004
- */
 package com.surveysampling.util;
 
-/**
- * TODO
- */
-public class BeanUtil
-{
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.Iterator;
 
+import javax.servlet.ServletRequest;
+
+public final class BeanUtil
+{
+    private BeanUtil()
+    {
+        throw new IllegalStateException();
+    }
+
+    public void parseParameters(ServletRequest request, Object bean) throws ParameterParseException
+    {
+        for (Iterator params = new EnumIter(request.getParameterNames()); params.hasNext();)
+        {
+            String param = (String)params.next();
+
+            Method method = null;
+            Class type = null;
+            Class propertyEditorClass = null;
+
+            BeanInfo info = Introspector.getBeanInfo(bean.getClass());
+            if (info != null)
+            {
+                PropertyDescriptor pd[] = info.getPropertyDescriptors();
+                for (int i = 0; i < pd.length; i++)
+                {
+                    if (pd[i].getName().equals(param))
+                    {
+                        method = pd[i].getWriteMethod();
+                        type = pd[i].getPropertyType();
+                        propertyEditorClass = pd[i].getPropertyEditorClass();
+                        break;
+                    }
+                }
+            }
+            if (method != null)
+            {
+                if (type.isArray())
+                {
+                    if (request == null)
+                    {
+                        throw new ParameterParseException();
+                    }
+                    Class t = type.getComponentType();
+                    String[] values = request.getParameterValues(param);
+                    if (values == null)
+                    {
+                        return;
+                    }
+                    if (t.equals(String.class))
+                    {
+                        method.invoke(bean, new Object[] { values });
+                    }
+                    else
+                    {
+                        Object tmpval = null;
+                        createTypedArray(param, bean, method, values, t, propertyEditorClass);
+                    }
+                }
+                else
+                {
+                    String value = request.getParameter(param);
+                    if (value == null || (param != null && value.equals("")))
+                        return;
+                    Object oval = convert(param, value, type, propertyEditorClass);
+                    if (oval != null)
+                        method.invoke(bean, new Object[] { oval });
+                }
+            }
+        }
+    }
 }
