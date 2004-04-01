3c9da6ab2bbf6f7777086a1c7e76e0776be52f5b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-01 20:12:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2944 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/util/ParameterParseException.java
+++ testjava/src/com/surveysampling/util/ParameterParseException.java
@@ -2,6 +2,9 @@ package com.surveysampling.util;
 
 public class ParameterParseException extends Exception
 {
+    public ParameterParseException()
+    {
+    }
     public ParameterParseException(Throwable e)
     {
         super(e);
