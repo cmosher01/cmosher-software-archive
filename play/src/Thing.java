dc47a3c29b4177ba0da30a20ef9d288e70b1eb1b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-02 00:52:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2956 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ play/src/Thing.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Apr 1, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Thing
+{
+
+}
