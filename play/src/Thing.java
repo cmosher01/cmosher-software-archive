f352d6b34b15aca1501101c0a21285a6d87c4720
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-02 00:53:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2957 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- play/src/Thing.java
+++ play/src/Thing.java
@@ -1,17 +1,8 @@
-/*
- * Created on Apr 1, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
-
-/**
- * @author Chris
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class Thing
 {
-
+	public void test() throws Exception
+	{
+		System.out.println("test");
+//		throw new Exception("test exception");
+	}
 }
