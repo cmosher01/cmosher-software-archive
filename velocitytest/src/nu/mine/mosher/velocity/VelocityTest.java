30c8443c6475c1a36d1b185eae40aad108a6b898
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-05 17:16:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@2980 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocitytest/src/nu/mine/mosher/velocity/VelocityTest.java
+++ velocitytest/src/nu/mine/mosher/velocity/VelocityTest.java
@@ -2,8 +2,9 @@ import org.apache.velocity.app.VelocityEngine;
 
 public class VelocityTest
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
         VelocityEngine velocity = new VelocityEngine();
+        velocity.init();
     }
 }
