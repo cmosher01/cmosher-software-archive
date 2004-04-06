37d2026e6fab404ec2ac879372db83a4c201dcf9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-06 14:47:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3046 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocitytest/src/nu/mine/mosher/velocity/Dependency.java
+++ velocitytest/src/nu/mine/mosher/velocity/Dependency.java
@@ -3,4 +3,35 @@ public class Dependency
     private final String name;
     private final String version;
     private final boolean source;
+
+    public Dependency(String name, String version, boolean source)
+    {
+        this.name = name;
+        this.version = version;
+        this.source = source;
+    }
+
+    /**
+     * @return
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @return
+     */
+    public boolean isSource()
+    {
+        return source;
+    }
+
+    /**
+     * @return
+     */
+    public String getVersion()
+    {
+        return version;
+    }
 }
