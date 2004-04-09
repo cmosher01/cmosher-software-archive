f11b77d52814cd6822ca37be286c1883bd15fe3a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-09 15:35:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3088 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/CollectionTests.java
+++ testjava/src/CollectionTests.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public final class CollectionTests
 {
     private CollectionTests() throws UnsupportedOperationException
@@ -7,5 +9,11 @@ public final class CollectionTests
 
     public static void main(String rArg[]) throws Throwable
     {
+        ArrayList somelist = new ArrayList();
+        somelist.add("test one");
+        somelist.add("test two");
+        somelist.add("test three");
+
+        somelist.toArray(new String[somelist.size()]);
     }
 }
