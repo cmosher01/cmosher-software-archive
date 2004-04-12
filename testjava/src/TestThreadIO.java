4dc432e3584ea3a357489816d2c24c721c6cb735
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-12 14:57:10 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3134 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/TestThreadIO.java
+++ testjava/src/TestThreadIO.java
@@ -95,12 +95,12 @@ public class TestThreadIO
         else
         {
             String s = (String)lines.get(0);
-            if (!s.equals("this is a test\r"))
+            if (!s.equals("this is a test"))
             {
                 System.err.println("s != this is a test");
             }
             s = (String)lines.get(1);
-            if (!s.equals("this is also a test\r"))
+            if (!s.equals("this is also a test"))
             {
                 System.err.println("s != this is also test");
             }
