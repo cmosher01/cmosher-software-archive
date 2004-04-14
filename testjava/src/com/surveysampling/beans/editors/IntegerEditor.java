b330b9517502e185c65b135dd9ccdece16245112
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-14 17:57:18 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3218 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
+++ testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
@@ -6,6 +6,7 @@ import java.beans.PropertyEditorSupport;
 public class IntegerEditor extends PropertyEditorSupport implements PropertyEditor
 {
     public static final int DEFAULT_INT = 0;
+
     public String getJavaInitializationString()
     {
         return Integer.toString(DEFAULT_INT);
