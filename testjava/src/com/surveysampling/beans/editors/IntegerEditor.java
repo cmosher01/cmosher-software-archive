a9b6584267ce99e74a14e71df7dd5f5b12209719
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-14 17:41:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3210 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
+++ testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
@@ -7,7 +7,7 @@ public class IntegerEditor extends PropertyEditorSupport implements PropertyEdit
 {
     public String getJavaInitializationString()
     {
-        return "0";
+        return "33";
     }
 
     public void setAsText(String text) throws IllegalArgumentException
