7fd5b3228f22891ec9427383136b867ea7a6c120
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-14 18:04:47 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3222 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/beans/editors/Editors.java
+++ testjava/src/com/surveysampling/beans/editors/Editors.java
@@ -1,10 +1,23 @@
 package com.surveysampling.beans.editors;
 
+import java.beans.PropertyEditorManager;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  */
 public final class Editors
 {
+    private Editors()
+    {
+        throw new UnsupportedOperationException();
+    }
+
     public static void register()
     {
+        String[] rp = PropertyEditorManager.getEditorSearchPath();
+        LinkedList listp = new LinkedList(Arrays.asList(rp));
+        listp.addFirst("com.surveysampling.beans.editors");
+        PropertyEditorManager.setEditorSearchPath((String[])listp.toArray(new String[listp.size()]));
     }
 }
