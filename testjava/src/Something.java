82fdf3d0bcb73855c7e6cc8f0a036e366a183d0f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-14 14:48:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3163 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Something.java
+++ testjava/src/Something.java
@@ -13,4 +13,184 @@ public class SomeBean
     private String sString;
     private File aFile;
     private Object aObject;
+
+    public SomeBean()
+    {
+    }
+
+    /**
+     * @return
+     */
+    public boolean isABoolean()
+    {
+        return aBoolean;
+    }
+
+    /**
+     * @return
+     */
+    public byte getAByte()
+    {
+        return aByte;
+    }
+
+    /**
+     * @return
+     */
+    public char getAChar()
+    {
+        return aChar;
+    }
+
+    /**
+     * @return
+     */
+    public double getADouble()
+    {
+        return aDouble;
+    }
+
+    /**
+     * @return
+     */
+    public File getAFile()
+    {
+        return aFile;
+    }
+
+    /**
+     * @return
+     */
+    public float getAFloat()
+    {
+        return aFloat;
+    }
+
+    /**
+     * @return
+     */
+    public int getAInt()
+    {
+        return aInt;
+    }
+
+    /**
+     * @return
+     */
+    public long getALong()
+    {
+        return aLong;
+    }
+
+    /**
+     * @return
+     */
+    public Object getAObject()
+    {
+        return aObject;
+    }
+
+    /**
+     * @return
+     */
+    public short getAShort()
+    {
+        return aShort;
+    }
+
+    /**
+     * @return
+     */
+    public String getSString()
+    {
+        return sString;
+    }
+
+    /**
+     * @param b
+     */
+    public void setABoolean(boolean b)
+    {
+        aBoolean = b;
+    }
+
+    /**
+     * @param b
+     */
+    public void setAByte(byte b)
+    {
+        aByte = b;
+    }
+
+    /**
+     * @param c
+     */
+    public void setAChar(char c)
+    {
+        aChar = c;
+    }
+
+    /**
+     * @param d
+     */
+    public void setADouble(double d)
+    {
+        aDouble = d;
+    }
+
+    /**
+     * @param file
+     */
+    public void setAFile(File file)
+    {
+        aFile = file;
+    }
+
+    /**
+     * @param f
+     */
+    public void setAFloat(float f)
+    {
+        aFloat = f;
+    }
+
+    /**
+     * @param i
+     */
+    public void setAInt(int i)
+    {
+        aInt = i;
+    }
+
+    /**
+     * @param l
+     */
+    public void setALong(long l)
+    {
+        aLong = l;
+    }
+
+    /**
+     * @param object
+     */
+    public void setAObject(Object object)
+    {
+        aObject = object;
+    }
+
+    /**
+     * @param s
+     */
+    public void setAShort(short s)
+    {
+        aShort = s;
+    }
+
+    /**
+     * @param string
+     */
+    public void setSString(String string)
+    {
+        sString = string;
+    }
 }
