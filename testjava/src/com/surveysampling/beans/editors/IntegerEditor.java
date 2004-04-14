a34ffc090e0d7e4654addfedc37e6f02e6dee528
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-14 17:12:51 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3183 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
+++ testjava/src/com/surveysampling/beans/editors/IntegerEditor.java
@@ -1,3 +1,10 @@
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+
 /*
  * TODO
  *
@@ -7,7 +14,114 @@
 /**
  * TODO
  */
-public class IntegerEditor
+public class IntegerEditor extends PropertyEditorSupport implements PropertyEditor
 {
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#setValue(java.lang.Object)
+     */
+    public void setValue(Object value)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#getValue()
+     */
+    public Object getValue()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#isPaintable()
+     */
+    public boolean isPaintable()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#paintValue(java.awt.Graphics, java.awt.Rectangle)
+     */
+    public void paintValue(Graphics gfx, Rectangle box)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#getJavaInitializationString()
+     */
+    public String getJavaInitializationString()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#getAsText()
+     */
+    public String getAsText()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#setAsText(java.lang.String)
+     */
+    public void setAsText(String text) throws IllegalArgumentException
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#getTags()
+     */
+    public String[] getTags()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#getCustomEditor()
+     */
+    public Component getCustomEditor()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#supportsCustomEditor()
+     */
+    public boolean supportsCustomEditor()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#addPropertyChangeListener(java.beans.PropertyChangeListener)
+     */
+    public void addPropertyChangeListener(PropertyChangeListener listener)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see java.beans.PropertyEditor#removePropertyChangeListener(java.beans.PropertyChangeListener)
+     */
+    public void removePropertyChangeListener(PropertyChangeListener listener)
+    {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
