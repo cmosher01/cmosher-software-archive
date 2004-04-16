27d2d3fd843e69d93261d0f8ed13f0ca40b089a6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 14:50:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3433 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocityUtilOrig-1-0/src/nu/mine/mosher/velocity/VelocityWrapper.java
+++ velocityUtilOrig-1-0/src/nu/mine/mosher/velocity/VelocityWrapper.java
@@ -77,6 +77,11 @@ public class VelocityWrapper
         }
     }
 
+    public boolean templateExists(String templateName)
+    {
+        return velocity.templateExists(templateName);
+    }
+
     public static VelocityException wrapInVelocityException(Throwable e)
     {
         VelocityException ve = new VelocityException(e.getMessage());
