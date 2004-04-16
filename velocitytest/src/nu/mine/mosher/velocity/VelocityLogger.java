1b0c78121ea6a0da1d85c34656e62fc7ab06f50a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 13:22:15 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3352 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocitytest/src/nu/mine/mosher/velocity/VelocityLogger.java
+++ velocitytest/src/nu/mine/mosher/velocity/VelocityLogger.java
@@ -5,6 +5,8 @@
  */
 package nu.mine.mosher.velocity;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import org.apache.velocity.runtime.RuntimeServices;
@@ -15,7 +17,7 @@ import org.apache.velocity.runtime.log.LogSystem;
  */
 public class VelocityLogger implements LogSystem
 {
-    Logger log;
+    private Logger log = Logger.global;
 
     public void init(RuntimeServices rs)
     {
@@ -23,5 +25,21 @@ public class VelocityLogger implements LogSystem
 
     public void logVelocityMessage(int level, String message)
     {
+        log.log(translateVelocityLogLevel(level),message);
+    }
+
+    public static Level translateVelocityLogLevel(int level)
+    {
+        switch (level)
+        {
+            case DEBUG_ID:
+                return Level.FINEST;
+            case INFO_ID:
+                return Level.INFO;
+            case ERROR_ID:
+                return Level.SEVERE;
+            default:
+                return Level.WARNING;
+        }
     }
 }
