9eb79b02c53fccb5eddabe3bc24071101f59713f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 18:04:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3491 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testlog/src/com/surveysampling/util/logger/MessageFormatter.java
@@ -0,0 +1,35 @@
+/*
+ * TODO
+ *
+ * Created on Apr 16, 2004
+ */
+package com.surveysampling.util.logger;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * TODO
+ */
+public class MessageFormatter extends Formatter
+{
+
+    /**
+     * 
+     */
+    public MessageFormatter()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+     */
+    public String format(LogRecord record)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
