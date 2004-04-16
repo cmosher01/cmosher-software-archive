80aadac8ecf52f62da6670e94c7591601bddad74
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 13:10:11 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3347 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ velocityUtilOrig-1-0/src/nu/mine/mosher/velocity/VelocityLogger.java
@@ -0,0 +1,44 @@
+/*
+ * TODO
+ *
+ * Created on Apr 16, 2004
+ */
+package nu.mine.mosher.velocity;
+
+import org.apache.velocity.runtime.RuntimeServices;
+import org.apache.velocity.runtime.log.LogSystem;
+
+/**
+ * TODO
+ */
+public class VelocityLogger implements LogSystem
+{
+
+    /**
+     * 
+     */
+    public VelocityLogger()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.velocity.runtime.log.LogSystem#init(org.apache.velocity.runtime.RuntimeServices)
+     */
+    public void init(RuntimeServices arg0) throws Exception
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.velocity.runtime.log.LogSystem#logVelocityMessage(int, java.lang.String)
+     */
+    public void logVelocityMessage(int arg0, String arg1)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
