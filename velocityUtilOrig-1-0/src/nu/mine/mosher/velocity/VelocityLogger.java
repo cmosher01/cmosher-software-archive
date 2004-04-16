f36e5becd6b7228746c65b6ed61ec4f9916469d2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 13:33:37 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3362 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocityUtilOrig-1-0/src/nu/mine/mosher/velocity/VelocityLogger.java
+++ velocityUtilOrig-1-0/src/nu/mine/mosher/velocity/VelocityLogger.java
@@ -6,7 +6,7 @@ package nu.mine.mosher.velocity;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeServices;
 import org.apache.velocity.runtime.log.LogSystem;
 
@@ -22,7 +22,7 @@ public class VelocityLogger implements LogSystem
 
     public void init(RuntimeServices rs)
     {
-        Object o = rs.getProperty(Velocity.RUNTIME_LOG_LOGSYSTEM);
+        Object o = rs.getProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM);
     }
 
     public void logVelocityMessage(int level, String message)
