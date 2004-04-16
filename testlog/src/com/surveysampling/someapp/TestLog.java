500aa987ac8b738496d2ea531edba7d2848be114
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 17:50:45 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3482 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testlog/src/com/surveysampling/someapp/TestLog.java
@@ -0,0 +1,39 @@
+package com.surveysampling.someapp;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public class TestLog
+{
+    public static void main(String[] args) throws Exception
+    {
+//        LoggerUtil.initPropertiesFile();
+//        LoggerUtil.createDefinedLoggers();
+//
+//        SomeClass sc = new SomeClass();
+//        sc.someMethod();
+
+        Handler[] rh = Logger.global.getHandlers();
+        for (int i = 0; i < rh.length; ++i)
+        {
+            Handler h = rh[i];
+            Logger.global.removeHandler(h);
+        }
+        Logger.global.addHandler(new Handler()
+        {
+            public void publish(LogRecord record)
+            {
+                System.err.println(getFormatter().format(record));
+            }
+
+            public void flush()
+            {
+            }
+
+            public void close() throws SecurityException
+            {
+            }
+        });
+    }
+}
