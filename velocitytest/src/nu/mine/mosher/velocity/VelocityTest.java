743a1eb3ec36c7b18828f1e4484baf63ae18b7e4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 13:08:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3343 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- velocitytest/src/nu/mine/mosher/velocity/VelocityTest.java
+++ velocitytest/src/nu/mine/mosher/velocity/VelocityTest.java
@@ -45,6 +45,7 @@ public class VelocityTest
 //                }
 //            }
 //        });
+        Class.forName("org.apache.velocity.runtime.log.LogManager");
         velocity.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM_CLASS,"nu.mine.mosher.velocity.VelocityTest.ErrLogger");
         velocity.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH,"C:\\Documents and Settings\\chrism\\My Documents");
         velocity.setProperty(VelocityEngine.VM_LIBRARY,"");
