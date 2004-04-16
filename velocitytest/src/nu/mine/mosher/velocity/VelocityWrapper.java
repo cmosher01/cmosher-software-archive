7f663250e7f04b0109e1f916d6a66d06c6540bf0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-16 13:42:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3379 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ velocitytest/src/nu/mine/mosher/velocity/VelocityWrapper.java
@@ -0,0 +1,23 @@
+/*
+ * TODO
+ *
+ * Created on Apr 16, 2004
+ */
+package nu.mine.mosher.velocity;
+
+/**
+ * TODO
+ */
+public class VelocityWrapper
+{
+
+    /**
+     * 
+     */
+    public VelocityWrapper()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
