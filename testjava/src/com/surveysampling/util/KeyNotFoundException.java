9ec14817496b6f529ed55e15078a51cf03a5b8fb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-21 12:40:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3526 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/src/com/surveysampling/util/KeyNotFoundException.java
@@ -0,0 +1,51 @@
+/*
+ * TODO
+ *
+ * Created on Apr 21, 2004
+ */
+package com.surveysampling.util;
+
+/**
+ * TODO
+ */
+public class KeyNotFoundException extends Exception
+{
+
+    /**
+     * 
+     */
+    public KeyNotFoundException()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public KeyNotFoundException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public KeyNotFoundException(String message, Throwable cause)
+    {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public KeyNotFoundException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
