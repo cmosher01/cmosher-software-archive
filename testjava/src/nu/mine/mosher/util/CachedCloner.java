d55aa38cc094b90b8f9d0db8c036f49537889f4a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-22 17:10:46 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3587 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/CachedCloner.java
+++ testjava/src/nu/mine/mosher/util/CachedCloner.java
@@ -1,14 +1,37 @@
-/*
- * TODO
- *
- * Created on Apr 22, 2004
- */
 package com.surveysampling.util;
 
-/**
- * TODO
- */
+import java.lang.reflect.Method;
+import java.util.Map;
+
 public class CachedCloner
 {
+    private final Map cache;
 
+    public CachedCloner(Map cache)
+    {
+        if (cache == null)
+        {
+            throw new NullPointerException();
+        }
+        this.cache = cache;
+    }
+
+    public void clone(Object obj)
+    {
+        try
+        {
+            Class cl = obj.getClass();
+            Method methodClose = (Method)cache.get(cl);
+            if (methodClose == null)
+            {
+                methodClose = cl.getMethod("clone",null);
+                cache.put(cl,methodClose);
+            }
+            methodClose.invoke(obj,null);
+        }
+        catch (Throwable ignore)
+        {
+            ignore.printStackTrace();
+        }
+    }
 }
