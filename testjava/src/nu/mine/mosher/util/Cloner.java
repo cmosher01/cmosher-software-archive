ffe7003e352be0884a9f335120351c40c67a4cc5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-22 20:35:47 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3607 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Cloner.java
+++ testjava/src/nu/mine/mosher/util/Cloner.java
@@ -2,8 +2,6 @@ package com.surveysampling.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Provides a static method that will clone any Cloneable Object.
