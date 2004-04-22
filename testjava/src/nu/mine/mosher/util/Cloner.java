657b6de942f26dbdde168fd214314d2d4f20aa76
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-22 20:36:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3608 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/nu/mine/mosher/util/Cloner.java
+++ testjava/src/nu/mine/mosher/util/Cloner.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Method;
  */
 public final class Cloner
 {
-    private Cloner()
+    private Cloner() throws UnsupportedOperationException
     {
         throw new UnsupportedOperationException();
     }
