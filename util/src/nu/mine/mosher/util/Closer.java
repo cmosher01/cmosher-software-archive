5ba40efa66cd89ec9a418dc4917f9680da60e26d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-22 21:44:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3632 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ util/src/nu/mine/mosher/util/Closer.java
@@ -0,0 +1,41 @@
+package nu.mine.mosher.util;
+
+public final class Closer
+{
+    private Closer() throws UnsupportedOperationException
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Calls the given object's "close()" method, if it has one.
+     * Any exceptions are ignored.
+     * 
+     * For example:
+     * <pre>
+     * OutputStream os = null;
+     * try
+     * {
+     *     os = new FileOutputStream(new File("test.txt"));
+     *     os.write(65);
+     * }
+     * finally
+     * {
+     *     UniversalCloser.close(os);
+     * }
+     * </pre>
+     * 
+     * @param obj the Object whose close() method is to be called.
+     */
+    public static void close(Object obj)
+    {
+        try
+        {
+            obj.getClass().getMethod("close",null).invoke(obj,null);
+        }
+        catch (Throwable ignore)
+        {
+            ignore.printStackTrace();
+        }
+    }
+}
