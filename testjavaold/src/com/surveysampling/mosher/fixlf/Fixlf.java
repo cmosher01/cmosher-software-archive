44ad66223a2c6d14b3d89d455020734a7bdae6f0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-04-23 19:49:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3662 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjavaold/src/com/surveysampling/mosher/fixlf/Fixlf.java
+++ testjavaold/src/com/surveysampling/mosher/fixlf/Fixlf.java
@@ -3,8 +3,6 @@ package com.surveysampling.mosher.fixlf;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-import com.surveysampling.util.UniversalCloser;
-
 public class Fixlf
 {
     public static void main(String[] rArg) throws Throwable
@@ -25,7 +23,7 @@ public class Fixlf
         }
         finally
         {
-            UniversalCloser.close(br);
+            br.close();
         }
     }
 }
