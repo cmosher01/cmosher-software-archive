b01b7a129ee37c3850ab24203bfe7e2b249faedc
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-05-04 16:57:51 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3673 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -71,6 +71,7 @@ public class Test
 
         URI uri = new URI("smtp://sparky:25");
         System.out.println(uri.toASCIIString());
+        System.out.println(uri.getSchemeSpecificPart());
 
 
 
