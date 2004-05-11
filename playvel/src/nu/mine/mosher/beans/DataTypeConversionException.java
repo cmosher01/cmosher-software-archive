5fc959466eba1468addb41d4b596486724d6125a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-05-11 02:11:54 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3694 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ playvel/src/nu/mine/mosher/beans/DataTypeConversionException.java
@@ -0,0 +1,36 @@
+/*
+ * Created on Apr 15, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package nu.mine.mosher.beans;
+
+/**
+ * @author Chris
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class DataTypeConversionException extends Exception
+{
+    public DataTypeConversionException()
+    {
+        super();
+    }
+
+    public DataTypeConversionException(String message)
+    {
+        super(message);
+    }
+
+    public DataTypeConversionException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public DataTypeConversionException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
