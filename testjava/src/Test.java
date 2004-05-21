b838df4d6b35276e4ed77ce76caf02151e378ae1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-05-21 20:05:54 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3825 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -57,7 +57,7 @@ public class Test
 	{
 
         SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss.SSS");
-        long x = 86400000L*30;
+        long x = 86400000L*300;
         System.out.print(x/86400000L+" ");
         System.out.println(f.format(new Date(x%86400000L-68400000L)));
 
