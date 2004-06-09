edb45c10e1c34aedc4afaa8acbdfc7de77238011
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-09 14:43:59 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3879 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/time/TimeOfDay.java
+++ testjava/src/com/surveysampling/time/TimeOfDay.java
@@ -1,14 +1,25 @@
 /*
- * TODO
- *
  * Created on Jun 9, 2004
  */
 package com.surveysampling.time;
 
 /**
- * TODO
+ * Represents a time of day, for example, "3:00 AM".
+ * It contains hours, minutes, seconds, and milliseconds
+ * since midnight. Instances of this class are immutable.
  */
 public class TimeOfDay
 {
+    private final int hours;
+    private final int minutes;
+    private final int seconds;
+    private final int milliseconds;
 
+    public TimeOfDay(int hours, int minutes, int seconds, int milliseconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.milliseconds = milliseconds;
+    }
 }
