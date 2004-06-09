d13a4b676607cc32d45df62608626e906dee013b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-09 19:59:11 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3940 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/CloneThreadTest.java
+++ testjava/src/CloneThreadTest.java
@@ -8,18 +8,59 @@ public class CloneThreadTest
         {
             return ""+a+","+b;
         }
+        public Object clone()
+        {
+            try
+            {
+                return super.clone();
+            }
+            catch (CloneNotSupportedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         final X x = new X();
+        x.a = 6;
+        x.b = 7;
 
-        Thread t = new Thread(new Runnable()
+        Thread tPrint = new Thread(new Runnable()
         {
             public void run()
             {
-                System.out.println(x);
+                while (true)
+                {
+                    System.out.println(x);
+                    try
+                    {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+        tPrint.setDaemon(true);
+
+        Thread tSet = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                X y = x.clone();
+            }
+        });
+        tSet.setDaemon(true);
+
+        tSet.start();
+        tPrint.start();
+
+        Thread.sleep(30000);
     }
 }
