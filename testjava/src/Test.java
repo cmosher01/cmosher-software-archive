1afd7efd4b725927622481400e92d0975b651ca6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-09 14:23:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3868 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/Test.java
+++ testjava/src/Test.java
@@ -60,7 +60,7 @@ public class Test
 
 
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
-        long ms = fmt.parse("2004/04/01 00:00:00.000").getTime();
+        long ms = fmt.parse("2004/04/03 00:00:00.000").getTime();
         for (int i = 0; i < 4*24; ++i)
         {
             System.out.println(fmt.format(new Date(ms)));
