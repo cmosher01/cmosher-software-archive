c738c33c03ebc04340242f675b3e695956903beb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-09 19:53:27 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@3938 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/CloneThreadTest.java
+++ testjava/src/CloneThreadTest.java
@@ -1,16 +1,17 @@
-/*
- * TODO
- *
- * Created on Jun 9, 2004
- */
-
-/**
- * TODO
- */
 public class CloneThreadTest
 {
+    private static class X
+    {
+        public int a;
+        public int b;
+        public String toString()
+        {
+            return ""+a+","+b;
+        }
+    }
 
     public static void main(String[] args)
     {
+        
     }
 }
