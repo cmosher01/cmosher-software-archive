5b815e161f2c267c12f629808d4ad4f9653a00b4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-10 14:07:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4028 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/src/com/surveysampling/time/TimeOfDayParser.java
+++ testjava/src/com/surveysampling/time/TimeOfDayParser.java
@@ -5,10 +5,48 @@
  */
 package com.surveysampling.time;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * TODO
  */
 public class TimeOfDayParser
 {
+    /**
+     * Allowable formats:
+     * 
+     * H:mm
+     * HH:mm
+     * HH:mm:ss
+     * HH:mm:ss.SSS
+     * 
+     * @param s
+     * @return
+     */
+    public static TimeOfDay parse(String s) throws ParseException
+    {
+        if (s.length() == 4)
+        {
+            s = "0"+s;
+        }
 
+        SimpleDateFormat fmt = null;
+        if (s.length() == 5)
+        {
+            fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+        }
+        else if (s.length() == 8)
+        {
+            fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+        }
+        else if (s.length() == 12)
+        {
+            fmt = new SimpleDateFormat("HH:mm:ss.SSS");
+        }
+        else
+        {
+            throw new ParseException("Invalid time-of-day string",0);
+        }
+    }
 }
