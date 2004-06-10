960e1bf08b70a3e1bd48cc2140b19e61938faa51
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-10 13:39:59 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4007 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testjava/test/com/surveysampling/time/TimeOfDayTest.java
@@ -0,0 +1,30 @@
+/*
+ * TODO
+ *
+ * Created on Jun 10, 2004
+ */
+package com.surveysampling.time;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO
+ */
+public class TimeOfDayTest extends TestCase
+{
+
+    /**
+     * Constructor for TimeOfDayTest.
+     * @param arg0
+     */
+    public TimeOfDayTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(TimeOfDayTest.class);
+    }
+
+}
