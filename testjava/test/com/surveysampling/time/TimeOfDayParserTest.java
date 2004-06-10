a38edd14ed132c97e26ee3039daaaa7117f06ffd
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-10 14:55:54 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4047 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- testjava/test/com/surveysampling/time/TimeOfDayParserTest.java
+++ testjava/test/com/surveysampling/time/TimeOfDayParserTest.java
@@ -37,4 +37,30 @@ public class TimeOfDayParserTest extends TestCase
         assertEquals(58,p.getSecond());
         assertEquals(999,p.getMillisecond());
     }
+
+    public void testFormats()
+    {
+        makeAndCheck("23:59:58.999",23,59,58,999);
+        TimeOfDayParser p = new TimeOfDayParser("23:59:58.999");
+        assertEquals(23,p.getHour());
+        assertEquals(59,p.getMinute());
+        assertEquals(58,p.getSecond());
+        assertEquals(999,p.getMillisecond());
+    }
+
+    /**
+     * @param string
+     * @param i
+     * @param j
+     * @param k
+     * @param l
+     */
+    private void makeAndCheck(String s, int hour, int minute, int second, int millisecond) throws ParseException
+    {
+        TimeOfDayParser p = new TimeOfDayParser(s);
+        assertEquals(hour,p.getHour());
+        assertEquals(minute,p.getMinute());
+        assertEquals(second,p.getSecond());
+        assertEquals(millisecond,p.getMillisecond());
+    }
 }
