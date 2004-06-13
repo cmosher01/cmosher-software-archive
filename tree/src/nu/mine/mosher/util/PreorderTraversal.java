7aa6f2dae972b6927d947a4c2512a8c13683b804
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-13 20:28:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4170 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ tree/src/nu/mine/mosher/util/PreorderTraversal.java
@@ -0,0 +1,34 @@
+/*
+ * Created on Jun 13, 2004
+ */
+package nu.mine.mosher.util;
+
+import java.util.Iterator;
+
+/**
+ * @author Chris Mosher
+ */
+public class PreorderTraversal
+{
+	public static void traverse(TreeNode node)
+	{
+		Traversable tr = (Traversable)node.getObject();
+		if (tr == null)
+		{
+			tr = new NullTraversable();
+		}
+
+		tr.enter();
+		for (Iterator children = node.children(); children.hasNext();)
+		{
+			traverse((TreeNode)children.next());
+		}
+		tr.leave();
+	}
+
+	private static class NullTraversable implements Traversable
+	{
+        public void enter() { }
+        public void leave() { }
+	}
+}
