5544c68107e9032f7f35e2e46183ede923c1d4b1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-06-15 16:03:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4184 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ ByteAnalyzer/src/ByteAnalyzer.java
@@ -0,0 +1,156 @@
+import java.io.BufferedInputStream;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+ * TODO
+ *
+ * Created on Jun 15, 2004
+ */
+
+/**
+ * TODO
+ */
+public class ByteAnalyzer
+{
+    private static final int NORMAL = 0;
+    private static final int CR = 1;
+    private static final int LF = 2;
+
+    private Map cBytes = new TreeMap();
+    private int cCRLF;
+    private int cLFCR;
+
+    private int state;
+
+    public static void main(String[] args) throws IOException
+    {
+        ByteAnalyzer prog = new ByteAnalyzer();
+        prog.execute();
+    }
+
+    public void execute() throws IOException
+    {
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(FileDescriptor.in));
+        int x = in.read();
+        while (x != -1)
+        {
+            switch (state)
+            {
+                case NORMAL:
+                {
+                    if (x == 0x0d)
+                    {
+                        state = CR;
+                    }
+                    else if (x == 0x0a)
+                    {
+                        state = LF;
+                    }
+                    else
+                    {
+                        incByte(x);
+                    }
+                }
+                break;
+                case CR:
+                {
+                    if (x == 0x0a)
+                    {
+                        ++cCRLF;
+                        state = NORMAL;
+                    }
+                    else if (x == 0x0d)
+                    {
+                        incByte(0x0d);
+                    }
+                    else
+                    {
+                        incByte(0x0d);
+                        incByte(x);
+                        state = NORMAL;
+                    }
+                }
+                break;
+                case LF:
+                {
+                    if (x == 0x0d)
+                    {
+                        ++cLFCR;
+                        state = NORMAL;
+                    }
+                    else if (x == 0x0a)
+                    {
+                        incByte(0x0a);
+                    }
+                    else
+                    {
+                        incByte(0x0a);
+                        incByte(x);
+                        state = NORMAL;
+                    }
+                }
+                break;
+            }
+            x = in.read();
+        }
+        in.close();
+        showMap();
+    }
+
+    private void incByte(int x)
+    {
+        Integer i = new Integer(x);
+        if (!cBytes.containsKey(i))
+        {
+            cBytes.put(i,new Integer(1));
+        }
+        else
+        {
+            int cx = ((Integer)cBytes.get(i)).intValue();
+            cBytes.put(i,new Integer(++cx));
+        }
+    }
+
+    public void showMap()
+    {
+        if (cCRLF > 0)
+        {
+            System.out.println("CRLF  : "+cCRLF);
+        }
+        if (cLFCR > 0)
+        {
+            System.out.println("LFCR  : "+cLFCR);
+        }
+        for (Iterator i = cBytes.entrySet().iterator(); i.hasNext();)
+        {
+            Map.Entry entry = (Map.Entry)i.next();
+            int byt = ((Integer)entry.getKey()).intValue();
+            int cnt = ((Integer)entry.getValue()).intValue();
+            String s = Integer.toHexString(byt);
+            if (s.length() == 1)
+            {
+                s = "0"+s;
+            }
+            System.out.print(s);
+            System.out.print(" ");
+            if ('!' <= byt && byt <= '~')
+            {
+                char c = (char)byt;
+                System.out.print('\'');
+                System.out.print(c);
+                System.out.print('\'');
+            }
+            else
+            {
+                System.out.print("   ");
+            }
+            System.out.print(": ");
+            System.out.println(cnt);
+        }
+    }
+}
