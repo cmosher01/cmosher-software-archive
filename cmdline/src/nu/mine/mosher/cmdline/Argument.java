631119b8d1cb8b764fa6ddb5339eecb8334704b8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 18:08:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4447 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- cmdline/src/nu/mine/mosher/cmdline/Argument.java
+++ cmdline/src/nu/mine/mosher/cmdline/Argument.java
@@ -11,7 +11,53 @@ package nu.mine.mosher.cmdline;
  */
 public class Argument
 {
-    public Argument(String name, String abbrev)
+    private final boolean mIsOption;
+    private String mName;
+    private String mAbbrev;
+    private Object mValue;
+    private boolean mIsSpecified;
+    private String mDesc;
+
+    public Argument(String name, String abbrev, String desc, Object value, boolean isSpecified)
     {
+        mIsOption = name.length() > 0 || abbrev.length() > 0;
+        mName = name;
+        mAbbrev = abbrev;
+        mDesc = desc;
+        mValue = value;
+        mIsSpecified = isSpecified;
+    }
+
+    boolean isOption()
+    {
+        return mIsOption;
+    }
+    boolean isSpecified()
+    {
+        return mIsSpecified;
+    }
+    String getName()
+    {
+        if (mName.length() > 0)
+        {
+            return mName;
+        }
+        return mAbbrev;
+    }
+    String getLongName()
+    {
+        return mName;
+    }
+    String getShortName()
+    {
+        return mAbbrev;
+    }
+    Object getValue()
+    {
+        return mValue;
+    }
+    String getDescription()
+    {
+        return mDesc;
     }
 }
