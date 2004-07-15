d51573ed7f046cd63c154959b2dededee654d1bf
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 13:25:56 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4382 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- swingapp/src/nu/mine/mosher/swingapp/SwingApplication.java
+++ swingapp/src/nu/mine/mosher/swingapp/SwingApplication.java
@@ -22,7 +22,40 @@ public class Ja2
      */
     public void run()
     {
-        // TODO Auto-generated method stub
-        
+        // We don't allow any command line arguments.
+        if (rArg.length > 0)
+        {
+            throw new IllegalArgumentException("No arguments allowed.");
+        }
+
+        /*
+         * Start the GUI, making sure all Swing calls
+         * are done on the event-dispatching thread.
+         * Catch any exceptions and pass them back to
+         * the main thread.
+         */
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    createGUI();
+                }
+                catch (Throwable th)
+                {
+                    MetadataViewer.sendException(th);
+                }
+            }
+        });
+
+        /*
+         * Wait for an exception (from another thread), and throw it.
+         * This also prevents the main thread from exiting before
+         * Swing's event-dispatching thread starts (which would have
+         * caused the program to exit immeiately without doing
+         * anything) (a race conditon).
+         */
+        waitForException();
     }
 }
