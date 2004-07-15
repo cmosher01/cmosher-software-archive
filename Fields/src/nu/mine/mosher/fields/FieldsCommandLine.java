00e06c649a61ac4dce88676e00d6e3b8343167b3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 14:11:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4423 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- Fields/src/nu/mine/mosher/fields/FieldsCommandLine.java
+++ Fields/src/nu/mine/mosher/fields/FieldsCommandLine.java
@@ -1,14 +1,19 @@
 /*
- * Created on Jul 15, 2004
+ * Created on July 15, 2004
  */
 package nu.mine.mosher.ja2;
 
 /**
- * TODO
+ * Handles any command line arguments for the application
  * 
- * @author chrism
+ * @author Chris Mosher
  */
 public class CommandLineArgHandler
 {
+    private final String[] rArg;
 
+    public CommandLineArgHandler(String[] rArg)
+    {
+        this.rArg = rArg;
+    }
 }
