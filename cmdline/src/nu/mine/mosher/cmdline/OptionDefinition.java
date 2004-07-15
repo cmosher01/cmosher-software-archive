044a953293aa59a53162eb1ae303729044d5bad3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 19:04:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4486 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- cmdline/src/nu/mine/mosher/cmdline/OptionDefinition.java
+++ cmdline/src/nu/mine/mosher/cmdline/OptionDefinition.java
@@ -10,24 +10,24 @@ package nu.mine.mosher.cmdline;
  */
 public class OptionDefinition
 {
-    private final String name;
+    private final String optionLong;
     private final char abbrev;
     private final String desc;
     private final boolean hasValue;
 
     /**
-     * @param name
+     * @param optionLong
      * @param abbrev
      * @param desc
      */
     public OptionDefinition(final String name, final char abbrev, final String desc, boolean hasValue)
     {
-        this.name = name;
+        this.optionLong = name;
         this.abbrev = abbrev;
         this.desc = desc;
         this.hasValue = hasValue;
 
-        if (this.name.length() < 1)
+        if (this.optionLong.length() < 1)
         {
             throw new IllegalArgumentException("long option must be 2 or more characters in length");
         }
