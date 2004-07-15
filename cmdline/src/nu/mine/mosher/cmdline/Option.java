edbb37bacc836207db485923b5674b2d6d07db27
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 20:30:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4523 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- cmdline/src/nu/mine/mosher/cmdline/Option.java
+++ cmdline/src/nu/mine/mosher/cmdline/Option.java
@@ -1,14 +1,58 @@
 /*
- * Created on Jul 15, 2004
+ * Created on July 15, 2004
  */
 package nu.mine.mosher.cmdline;
 
 /**
- * TODO
+ * Represents an option (typically specified
+ * by the user on the command line).
  * 
- * @author chrism
+ * @author Chris Mosher
  */
 public class Option
 {
+    private OptionDefinition mDef;
+    private String mValue;
+    private boolean mIsSpecified;
 
+    public Option(OptionDefinition def, String value, boolean isSpecified)
+    {
+        mDef = def;
+        mValue = value;
+        mIsSpecified = isSpecified;
+    }
+
+    boolean isSpecified()
+    {
+        return mIsSpecified;
+    }
+    String getValue()
+    {
+        return mValue;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Option))
+        {
+            return false;
+        }
+        Option that = (Option)obj;
+        return
+            this.mDef.equals(that.mDef) &&
+            this.mValue.equals(that.mValue) &&
+            this.mIsSpecified==that.mIsSpecified;
+    }
+
+    public int hashCode()
+    {
+        int hash = 17;
+        hash *= 37;
+        hash += mDef.hashCode();
+        hash *= 37;
+        hash += mValue.hashCode();
+        hash *= 37;
+        hash += (mIsSpecified ? 0 : 1);
+        return hash;
+    }
 }
