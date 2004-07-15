0df722e516ee51663e13258413f3f5e60c3211eb
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 18:10:51 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4450 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ cmdline/test/nu/mine/mosher/cmdline/CommandLineParserTest.java
@@ -0,0 +1,26 @@
+/*
+ * Created on Jul 15, 2004
+ */
+package nu.mine.mosher.cmdline;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO
+ * 
+ * @author chrism
+ */
+public class CommandLineParserTest extends TestCase
+{
+
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(CommandLineParserTest.class);
+    }
+
+    public void testCommandLineParser()
+    {
+        //TODO Implement CommandLineParser().
+    }
+
+}
