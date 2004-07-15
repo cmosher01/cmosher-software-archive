12176077e444d79419e9da71a2308520b3d8f023
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-15 18:01:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4443 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- cmdline/src/nu/mine/mosher/cmdline/CommandLineParser.java
+++ cmdline/src/nu/mine/mosher/cmdline/CommandLineParser.java
@@ -1,14 +1,49 @@
 /*
- * Created on Jul 15, 2004
+ * Created on July 15, 2004
  */
 package nu.mine.mosher.cmdline;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * TODO
  * 
- * @author chrism
+ * @author Chris Mosher
  */
 public class CommandLineParser
 {
+    private final String[] mrArg;
 
+    public CommandLineParser(String[] rArg)
+    {
+        mrArg = rArg;
+    }
+
+    public Iterator getArguments()
+    {
+        return new Iter();
+    }
+
+    private static class Iter implements Iterator
+    {
+        public boolean hasNext()
+        {
+            return false;
+        }
+
+        public Object next() throws NoSuchElementException
+        {
+            if (!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
+            return null;
+        }
+
+        public void remove() throws UnsupportedOperationException
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
