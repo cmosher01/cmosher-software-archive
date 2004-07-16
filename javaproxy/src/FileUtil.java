cae38114a8a503e3892fb719755055d5d6993f32
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-16 19:16:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4550 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ javaproxy/src/FileUtil.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)FileUtil.java	April 3, 1998
+ *
+ * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
+ * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
+ *
+ * Author : Steve Yeong-Ching Hsueh
+ */
+
+import java.io.*;
+
+/**
+ * common file utilities
+ */
+public class FileUtil {
+
+    public FileUtil() { }
+
+    /**
+     * read file and return its content in a String
+     */
+    public static String readFile(String filename) {
+        StringBuffer content = new StringBuffer();
+        try {
+            FileInputStream fs = new FileInputStream(filename);
+            byte buf[] = null;
+            int  len   = 0;
+            while ((len = fs.available()) > 0) {
+                buf = new byte[len];
+                fs.read(buf);
+                content.append(new String(buf));
+            }
+            fs.close();
+        }
+        catch(IOException e) { }
+
+        return content.toString();
+    }
+
+
+
+}
