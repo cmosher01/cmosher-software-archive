e119606ac162857334eceb93927d715a5bed8ef9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-07-16 19:16:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4566 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ javaproxy/src/Tag.java
@@ -0,0 +1,57 @@
+/**
+ * @(#)Tag.java	April 3, 1998
+ *
+ * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
+ * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
+ * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
+ *
+ * Author : Steve Yeong-Ching Hsueh
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+/**
+ * Tag
+ */
+class Tag {
+ private String name;
+ private Rectangle position_size;
+
+ /**
+  * constructor
+  */
+ public Tag() {
+    this("  ");
+ }
+
+ /**
+  * constructor
+  */
+ public Tag(String tagname) {
+    name = tagname;
+ }
+
+ /**
+  * get name of this tag
+  */
+ public String getName() {
+    return name;
+ }
+
+ /**
+  * set position size
+  */
+ public void setPositionSize(Rectangle rect) {
+    position_size = rect;
+ }
+
+ /**
+  * get positiion size
+  */
+ public Rectangle getPositionSize() {
+    return position_size;
+ }
+
+}
