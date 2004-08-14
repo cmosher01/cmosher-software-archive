34c070f4b96c108bd5d56d8a02a27b555b1c52dc
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-14 14:24:15 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4629 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ gdiff2html/src/GDiffCmd.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Aug 14, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author Chris
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class GDiffCmd {
+
+}
