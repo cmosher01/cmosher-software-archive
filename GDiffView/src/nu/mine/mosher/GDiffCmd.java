cb28db85e02589016b68083ce93cfbd4e0e6e195
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-25 02:10:51 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4661 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- GDiffView/src/nu/mine/mosher/GDiffCmd.java
+++ GDiffView/src/nu/mine/mosher/GDiffCmd.java
@@ -11,6 +11,6 @@
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class GDiffCmd {
-
+public class GDiffCmd
+{
 }
