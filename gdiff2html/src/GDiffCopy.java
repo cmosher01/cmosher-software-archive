a745dd6188904c3a160394102eedfcc178518316
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-26 03:10:25 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4685 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- gdiff2html/src/GDiffCopy.java
+++ gdiff2html/src/GDiffCopy.java
@@ -7,56 +7,20 @@
  */
 public class GDiffCopy extends GDiffCmd
 {
-	private final long pos;
-	private final int len;
-	public GDiffCopy(long pos, int len)
+	private final Range range;
+	public GDiffCopy(Range range)
 	{
-		this.pos = pos;
-		this.len = len;
+		this.range = range;
 	}
-    public GDiffCopy(long pos, long end)
-    {
-        this.pos = pos;
-        this.len = (int)(end-pos);
-    }
-	public long getPosition()
+	public long getRange()
 	{
-		return pos;
+		return range;
 	}
-	public int getLength()
-	{
-		return len;
-	}
-    public long getLimit()
-    {
-        return pos+len;
-    }
-    public long getEnd()
-    {
-        return pos+len-1;
-    }
-    public boolean overlaps(GDiffCopy that)
-    {
-        return this.getPosition() <= that.getEnd() && that.getPosition() <= this.getEnd();
-    }
-    public GDiffCopy[] normalize(GDiffCopy that)
-    {
-        GDiffCopy dif1 = this;
-        GDiffCopy dif2 = that;
-        if (that.getPosition() < this.getPosition())
-        {
-            dif1 = that;
-        }
-        GDiffCopy[] rnorm = new GDiffCopy[3];
-//        GDiffCopy[0] = new GDiffCopy()
-        return rnorm;
-    }
-
     /**
      * @see java.lang.Object#toString()
      */
     public String toString()
     {
-        return "copy "+getPosition()+"-"+getEnd();
+        return "copy "+range.getBegin()+"-"+range.getEnd();
     }
 }
