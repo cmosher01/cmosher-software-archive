97fc03fcd9ebb2577d3ec0836302dd5f61b689a3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-28 00:47:51 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4694 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ GDiffView/src/nu/mine/mosher/GDiffView.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Aug 27, 2004
+ */
+package nu.mine.mosher;
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class GDiffVeiew
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
