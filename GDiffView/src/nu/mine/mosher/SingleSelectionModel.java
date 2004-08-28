5eef1ae90d3d8ec29384710d36bd2a94af7088e5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-28 14:21:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4779 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ GDiffView/src/nu/mine/mosher/SingleSelectionModel.java
@@ -0,0 +1,25 @@
+/*
+ * Created on Aug 28, 2004
+ */
+package nu.mine.mosher;
+
+import javax.swing.DefaultListSelectionModel;
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class SingleSelectionModel extends DefaultListSelectionModel
+{
+
+    /**
+     * 
+     */
+    public SingleSelectionModel()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
