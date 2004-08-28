f10154aa1a1b7192d85c3d0173bf88491d6409c4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-28 03:08:30 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4749 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- GDiffView/src/nu/mine/mosher/GDiffView.java
+++ GDiffView/src/nu/mine/mosher/GDiffView.java
@@ -110,7 +110,7 @@ public class GDiffVeiew extends JFrame
 
         readSrc();
         doc.insertString(0,sb.toString(),(AttributeSet)styles.get("body"));
-        highlightSrc(2,0x23);
+        highlightSrc(0,0x2E);
         pack();
         setVisible(true);
     }
