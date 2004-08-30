f688bfd3c29694b3f87822f3a464f2033bdc7b61
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-30 23:27:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4791 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ GDiffView/src/nu/mine/mosher/GDiffCmdListModel.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Aug 30, 2004
+ */
+package nu.mine.mosher;
+
+import javax.swing.AbstractListModel;
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class GDiffCmdListModel extends AbstractListModel
+{
+
+    /**
+     * 
+     */
+    public GDiffCmdListModel()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.ListModel#getSize()
+     */
+    public int getSize()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.ListModel#getElementAt(int)
+     */
+    public Object getElementAt(int index)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
