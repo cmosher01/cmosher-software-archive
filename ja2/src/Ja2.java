ac8f88ca5bb6466178b32a6c53fd4bfd8c52fc54
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-31 15:51:02 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4852 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ja2/src/Ja2.java
+++ ja2/src/Ja2.java
@@ -8,7 +8,7 @@ import nu.mine.mosher.ja2.Main;
  * Contains the "main" method (external entry point) for
  * the application. This class is in the default package
  * so that the program can be run with the following command:
- * <code>java Ja2 [args]</code>
+ * <code>java Ja2 [arguments]</code>
  * 
  * @author Chris Mosher
  */
