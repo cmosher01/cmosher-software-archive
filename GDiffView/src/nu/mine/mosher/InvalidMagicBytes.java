1a829ccd6342be98146a67098f61f400608deab5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-31 00:58:59 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4832 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ GDiffView/src/nu/mine/mosher/InvalidMagicBytes.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Aug 30, 2004
+ */
+package nu.mine.mosher;
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class InvalidMagicBytes extends Exception
+{
+
+    /**
+     * 
+     */
+    public InvalidMagicBytes()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public InvalidMagicBytes(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public InvalidMagicBytes(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public InvalidMagicBytes(String message, Throwable cause)
+    {
+        super(message,cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
