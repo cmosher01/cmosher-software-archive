13040fa350b69a6dae92e58166776365adb7cbae
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-08-31 15:48:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4844 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ja2/src/Ja2.java
+++ ja2/src/Ja2.java
@@ -2,15 +2,16 @@
  * Created on Aug 31, 2004
  */
 
-
 /**
- * TODO
+ * Contains the "main" method (external entry point) for
+ * the application. This class is in the default package
+ * so that it can be run as follows:
+ * <code>java Ja2</code>
  * 
- * @author chrism
+ * @author Chris Mosher
  */
 public class Ja2
 {
-
     public static void main(String[] args)
     {
     }
