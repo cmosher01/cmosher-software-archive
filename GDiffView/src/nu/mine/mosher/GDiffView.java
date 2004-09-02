044f27d4589a1443837b9671231806b0505b996e
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-02 00:35:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4951 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- GDiffView/src/nu/mine/mosher/GDiffView.java
+++ GDiffView/src/nu/mine/mosher/GDiffView.java
@@ -165,7 +165,7 @@ public class GDiffView extends JFrame
         listGDiff.setSelectionBackground(new Color(173,194,245));
         listGDiff.setFont(Font.decode("Courier-PLAIN-10"));
         listGDiff.setPrototypeCellValue("@FFFFFFFF LFFFFFFFF");
-        listGDiff.setMaximumSize(listGDiff.getPreferredSize()); // ???
+        listGDiff.setMaximumSize(new Dimension(listGDiff.getFixedCellWidth(),listGDiff.getFixedCellHeight()));
         ListSelectionModel selectionModel = new SingleSelectionModel()
         {
             public void updateSingleSelection(int oldIndex, int newIndex)
