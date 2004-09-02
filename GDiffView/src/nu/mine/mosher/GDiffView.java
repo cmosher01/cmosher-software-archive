52ba93f8b9ff4ac941b5ab8e9e71e492c81384c2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-02 01:05:15 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4971 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- GDiffView/src/nu/mine/mosher/GDiffView.java
+++ GDiffView/src/nu/mine/mosher/GDiffView.java
@@ -154,7 +154,7 @@ public class GDiffView extends JFrame
         paneSrc = new JTextPaneNoWrap(docSrc);
         paneSrc.setEditable(false);
         JScrollPane scrSrc = new JScrollPane(paneSrc);
-        Dimension d = scrSrc.getPreferredSize();
+        Dimension d = scrSrc.getMaximumSize();
 //        scrSrc.setMinimumSize(new Dimension(100,100));
 //        scrSrc.setPreferredSize(new Dimension(400,430));
 //        scrSrc.setMaximumSize(new Dimension(Integer.MAX_VALUE,Integer.MAX_VALUE));
