49dcab59a48c0054cc8a94c9ce72d4f6e5f1f54a
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-02 01:21:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@4983 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ GDiffView/src/nu/mine/mosher/SpringMid.java
@@ -0,0 +1,69 @@
+import javax.swing.Spring;
+/*
+ * Created on Sep 1, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class SpringMid extends Spring
+{
+
+    /**
+     * 
+     */
+    public SpringMid()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.Spring#getMaximumValue()
+     */
+    public int getMaximumValue()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.Spring#getMinimumValue()
+     */
+    public int getMinimumValue()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.Spring#getPreferredValue()
+     */
+    public int getPreferredValue()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.Spring#getValue()
+     */
+    public int getValue()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.swing.Spring#setValue(int)
+     */
+    public void setValue(int value)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
