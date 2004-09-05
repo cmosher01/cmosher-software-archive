1eb46dde5f25ba7729c470c07fdc57f3d5d7b3c3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-05 05:11:53 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5178 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- checksum/src/nu/mine/mosher/checksum/StreamChecksum.java
+++ checksum/src/nu/mine/mosher/checksum/StreamChecksum.java
@@ -3,6 +3,10 @@
  */
 package nu.mine.mosher.checksum;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * TODO
  * 
@@ -10,5 +14,42 @@ package nu.mine.mosher.checksum;
  */
 public class ChecksumStream
 {
+    /**
+     * Computes checksums for non-overlapping
+     * windows of the given size over all bytes
+     * from the given binary stream.
+     * @param is
+     * @param cWindow
+     */
+    public void init(InputStream is, int cWindow)
+    {
+        is = new BufferedInputStream(is);
+        byte[] rs = new byte[cWindow];
+        RollingChecksum rollCheck = new RollingChecksum();
 
+        if (is.available() < cWindow)
+        {
+            cWindow = is.available();
+            rs = new byte[cWindow];
+        }
+        int c = is.read(rs);
+        if (c != cWindow)
+        {
+            throw new IOException("Error reading from input stream.");
+        }
+        rollCheck.init(rs);
+
+        RollingChecksum rollCheck2 = new RollingChecksum();
+        for (int k = 0; k < rb.length-cWindow; ++k)
+        {
+            rollCheck.increment(rb[k], rb[k+cWindow]);
+            int check = rollCheck.getChecksum();
+
+            System.arraycopy(rb, k+1, rs, 0, cWindow);
+            rollCheck2.init(rs);
+            int check2 = rollCheck2.getChecksum();
+
+            assertEquals(check2,check);
+        }
+    }
 }
