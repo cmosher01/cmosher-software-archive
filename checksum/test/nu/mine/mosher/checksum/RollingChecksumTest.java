ff01a66a76e39f438dcb7748fb5c310635443251
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-05 04:23:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5141 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- checksum/test/nu/mine/mosher/checksum/RollingChecksumTest.java
+++ checksum/test/nu/mine/mosher/checksum/RollingChecksumTest.java
@@ -22,21 +22,22 @@ public class RollingChecksumTest extends TestCase
             rb[i] = (byte)rc[i];
         }
 
-        int S = 3;
-        byte[] rs = new byte[S];
-        for (int k = 0; k <= s.length()-S; ++k)
+        int n = 3;
+        byte[] rs = new byte[n];
+
+        System.arraycopy(rb, 0, rs, 0, n);
+
+        RollingChecksum rollCheck = new RollingChecksum();
+        rollCheck.init(rs);
+        int check = rollCheck.getChecksum();
+
+        for (int k = 0; k <= s.length()-n; ++k)
         {
-            System.arraycopy(rb, k, rs, 0, S);
-
-            RollingChecksum rollCheck = new RollingChecksum();
-            rollCheck.init(rs);
-            int check = rollCheck.getChecksum();
-
-            rollCheck.increment(rb[k], rb[(k+S-1)+1]);
+            rollCheck.increment(rb[k], rb[(k+n-1)+1]);
             check = rollCheck.getChecksum();
 
             ++k;
-            System.arraycopy(rb, k, rs, 0, S);
+            System.arraycopy(rb, k, rs, 0, n);
             RollingChecksum rollCheck2 = new RollingChecksum();
             rollCheck2.init(rs);
             int check2 = rollCheck2.getChecksum();
