bf2aa75e6007faedd322c263f606c64d51dbc4d0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-05 20:54:57 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5208 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- GDiffView/src/nu/mine/mosher/SourceFile.java
+++ GDiffView/src/nu/mine/mosher/SourceFile.java
@@ -3,6 +3,14 @@
  */
 package nu.mine.mosher;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import nu.mine.mosher.checksum.StreamChecksum;
+
 /**
  * TODO
  * 
@@ -10,5 +18,15 @@ package nu.mine.mosher;
  */
 public class TargetFile
 {
-
+    /**
+     * @param target target file
+     * @param cWindow window size, in bytes
+     */
+    public void calculateWindowChecksums(File target, int cWindow)
+    {
+        InputStream in = new FileInputStream(target);
+        SortedMap map = new TreeMap();
+        StreamChecksum inCheck = new StreamChecksum(map);
+        inCheck.init(in, cWindow)
+    }
 }
