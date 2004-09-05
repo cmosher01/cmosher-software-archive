17ecaea32d962ce67f3bda7be015a11d148ba7c8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-05 01:42:43 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5124 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- checksum/src/nu/mine/mosher/checksum/RollingChecksum.java
+++ checksum/src/nu/mine/mosher/checksum/RollingChecksum.java
@@ -10,5 +10,52 @@ package nu.mine.mosher.checksum;
  */
 public class RollingChecksum
 {
+    /**
+     * 0xFFF1, the largest prime number less than 0x10000.
+     */
+    private static final int M = 0xFFF1;
+    private int checksum;
+    private int len;
 
+    public RollingChecksum()
+    {
+    }
+
+    void init(byte[] rx, int k)
+    {
+        len = rx.length;
+        int a = 0;
+        int b = 0;
+        int l = len+k-1;
+        for (int i = 0; i < len; i++)
+        {
+            byte x = rx[i];
+            a += x;
+            b += (l-(i+k)-1)*x;
+        }
+        a %= M;
+        b %= M;
+        checksum = (b << 16) + a;
+    }
+
+    public int getChecksum()
+    {
+        return checksum;
+    }
+
+    public void increment(byte xk, byte xlplus1)
+    {
+        int a = checksum & 0xFFFF;
+        a -= xk;
+        a += xlplus1;
+        a %= M;
+
+        int b = (checksum >> 16) & 0xFFFF;
+        b -= len;
+        b *= xk;
+        b += a;
+        b %= M;
+
+        checksum = (b << 16) + a;
+    }
 }
