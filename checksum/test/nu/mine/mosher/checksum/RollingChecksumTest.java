3d2f1547c25ea28ca5c5946c4c0df5060bf2a8fa
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-05 01:44:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5126 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ checksum/test/nu/mine/mosher/checksum/RollingChecksumTest.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Sep 4, 2004
+ */
+package nu.mine.mosher.checksum;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class RollingChecksumTest extends TestCase
+{
+
+    public void testGetChecksum()
+    {
+    }
+
+}
