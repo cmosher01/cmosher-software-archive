1146c91b8109c42bb47053a51167b0447d786088
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-16 20:46:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5450 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/DiskPos.java
+++ apple2/src/DiskPos.java
@@ -53,6 +53,11 @@ public class DiskPos
         this.iDisk = block*cBlock+byt;
     }
 
+    protected DiskPos(int iDisk)
+    {
+        this.iDisk = iDisk;
+    }
+
     /**
      * @param block
      * @param allowLarge
