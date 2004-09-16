790b2aa8cdd655d1971066366dd31bde4d3df12f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-16 20:37:21 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5440 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Disk.java
+++ apple2/src/Disk.java
@@ -10,5 +10,11 @@
  */
 public class Disk
 {
+    private final byte[] disk;
+    private DiskPos pos = new DiskPos(0,0,0,false);
 
+    public void seek(DiskPos pos)
+    {
+        this.pos = pos;
+    }
 }
