b60a918047df9d03435393b03290db162dbe08ee
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-16 01:25:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5436 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ apple2_v1/src/InvalidPosException.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Sep 15, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class InvalidPosException extends Exception
+{
+
+    /**
+     * 
+     */
+    public InvalidPosException()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public InvalidPosException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public InvalidPosException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public InvalidPosException(String message, Throwable cause)
+    {
+        super(message,cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
