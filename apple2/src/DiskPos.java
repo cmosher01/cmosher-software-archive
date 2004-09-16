db1c925fcf1ccc5e2b89657cd2a120dc2400efa8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-16 00:24:54 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5420 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/DiskPos.java
+++ apple2/src/DiskPos.java
@@ -10,12 +10,39 @@
  */
 public class DiskPos
 {
+    private static final int cSector = 0x100;
     private static final int cSectorsPerTrack = 0x10;
     private static final int cSectorsPerBlock = 2;
-    private static final int cSector = 0x100;
     private static final int cTrack = cSectorsPerTrack*cSector;
     private static final int cBlock = cSectorsPerBlock*cSector;
 
     private static final int cTracksPerDisk = 0x23;
     private static final int cBlocksPerDisk = cTracksPerDisk*cSectorsPerTrack/cSectorsPerBlock;
+
+
+
+    private final int iDisk;
+
+
+
+    public DiskPos(int track, int sector, int byt, boolean allowLarge)
+    {
+        if (!allowLarge)
+        {
+            verifyTrack(track);
+        }
+        verifySector(sector);
+        verifyByte(byt);
+    }
+
+    /**
+     * @param track
+     */
+    private void verifyTrack(int track)
+    {
+        if (track < 0 || cTracksPerDisk <= track)
+        {
+            throw new InvalidPosException("Invalid track: "+track);
+        }
+    }
 }
