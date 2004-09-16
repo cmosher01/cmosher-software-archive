876a9283e2f5a9dafc017822ef2ca411dc3b1740
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-16 00:16:34 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5419 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/DiskPos.java
+++ apple2/src/DiskPos.java
@@ -10,5 +10,12 @@
  */
 public class DiskPos
 {
+    private static final int cSectorsPerTrack = 0x10;
+    private static final int cSectorsPerBlock = 2;
+    private static final int cSector = 0x100;
+    private static final int cTrack = cSectorsPerTrack*cSector;
+    private static final int cBlock = cSectorsPerBlock*cSector;
 
+    private static final int cTracksPerDisk = 0x23;
+    private static final int cBlocksPerDisk = cTracksPerDisk*cSectorsPerTrack/cSectorsPerBlock;
 }
