44e4a28fdcee25fd205847ad1f62ef730edab86f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-24 01:41:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5599 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Disk.java
+++ apple2/src/Disk.java
@@ -169,13 +169,22 @@ public class Disk
         {
             DiskPos cur = this.pos;
             byte[] sector = read(DiskPos.cSector);
-            if ((sector[3]==3 || sector[3]==2) &&
-                match(sector,0x04,new byte[]{0x00,0x00}) &&
-                match(sector,0x34,new byte[]{0x23,0x10,0x00,0x01}) &&
-                match(sector,0x3a,new byte[]{0x00,0x00}))
-            {
-                System.out.println("VTOC @ T$"+Integer.toHexString(cur.getTrackInDisk())+", S$"+Integer.toHexString(cur.getSectorInTrack()));
-            }
+            isDos33VTOC(sector,cur);
+        }
+    }
+
+    /**
+     * @param sector
+     * @param cur
+     */
+    protected void isDos33VTOC(byte[] sector, DiskPos cur)
+    {
+        if ((sector[3]==3 || sector[3]==2) &&
+            match(sector,0x04,new byte[]{0x00,0x00}) &&
+            match(sector,0x34,new byte[]{0x23,0x10,0x00,0x01}) &&
+            match(sector,0x3a,new byte[]{0x00,0x00}))
+        {
+            System.out.println("VTOC @ T$"+Integer.toHexString(cur.getTrackInDisk())+", S$"+Integer.toHexString(cur.getSectorInTrack()));
         }
     }
 
