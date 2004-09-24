bfd29eddeddb0c5a0f6939aee80cab58318764ea
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-24 01:14:24 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5583 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ apple2/test/DiskVTOCTest.java
@@ -0,0 +1,27 @@
+import junit.framework.TestCase;
+/*
+ * Created on Sep 23, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class DiskTest extends TestCase
+{
+
+    public void testFindDos33VTOC()
+    {
+    }
+
+    public void testFindDos33CatalogSector()
+    {
+    }
+
+    public void testFindDos33TSMapSector()
+    {
+    }
+
+}
