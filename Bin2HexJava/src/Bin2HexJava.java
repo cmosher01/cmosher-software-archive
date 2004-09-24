cd13d6413f602ab77fd376c62127f7c5bf8ff838
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-24 01:33:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5595 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- Bin2HexJava/src/Bin2HexJava.java
+++ Bin2HexJava/src/Bin2HexJava.java
@@ -2,7 +2,6 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
