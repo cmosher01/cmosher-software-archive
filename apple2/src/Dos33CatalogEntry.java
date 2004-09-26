b92a88abaf52486702dc0e6fe5d5ca807211f14b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-26 03:44:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5687 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Dos33CatalogEntry.java
+++ apple2/src/Dos33CatalogEntry.java
@@ -16,4 +16,21 @@ public class Dos33CatalogEntry
     private final int cSector;
     private final byte[] nameOrig;
     private final String name;
+    /**
+     * @param dataPos
+     * @param locked
+     * @param fileTypeID
+     * @param sector
+     * @param nameOrig
+     * @param name
+     */
+    public Dos33CatalogEntry(final DiskPos dataPos, final boolean locked, final int fileTypeID, final int sector, final byte[] nameOrig, final String name)
+    {
+        this.dataPos = dataPos;
+        this.locked = locked;
+        this.fileTypeID = fileTypeID;
+        cSector = sector;
+        this.nameOrig = nameOrig;
+        this.name = name;
+    }
 }
