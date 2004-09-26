7e90b7896df5938bcc36a548d8fc2dedcaf37e69
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-26 03:44:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5686 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Dos33CatalogEntry.java
+++ apple2/src/Dos33CatalogEntry.java
@@ -10,5 +10,10 @@
  */
 public class Dos33CatalogEntry
 {
-
+    private final DiskPos dataPos;
+    private final boolean locked;
+    private final int fileTypeID;
+    private final int cSector;
+    private final byte[] nameOrig;
+    private final String name;
 }
