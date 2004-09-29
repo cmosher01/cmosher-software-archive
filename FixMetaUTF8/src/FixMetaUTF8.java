6e702b1354095547c827cef462d9502397f54ad8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-29 23:47:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5852 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/src/FixMetaUTF8.java
+++ FixMetaUTF8/src/FixMetaUTF8.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * Created on Sep 29, 2004
  */
@@ -11,7 +17,22 @@
 public class FixMetaUTF8
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        if (args.length != 1)
+        {
+            throw new IllegalArgumentException("Usage: java FixMetaUTF8 htmlfile");
+        }
+        File f = new File(args[0]);
+        f = f.getAbsoluteFile();
+        f = f.getCanonicalFile();
+        int siz = f.length();
+        if (siz >= MAX_SIZ)
+        {
+            throw new UnsupportedOperationException("file "+f+" is too big: "+siz+" (max supported is "+MAX_SIZ);
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(f),"ISO-8859-1"));
+        if (in.)
     }
 }
