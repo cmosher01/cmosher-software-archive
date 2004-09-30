b633cbb9e4f6d1adac115687c2527224ab3fe493
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-30 01:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5906 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/test/FixMetaUTF8Test.java
+++ FixMetaUTF8/test/FixMetaUTF8Test.java
@@ -48,6 +48,26 @@ public class FixMetaUTF8Test extends TestCase
         shouldBe(sbExpect,sb);
     }
 
+    public void testMissingButOtherMeta()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>\n");
+        sb.append("<head>\n");
+        sb.append("<META http-equiv=\"Junk\" content=\"stuff\">\n");
+        sb.append("</head>\n");
+        sb.append("</html>\n");
+
+        StringBuffer sbExpect = new StringBuffer();
+        sbExpect.append("<html>\n");
+        sbExpect.append("<HEAD>\n");
+        sbExpect.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+        sbExpect.append("<META http-equiv=\"Junk\" content=\"stuff\">\n");
+        sbExpect.append("</head>\n");
+        sbExpect.append("</html>\n");
+
+        shouldBe(sbExpect,sb);
+    }
+
     public void testMetaBefore()
     {
         StringBuffer sb = new StringBuffer();
