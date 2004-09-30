9d72f65dabbbb33bd4471da99f4c8ae2187b88e6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-30 01:25:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5896 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/test/FixMetaUTF8Test.java
+++ FixMetaUTF8/test/FixMetaUTF8Test.java
@@ -61,8 +61,8 @@ public class FixMetaUTF8Test extends TestCase
         StringBuffer sbExpect = new StringBuffer();
         sbExpect.append("<html>\n");
         sbExpect.append("<HEAD>\n");
-        sbExpect.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n\n");
-        sbExpect.append("<META http-equiv=\"Junk\" content=\"stuff\">\n");
+        sbExpect.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+        sbExpect.append("<META http-equiv=\"Junk\" content=\"stuff\">\n\n");
         sbExpect.append("</head>\n");
         sbExpect.append("</html>\n");
 
