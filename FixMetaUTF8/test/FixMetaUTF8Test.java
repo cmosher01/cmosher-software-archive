611d702bc109d7e027d759c2af071195a7d7351c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-30 00:42:02 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5887 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/test/FixMetaUTF8Test.java
+++ FixMetaUTF8/test/FixMetaUTF8Test.java
@@ -34,6 +34,24 @@ public class FixMetaUTF8Test extends TestCase
         shouldBe(sbExpect,sb);
     }
 
+    public void testMissing() throws IOException
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>\n");
+        sb.append("<head>\n");
+        sb.append("</head>\n");
+        sb.append("</html>\n");
+
+        StringBuffer sbExpect = new StringBuffer();
+        sbExpect.append("<html>\n");
+        sbExpect.append("<HEAD>\n");
+        sbExpect.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+        sbExpect.append("</head>\n");
+        sbExpect.append("</html>\n");
+
+        shouldBe(sbExpect,sb);
+    }
+
     private void shouldBe(StringBuffer sbExpect, StringBuffer in)
     {
         in = FixMetaUTF8.removeContentTypeMeta(in);
