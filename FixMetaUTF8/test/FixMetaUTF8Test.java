e6339d45d20177f36e44d3f5cc55a48a71a3bca7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-30 00:36:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5879 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/test/FixMetaUTF8Test.java
+++ FixMetaUTF8/test/FixMetaUTF8Test.java
@@ -22,6 +22,16 @@ public class FixMetaUTF8Test extends TestCase
         sb.append("<META http-equiv=\"Content-Type\" \ncontent=\"text/html; charset=windows-1252\">\n");
         sb.append("</head>\n");
         sb.append("</html>\n");
+
+        StringBuffer sbExpect = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<HEAD>\");
+        sb.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\");
+
+        sb.append("</head>\");
+        sb.append("</html>\");
+
+
         System.out.println(sb);
         sb = FixMetaUTF8.removeContentTypeMeta(sb);
         System.out.println(sb);
