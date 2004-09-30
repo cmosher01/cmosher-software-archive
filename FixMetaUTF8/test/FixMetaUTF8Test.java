e2c38de70d582c18dd932d0827dc796d50e9dba3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-09-30 01:23:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@5895 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- FixMetaUTF8/test/FixMetaUTF8Test.java
+++ FixMetaUTF8/test/FixMetaUTF8Test.java
@@ -48,6 +48,27 @@ public class FixMetaUTF8Test extends TestCase
         shouldBe(sbExpect,sb);
     }
 
+    public void testInterviening()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>\n");
+        sb.append("<head>\n");
+        sb.append("<META http-equiv=\"Junk\" content=\"stuff\">\n");
+        sb.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">\n");
+        sb.append("</head>\n");
+        sb.append("</html>\n");
+
+        StringBuffer sbExpect = new StringBuffer();
+        sbExpect.append("<html>\n");
+        sbExpect.append("<HEAD>\n");
+        sbExpect.append("<META http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n\n");
+        sbExpect.append("<META http-equiv=\"Junk\" content=\"stuff\">\n");
+        sbExpect.append("</head>\n");
+        sbExpect.append("</html>\n");
+
+        shouldBe(sbExpect,sb);
+    }
+
     public void testTwoLines()
     {
         StringBuffer sb = new StringBuffer();
