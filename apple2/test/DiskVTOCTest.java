9d30b39aaaf2245717961a29c46fea0688e858f7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-02 17:42:25 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6009 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/test/DiskVTOCTest.java
+++ apple2/test/DiskVTOCTest.java
@@ -25,6 +25,7 @@ public class DiskVTOCTest extends TestCase
         Disk disk = readDiskResource("DOS33_SystemMaster_19800825.dsk");
         List rPos = new ArrayList();
         disk.findDos33VTOC(rPos);
+        this.assertEquals(1, rPos.size());
     }
 //    private static byte[] zeroes = new byte[0x100];
 //
