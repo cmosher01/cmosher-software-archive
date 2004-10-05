cc428bab50f61d3ea829813980423672b3a9ce92
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-05 00:15:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6078 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/test/DiskCatalogTest.java
+++ apple2/test/DiskCatalogTest.java
@@ -309,6 +309,20 @@ public class DiskCatalogTest extends TestCase
         assertManyPos("TIP1.dsk",r);
     }
 
+    /**
+     * @throws IOException
+     * @throws InvalidPosException
+     */
+    public void testDOS34() throws IOException, InvalidPosException
+    {
+        List r = new ArrayList();
+        r.add(new DiskPos(0x11,0xC));
+        r.add(new DiskPos(0x11,0xD));
+        r.add(new DiskPos(0x11,0xE));
+        r.add(new DiskPos(0x11,0xF));
+        assertManyPos("UTY4-1.dsk",r);
+    }
+
     //    public void testDOS33_Others() throws IOException, InvalidPosException
 //    {
 //        assertOnePos("daviddos_2.dsk",new DiskPos(0x11,0));
