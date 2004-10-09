b42186fa4eb5f4cec522204eb2bfce51458657ea
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-09 15:34:23 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6143 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeTableOfContents.java
+++ apple2/src/VolumeTableOfContents.java
@@ -10,5 +10,13 @@
  */
 public class VolumeTableOfContents
 {
+    private final DiskPos pos;
 
+    /**
+     * @param pos
+     */
+    public VolumeTableOfContents(final DiskPos pos)
+    {
+        this.pos = pos;
+    }
 }
