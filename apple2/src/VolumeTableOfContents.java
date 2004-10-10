ec7cdc4baa7882f3584d48e547b6e7266a23f07b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:43:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6186 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeTableOfContents.java
+++ apple2/src/VolumeTableOfContents.java
@@ -13,6 +13,8 @@ import java.util.List;
  */
 public class VolumeTableOfContents extends VolumeEntity
 {
+    private DiskPos pCat;
+
     /**
      * @return
      */
@@ -25,8 +27,9 @@ public class VolumeTableOfContents extends VolumeEntity
      * @param disk
      * @throws VTOCNotFoundException
      * @throws MultipleVTOCException
+     * @throws InvalidPosException
      */
-    public void readFromMedia(Disk disk) throws VTOCNotFoundException, MultipleVTOCException
+    public void readFromMedia(Disk disk) throws VTOCNotFoundException, MultipleVTOCException, InvalidPosException
     {
         List rPosVTOC = new ArrayList();
         disk.findDos33VTOC(rPosVTOC);
@@ -42,5 +45,12 @@ public class VolumeTableOfContents extends VolumeEntity
         DiskPos p = (DiskPos)rPosVTOC.get(0);
 
         rSector.add(new VolumeSector(p,0));
+
+        pCat = disk.getDos33Next(p);
+    }
+
+    public DiskPos getCatalogPointer()
+    {
+        return pCat;
     }
 }
