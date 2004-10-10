2c1fd577c8eccd8752650b915fb7209cfc868db8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:30:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6171 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ apple2/src/VTOCNotFoundException.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Oct 10, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class VTOCNotFoundException extends Exception
+{
+
+    /**
+     * 
+     */
+    public VTOCNotFoundException()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public VTOCNotFoundException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public VTOCNotFoundException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public VTOCNotFoundException(String message, Throwable cause)
+    {
+        super(message,cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
