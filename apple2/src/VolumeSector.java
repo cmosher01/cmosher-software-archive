9c11d163355f1b5aa40206c9aedf583988a684db
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:37:21 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6176 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeSector.java
+++ apple2/src/VolumeSector.java
@@ -11,12 +11,14 @@
 public class VolumeSector
 {
     private final DiskPos pos;
+    private final int i;
 
     /**
      * @param pos
      */
-    public VolumeSector(DiskPos pos)
+    public VolumeSector(DiskPos pos, int index)
     {
         this.pos = pos;
+        this.i = index;
     }
 }
