94646768df64edd5b173ad94040d91b53a6233c1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:31:08 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6173 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ apple2/src/MultipleVTOCException.java
@@ -0,0 +1,51 @@
+/*
+ * Created on Oct 10, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class MultipleVTOCException extends Exception
+{
+
+    /**
+     * 
+     */
+    public MultipleVTOCException()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     */
+    public MultipleVTOCException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public MultipleVTOCException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public MultipleVTOCException(String message, Throwable cause)
+    {
+        super(message,cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
