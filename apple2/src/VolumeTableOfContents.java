128df4831598d11440cec41703e7a10f01448c3f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:35:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6175 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeTableOfContents.java
+++ apple2/src/VolumeTableOfContents.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Created on Oct 9, 2004
  */
@@ -21,5 +24,26 @@ public class VolumeTableOfContents extends VolumeEntity
     public String toString()
     {
         return "Table of Contents";
+    }
+
+    /**
+     * @param disk
+     * @throws VTOCNotFoundException
+     * @throws MultipleVTOCException
+     */
+    public void readFromMedia(Disk disk) throws VTOCNotFoundException, MultipleVTOCException
+    {
+        List rPosVTOC = new ArrayList();
+        disk.findDos33VTOC(rPosVTOC);
+
+        if (rPosVTOC.size() == 0)
+        {
+            throw new VTOCNotFoundException();
+        }
+        else if (rPosVTOC.size() > 1)
+        {
+            throw new MultipleVTOCException();
+        }
+        DiskPos p = (DiskPos)rPosVTOC.get(0);
     }
 }
