c52acbe3e9a8e8ce39be6f4081b1a7b6e0048fe2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 20:31:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6174 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Volume.java
+++ apple2/src/Volume.java
@@ -19,4 +19,24 @@ public class Volume
     private VolumeBoot boot;
     private VolumeUnusedBlank blank;
     private VolumeUnusedData data;
+    /**
+     * @param disk
+     */
+    public void readFromMedia(Disk disk)
+    {
+        List rPosVTOC = new ArrayList();
+        disk.findDos33VTOC(rPosVTOC);
+
+        if (rPosVTOC.size() == 0)
+        {
+            throw new VTOCNotFoundException();
+        }
+        else if (rPosVTOC.size() > 1)
+        {
+            throw new MultipleVTOCException();
+        }
+        DiskPos p = (DiskPos)rPosVTOC.get(0);
+
+        new VolumeTableOfContents(p);
+    }
 }
