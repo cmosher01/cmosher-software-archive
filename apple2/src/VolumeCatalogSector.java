791ab2aa9c6337e81be1d79614e821311f7775dd
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-10 21:07:00 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6192 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeCatalogSector.java
+++ apple2/src/VolumeCatalogSector.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Created on Oct 10, 2004
  */
@@ -18,6 +21,9 @@ public class VolumeCatalogSector extends VolumeEntity
     public void readFromMedia(DiskPos p, Disk disk)
     {
         rSector.add(p);
+
+        List rEnt = new ArrayList();
+        disk.getDos33CatalogEntries(p,rEnt);
     }
 
 }
