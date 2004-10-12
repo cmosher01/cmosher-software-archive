eaf1e1a8ee4a56d04d60ebf70d05a0d5d6c0f5f3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-12 23:10:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6260 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Apple2.java
+++ apple2/src/Apple2.java
@@ -33,8 +33,10 @@ public class Apple2
     {
 //        mainAll(args);
 //        doOneDisk(readDisk(new File("test/SARG2.DSK")));
-//        doOneDisk(readDisk(new File("test/DOS33_SystemMaster_19800825.dsk")));
-        doOneDisk(readDisk(new File("test/montezuma.dsk")));
+//
+        
+        doOneDisk(readDisk(new File("test/DOS33_SystemMaster_19800825.dsk")));
+//        doOneDisk(readDisk(new File("test/montezuma.dsk")));
     }
 
     /**
