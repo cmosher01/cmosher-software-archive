8b832f21752d47985cea7003c25b8842a324d4be
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-12 01:59:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6245 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeTSMap.java
+++ apple2/src/VolumeTSMap.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Created on Oct 11, 2004
  */
@@ -10,5 +13,15 @@
  */
 public class VolumeTSMap extends VolumeEntity
 {
+    private List rTS = new ArrayList();
 
+    /**
+     * @param start
+     * @param disk
+     * @throws InvalidPosException
+     */
+    public void readFromMedia(DiskPos start, Disk disk) throws InvalidPosException
+    {
+        disk.getDos33TSMapEntries(start, rTS);
+    }
 }
