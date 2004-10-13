d7770bad9f69d49817d1562739d24758871893a1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:57:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6380 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeDOS.java
+++ apple2/src/VolumeDOS.java
@@ -101,4 +101,39 @@ public class VolumeDOS extends VolumeEntity
         }
         s.append("\n");
     }
+
+    private byte[] rb;
+    public boolean hasProtodosSignature()
+    {
+        return (rb[0x1602] == 0x54 && rb[0x1603] == 0x59 && rb[0x1604] == 0x50 && rb[0x1605] == 0xFFFFFFC5);
+    }
+    public boolean hasDaviddosSignature()
+    {
+        return (rb[0x1924] == 0x44 && rb[0x1925] == 0x2D && rb[0x1926] == 0x44 && rb[0x1927] == 0x4F && rb[0x1928] == 0x53);
+    }
+    public boolean hasDaviddos2Signature()
+    {
+        return hasDaviddosSignature() && 
+        (rb[0x2701] == 0xFFFFFFC4 && rb[0x2702] == 0xFFFFFFC1 && rb[0x2703] == 0xFFFFFFD6 && rb[0x270B] == 0xFFFFFFC9 && rb[0x270C] == 0xFFFFFFC9);
+    }
+    public boolean hasDiversidos2cSignature()
+    {
+        return (rb[0xA10] == 0x20 && rb[0xA11] == 0xFFFFFF84 && rb[0xA12] == 0xFFFFFF9D && rb[0xA13] == 0xFFFFFFA0);
+    }
+    public boolean hasDiversidos41cSignature()
+    {
+        return (rb[0xA10] == 0xFFFFFFA9 && rb[0xA11] == 0xFFFFFFFF && rb[0xA12] == 0xFFFFFF8D && rb[0xA13] == 0xFFFFFFFB);
+    }
+    public boolean hasEsdosSignature()
+    {
+        return (rb[0x1671] == 0x4C && rb[0x1672] == 0x4E && rb[0x1673] == 0xFFFFFFC1 && rb[0x1674] == 0x52);
+    }
+    public boolean hasHyperdosSignature()
+    {
+        return (rb[0x656] == 0xFFFFFFAD && rb[0x657] ==  0x61 && rb[0x658] ==  0xFFFFFFAA && rb[0x659] ==  0xFFFFFFC9 && rb[0x65A] ==  0x01 && rb[0x65B] ==  0xFFFFFFB0);
+    }
+    public boolean hasRdosSignature()
+    {
+        return (rb[0x100] == 0x4C && rb[0x101] ==  0x74 && rb[0x102] ==  0xFFFFFFB9 && rb[0x103] ==  0xFFFFFFA0);
+    }
 }
