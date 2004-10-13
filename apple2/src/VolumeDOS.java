6ab60c4f5775b82bcb3e93f3fcfea41a4207b53f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:36:13 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6358 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeDOS.java
+++ apple2/src/VolumeDOS.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 /*
  * Created on Oct 13, 2004
  */
@@ -11,4 +13,65 @@
 public class VolumeDOS extends VolumeEntity
 {
 
+    /**
+     * @param disk
+     */
+    public void readFromMedia(Disk disk)
+    {
+        rSector.add(new VolumeSector(p,0));
+        rSector.add(VolumeSector(new DiskPos(0,1));
+        rSector.add(VolumeSector(new DiskPos(0,2));
+        rSector.add(VolumeSector(new DiskPos(0,3));
+        rSector.add(VolumeSector(new DiskPos(0,4));
+        rSector.add(VolumeSector(new DiskPos(0,5));
+        rSector.add(VolumeSector(new DiskPos(0,6));
+        rSector.add(VolumeSector(new DiskPos(0,7));
+        rSector.add(VolumeSector(new DiskPos(0,8));
+        rSector.add(VolumeSector(new DiskPos(0,9));
+        rSector.add(VolumeSector(new DiskPos(0,10));
+        rSector.add(VolumeSector(new DiskPos(0,11));
+        rSector.add(VolumeSector(new DiskPos(0,12));
+        rSector.add(VolumeSector(new DiskPos(0,13));
+        rSector.add(VolumeSector(new DiskPos(0,14));
+        rSector.add(VolumeSector(new DiskPos(0,15));
+        rSector.add(VolumeSector(new DiskPos(1,0));
+        rSector.add(VolumeSector(new DiskPos(1,1));
+        rSector.add(VolumeSector(new DiskPos(1,2));
+        rSector.add(VolumeSector(new DiskPos(1,3));
+        rSector.add(VolumeSector(new DiskPos(1,4));
+        rSector.add(VolumeSector(new DiskPos(1,5));
+        rSector.add(VolumeSector(new DiskPos(1,6));
+        rSector.add(VolumeSector(new DiskPos(1,7));
+        rSector.add(VolumeSector(new DiskPos(1,8));
+        rSector.add(VolumeSector(new DiskPos(1,9));
+        rSector.add(VolumeSector(new DiskPos(1,10));
+        rSector.add(VolumeSector(new DiskPos(1,11));
+        rSector.add(VolumeSector(new DiskPos(1,12));
+        rSector.add(VolumeSector(new DiskPos(1,13));
+        rSector.add(VolumeSector(new DiskPos(1,14));
+        rSector.add(VolumeSector(new DiskPos(1,15));
+        rSector.add(VolumeSector(new DiskPos(2,0));
+        rSector.add(VolumeSector(new DiskPos(2,1));
+        rSector.add(VolumeSector(new DiskPos(2,2));
+        rSector.add(VolumeSector(new DiskPos(2,3));
+        rSector.add(VolumeSector(new DiskPos(2,4));
+        rSector.add(VolumeSector(new DiskPos(2,5));
+        rSector.add(VolumeSector(new DiskPos(2,6));
+        rSector.add(VolumeSector(new DiskPos(2,7));
+        rSector.add(VolumeSector(new DiskPos(2,8));
+        rSector.add(VolumeSector(new DiskPos(2,9));
+        rSector.add(VolumeSector(new DiskPos(2,10));
+        rSector.add(VolumeSector(new DiskPos(2,11));
+        rSector.add(VolumeSector(new DiskPos(2,12));
+        rSector.add(VolumeSector(new DiskPos(2,13));
+        rSector.add(VolumeSector(new DiskPos(2,14));
+        rSector.add(VolumeSector(new DiskPos(2,15));
+    }
+
+    /**
+     * @param rPos
+     */
+    public void getUsed(Collection rPos)
+    {
+    }
 }
