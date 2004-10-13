8d5de3edfebb2d42cfbb34bb7590a54747ef1775
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:00:45 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6341 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Apple2.java
+++ apple2/src/Apple2.java
@@ -32,9 +32,9 @@ public class Apple2
     public static void main(String[] args) throws VTOCNotFoundException, MultipleVTOCException, InvalidPosException, IOException
     {
 //        mainAll(args);
-        doOneDisk(readDisk(new File("test/SARG2.DSK")));
+//        doOneDisk(readDisk(new File("test/SARG2.DSK")));
 //        doOneDisk(readDisk(new File("test/DOS33_SystemMaster_19800825.dsk")));
-//        doOneDisk(readDisk(new File("test/montezuma.dsk")));
+        doOneDisk(readDisk(new File("test/montezuma.dsk")));
     }
 
     /**
