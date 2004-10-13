d6498c26d90ed5a69906595d3265482ec709eba0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:02:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6344 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/Apple2.java
+++ apple2/src/Apple2.java
@@ -1,14 +1,10 @@
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /*
  * Created on Sep 17, 2004
