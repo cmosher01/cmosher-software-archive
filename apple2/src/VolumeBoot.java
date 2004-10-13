f9c11c2fabc6a84935b70503068473a7fb945cb1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:30:21 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6351 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeBoot.java
+++ apple2/src/VolumeBoot.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 /*
  * Created on Oct 13, 2004
  */
@@ -24,11 +26,27 @@ public class VolumeBoot extends VolumeEntity
         }
         catch (InvalidPosException e)
         {
-            throw new RuntimeException("can't happen.");
+            throw new RuntimeException(e,"can't happen.");
         }
 
         rSector.add(new VolumeSector(p,0));
 
         data = disk.readSector(p);
     }
+
+    /**
+     * @param rPos
+     */
+    public void getUsed(Collection rPos)
+    {
+        try
+        {
+            rPos.add(new DiskPos(0,0));
+        }
+        catch (InvalidPosException e)
+        {
+            throw new RuntimeException(e,"can't happen.");
+        }
+    }
+
 }
