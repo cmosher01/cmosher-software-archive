c5b59e9ab81ad43dc6057a35ded11d5627c9f474
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-13 23:47:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6370 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeUnusedData.java
+++ apple2/src/VolumeUnusedData.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.List;
+
 /*
  * Created on Oct 13, 2004
  */
@@ -11,4 +14,17 @@
 public class VolumeUnusedData extends VolumeEntity
 {
 
+    /**
+     * @param rPos
+     * @param disk
+     */
+    public void readFromMedia(List rPos, Disk disk)
+    {
+        int i = 0;
+        for (Iterator i = rPos.iterator(); i.hasNext();)
+        {
+            DiskPos p = (DiskPos)i.next();
+            rSector.add(new VolumeSector(p,i++));
+        }
+    }
 }
