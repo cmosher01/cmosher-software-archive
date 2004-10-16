26601eaf720e26e2555611a0e078de6ded8f824c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-16 04:39:37 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6419 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ apple2/src/MainFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+/*
+ * Created on Oct 16, 2004
+ */
+
+
+/**
+ * TODO
+ * 
+ * @author Chris
+ */
+public class MainFrame extends JFrame
+{
+
+    public static void main(String[] args)
+    {
+    }
+}
