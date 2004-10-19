441cfecf1ff1d43a8d366cdc768f05384f155931
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-19 01:38:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6482 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/MainFrame.java
+++ apple2/src/MainFrame.java
@@ -76,11 +76,7 @@ public class MainFrame extends JFrame
 
         StringBuffer s = new StringBuffer();
 
-        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode("Prontodos.dsk");
-//        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode("CLC11.dsk");
-//        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode("SARG2.dsk");
-//        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode("DOS33_SystemMaster_19830101.dsk");
-//        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode("DiversiDOS_2_C_1982.dsk");
+        DefaultMutableTreeNode nDisk = new DefaultMutableTreeNode(f.getName());
         DefaultMutableTreeNode nBoot = new DefaultMutableTreeNode("Bootstrap Loader");
         s.append("APPLE ][ DOS 3.3");
         VolumeDOS dos = vol.getDos();
