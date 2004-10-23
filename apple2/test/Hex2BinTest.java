71e65dc355648b905a23bac0230dcb7d16bf48a8
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-10-23 22:24:40 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6533 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/test/Hex2BinTest.java
+++ apple2/test/Hex2BinTest.java
@@ -15,6 +15,17 @@ public class Hex2BinTest extends TestCase
     public void testHexbyte()
     {
         assertEquals("00",Hex2Bin.hexbyte((byte)0));
+        assertEquals("01",Hex2Bin.hexbyte((byte)1);
+        assertEquals("02",Hex2Bin.hexbyte((byte)2));
+        assertEquals("03",Hex2Bin.hexbyte((byte)3));
+        assertEquals("04",Hex2Bin.hexbyte((byte)4));
+        assertEquals("0A",Hex2Bin.hexbyte((byte)10));
+        assertEquals("0B",Hex2Bin.hexbyte((byte)11));
+        assertEquals("0C",Hex2Bin.hexbyte((byte)12));
+        assertEquals("0D",Hex2Bin.hexbyte((byte)13));
+        assertEquals("0E",Hex2Bin.hexbyte((byte)14));
+        assertEquals("0F",Hex2Bin.hexbyte((byte)15));
+        assertEquals("10",Hex2Bin.hexbyte((byte)16));
     }
 
 }
