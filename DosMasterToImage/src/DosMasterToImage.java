4e7d46ee0c2900f0285387b0946543a06e6a327b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-11-03 03:14:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6614 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- DosMasterToImage/src/DosMasterToImage.java
+++ DosMasterToImage/src/DosMasterToImage.java
@@ -44,7 +44,11 @@ public class DosMasterToImage
 
         in.mark(in.available());
 
-        in.skip(0x1B00);
+        int c = in.skip(0x1B00);
+        if (c != 0x1B00)
+        {
+            throw new RuntimeException("error skipping bytes");
+        }
         for (int i = 0x3600; i < 0x4000; ++i)
         {
             out.write(in.read());
