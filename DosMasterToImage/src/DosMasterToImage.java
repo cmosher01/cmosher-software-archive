8937e55678bc25fba678dbba0ebb5c7530fa20f4
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-11-03 02:16:39 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6602 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- DosMasterToImage/src/DosMasterToImage.java
+++ DosMasterToImage/src/DosMasterToImage.java
@@ -1,3 +1,11 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /*
  * Created on Nov 2, 2004
  */
@@ -11,7 +19,19 @@
 public class DosMasterToImage
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        if (args.length != 2)
+        {
+            throw new IllegalArgumentException("Usage: java DosMasterToImage master-image disk-image");
+        }
+
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(new File(args[0])));
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(args[1])));
+
+        if (in.available() != 0xC000-0x9B00)
+        {
+            throw new IllegalArgumentException("Input file length must be "+(0xC000-0x9B00)+" bytes.");
+        }
     }
 }
