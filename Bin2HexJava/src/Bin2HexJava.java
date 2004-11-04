fdc03975c6ef2b10cd381ecfbd0e7a2abcf7b10b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-11-04 02:32:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6617 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- Bin2HexJava/src/Bin2HexJava.java
+++ Bin2HexJava/src/Bin2HexJava.java
@@ -20,9 +20,10 @@ public class Bin2HexJava
     private static final int BPL = 0x10;
     private static final boolean SHOW_CAST = false;
     private static final boolean SHOW_ZERO_X = false;
-    private static final boolean SHOW_COMMA = false;
-    private static final boolean SHOW_SPACE = true;
-    private static final boolean SHOW_QUOTES = true;
+    private static final boolean SHOW_DOLLAR = true;
+    private static final boolean SHOW_COMMA = true;
+    private static final boolean SHOW_SPACE = false;
+    private static final boolean SHOW_QUOTES = false;
 
     /**
      * @param args
@@ -86,6 +87,10 @@ public class Bin2HexJava
             if (SHOW_ZERO_X)
             {
                 System.out.print("0x");
+            }
+            if (SHOW_ZERO_X)
+            {
+                System.out.print("$");
             }
 
             outHexByte(c);
