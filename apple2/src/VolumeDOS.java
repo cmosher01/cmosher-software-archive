1cc08e92778ebcf584af57e66ed094de44a13fe2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2004-11-22 02:39:36 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6740 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2/src/VolumeDOS.java
+++ apple2/src/VolumeDOS.java
@@ -618,8 +618,8 @@ public class VolumeDOS extends VolumeEntity
                         else
                         {
                             s.append(" (DOS 3.3 1983 slave (A$");
-                            s.append(Integer.toHexString(0x1D00+dif*256));
-                            s.append("): unknown alteration)");
+                            s.append(Hex2Bin.hexbyte((byte)(0x1D+dif)));
+                            s.append("00): unknown alteration)");
                         }
                     }
                 }
@@ -649,8 +649,8 @@ public class VolumeDOS extends VolumeEntity
                         else
                         {
                             s.append(" (DOS 3.3 1986 slave (A$");
-                            s.append(Integer.toHexString(0x1D00+dif*256));
-                            s.append("): unknown alteration)");
+                            s.append(Hex2Bin.hexbyte((byte)(0x1D+dif)));
+                            s.append("00): unknown alteration)");
                         }
                     }
                 }
