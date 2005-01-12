a686eb8bc5d690dc64f3c65cfb974fbb3a1dd68e
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-12 02:38:12 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6791 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- OneFileCopy/src/OneFileCopyClient.java
+++ OneFileCopy/src/OneFileCopyClient.java
@@ -1,6 +1,10 @@
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class OneFileCopyClient
 {
 	public static void main(String[] args)
 	{
+		Socket s = new Socket(new InetAddress(),60013);
 	}
 }
