07ae2421eadd85ac068ac2b58556441c26f2eab3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-12 02:36:16 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6787 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- OneFileCopy/src/OneFileCopyServer.java
+++ OneFileCopy/src/OneFileCopyServer.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class OneFileCopy
+public class OneFileCopyServer
 {
 	private static final int cBufMax = 0x10000000;
 
