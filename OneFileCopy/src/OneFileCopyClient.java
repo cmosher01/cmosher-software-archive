87cdb45f813d532d94f5c7cee82e3792eaaedf24
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-12 02:41:49 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6793 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- OneFileCopy/src/OneFileCopyClient.java
+++ OneFileCopy/src/OneFileCopyClient.java
@@ -1,3 +1,7 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -7,5 +11,19 @@ public class OneFileCopyClient
 	public static void main(String[] args) throws IOException
 	{
 		Socket s = new Socket(InetAddress.getByName("192.168.1.102"),60013);
+		BufferedInputStream in = new BufferedInputStream(s.getInputStream());
+
+		String sFileName = "test.dat";
+		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(sFileName)));
+
+		int cb = in.read(rb);
+		while (cb > 0)
+		{
+			out.write(rb,0,cb);
+		}
+
+		out.flush();
+		out.close();
+		in.close();
 	}
 }
