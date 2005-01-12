5dee5cfb1a656893d192ca4f79f4a7d7e6ce54df
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-12 02:39:32 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6792 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- OneFileCopy/src/OneFileCopyClient.java
+++ OneFileCopy/src/OneFileCopyClient.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class OneFileCopyClient
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		Socket s = new Socket(new InetAddress(),60013);
+		Socket s = new Socket(InetAddress.getByName("192.168.1.102"),60013);
 	}
 }
