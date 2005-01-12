dd995fdacddea9ab28ab361bbb9824194bb005d0
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-12 02:20:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6780 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ OneFileCopy/src/OneFileCopyServer.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jan 11, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author Chris
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class OneFileCopy {
+
+	public static void main(String[] args) {
+	}
+}
