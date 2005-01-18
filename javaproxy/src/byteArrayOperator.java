5887dee8cc7784350e276b4479d7fbcd49d74ee1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6906 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/byteArrayOperator.java
+++ javaproxy/src/byteArrayOperator.java
@@ -1,11 +1,8 @@
 /**
- * @(#)byteArrayOperator.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)byteArrayOperator.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR
+ * OWN RISK. Author : Steve Yeong-Ching Hsueh
  */
 
 
@@ -13,53 +10,70 @@
 /**
  * This class contains common functions for byte array operation
  */
-public class byteArrayOperator {
+public class byteArrayOperator
+{
 
-    byteArrayOperator() { }
+    byteArrayOperator()
+    {
+    }
 
     /**
      * copy length of source to end of target. target must end with 0
      */
-    public static void copy(byte[] target, int last_byte, byte[] source, int length) {
-        for(int i=0; i<length; i++) {
-            target[last_byte+i] = source[i];
+    public static void copy(byte[] target, int last_byte, byte[] source, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            target[last_byte + i] = source[i];
         }
     }
 
     /**
-     * check if this byte array is printable
-     * true if printable is 3 times or greater the number of none-printable
+     * check if this byte array is printable true if printable is 3 times or
+     * greater the number of none-printable
      */
-    public static boolean isPrintable(byte[] in) {
-        if(in == null) return false;
-        return isPrintable(in, in.length);
+    public static boolean isPrintable(byte[] in)
+    {
+        if (in == null)
+            return false;
+        return isPrintable(in,in.length);
     }
 
     /**
-     * check if this byte array is printable
-     * true if printable is 3 times or greater the number of none-printable
+     * check if this byte array is printable true if printable is 3 times or
+     * greater the number of none-printable
      */
-    public static boolean isPrintable(byte[] in, int length) {
-        if(in == null || length <= 0) return false;
+    public static boolean isPrintable(byte[] in, int length)
+    {
+        if (in == null || length <= 0)
+            return false;
 
-        int pcount  = 0;  // printable count
-        int npcount = 0;  // none printable count
-        int dif     = 0;
+        int pcount = 0; // printable count
+        int npcount = 0; // none printable count
+        int dif = 0;
 
-        if(length < 10) {
-            for(int i=0; i<length; i++)
-                if(in[i] > 31 && in[i] < 127) pcount++;
-                else npcount++;
+        if (length < 10)
+        {
+            for (int i = 0; i < length; i++)
+                if (in[i] > 31 && in[i] < 127)
+                    pcount++;
+                else
+                    npcount++;
         }
-        else {
-            dif = length/10;
-            for(int i=0; i<length; i += dif) {
-                if(in[i] > 31 && in[i] < 127) pcount++;
-                else npcount++;
+        else
+        {
+            dif = length / 10;
+            for (int i = 0; i < length; i += dif)
+            {
+                if (in[i] > 31 && in[i] < 127)
+                    pcount++;
+                else
+                    npcount++;
             }
         }
 
-        if(npcount == 0 || (pcount/npcount) >= 3) return true;
+        if (npcount == 0 || (pcount / npcount) >= 3)
+            return true;
 
         return false;
     }
