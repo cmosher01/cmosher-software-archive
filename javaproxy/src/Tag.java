010e81d112a1ec3484bcdda4543d175c4e8234e3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6911 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/Tag.java
+++ javaproxy/src/Tag.java
@@ -1,55 +1,63 @@
 /**
- * @(#)Tag.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)Tag.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK. Author :
+ * Steve Yeong-Ching Hsueh
  */
 
 import java.awt.Rectangle;
 
+
+
 /**
  * Tag
  */
-class Tag {
- private String name;
- private Rectangle position_size;
+class Tag
+{
+    private String name;
 
- /**
-  * constructor
-  */
- public Tag() {
-    this("  ");
- }
+    private Rectangle position_size;
 
- /**
-  * constructor
-  */
- public Tag(String tagname) {
-    name = tagname;
- }
 
- /**
-  * get name of this tag
-  */
- public String getName() {
-    return name;
- }
 
- /**
-  * set position size
-  */
- public void setPositionSize(Rectangle rect) {
-    position_size = rect;
- }
+    /**
+     * constructor
+     */
+    public Tag()
+    {
+        this("  ");
+    }
 
- /**
-  * get positiion size
-  */
- public Rectangle getPositionSize() {
-    return position_size;
- }
+    /**
+     * constructor
+     */
+    public Tag(String tagname)
+    {
+        name = tagname;
+    }
+
+    /**
+     * get name of this tag
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * set position size
+     */
+    public void setPositionSize(Rectangle rect)
+    {
+        position_size = rect;
+    }
+
+    /**
+     * get positiion size
+     */
+    public Rectangle getPositionSize()
+    {
+        return position_size;
+    }
 
 }
