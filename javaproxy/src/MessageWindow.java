d717684fc66305c16d1d1a95c1507d27147ec026
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6912 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/MessageWindow.java
+++ javaproxy/src/MessageWindow.java
@@ -1,11 +1,8 @@
 /**
- * @(#)MessageWindow.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)MessageWindow.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR
+ * OWN RISK. Author : Steve Yeong-Ching Hsueh
  */
 
 import java.awt.AWTEvent;
@@ -27,30 +24,48 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+
+
 /**
  * MessageWindow shows message in a frame
  */
 class MessageWindow extends Frame implements ActionListener
 {
-    final static int MAX_ROW_NUM =800;  // max number of lines in the window
-    final static int TOLERANCE   = 10;  // extra line allowed
-    private TextArea MessageArea;       // TextArea object
+    final static int MAX_ROW_NUM = 800; // max number of lines in the window
+
+    final static int TOLERANCE = 10; // extra line allowed
+
+    private TextArea MessageArea; // TextArea object
+
     MenuBar mb;
-    Menu    file_menu;
-    MenuItem mi_save  = new MenuItem("Save");
+
+    Menu file_menu;
+
+    MenuItem mi_save = new MenuItem("Save");
+
     MenuItem mi_print = new MenuItem("Print");
-    MenuItem mi_exit  = new MenuItem("Exit");
-    int  pcount        = 0; // print job counter
-    int  font_height   = 0; // system font's height
-    Font cur_font      = null;
+
+    MenuItem mi_exit = new MenuItem("Exit");
+
+    int pcount = 0; // print job counter
+
+    int font_height = 0; // system font's height
+
+    Font cur_font = null;
+
     String default_savename = "";
-    String default_fileext  = ".msg";
+
+    String default_fileext = ".msg";
+
     Graphics pg = null;
+
+
 
     /**
      * constructor
      */
-    MessageWindow(String Title, String savefile) {
+    MessageWindow(String Title, String savefile)
+    {
         this(Title);
         default_savename = savefile;
     }
@@ -58,12 +73,13 @@ class MessageWindow extends Frame implements ActionListener
     /**
      * constructor
      */
-    MessageWindow(String Title) {
+    MessageWindow(String Title)
+    {
         super(Title);
 
         mb = new MenuBar();
         this.setMenuBar(mb);
-        file_menu = new Menu("File", false);
+        file_menu = new Menu("File",false);
         file_menu.add(mi_save);
         mi_save.addActionListener(this);
         file_menu.add(mi_print);
@@ -74,35 +90,40 @@ class MessageWindow extends Frame implements ActionListener
         mb.add(file_menu);
 
         setLayout(new BorderLayout());
-        MessageArea = new TextArea("", 10, 20);
+        MessageArea = new TextArea("",10,20);
         MessageArea.setEditable(false);
-        add("Center", MessageArea);
-        enableEvents(AWTEvent.WINDOW_EVENT_MASK );
+        add("Center",MessageArea);
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
 
     }
 
-    public void setSaveEnabled(boolean mode) {
+    public void setSaveEnabled(boolean mode)
+    {
         mi_save.setEnabled(mode);
     }
 
-    public void setPrintEnabled(boolean mode) {
+    public void setPrintEnabled(boolean mode)
+    {
         mi_print.setEnabled(mode);
     }
 
 
-    public boolean addMessage(String msg) {
+    public boolean addMessage(String msg)
+    {
         int newlineposition = 0;
         String Text;
 
         Text = MessageArea.getText();
-        StringTokenizer st = new StringTokenizer(Text, "\n");
+        StringTokenizer st = new StringTokenizer(Text,"\n");
 
-        if(st.countTokens() > MAX_ROW_NUM + TOLERANCE) {
+        if (st.countTokens() > MAX_ROW_NUM + TOLERANCE)
+        {
             //System.out.println("tokens = " + st.countTokens());
-            for(int j=0; j<TOLERANCE; j++) {
-                newlineposition = Text.indexOf(10, newlineposition) + 1;
+            for (int j = 0; j < TOLERANCE; j++)
+            {
+                newlineposition = Text.indexOf(10,newlineposition) + 1;
             }
-            MessageArea.replaceRange("", 0, newlineposition);
+            MessageArea.replaceRange("",0,newlineposition);
         }
 
         MessageArea.append(msg); // append the message to textarea too
@@ -111,19 +132,23 @@ class MessageWindow extends Frame implements ActionListener
     } // end of addMessage
 
 
-    public void processWindowEvent(WindowEvent winevt) {
+    public void processWindowEvent(WindowEvent winevt)
+    {
 
         // check if the event is widow_destroy
-        if( winevt.getID() == Event.WINDOW_DESTROY ) {
+        if (winevt.getID() == Event.WINDOW_DESTROY)
+        {
             this.dispose();
         }
-        super.processWindowEvent(winevt);  // parent class need to handle the event too
+        super.processWindowEvent(winevt); // parent class need to handle the
+                                          // event too
 
     } // end of processWindowEvent
 
 
-    private void save_content() {
-        FileDialog fd = new FileDialog(this, "Save File", FileDialog.SAVE);
+    private void save_content()
+    {
+        FileDialog fd = new FileDialog(this,"Save File",FileDialog.SAVE);
         fd.setFile(default_savename);
         fd.setVisible(true);
 
@@ -131,56 +156,57 @@ class MessageWindow extends Frame implements ActionListener
         String path = fd.getDirectory();
         String file = fd.getFile();
 
-        if(path == null || file == null) return ;
+        if (path == null || file == null)
+            return;
 
         String pathname = path + file;
 
         System.out.println("Saving  " + pathname);
-        try {
+        try
+        {
             FileOutputStream target = new FileOutputStream(pathname);
             String content = MessageArea.getText();
-            byte [] buf = new byte[content.length()];
+            byte[] buf = new byte[content.length()];
             buf = content.getBytes("8859_1");
             target.write(buf);
             target.flush();
             target.close();
             System.out.println(content);
         }
-        catch (IOException e) {
+        catch (IOException e)
+        {
             System.out.println("Failed: " + e);
         }
-        finally {
+        finally
+        {
 
         }
 
     }
 
-    private void print_content() {
+    private void print_content()
+    {
 
-        if(pcount++ > 99) pcount = 0;
+        if (pcount++ > 99)
+            pcount = 0;
 
-        PrintJob pj = getToolkit().getPrintJob(this, "server print_" + pcount, null);
+        PrintJob pj = getToolkit().getPrintJob(this,"server print_" + pcount,null);
 
-        if(pj != null) {
+        if (pj != null)
+        {
             pg = pj.getGraphics();
         }
-        else return;
+        else
+            return;
 
 
 
         /*
-        pg.setFont(cur_font); // ok
-        int x = 0;
-        int y = 0;
-
-        String fcontent    = MessageArea.getText();
-        StringTokenizer st = new StringTokenizer(fcontent, "\n");
-
-        while (st.hasMoreTokens()) {
-              pg.drawString(st.nextToken(), x, y);
-              y += font_height;
-        }
-        */
+         * pg.setFont(cur_font); // ok int x = 0; int y = 0; String fcontent =
+         * MessageArea.getText(); StringTokenizer st = new
+         * StringTokenizer(fcontent, "\n"); while (st.hasMoreTokens()) {
+         * pg.drawString(st.nextToken(), x, y); y += font_height; }
+         */
         printAll(pg);
         pg.dispose();
         pj.end();
@@ -189,22 +215,28 @@ class MessageWindow extends Frame implements ActionListener
     }
 
 
-    public void paint (Graphics g) {
-        if(cur_font == null) cur_font = g.getFont();
-        if(font_height == 0)
+    public void paint(Graphics g)
+    {
+        if (cur_font == null)
+            cur_font = g.getFont();
+        if (font_height == 0)
             font_height = g.getFontMetrics(g.getFont()).getHeight();
         super.paint(g);
     }
 
-    public void actionPerformed(ActionEvent actevt) {
+    public void actionPerformed(ActionEvent actevt)
+    {
 
         Object evtsrc = actevt.getSource();
 
-        if(evtsrc == mi_save ) save_content();
-        else if(evtsrc == mi_print ) print_content();
-        else if(evtsrc == mi_exit  ) {
+        if (evtsrc == mi_save)
+            save_content();
+        else if (evtsrc == mi_print)
+            print_content();
+        else if (evtsrc == mi_exit)
+        {
             // a trick here, calls processWindowEvent
-            processWindowEvent(new WindowEvent(this, Event.WINDOW_DESTROY));
+            processWindowEvent(new WindowEvent(this,Event.WINDOW_DESTROY));
         }
 
     }
