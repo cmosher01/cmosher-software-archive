e8edd4cdc6c701fc10552de0639788b7211c0784
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6914 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ServerInterface.java
+++ javaproxy/src/ServerInterface.java
@@ -1,22 +1,29 @@
 /**
- * @(#)ServerInterface.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)ServerInterface.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR
+ * OWN RISK. Author : Steve Yeong-Ching Hsueh
  */
 
 import java.util.Properties;
 
-interface ServerInterface {
+
+
+interface ServerInterface
+{
     public void showServerResponse(String msg);
+
     public void showClientRequest(String msg);
+
     public void updateHTTPCounter();
+
     public void logAccess(int level, String src, String msg);
+
     public void logError(String src, String msg);
+
     public boolean re_initialization();
+
     public Properties getHTTPConfiguration();
+
     public ProxyCachePool getProxyCachePool();
 }
