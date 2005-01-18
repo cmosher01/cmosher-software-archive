f6ccc319f548a5c897eeb8229235b7324b37291d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6910 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/FileViewer.java
+++ javaproxy/src/FileViewer.java
@@ -1,10 +1,7 @@
 /**
- * @(#)FileViewer.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)FileViewer.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -17,43 +14,53 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+
+
 /**
  * FileViewer display file content in a frame window
  */
-public class FileViewer extends Frame {
+public class FileViewer extends Frame
+{
 
     /**
      * constructor
      */
-    public FileViewer(String filename) {
-        super("Content of " + filename );
+    public FileViewer(String filename)
+    {
+        super("Content of " + filename);
         File f = new File(filename);
-        byte [] buffer = null;
+        byte[] buffer = null;
 
-        try {
-            int fsize = (int) f.length();
+        try
+        {
+            int fsize = (int)f.length();
             buffer = new byte[fsize];
             int bytes_read = 0;
 
             FileInputStream in = new FileInputStream(f);
-            while(bytes_read < fsize) bytes_read += in.read(buffer, bytes_read, fsize-bytes_read);
+            while (bytes_read < fsize)
+                bytes_read += in.read(buffer,bytes_read,fsize - bytes_read);
         }
-        catch (IOException e) { }
+        catch (IOException e)
+        {
+        }
 
-        TextArea ta = new TextArea(new String(buffer), 30, 42);
+        TextArea ta = new TextArea(new String(buffer),30,42);
         ta.setEditable(false);
-        add("Center", ta);
+        add("Center",ta);
         pack();
         show();
-        enableEvents(AWTEvent.WINDOW_EVENT_MASK );
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
 
     }
 
     /**
      * event handler for window events
      */
-    public void processWindowEvent(WindowEvent winevt) {
-        if( winevt.getID() == Event.WINDOW_DESTROY ) this.setVisible(false);
+    public void processWindowEvent(WindowEvent winevt)
+    {
+        if (winevt.getID() == Event.WINDOW_DESTROY)
+            this.setVisible(false);
     }
 
 }
