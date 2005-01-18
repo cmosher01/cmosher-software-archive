7f4529ab9e6f3f3d21d327b620e3c2a0ee85e620
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:37:48 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6886 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/AboutDialog.java
+++ javaproxy/src/AboutDialog.java
@@ -8,10 +8,19 @@
  * Author : Steve Yeong-Ching Hsueh
  */
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import java.lang.*;
+import java.awt.AWTEvent;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dialog;
+import java.awt.Event;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.util.Enumeration;
+import java.util.Properties;
 
 /**
  * This class shows the About dialog box
