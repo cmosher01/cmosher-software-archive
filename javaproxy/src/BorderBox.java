dd3d0dc36952ef0f1a0d53a0f5876baebbc9c08d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6917 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/BorderBox.java
+++ javaproxy/src/BorderBox.java
@@ -1,59 +1,67 @@
 /**
- * @(#)BorderBox.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)BorderBox.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
 import java.awt.Component;
 import java.awt.Rectangle;
 
+
+
 /**
- * BorderBox is an object that sets the border properties
- * in the configuration window
+ * BorderBox is an object that sets the border properties in the configuration
+ * window
  */
-class BorderBox extends Component {
+class BorderBox extends Component
+{
     public String title;
+
     public Rectangle border;
+
+
 
     /**
      * constructor
      */
-    public BorderBox(String t, int x, int y, int w, int h) {
-        title  = t;
-        border = new Rectangle(x, y, w, h);
+    public BorderBox(String t, int x, int y, int w, int h)
+    {
+        title = t;
+        border = new Rectangle(x,y,w,h);
     }
 
     /**
      * constructor
      */
-    public BorderBox(String t, Rectangle b) {
-        title  = t;
+    public BorderBox(String t, Rectangle b)
+    {
+        title = t;
         border = b;
     }
 
     /**
      * set title
      */
-    public void setTitle(String t) {
-        title  = t;
+    public void setTitle(String t)
+    {
+        title = t;
     }
 
     /**
      * set border
      */
-    public void setBorder(Rectangle b) {
+    public void setBorder(Rectangle b)
+    {
         border = b;
     }
 
     /**
      * set title and border
      */
-    public void setTitleBorder(String t, Rectangle b) {
-        title  = t;
+    public void setTitleBorder(String t, Rectangle b)
+    {
+        title = t;
         border = b;
     }
 }
