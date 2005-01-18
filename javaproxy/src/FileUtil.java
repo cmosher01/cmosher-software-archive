23ab69f0a15a875af79c50222cbdc5993c8e7e33
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6920 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/FileUtil.java
+++ javaproxy/src/FileUtil.java
@@ -1,40 +1,47 @@
 /**
- * @(#)FileUtil.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)FileUtil.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF
+ * THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
+
+
 /**
  * common file utilities
  */
-public class FileUtil {
+public class FileUtil
+{
 
-    public FileUtil() { }
+    public FileUtil()
+    {
+    }
 
     /**
      * read file and return its content in a String
      */
-    public static String readFile(String filename) {
+    public static String readFile(String filename)
+    {
         StringBuffer content = new StringBuffer();
-        try {
+        try
+        {
             FileInputStream fs = new FileInputStream(filename);
             byte buf[] = null;
-            int  len   = 0;
-            while ((len = fs.available()) > 0) {
+            int len = 0;
+            while ((len = fs.available()) > 0)
+            {
                 buf = new byte[len];
                 fs.read(buf);
                 content.append(new String(buf));
             }
             fs.close();
         }
-        catch(IOException e) { }
+        catch (IOException e)
+        {
+        }
 
         return content.toString();
     }
