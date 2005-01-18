d62bf013457c41b0e96cdca0c99365f25c9ce763
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6913 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/AboutDialog.java
+++ javaproxy/src/AboutDialog.java
@@ -1,10 +1,7 @@
 /**
- * @(#)AboutDialog.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)AboutDialog.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -22,71 +19,89 @@ import java.awt.event.WindowEvent;
 import java.util.Enumeration;
 import java.util.Properties;
 
+
+
 /**
- * This class shows the About dialog box
- * The dialog box display information about the system as well
- * as the java vm.
+ * This class shows the About dialog box The dialog box display information
+ * about the system as well as the java vm.
  */
-class AboutDialog extends Dialog implements ActionListener{
+class AboutDialog extends Dialog implements ActionListener
+{
 
-    public static final String _JAVA_VERSION  = "java.version";
-    public static final String _JAVA_VENDOR   = "java.vendor";
+    public static final String _JAVA_VERSION = "java.version";
+
+    public static final String _JAVA_VENDOR = "java.vendor";
+
     public static final String _USER_TIMEZONE = "user.timezone";
-    public static final String _OS_ARCH       = "os.arch";
-    public static final String _OS_NAME       = "os.name";
-    public static final String _OS_VERSION    = "os.version";
-    public static final String _USER_NAME     = "user.name";
-    public static final String _USER_HOME     = "user.home";
-    public static final String _USER_DIR      = "user.dir";
 
-    public static final String _VERSION    = "Version 0.1 Beta";
+    public static final String _OS_ARCH = "os.arch";
+
+    public static final String _OS_NAME = "os.name";
+
+    public static final String _OS_VERSION = "os.version";
+
+    public static final String _USER_NAME = "user.name";
+
+    public static final String _USER_HOME = "user.home";
+
+    public static final String _USER_DIR = "user.dir";
+
+    public static final String _VERSION = "Version 0.1 Beta";
 
     Button ok;
 
     Properties p = System.getProperties();
+
     static Enumeration property_keys;
+
     static String system_properties = null;
+
     MultiLineLabel system_info;
+
     String key;
 
-    public AboutDialog(Frame parent) {  // constructor
-
-        super(parent, "About Java HTTP Proxy Server", true);
-        this.setLayout(new BorderLayout(30, 30));
 
 
-        ok = new Button("OK");        // create 'OK' button
-        ok.addActionListener(this);   // add this to actionlistener list
+    public AboutDialog(Frame parent)
+    { // constructor
+
+        super(parent,"About Java HTTP Proxy Server",true);
+        this.setLayout(new BorderLayout(30,30));
+
+
+        ok = new Button("OK"); // create 'OK' button
+        ok.addActionListener(this); // add this to actionlistener list
 
 
 
-        if( system_properties == null ) {
+        if (system_properties == null)
+        {
             system_properties = " \nSystem Infomation:\n \n";
-            system_properties += ("Java Version" + " : " + "JDK "+ p.getProperty(_JAVA_VERSION) +"\n");
-            system_properties += ("Java Vendor" + " : " + p.getProperty(_JAVA_VENDOR)+"\n");
-            system_properties += ("Local Timezone " + " : " + p.getProperty(_USER_TIMEZONE)+"\n");
-            system_properties += ("Hardware Architecture"  + " : " + p.getProperty(_OS_ARCH)+"\n");
-            system_properties += ("Operating System" + " : " + p.getProperty(_OS_NAME)+"\n");
-            system_properties += ("OS Version" + " : " + p.getProperty(_OS_VERSION)+"\n");
-            system_properties += ("User Name" + " : " + p.getProperty(_USER_NAME)+"\n");
-            system_properties += ("User Home Dir" + " : " + p.getProperty(_USER_HOME)+"\n");
-            system_properties += ("Current Dir" + " : " + p.getProperty(_USER_DIR)+"\n");
+            system_properties += ("Java Version" + " : " + "JDK " + p.getProperty(_JAVA_VERSION) + "\n");
+            system_properties += ("Java Vendor" + " : " + p.getProperty(_JAVA_VENDOR) + "\n");
+            system_properties += ("Local Timezone " + " : " + p.getProperty(_USER_TIMEZONE) + "\n");
+            system_properties += ("Hardware Architecture" + " : " + p.getProperty(_OS_ARCH) + "\n");
+            system_properties += ("Operating System" + " : " + p.getProperty(_OS_NAME) + "\n");
+            system_properties += ("OS Version" + " : " + p.getProperty(_OS_VERSION) + "\n");
+            system_properties += ("User Name" + " : " + p.getProperty(_USER_NAME) + "\n");
+            system_properties += ("User Home Dir" + " : " + p.getProperty(_USER_HOME) + "\n");
+            system_properties += ("Current Dir" + " : " + p.getProperty(_USER_DIR) + "\n");
             system_properties += " \nJava HTTP Proxy Server, " + _VERSION + "\nby Steve Y.C. Hsueh,  April 1998";
         } // end of if
 
         //  this.add("North", new Label("System Infomation:"));
-        system_info = new MultiLineLabel(system_properties, 10, 10);
-        this.add("Center", system_info);
+        system_info = new MultiLineLabel(system_properties,10,10);
+        this.add("Center",system_info);
 
         Panel pnl = new Panel();
-        pnl.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        pnl.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
         pnl.add(ok);
 
         //System.out.println(system_properties);
         this.add("South",pnl);
         resize(300,350);
 
-        enableEvents(AWTEvent.WINDOW_EVENT_MASK );
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
 
 
     } // end of public AboutDialog(Frame parent)
@@ -95,9 +110,11 @@ class AboutDialog extends Dialog implements ActionListener{
     /**
      * event handler for action events
      */
-    public void actionPerformed(ActionEvent evt) {
+    public void actionPerformed(ActionEvent evt)
+    {
 
-        if((evt.getActionCommand()).equals("OK")) {
+        if ((evt.getActionCommand()).equals("OK"))
+        {
             this.dispose();
         }
 
@@ -106,10 +123,12 @@ class AboutDialog extends Dialog implements ActionListener{
     /**
      * event handler for window event
      */
-    public void processWindowEvent(WindowEvent winevt) {
+    public void processWindowEvent(WindowEvent winevt)
+    {
 
-            // check if the event is widow_destroy
-           if( winevt.getID() == Event.WINDOW_DESTROY ) this.dispose();
+        // check if the event is widow_destroy
+        if (winevt.getID() == Event.WINDOW_DESTROY)
+            this.dispose();
 
     }
 
