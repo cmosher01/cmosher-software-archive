2ce6ca426ab9303b68499b3d8edc59ed026a17c9
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 17:27:58 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6848 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/HostsFile.java
+++ javaproxy/src/HostsFile.java
@@ -1,3 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 /*
  * Created on Jan 18, 2005
  */
@@ -6,9 +15,34 @@
 /**
  * TODO
  * 
- * @author chrism
+ * @author Chris Mosher
  */
 public class HostsFile
 {
+    Map map = new HashMap();
 
+    public void loadFromFile() throws IOException
+    {
+        String sf = "C:\\winnt\\system32\\drivers\\etc\\hosts";
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(sf))));
+        for (String s = in.readLine(); in != null; s = in.readLine())
+        {
+            StringTokenizer st = new StringTokenizer(s);
+            String ip = st.nextToken();
+            while (st.hasMoreTokens())
+            {
+                String host = st.nextToken().toLowerCase();
+                map.put(host, ip);
+            }
+        }
+    }
+
+    public String lookupHost(String host)
+    {
+        if (map.containsKey(host))
+        {
+            return (String)map.get(host);
+        }
+        return "";
+    }
 }
