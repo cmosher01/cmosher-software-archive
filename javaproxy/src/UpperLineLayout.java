5617a3dde68c314dfe27f26602415dd0d018012f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6905 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/UpperLineLayout.java
+++ javaproxy/src/UpperLineLayout.java
@@ -1,11 +1,8 @@
 /**
- * @(#)UpperLineLayout.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)UpperLineLayout.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR
+ * OWN RISK. Author : Steve Yeong-Ching Hsueh
  */
 
 import java.awt.Component;
@@ -16,125 +13,155 @@ import java.awt.LayoutManager;
 import java.awt.Rectangle;
 import java.io.Serializable;
 
-/**
- * UpperLineLayout is the LayoutManager for the buttons on the upper part
- * of the jproxy main window
- */
-public class UpperLineLayout implements LayoutManager, Serializable {
 
-    private int minWidth  = 0;
+
+/**
+ * UpperLineLayout is the LayoutManager for the buttons on the upper part of the
+ * jproxy main window
+ */
+public class UpperLineLayout implements LayoutManager, Serializable
+{
+
+    private int minWidth = 0;
+
     private int minHeight = 0;
+
     private int preferredWidth = 0;
-    private int preferredHeight= 0;
+
+    private int preferredHeight = 0;
+
     private boolean sizeset = false;
-    private int maxComponentWidth  = 0;
+
+    private int maxComponentWidth = 0;
+
     private int maxComponentHeight = 0;
+
     private int vgap, hgap, rgap, lgap, cgap;
+
+
 
     /**
      * constructor
      */
-    public UpperLineLayout() {
+    public UpperLineLayout()
+    {
         vgap = hgap = rgap = lgap = cgap = 5;
     }
 
     /**
      * addLayoutComponent
      */
-    public void addLayoutComponent(String name, Component comp) {
+    public void addLayoutComponent(String name, Component comp)
+    {
     }
 
     /**
      * removeLayoutComponent
      */
-    public void removeLayoutComponent(Component comp) {
+    public void removeLayoutComponent(Component comp)
+    {
     }
 
     /**
      * set sizes
      */
-    public void setSizes(Container parent) {
+    public void setSizes(Container parent)
+    {
         int n, i;
         Component comp;
         Dimension dim;
 
-        if( sizeset ) return;
+        if (sizeset)
+            return;
         n = parent.countComponents();
 
-        for(i=0; i< n; i++) {
+        for (i = 0; i < n; i++)
+        {
             comp = parent.getComponent(i);
-            if(comp.isVisible()) {
+            if (comp.isVisible())
+            {
                 dim = comp.preferredSize();
-                maxComponentWidth  = Math.max(maxComponentWidth, dim.width);
-                maxComponentHeight = Math.max(maxComponentHeight, dim.height);
+                maxComponentWidth = Math.max(maxComponentWidth,dim.width);
+                maxComponentHeight = Math.max(maxComponentHeight,dim.height);
                 preferredHeight += dim.height;
             }
         }
 
         preferredWidth = maxComponentWidth;
         preferredHeight += maxComponentHeight;
-        minWidth  = preferredWidth ;
+        minWidth = preferredWidth;
         minHeight = preferredHeight;
-        sizeset   = true;
+        sizeset = true;
     }
 
     /**
      * calculate the preferred layout size
      */
-    public Dimension preferredLayoutSize(Container parent) {
-        Dimension dim = new Dimension(0, 0);
+    public Dimension preferredLayoutSize(Container parent)
+    {
+        Dimension dim = new Dimension(0,0);
         setSizes(parent);
 
         Rectangle rec = parent.getBounds();
         Insets insets = parent.insets();
-        if( rec.width <= 0 || rec.height <= 0 ) {
+        if (rec.width <= 0 || rec.height <= 0)
+        {
             dim.width = preferredWidth + insets.left + insets.right;
-            dim.height= preferredHeight+ insets.top  + insets.bottom;
+            dim.height = preferredHeight + insets.top + insets.bottom;
             return dim;
         }
 
-        dim.width = rec.width ;
-        dim.height= rec.height;
-        //System.out.println(insets.left + ":" + insets.right + ":" + insets.top + ":" + insets.bottom );
-        //System.out.println("width = " + dim.width + "height = " + dim.height);
+        dim.width = rec.width;
+        dim.height = rec.height;
+        //System.out.println(insets.left + ":" + insets.right + ":" +
+        // insets.top + ":" + insets.bottom );
+        //System.out.println("width = " + dim.width + "height = " +
+        // dim.height);
         return dim;
     }
 
     /**
      * calculate the minimum layout size
      */
-    public Dimension minimumLayoutSize(Container parent) {
-        Dimension dim = new Dimension(0, 0);
+    public Dimension minimumLayoutSize(Container parent)
+    {
+        Dimension dim = new Dimension(0,0);
         setSizes(parent);
         Insets insets = parent.insets();
         dim.width = minWidth + insets.left + insets.right;
-        dim.height= minHeight+ insets.top  + insets.bottom;
+        dim.height = minHeight + insets.top + insets.bottom;
         return dim;
     }
 
     /**
      * layout container
      */
-    public void layoutContainer(Container parent) {
+    public void layoutContainer(Container parent)
+    {
         Insets insets = parent.insets();
-        int containerWidth  = parent.size().width - insets.left - insets.right;
+        int containerWidth = parent.size().width - insets.left - insets.right;
         int containerHeight = parent.size().height - insets.top - insets.bottom;
-        int n, i, cxpos = 0, cypos = 0;  // component's x/y position
+        int n, i, cxpos = 0, cypos = 0; // component's x/y position
 
         setSizes(parent);
         cypos = insets.top + vgap;
-        if(( n=parent.countComponents()) <= 0 ) return;
-        for( i = 0; i < n; i++ ) {
+        if ((n = parent.countComponents()) <= 0)
+            return;
+        for (i = 0; i < n; i++)
+        {
             Component comp = parent.getComponent(i);
-            if(comp.isVisible()) {
+            if (comp.isVisible())
+            {
 
                 Dimension d = comp.getPreferredSize();
                 //comp.setSize(d.width, d.height);
 
-                //System.out.println("drawing " + cxpos +":"+ cypos +":"+d.width+":"+d.height);
+                //System.out.println("drawing " + cxpos +":"+ cypos
+                // +":"+d.width+":"+d.height);
                 cxpos += (d.width + cgap);
-                if(d.width <= 1 ) continue;
-                comp.setBounds(cxpos, cypos ,d.width, d.height);
+                if (d.width <= 1)
+                    continue;
+                comp.setBounds(cxpos,cypos,d.width,d.height);
             }
         }
 
