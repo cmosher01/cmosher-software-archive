7ab4e952f89fe22145376bdf190e8053ebb1c491
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:51:20 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6971 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ProxyConnection.java
+++ javaproxy/src/ProxyConnection.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
-import java.net.URL;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
