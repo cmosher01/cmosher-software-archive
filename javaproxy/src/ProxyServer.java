5f2116e9abba2fa51c95aa9b921b4f10898aaa6c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6928 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ProxyServer.java
+++ javaproxy/src/ProxyServer.java
@@ -1,10 +1,7 @@
 /**
- * @(#)ProxyServer.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)ProxyServer.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -14,27 +11,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
+
 /**
  * ProxyServer
  */
-public class ProxyServer implements Runnable {
+public class ProxyServer implements Runnable
+{
 
-    String  server_ip       = null;      // ip this server binds to, (JDK 1.1 only)
-    int     server_port     = 80;        // port this server listens on
-    int     max_connections = 8;         // maximum connection allowed
-    private Thread myThread;             // thread for this object
-    private ServerSocket ss;             // server socket
+    String server_ip = null; // ip this server binds to, (JDK 1.1 only)
+
+    int server_port = 80; // port this server listens on
+
+    int max_connections = 8; // maximum connection allowed
+
+    private Thread myThread; // thread for this object
+
+    private ServerSocket ss; // server socket
+
     private boolean server_running = false;
-    private boolean server_exit    = false;
-    Object  console;
-    final static String  localhost  = "127.0.0.1";
-    final static boolean debug_mode = false;   // for debugging
 
-    ProxyServer(Object p, String ip, int port) {
-        console     = p;
-        server_ip   = ip;
+    private boolean server_exit = false;
+
+    Object console;
+
+    final static String localhost = "127.0.0.1";
+
+    final static boolean debug_mode = false; // for debugging
+
+
+
+    ProxyServer(Object p, String ip, int port)
+    {
+        console = p;
+        server_ip = ip;
         server_port = port;
-        myThread    = new Thread(this);
+        myThread = new Thread(this);
         myThread.setDaemon(true);
         myThread.start();
     }
@@ -42,39 +53,45 @@ public class ProxyServer implements Runnable {
     /**
      * constructor
      */
-    ProxyServer(Object p, int port) {
-        this(p, localhost, port);
+    ProxyServer(Object p, int port)
+    {
+        this(p,localhost,port);
     }
 
     /**
      * test if the server is running
      */
-    public boolean isServerRunning() {
+    public boolean isServerRunning()
+    {
         return server_running;
     }
 
     /**
      * start server
      */
-    public synchronized boolean startServer() {
+    public synchronized boolean startServer()
+    {
 
-        if( server_running ) return true; // check if server is running already
+        if (server_running)
+            return true; // check if server is running already
 
-        try {
-            if( server_ip == null )
-                ss = new ServerSocket(server_port, max_connections);
+        try
+        {
+            if (server_ip == null)
+                ss = new ServerSocket(server_port,max_connections);
             else
-                ss = new ServerSocket(server_port, max_connections,
-                                      InetAddress.getByName(server_ip));
-                ss.setSoTimeout(1000); // set timeout to 1 second
+                ss = new ServerSocket(server_port,max_connections,InetAddress.getByName(server_ip));
+            ss.setSoTimeout(1000); // set timeout to 1 second
         }
-        catch(IOException e) {
+        catch (IOException e)
+        {
             ss = null;
-            if(debug_mode) System.out.println("Error: Can't set up server socket on " + server_ip + ":" + server_port + "\n" + e );
+            if (debug_mode)
+                System.out.println("Error: Can't set up server socket on " + server_ip + ":" + server_port + "\n" + e);
             return false;
         }
 
-        System.out.println("Server Started on " + server_ip + ", port: "+ server_port);
+        System.out.println("Server Started on " + server_ip + ", port: " + server_port);
         server_running = true;
         return true;
     }
@@ -82,24 +99,30 @@ public class ProxyServer implements Runnable {
     /**
      * stop server
      */
-    public synchronized boolean stopServer() {
+    public synchronized boolean stopServer()
+    {
 
         server_running = false;
-        try {
+        try
+        {
             ss.close();
         }
-        catch(IOException e) {
-            if(debug_mode) System.out.println("Can't close server socket");
+        catch (IOException e)
+        {
+            if (debug_mode)
+                System.out.println("Can't close server socket");
         }
         ss = null;
-        if(debug_mode) System.out.println("Server socket closed.");
+        if (debug_mode)
+            System.out.println("Server socket closed.");
         return true;
     }
 
     /**
      * set server_exit flag
      */
-    public void serverExit() {
+    public void serverExit()
+    {
 
         server_exit = true;
     }
@@ -107,19 +130,33 @@ public class ProxyServer implements Runnable {
     /**
      * run
      */
-    public void run() {
+    public void run()
+    {
 
-        while( !server_exit ) {
-            if( server_running ) {
-                try {
+        while (!server_exit)
+        {
+            if (server_running)
+            {
+                try
+                {
                     Socket s = ss.accept();
-                    new ProxyConnection( console, this, s );
-                    ((ServerInterface) console).updateHTTPCounter();
+                    new ProxyConnection(console,this,s);
+                    ((ServerInterface)console).updateHTTPCounter();
                 }
-                catch(IOException e) { }
+                catch (IOException e)
+                {
+                }
             }
-            else {
-                try{ myThread.sleep(500); } catch (InterruptedException e) { } ;
+            else
+            {
+                try
+                {
+                    myThread.sleep(500);
+                }
+                catch (InterruptedException e)
+                {
+                }
+                ;
             } // end of if( server_running )
 
         } // end of while( true )
