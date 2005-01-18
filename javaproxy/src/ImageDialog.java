80817f2ebb7da42015e296494768bb80367b0aed
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6923 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ImageDialog.java
+++ javaproxy/src/ImageDialog.java
@@ -1,10 +1,7 @@
 /**
- * @(#)ImageDialog.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)ImageDialog.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -19,74 +16,89 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.WindowEvent;
 
+
+
 /**
  * ImageDialog shows image as the background of a dialog box
  */
-class ImageDialog extends Dialog {
+class ImageDialog extends Dialog
+{
     Image image = null;
+
     String message = null;
+
+
 
     /**
      * constructor
      */
-    ImageDialog(Frame p, String title) {
-        super(p, title, true);
+    ImageDialog(Frame p, String title)
+    {
+        super(p,title,true);
         enableEvents(AWTEvent.WINDOW_EVENT_MASK);
     }
 
     /**
      * constructor
      */
-    ImageDialog(Frame p, String title, Image img, String msg) {
-        this(p, title);
-        this.image   = img;
+    ImageDialog(Frame p, String title, Image img, String msg)
+    {
+        this(p,title);
+        this.image = img;
         this.message = msg;
     }
 
     /**
      * set image
      */
-    public void setImage(Image img) {
+    public void setImage(Image img)
+    {
         this.image = img;
     }
 
     /**
      * set message
      */
-    public void setMessage(String msg) {
+    public void setMessage(String msg)
+    {
         this.message = msg;
     }
 
     /**
      * paint
      */
-    public void paint(Graphics g) {
+    public void paint(Graphics g)
+    {
         Insets in = insets();
         Dimension d = size();
         FontMetrics fm = g.getFontMetrics();
         int imgsx = 10;
-        int imgsy =  0;
+        int imgsy = 0;
         int msgsx = 10;
-        int msgsy = (int) ((d.height - in.top)/2) + in.top;
+        int msgsy = (int)((d.height - in.top) / 2) + in.top;
 
-        if( image != null ) {
-            int img_width = image.getWidth(this) ;
-            int img_height= image.getHeight(this);
+        if (image != null)
+        {
+            int img_width = image.getWidth(this);
+            int img_height = image.getHeight(this);
 
-            imgsy = msgsy - (int)(img_height/2);
+            imgsy = msgsy - (int)(img_height / 2);
             msgsx += img_width;
-            if(imgsy <= 0) return;
-            g.drawImage(image, imgsx, imgsy, this);
+            if (imgsy <= 0)
+                return;
+            g.drawImage(image,imgsx,imgsy,this);
         }
-        msgsy += fm.getAscent()/2;
-        g.drawString(message, msgsx, msgsy);
+        msgsy += fm.getAscent() / 2;
+        g.drawString(message,msgsx,msgsy);
     }
 
     /**
      * event handler
      */
-    public void processWindowEvent(WindowEvent winevt) {
-        if( winevt.getID() == Event.WINDOW_DESTROY ) this.dispose();
+    public void processWindowEvent(WindowEvent winevt)
+    {
+        if (winevt.getID() == Event.WINDOW_DESTROY)
+            this.dispose();
     }
 
 }
