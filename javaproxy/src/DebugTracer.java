54e9accf29c113883b45526931c95b9e91df6862
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6916 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/DebugTracer.java
+++ javaproxy/src/DebugTracer.java
@@ -1,10 +1,7 @@
 /**
- * @(#)DebugTracer.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)DebugTracer.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -15,99 +12,128 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
+
+
 /**
- * DebugTracer is used for tracking the calling functions in the stack.
- * It could be used to print out function names and line numbers for debugging.
+ * DebugTracer is used for tracking the calling functions in the stack. It could
+ * be used to print out function names and line numbers for debugging.
  */
-public class DebugTracer extends Throwable {
- PrintWriter pw;
- PipedOutputStream ppo = new PipedOutputStream();
- PipedInputStream  ppi;
+public class DebugTracer extends Throwable
+{
+    PrintWriter pw;
 
- /**
-  * constructor
-  */
- public DebugTracer() {
-    try {
-        ppi = new PipedInputStream(ppo);
-    }
-    catch (IOException e) { }
-    pw  = new PrintWriter(ppo, true);
- }
+    PipedOutputStream ppo = new PipedOutputStream();
 
- /**
-  * return the calling source
-  */
- public String getSource() {
+    PipedInputStream ppi;
 
-    Vector vt = getVectorStack(3);
-    if(vt == null) return "source unknown";
-    if(vt.size() < 2) return "source unknown";
-    return (String)vt.elementAt(2);
- }
 
- /**
-  * return the calling source
-  */
- public String getSource(int depth) {
 
-    Vector vt = getVectorStack(depth+2); // need to add this method's level
-    StringBuffer source = new StringBuffer();
-    if(vt.size() < 2) return "";
-    for(int i=2; i<vt.size(); i++) {
-        source.append((String)vt.elementAt(i)).append("\n");
-    }
-    return source.toString();
- }
-
- /**
-  * print current stack
-  */
- public void printStack(int depth) {
-
-    Vector vt = getVectorStack(depth+1);
-    for(int i=1;  i<vt.size(); i++) {
-     System.out.println((String)vt.elementAt(i));
-    }
-
- }
-
- /**
-  * return stack stored in a vector
-  */
- public Vector getVectorStack(int depth) {
-    byte   buf[] = new byte[2048];
-    String stack = "";
-    String temp  = "";
-    short  count = 0;
-    fillInStackTrace();
-    printStackTrace(pw);
-
-    try {
-        int avail;
-        while((avail = ppi.available()) > 0) {
-            ppi.read(buf, 0, avail);
-            stack += new String(buf, 0, avail);
+    /**
+     * constructor
+     */
+    public DebugTracer()
+    {
+        try
+        {
+            ppi = new PipedInputStream(ppo);
         }
+        catch (IOException e)
+        {
+        }
+        pw = new PrintWriter(ppo,true);
     }
-    catch (IOException e) { }
-    int l = 0;
-    // System.out.println(stack);
 
-    // not a safe way, may not work with all jdks
-    StringTokenizer st = new StringTokenizer(stack, "\n");
-    Vector vt = new Vector();
-    while (st.hasMoreTokens()) {
+    /**
+     * return the calling source
+     */
+    public String getSource()
+    {
 
-        temp = st.nextToken();
-        if(++count < 2) continue;              // don't count the first 2 level
-        l = temp.indexOf("at ");               // search for "at "
-        if(l >= 0) temp = temp.substring(l+3); // skip  over "at "
-        vt.addElement(temp);
-        if(count >= depth+1 ) break;           // check depth; first 2 level doesn't count
+        Vector vt = getVectorStack(3);
+        if (vt == null)
+            return "source unknown";
+        if (vt.size() < 2)
+            return "source unknown";
+        return (String)vt.elementAt(2);
     }
-    return vt;
- }
+
+    /**
+     * return the calling source
+     */
+    public String getSource(int depth)
+    {
+
+        Vector vt = getVectorStack(depth + 2); // need to add this method's
+                                               // level
+        StringBuffer source = new StringBuffer();
+        if (vt.size() < 2)
+            return "";
+        for (int i = 2; i < vt.size(); i++)
+        {
+            source.append((String)vt.elementAt(i)).append("\n");
+        }
+        return source.toString();
+    }
+
+    /**
+     * print current stack
+     */
+    public void printStack(int depth)
+    {
+
+        Vector vt = getVectorStack(depth + 1);
+        for (int i = 1; i < vt.size(); i++)
+        {
+            System.out.println((String)vt.elementAt(i));
+        }
+
+    }
+
+    /**
+     * return stack stored in a vector
+     */
+    public Vector getVectorStack(int depth)
+    {
+        byte buf[] = new byte[2048];
+        String stack = "";
+        String temp = "";
+        short count = 0;
+        fillInStackTrace();
+        printStackTrace(pw);
+
+        try
+        {
+            int avail;
+            while ((avail = ppi.available()) > 0)
+            {
+                ppi.read(buf,0,avail);
+                stack += new String(buf,0,avail);
+            }
+        }
+        catch (IOException e)
+        {
+        }
+        int l = 0;
+        // System.out.println(stack);
+
+        // not a safe way, may not work with all jdks
+        StringTokenizer st = new StringTokenizer(stack,"\n");
+        Vector vt = new Vector();
+        while (st.hasMoreTokens())
+        {
+
+            temp = st.nextToken();
+            if (++count < 2)
+                continue; // don't count the first 2 level
+            l = temp.indexOf("at "); // search for "at "
+            if (l >= 0)
+                temp = temp.substring(l + 3); // skip over "at "
+            vt.addElement(temp);
+            if (count >= depth + 1)
+                break; // check depth; first 2 level doesn't count
+        }
+        return vt;
+    }
 
 
 
