bf0bdffafe9ae4b3b4a3c72f6be84edfafa44d56
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6909 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ServerTest.java
+++ javaproxy/src/ServerTest.java
@@ -1,10 +1,7 @@
 /**
- * @(#)ServerTest.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)ServerTest.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
@@ -12,37 +9,51 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+
+
 /**
  * ServerTest is an application the works as an HTTP client
  */
-public class ServerTest {
+public class ServerTest
+{
     String host = "127.0.0.1";
-    int    port = 2004;
+
+    int port = 2004;
+
     int MAX_TXN = 25;
 
-    public ServerTest(int txn) {
-        Socket sock ;
+
+
+    public ServerTest(int txn)
+    {
+        Socket sock;
         DataOutputStream dout;
         //String msg = "GET / HTTP/1.0\n\n";
         String msg = "Hello\n\n";
         int sleeptime = 200;
-        if(txn > 0) MAX_TXN = txn;
-        for(int i=0; i<MAX_TXN; i++) {
-            try{
+        if (txn > 0)
+            MAX_TXN = txn;
+        for (int i = 0; i < MAX_TXN; i++)
+        {
+            try
+            {
                 Thread.sleep(sleeptime);
-                sock = new Socket(host, port);
+                sock = new Socket(host,port);
                 dout = new DataOutputStream(sock.getOutputStream());
                 dout.writeBytes(msg);
                 dout.flush();
-                // System.out.println(i + "  sending " + msg);
+                // System.out.println(i + " sending " + msg);
                 sock.close();
             }
-            catch(IOException e) {
+            catch (IOException e)
+            {
                 //System.exit(0);
-                sleeptime+=10;
-                System.out.println("sleeptime=" + sleeptime +"\nI/O error : " + e );
+                sleeptime += 10;
+                System.out.println("sleeptime=" + sleeptime + "\nI/O error : " + e);
             }
-            catch(InterruptedException e) { }
+            catch (InterruptedException e)
+            {
+            }
 
             //System.gc();
         }
@@ -51,10 +62,12 @@ public class ServerTest {
     /**
      * main, argv is the number of requests to send
      */
-    public static void main(String argv[]) {
+    public static void main(String argv[])
+    {
         int txn = 0;
         System.out.println(argv.length + ":" + argv[0]);
-        if(argv.length >= 1 && argv[0] != null) txn = Integer.parseInt(argv[0]);
+        if (argv.length >= 1 && argv[0] != null)
+            txn = Integer.parseInt(argv[0]);
 
         ServerTest st = new ServerTest(txn);
     }
