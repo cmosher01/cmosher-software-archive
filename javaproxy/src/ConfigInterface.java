f72922d44b5f025c9ee505692c9a612bfc7c56ad
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6930 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ConfigInterface.java
+++ javaproxy/src/ConfigInterface.java
@@ -1,17 +1,15 @@
 /**
- * @(#)ConfigInterface.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
- * Author : Steve Yeong-Ching Hsueh
+ * @(#)ConfigInterface.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE
+ * LIABLE FOR ANY DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR
+ * OWN RISK. Author : Steve Yeong-Ching Hsueh
  */
 
 /**
  * interface for configuration class
  */
-interface ConfigInterface {
+interface ConfigInterface
+{
     public void updateConfiguration();
 }
 
