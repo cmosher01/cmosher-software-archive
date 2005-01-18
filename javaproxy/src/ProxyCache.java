176cdb3dbbcfaa25e5e56d10f966797aa749e29f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-01-18 18:38:04 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@6918 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- javaproxy/src/ProxyCache.java
+++ javaproxy/src/ProxyCache.java
@@ -1,111 +1,130 @@
 /**
- * @(#)ProxyCache.java	April 3, 1998
- *
- * I MAKE NO WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER
- * EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY DAMAGES THIS
- * SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
- *
+ * @(#)ProxyCache.java April 3, 1998 I MAKE NO WARRANTIES ABOUT THE SUITABILITY
+ * OF THIS SOFTWARE, EITHER EXPRESS OR IMPLIED AND SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES THIS SOFTWARE MAY BRING TO YOUR SYSTEM. USE IT AT YOUR OWN RISK.
  * Author : Steve Yeong-Ching Hsueh
  */
 
 import java.util.Date;
 
+
+
 /**
  * ProxyCache stores the server response of an url
  */
-public class ProxyCache {
+public class ProxyCache
+{
     String url = "";
-    Date   expiration;
-    byte   header[]  = null;
-    byte   content[] = null;
+
+    Date expiration;
+
+    byte header[] = null;
+
+    byte content[] = null;
+
+
 
     /**
      * constructor
      */
-    ProxyCache() { }
+    ProxyCache()
+    {
+    }
 
     /**
      * test if this cache is expired
      */
-    public boolean isExpired(Date d) {
+    public boolean isExpired(Date d)
+    {
         return expiration.after(d);
     }
 
     /**
      * set expiration date/time
      */
-    public void setExpiration(Date d) {
+    public void setExpiration(Date d)
+    {
         expiration = d;
     }
 
     /**
      * set expiration date/time
      */
-    public void setExpiration(long d) {
+    public void setExpiration(long d)
+    {
         expiration = new Date(d);
     }
 
     /**
      * get expiration date/time
      */
-    public Date getExpiration() {
+    public Date getExpiration()
+    {
         return expiration;
     }
 
     /**
      * set url
      */
-    public void setURL(String newurl) {
+    public void setURL(String newurl)
+    {
         url = newurl;
     }
 
     /**
      * set server response header
      */
-    public void setHeader(String h) {
+    public void setHeader(String h)
+    {
         header = h.getBytes();
     }
 
     /**
      * set server response header
      */
-    public void setHeader(byte h[]) {
+    public void setHeader(byte h[])
+    {
         header = h;
     }
 
     /**
      * set server response content
      */
-    public void setContent(String c) {
+    public void setContent(String c)
+    {
         content = c.getBytes();
     }
 
     /**
      * set server response content
      */
-    public void setContent(byte c[]) {
+    public void setContent(byte c[])
+    {
         content = c;
     }
 
     /**
      * get server response header
      */
-    public byte[] getHeader() {
+    public byte[] getHeader()
+    {
         return header;
     }
 
     /**
      * get server response content
      */
-    public byte[] getContent() {
+    public byte[] getContent()
+    {
         return content;
     }
 
     /**
      * test if the url match this cached url
      */
-    public boolean matchURL(String target) {
-        return  url.equals(target);
+    public boolean matchURL(String target)
+    {
+        return url.equals(target);
     }
 
 }
