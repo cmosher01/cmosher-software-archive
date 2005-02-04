3cbf58727c2b5d0b6a053ddc49c23ffc61d7a0af
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-04 18:33:31 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7031 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- swingapp/src/nu/mine/mosher/swingapp/SwingGUI.java
+++ swingapp/src/nu/mine/mosher/swingapp/SwingGUI.java
@@ -3,15 +3,40 @@
  */
 package nu.mine.mosher.ja2;
 
+import javax.swing.JFrame;
+
 /**
  * @author Chris Mosher
  */
 public class GUI
 {
+    private JFrame mFrame;
+
     /**
      * 
      */
     public void create()
     {
+        // Use look and feel for current OS.
+        SwingUtil.useOSLookAndFeel();
+
+        // Use look and feel's (not OS's) decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        // Create the window.
+        mFrame = new JFrame("Metadata Viewer");
+
+        // Closing the window exits the program.
+        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create and set up the content pane.
+        mFrame.setContentPane(new MetadataViewerContentPane());
+
+        // Set the window's size and position.
+        mFrame.pack();
+        mFrame.setLocationRelativeTo(null);
+
+        // Display the window.
+        mFrame.setVisible(true);
     }
 }
