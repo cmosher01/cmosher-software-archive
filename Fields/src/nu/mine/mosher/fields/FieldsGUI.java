8327434a182e91704ad56b5a524b9fd4ea5611c3
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-08 18:06:33 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7234 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- Fields/src/nu/mine/mosher/fields/FieldsGUI.java
+++ Fields/src/nu/mine/mosher/fields/FieldsGUI.java
@@ -19,6 +19,8 @@ import javax.swing.UIManager;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
+import nu.mine.mosher.fields.BigTableModel;
+
 /**
  * @author Chris Mosher
  */
