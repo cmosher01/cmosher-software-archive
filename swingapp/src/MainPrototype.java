5cd5d5ea2eec773db1ff723a389f949f2f13bce7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-08 17:54:31 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7215 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- swingapp/src/MainPrototype.java
+++ swingapp/src/MainPrototype.java
@@ -1,3 +1,9 @@
+import nu.mine.mosher.swingapp.ApplicationAborting;
+import nu.mine.mosher.swingapp.CommandLineArgHandler;
+import nu.mine.mosher.swingapp.ExceptionHandler;
+import nu.mine.mosher.swingapp.SwingApplication;
+import nu.mine.mosher.swingapp.SwingGUI;
+
 /*
  * Created on Feb 8, 2005
  */
@@ -9,7 +15,26 @@
  */
 public class MainPrototype
 {
-    public static void main(String[] args)
+    /**
+     * This class is never instantiated.
+     */
+    private MainPrototype()
     {
+        assert false : "Cannot instantiate Ja2 class.";
+    }
+
+    /**
+     * @param args
+     * @throws ApplicationAborting
+     */
+    public static void main(String[] args) throws ApplicationAborting
+    {
+        ExceptionHandler eh = new ExceptionHandler();
+        CommandLineArgHandler ch = new CommandLineArgHandler(args);
+        SwingGUI gui = new SwingGUI();
+
+        SwingApplication app = new SwingApplication(eh,ch,gui);
+
+        app.run();
     }
 }
