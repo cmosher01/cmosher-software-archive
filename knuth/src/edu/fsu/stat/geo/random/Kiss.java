6fcad79b453f6120068fcd6e5bbbe6491b2be9ec
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-11 20:34:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7287 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ knuth/src/edu/fsu/stat/geo/random/Kiss.java
@@ -0,0 +1,45 @@
+package edu.fsu.stat.geo.random;
+
+import nu.mine.mosher.random.RandomNumberGenerator;
+
+public class Kiss implements RandomNumberGenerator
+{
+    private static final long a = 698769069L;
+    private final long seed;
+    private int x;
+    private int y = 362436;
+    private int z;
+    private int c = 7654321;
+
+    public Kiss()
+    {
+        this(System.currentTimeMillis());
+    }
+
+    public Kiss(long seed)
+    {
+        this.seed = seed;
+        this.x = (int)seed;
+        this.z = (int)(seed >> 32);
+    }
+
+    public long getSeed()
+    {
+        return seed;
+    }
+
+    public int nextInt()
+    {
+        x *= 69069;
+        x += 12345;
+        y ^= (y << 13);
+        y ^= (y >> 17);
+        y ^= (y << 5);
+
+        long t = a * z + c;
+        c = (int)(t >> 32);
+        z = (int)t;
+
+        return x + y + z;
+    }
+}
