7822fda668ce37daadd498b942b7a34e0aa36d40
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-16 02:46:17 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7441 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- fieldizer/src/nu/mine/mosher/util/text/UnmatchedQuoteException.java
+++ fieldizer/src/nu/mine/mosher/util/text/UnmatchedQuoteException.java
@@ -16,21 +16,47 @@ public class UnmatchedQuoteException extends Exception
 {
     private static final String MSG = "Unmatched quotation mark";
 
+    /**
+     * Initializes an <code>UnmatchedQuoteException</code> with a
+     * generic, English error message.
+     */
     public UnmatchedQuoteException()
     {
         super(MSG);
     }
 
+    /**
+     * Initializes an <code>UnmatchedQuoteException</code> with a
+     * generic, English error message and a given chained <code>Exception</code>.
+     * @param cause the chained <code>Throwable</code>
+     */
     public UnmatchedQuoteException(Throwable cause)
     {
         super(MSG,cause);
     }
 
+    /**
+     * Initializes an <code>UnmatchedQuoteException</code> with a
+     * generic, English error message incorporating the given
+     * piece of source string.
+     * @param near some context from the source buffer, intended to
+     * provide additional information to the end user about where the
+     * error occurred.
+     */
     public UnmatchedQuoteException(String near)
     {
         super(MSG+" near: "+near);
     }
 
+    /**
+     * Initializes an <code>UnmatchedQuoteException</code> with a
+     * generic, English error message incorporating the given
+     * piece of source string, and a given chained <code>Exception</code>.
+     * @param near some context from the source buffer, intended to
+     * provide additional information to the end user about where the
+     * error occurred.
+     * @param cause the chained <code>Throwable</code>
+     */
     public UnmatchedQuoteException(String near, Throwable cause)
     {
         super(MSG+" near: "+near,cause);
