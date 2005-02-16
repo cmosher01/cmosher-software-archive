0843d1abebf726c23dadebbc8d21b0515a141b01
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-16 03:06:49 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7454 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ByteAnalyzer/src/ByteAnalyzer.java
+++ ByteAnalyzer/src/ByteAnalyzer.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.Map.Entry;
 
 /**
  * Checks each byte of a file, and maintains a count of
