671e0aaff09a883a1411a7b20396781c11e23d83
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-17 01:34:34 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7475 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ knuth/src/nu/mine/mosher/random/RNGDefault.java
@@ -0,0 +1,28 @@
+/*
+ * Created on Feb 16, 2005
+ */
+package nu.mine.mosher.random;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class RNGDefault implements RandomNumberGenerator
+{
+	/**
+	 * @return
+	 */
+	public long getSeed()
+	{
+		return 0;
+	}
+
+	/**
+	 * @return
+	 */
+	public int nextInt()
+	{
+		return 0;
+	}
+}
