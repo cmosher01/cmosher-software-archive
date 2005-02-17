b253303e889df76dd43cb81800ad219427642bed
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-02-17 01:31:02 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7468 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- knuth/src/nu/mine/mosher/random/RandomNumberGenerator.java
+++ knuth/src/nu/mine/mosher/random/RandomNumberGenerator.java
@@ -1,9 +1,25 @@
 package nu.mine.mosher.random;
 
+/**
+ * A generator of a random sequence of numbers. Implementers of this interface
+ * should provide a constructor that takes an integer used to seed the sequence.
+ * Users then call <code>getSeed</code> (repeatedly if desired) to get a random
+ * number (or sequence of numbers).
+ *
+ * @author Chris Mosher
+ */
 public interface RandomNumberGenerator
 {
-    //Ctor() { this(System.currentTimeMillis()); }
-    //Ctor(long seed);
+    /**
+     * Gets the seed, as passed in to the constructor.
+     * @return the original seed
+     */
     long getSeed();
+
+    /**
+     * Gets the next number in this generator's sequence of
+     * random numbers.
+     * @return random number
+     */
     int nextInt();
 }
