0cd08b7adf994af90aa923c2df4399a420109527
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-10 04:23:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7574 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/CloneFactory.java
+++ core50/src/nu/mine/mosher/core/CloneFactory.java
@@ -42,10 +42,10 @@ public final class Cloner
 			 */
 			return (T)methodClone.invoke(cloneableObject,(Object[])null);
 		}
-		catch (CloneNotSupportedException e)
-		{
-		    throw e;
-		}
+//		catch (CloneNotSupportedException e)
+//		{
+//		    throw e;
+//		}
 		catch (Throwable cause)
 		{
 			CloneNotSupportedException ex = new CloneNotSupportedException();
