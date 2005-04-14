98a3397784e5ff37efd1d472aa9ff7e2266d5f06
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-14 01:45:28 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7591 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- grodb50/src/nu/mine/mosher/gedcom/GedcomDateParser.java
+++ grodb50/src/nu/mine/mosher/gedcom/GedcomDateParser.java
@@ -1,13 +1,23 @@
 /*
- * Created on Apr 12, 2005
+ * Created on April 12, 2005
  */
 package nu.mine.mosher.gedcom;
 
 /**
- * TODO
+ * Parses GEDCOM dates.
+ * <ul>
+ * <li>@#DGREGORIAN@ "using Gregorian Calendar"--use Gregorian calendar
+ * <li>ABT "about"--set circa flag</li>
+ * <li>CAL "calculated"--ignore this indicator</li>
+ * <li>EST "estimated"--set circa flag</li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * </ul>
  *
  * @author Chris Mosher
  */
 public class GedcomDateParser
 {
+	
 }
