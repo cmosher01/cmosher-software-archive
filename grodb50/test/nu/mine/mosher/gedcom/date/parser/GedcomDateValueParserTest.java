8b2f48905dc152165f5dcfc6f3bfb2d3e5b40220
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-20 02:30:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7615 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/test/nu/mine/mosher/gedcom/date/parser/GedcomDateValueParserTest.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Apr 19, 2005
+ */
+package nu.mine.mosher.gedcom.date.parser;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class GedcomDateValueParserTest extends TestCase
+{
+	public static void main(String[] args)
+	{
+	}
+
+	public void testDateValue()
+	{
+	}
+}
