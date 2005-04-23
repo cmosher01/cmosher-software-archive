3243201713ad84388303b2362b6edffa53c52c51
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 21:23:09 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7766 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
+++ randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
@@ -5,6 +5,7 @@ package nu.mine.mosher.rand.seed;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /**
@@ -17,8 +18,24 @@ public class RandSeedListener implements KeyListener
 	/**
 	 * @param e
 	 */
-	public void actionPerformed(final ActionEvent e)
+	public void keyTyped(KeyEvent e)
 	{
-		e.getID();
+		System.out.print(System.currentTimeMillis());
+		System.out.print(": ");
+		System.out.println(e.getKeyChar());
+	}
+
+	/**
+	 * @param e
+	 */
+	public void keyPressed(KeyEvent e)
+	{
+	}
+
+	/**
+	 * @param e
+	 */
+	public void keyReleased(KeyEvent e)
+	{
 	}
 }
