0cc7f5c1a0722ebbf70e742800af4bff142306fc
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 22:36:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7789 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
+++ randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
@@ -15,7 +15,7 @@ import java.util.Random;
  *
  * @author Chris Mosher
  */
-public class RandSeedListener implements KeyListener
+public class RandomSeedKeyListener implements KeyListener
 {
 	private final LinkedList rBytes = new LinkedList();
 
