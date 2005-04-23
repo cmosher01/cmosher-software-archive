12612109213c6a82b96e5034ad5d9e098265a579
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 22:11:46 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7781 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ randseed/src/nu/mine/mosher/rand/seed/ShowSeed.java
@@ -0,0 +1,30 @@
+/*
+ * Created on Apr 23, 2005
+ */
+package nu.mine.mosher.rand.seed;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class ShowSeed implements ActionListener
+{
+	/**
+	 * 
+	 */
+	public ShowSeed()
+	{
+		super();
+	}
+
+	/**
+	 * @param e
+	 */
+	public void actionPerformed(ActionEvent e)
+	{
+	}
+}
