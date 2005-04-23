82a55724a28cd69d6cb5d85b4df0066a8fee8ab7
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 21:23:37 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7768 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
+++ randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
@@ -3,8 +3,6 @@
  */
 package nu.mine.mosher.rand.seed;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
