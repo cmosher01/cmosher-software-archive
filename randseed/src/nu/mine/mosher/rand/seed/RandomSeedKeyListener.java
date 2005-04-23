5e8846dc783544ed2e3cce03c0ffb4d3fcffac58
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 21:36:07 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7771 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
+++ randseed/src/nu/mine/mosher/rand/seed/RandomSeedKeyListener.java
@@ -23,10 +23,7 @@ public class RandSeedListener implements KeyListener
 	{
 		long t = System.currentTimeMillis();
 		int lowByte = (int)(t & 0xFF);
-		rBytes.add(`)
-		System.out.print(Long.toHexString(t));
-		System.out.print(": ");
-		System.out.println(Integer.toHexString(lowByte));
+		this.rBytes.add(new Integer(lowByte));
 	}
 
 	/**
