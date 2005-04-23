f2494ee2ac961b052f08a9551cdd7b19ec3637e6
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 21:05:21 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7743 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ randseed/src/nu/mine/mosher/rand/seed/RandPanel.java
@@ -0,0 +1,48 @@
+/*
+ * Created on Apr 23, 2005
+ */
+package nu.mine.mosher.rand.seed;
+
+import java.awt.LayoutManager;
+import javax.swing.JPanel;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class RandPanel extends JPanel
+{
+	/**
+	 * @param layout
+	 * @param isDoubleBuffered
+	 */
+	public RandPanel(LayoutManager layout, boolean isDoubleBuffered)
+	{
+		super(layout,isDoubleBuffered);
+	}
+
+	/**
+	 * @param layout
+	 */
+	public RandPanel(LayoutManager layout)
+	{
+		super(layout);
+	}
+
+	/**
+	 * @param isDoubleBuffered
+	 */
+	public RandPanel(boolean isDoubleBuffered)
+	{
+		super(isDoubleBuffered);
+	}
+
+	/**
+	 * 
+	 */
+	public RandPanel()
+	{
+		super();
+	}
+}
