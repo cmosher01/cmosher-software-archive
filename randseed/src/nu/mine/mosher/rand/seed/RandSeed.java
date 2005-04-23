c8ca51d87f99da6f71d52398b603e38b859231ca
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-23 20:58:44 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7736 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ randseed/src/nu/mine/mosher/rand/seed/RandSeed.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Apr 23, 2005
+ */
+package nu.mine.mosher.rand.seed;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class RandSeed
+{
+	public static void main(String[] args)
+	{
+	}
+}
