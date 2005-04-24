02a693328ce6493a4da40cd9b24ffdee6a0c6eaa
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 01:22:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7813 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/AudioRand.java
+++ randseed/src/AudioRand.java
@@ -1,3 +1,6 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer.Info;
+
 /*
  * Created on Apr 23, 2005
  */
@@ -10,5 +13,14 @@ public class AudioRand
 {
 	public static void main(String[] args)
 	{
+    	Info[] rInfo = AudioSystem.getMixerInfo();
+    	for (int i = 0; i < rInfo.length; i++)
+		{
+			Info info = rInfo[i];
+			System.out.println(info.toString());
+			System.out.println(info.getName());
+			System.out.println(info.getDescription());
+			System.out.println();
+		}
 	}
 }
