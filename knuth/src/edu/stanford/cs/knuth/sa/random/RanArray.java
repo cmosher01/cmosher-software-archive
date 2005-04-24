87dc22bed2530dea2b4cb3bd29bd9614c0f25bf5
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 19:09:22 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7925 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- knuth/src/edu/stanford/cs/knuth/sa/random/RanArray.java
+++ knuth/src/edu/stanford/cs/knuth/sa/random/RanArray.java
@@ -84,6 +84,7 @@ public class RanArray extends RNGDefault implements RandomNumberGenerator
 		for (int j = 0; j < KK; ++j)
         {
             x[j] = ss;
+
             /*
              * Cyclic shift 29 bits
              */
