4243805b0b44dcd4f70eb4e00ce919218f29f598
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 22:58:03 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7963 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/HotBitsAndRanArray.java
+++ randseed/src/HotBitsAndRanArray.java
@@ -2,6 +2,9 @@
  * Created on April 24, 2005
  */
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import edu.stanford.cs.knuth.sa.random.RanArray;
 
 /**
@@ -13,9 +16,29 @@ public class HotBitsAndRanArray
 {
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
+		final URL urlHotBits = new URL("http://www.fourmilab.ch/cgi-bin/uncgi/Hotbits?nbytes=4&fmt=bin");
+
+		InputStream inHotBits = null;
+		try
+		{
+			inHotBits = urlHotBits.openStream();
+			int ubyte;
+			int iUByte = 0;
+			int[] rUByte = new int[4];
+			while ((ubyte = inHotBits.read()) != -1)
+			{
+				rUByte[iUByte++] = (byte)ubyte;
+			}
+		}
+		finally
+		{
+			inHotBits.close();
+		}
+
 		RanArray ra = new RanArray(0x12345678Fabcdef0L);
 		int r = ra.nextInt();
 		r = ra.nextInt();
