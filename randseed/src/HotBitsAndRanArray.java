91b3289f71d4514ac89df63b1b3b8beec274106f
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 23:23:14 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7980 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/HotBitsAndRanArray.java
+++ randseed/src/HotBitsAndRanArray.java
@@ -4,6 +4,7 @@
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import edu.stanford.cs.knuth.sa.random.RanArray;
 
@@ -22,10 +23,23 @@ public class HotBitsAndRanArray
 	 */
 	public static void main(String[] args) throws IOException
 	{
+		int seed = getIntFromHotBits();
+
+		RanArray ra = new RanArray(seed);
+		int r = ra.nextInt();
+		r = ra.nextInt();
+		int x = r; r = x;
+	}
+
+	/**
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	private static int getIntFromHotBits() throws MalformedURLException, IOException
+	{
 		final URL urlHotBits = new URL("http://www.fourmilab.ch/cgi-bin/uncgi/Hotbits?nbytes="+cSeedBytes+"&fmt=bin");
-
 		final int[] rUByte = new int[cSeedBytes];
-
 		InputStream inHotBits = null;
 		try
 		{
@@ -55,7 +69,6 @@ public class HotBitsAndRanArray
 				}
 			}
 		}
-
 		int seed = 0;
 		for (int iUByte = 0; iUByte < rUByte.length; ++iUByte)
 		{
@@ -63,10 +76,6 @@ public class HotBitsAndRanArray
 			seed |= rUByte[iUByte];
 		}
 		seed = Integer.reverseBytes(seed);
-
-		RanArray ra = new RanArray(seed);
-		int r = ra.nextInt();
-		r = ra.nextInt();
-		int x = r; r = x;
+		return seed;
 	}
 }
