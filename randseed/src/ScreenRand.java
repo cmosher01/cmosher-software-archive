8accae62229b045166758e8c130c2fa95c47b758
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 03:58:46 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7858 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/ScreenRand.java
+++ randseed/src/ScreenRand.java
@@ -1,3 +1,13 @@
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import javax.swing.DesktopManager;
+import javax.swing.JDesktopPane;
+
 /*
  * Created on Apr 23, 2005
  */
@@ -8,7 +18,11 @@
  */
 public class ScreenRand
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws AWTException
 	{
+		Robot robot = new Robot();
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		Rectangle rect = new Rectangle(0, 0, d.width, d.height);
+		robot.createScreenCapture(regdesktopion);
 	}
 }
