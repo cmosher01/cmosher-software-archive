0a11dfba18f75ea0ed600f53ecb1bc0f25e9f3f2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 01:20:02 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7809 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/nu/mine/mosher/rand/seed/RandSeed.java
+++ randseed/src/nu/mine/mosher/rand/seed/RandSeed.java
@@ -4,6 +4,8 @@
 package nu.mine.mosher.rand.seed;
 
 import java.awt.FlowLayout;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer.Info;
 import javax.swing.JPanel;
 import nu.mine.mosher.swingapp.ApplicationAborting;
 import nu.mine.mosher.swingapp.CommandLineArgHandler;
@@ -35,13 +37,19 @@ public class RandSeed extends SwingGUI
      */
     public static void main(String[] args) throws ApplicationAborting
     {
-        ExceptionHandler eh = new ExceptionHandler();
-        CommandLineArgHandler ch = new CommandLineArgHandler(args);
-        SwingGUI gui = new RandSeed();
-
-        SwingApplication app = new SwingApplication(eh,ch,gui);
-
-        app.run();
+//        ExceptionHandler eh = new ExceptionHandler();
+//        CommandLineArgHandler ch = new CommandLineArgHandler(args);
+//        SwingGUI gui = new RandSeed();
+//
+//        SwingApplication app = new SwingApplication(eh,ch,gui);
+//
+//        app.run();
+    	Info[] rInfo = AudioSystem.getMixerInfo();
+    	for (int i = 0; i < rInfo.length; i++)
+		{
+			Info info = rInfo[i];
+			System.out.println(info.toString());
+		}
     }
 
     protected JPanel createContentPane()
