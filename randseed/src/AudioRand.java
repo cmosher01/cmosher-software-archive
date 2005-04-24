69022b02a2dbb3afb2169805754163d8a60ccfbd
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 04:03:01 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7860 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/AudioRand.java
+++ randseed/src/AudioRand.java
@@ -23,26 +23,6 @@ public class AudioRand
 	 */
 	public static void main(String[] args) throws LineUnavailableException
 	{
-    	Mixer.Info[] rInfo = AudioSystem.getMixerInfo();
-    	for (int i = 0; i < rInfo.length; i++)
-		{
-			Mixer.Info info = rInfo[i];
-			System.out.println(info.toString());
-			System.out.println(info.getDescription());
-
-			Mixer mixer = AudioSystem.getMixer(info);
-			Line[] lines = mixer.getTargetLines();
-			for (int j = 0; j < lines.length; j++)
-			{
-				Line line = lines[j];
-				Line.Info lineInfo = line.getLineInfo();
-				System.out.print("line "+(j+1)+". ");
-				System.out.println(lineInfo.toString());
-			}
-
-			System.out.println();
-		}
-
     	AudioFormat audioFormat = new AudioFormat(44100,8,1,true,false);
 		DataLine.Info info = new DataLine.Info(TargetDataLine.class,audioFormat);
 		TargetDataLine targetDataLine = null;
