faebbf30b1edce5f22a5ff1882e57265326c0a6d
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-24 04:02:55 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7859 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/ScreenRand.java
+++ randseed/src/ScreenRand.java
@@ -5,6 +5,7 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import javax.swing.DesktopManager;
 import javax.swing.JDesktopPane;
 
@@ -23,6 +24,14 @@ public class ScreenRand
 		Robot robot = new Robot();
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		Rectangle rect = new Rectangle(0, 0, d.width, d.height);
-		robot.createScreenCapture(regdesktopion);
+		BufferedImage image = robot.createScreenCapture(rect);
+		for (int y = 0; y < image.getHeight(); ++y)
+		{
+			for (int x = 0; x < image.getWidth(); ++x)
+			{
+				int rgb = image.getRGB(x,y);
+				System.out.println(Integer.toHexString(rgb));
+			}
+		}
 	}
 }
