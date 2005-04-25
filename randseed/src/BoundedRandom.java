467d5a6417c594009d3004d1879b4405f67d3251
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-25 01:12:38 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@7994 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- randseed/src/BoundedRandom.java
+++ randseed/src/BoundedRandom.java
@@ -10,5 +10,17 @@ public class BoundedRandom
 {
 	public static void main(String[] args)
 	{
+		int n = 3;
+		for (int rand = 0; rand < 8; ++rand)
+		{
+	       int bits, val;
+	        do {
+	            bits = rand;
+	            val = bits % n;
+	            System.out.print("bits: "+bits);
+	            System.out.print(" val: "+val);
+	            System.out.print(" bits - val + (n-1): "+(bits - val + (n-1)));
+	        } while(bits - val + (n-1) < 0);
+		}
 	}
 }
