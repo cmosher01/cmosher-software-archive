458020d5dec473cfa6cbd09bedf8c7059b0914b2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-29 02:14:35 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8079 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/CloneFactory.java
+++ core50/src/nu/mine/mosher/core/CloneFactory.java
@@ -11,66 +11,70 @@ import java.lang.reflect.Method;
  */
 public final class Cloner<T extends Cloneable>
 {
-//	private static final Map<Class,Method> mClasses = new HashMap<Class,Method>();
+//	private Cloner()
+//	{
+//		assert false : "can't instantiate";
+//	}
+//
+//	/**
+//	 * Clones the given object. This method uses reflection to
+//	 * call the (otherwise protected) <code>clone</code> method
+//	 * of the givne object, which must be <code>Cloneable</code>.
+//	 * @param <T> class of <code>Cloneable</code> object to be cloned
+//	 * @param cloneable object to be cloned
+//	 * @return clone of <code>cloneableObject</code>
+//	 * @throws CloneNotSupportedException
+//	 */
+//    public static<T extends Cloneable> T cloneObject(final T cloneable) throws CloneNotSupportedException
+//    {
+//        try
+//        {
+//            return clone(cloneable,getCloneMethod(cloneable));
+//        }
+//        catch (final CloneNotSupportedException e)
+//        {
+//            throw e;
+//        }
+//        catch (final Throwable cause)
+//        {
+//            CloneNotSupportedException ex;
+//            ex = new CloneNotSupportedException();
+//            ex.initCause(cause);
+//            throw ex;
+//        }
+//    }
+//
+//    static<T extends Cloneable> T clone(final T cloneable, final Method methodClone)
+//        throws IllegalArgumentException, IllegalAccessException, CloneNotSupportedException, InvocationTargetException
+//    {
+//        T clon;
+//        try
+//        {
+//            clon = (T)methodClone.invoke(cloneable);
+//        }
+//        catch (final InvocationTargetException e)
+//        {
+//            final Throwable cause = e.getCause();
+//            if (cause instanceof CloneNotSupportedException)
+//            {
+//                throw (CloneNotSupportedException)cause;
+//            }
+//            throw e;
+//        }
+//        return clon;
+//    }
+//
+//    static<T extends Cloneable> Method getCloneMethod(final T x) throws SecurityException, NoSuchMethodException
+//    {
+//		final Class cl = x.getClass();
+//        final Method methodClone = cl.getMethod("clone");
+//        methodClone.setAccessible(true);
+//        return methodClone;
+//    }
 
-	private Cloner()
+	private final T cloneableSource;
+	public Cloner(final T cloneableSource)
 	{
-		assert false : "can't instantiate";
+		this.cloneableSource = cloneableSource;
 	}
-
-	/**
-	 * Clones the given object. This method uses reflection to
-	 * call the (otherwise protected) <code>clone</code> method
-	 * of the givne object, which must be <code>Cloneable</code>.
-	 * @param <T> class of <code>Cloneable</code> object to be cloned
-	 * @param cloneable object to be cloned
-	 * @return clone of <code>cloneableObject</code>
-	 * @throws CloneNotSupportedException
-	 */
-    public static<T extends Cloneable> T cloneObject(final T cloneable) throws CloneNotSupportedException
-    {
-        try
-        {
-            return clone(cloneable,getCloneMethod(cloneable));
-        }
-        catch (final CloneNotSupportedException e)
-        {
-            throw e;
-        }
-        catch (final Throwable cause)
-        {
-            CloneNotSupportedException ex;
-            ex = new CloneNotSupportedException();
-            ex.initCause(cause);
-            throw ex;
-        }
-    }
-
-    static<T extends Cloneable> T clone(final T cloneable, final Method methodClone)
-        throws IllegalArgumentException, IllegalAccessException, CloneNotSupportedException, InvocationTargetException
-    {
-        T clon;
-        try
-        {
-            clon = (T)methodClone.invoke(cloneable);
-        }
-        catch (final InvocationTargetException e)
-        {
-            final Throwable cause = e.getCause();
-            if (cause instanceof CloneNotSupportedException)
-            {
-                throw (CloneNotSupportedException)cause;
-            }
-            throw e;
-        }
-        return clon;
-    }
-
-    static<T extends Cloneable> Method getCloneMethod(final T x) throws SecurityException, NoSuchMethodException
-    {
-		final Class cl = x.getClass();
-        final Method methodClone = cl.getMethod("clone");
-        methodClone.setAccessible(true);
-        return methodClone;
-    }
 }
