6893e958f65d3f79e9a3dbc28a17591b0e5f5e69
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-29 00:29:06 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8054 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/CloneFactory.java
+++ core50/src/nu/mine/mosher/core/CloneFactory.java
@@ -1,5 +1,6 @@
 package nu.mine.mosher.core;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,24 +28,24 @@ public final class Cloner
 	 * @return clone of <code>cloneableObject</code>
 	 * @throws CloneNotSupportedException
 	 */
-	public static<T extends Cloneable> T cloneObject(final T cloneable) throws CloneNotSupportedException
-	{
-		try
-		{
-			final Method methodClone = getCloneMethod(cloneable);
-			/*
-			 * Unchecked cast is OK here, because we know that the
-			 * clone of a T will be a T:
-			 */
-			return (T)methodClone.invoke(cloneable,(Object[])null);
-		}
-		catch (Throwable cause)
-		{
-			CloneNotSupportedException ex = new CloneNotSupportedException();
-			ex.initCause(cause);
-			throw ex;
-		}
-	}
+//	public static<T extends Cloneable> T cloneObject(final T cloneable) throws CloneNotSupportedException
+//	{
+//		try
+//		{
+//			final Method methodClone = getCloneMethod(cloneable);
+//			/*
+//			 * Unchecked cast is OK here, because we know that the
+//			 * clone of a T will be a T:
+//			 */
+//			return (T)methodClone.invoke(cloneable,(Object[])null);
+//		}
+//		catch (Throwable cause)
+//		{
+//			CloneNotSupportedException ex = new CloneNotSupportedException();
+//			ex.initCause(cause);
+//			throw ex;
+//		}
+//	}
 
 	/**
 	 * @param <T>
@@ -65,4 +66,71 @@ public final class Cloner
 		}
 		return methodClone;
 	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * Clones the given object. This method uses reflection to
+	 * call the (otherwise protected) <code>clone</code> method
+	 * of the givne object, which must be <code>Cloneable</code>.
+	 * @param <T> class of <code>Cloneable</code> object to be cloned
+	 * @param cloneable object to be cloned
+	 * @return clone of <code>cloneableObject</code>
+	 * @throws CloneNotSupportedException
+	 */
+    public static<T extends Cloneable> T cloneObject(final T cloneableObject) throws CloneNotSupportedException
+    {
+        try
+        {
+            return clone(cloneableObject, getCloneMethod(cloneableObject.getClass()));
+        }
+        catch (CloneNotSupportedException e)
+        {
+            throw e;
+        }
+        catch (Throwable cause)
+        {
+            CloneNotSupportedException ex;
+            ex = new CloneNotSupportedException();
+            ex.initCause(cause);
+            throw ex;
+        }
+    }
+
+    static<T extends Cloneable> T clone(final T cloneableObject, final Method methodClone)
+        throws IllegalArgumentException, IllegalAccessException, CloneNotSupportedException, InvocationTargetException
+    {
+        T clon;
+        try
+        {
+            clon = methodClone.invoke(cloneableObject, null);
+        }
+        catch (InvocationTargetException e)
+        {
+            Throwable cause = e.getCause();
+            if (cause instanceof CloneNotSupportedException)
+            {
+                throw (CloneNotSupportedException)cause;
+            }
+            else
+            {
+                throw e;
+            }
+        }
+        return clon;
+    }
+
+    static Method getCloneMethod(Class cl) throws SecurityException, NoSuchMethodException
+    {
+        Method methodClone = cl.getMethod("clone", null);
+        methodClone.setAccessible(true);
+        return methodClone;
+    }
 }
