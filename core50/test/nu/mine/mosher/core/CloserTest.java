ceffa809b74d975ae9cd56cc5a57da2a5a68e0cd
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 00:06:27 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8154 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ core50/test/nu/mine/mosher/core/CloserTest.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Apr 29, 2005
+ */
+package nu.mine.mosher.core;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class CloserTest extends TestCase
+{
+	public static void main(String[] args)
+	{
+	}
+
+	public void testClose()
+	{
+	}
+}
