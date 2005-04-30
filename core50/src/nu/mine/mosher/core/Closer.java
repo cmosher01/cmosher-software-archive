a3f3d7cd7b90b85ba1cd1a73b1048f5676808d2c
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 00:26:05 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8171 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Closer.java
+++ core50/src/nu/mine/mosher/core/Closer.java
@@ -92,7 +92,7 @@ public final class Closer
 	 * 
 	 * @param obj the Object whose close() method is to be called.
 	 */
-	public static void close(final Object obj)
+	public static<T> void close(final T obj)
 	{
 		try
 		{
