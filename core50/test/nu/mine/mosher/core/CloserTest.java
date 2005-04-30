1d48cab4b765a45705d441b51c1d68dba9af7724
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 00:30:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8179 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/test/nu/mine/mosher/core/CloserTest.java
+++ core50/test/nu/mine/mosher/core/CloserTest.java
@@ -43,45 +43,39 @@ public class CloserTest extends TestCase
 
 	public void testClose2()
 	{
-		Closer clos1 = new Closer();
-
 		{
 			Thing1 t1 = new Thing1();
 			assertFalse(t1.closed);
-			clos1.close(t1);
+			Closer.close(t1);
 			assertTrue(t1.closed);
 		}
 
 		{
 			Thing1 u1 = new Thing1();
 			assertFalse(u1.closed);
-			clos1.close(u1);
+			Closer.close(u1);
 			assertTrue(u1.closed);
 		}
 
-		assertFalse(clos1.hasErrors());
+		assertFalse(Closer.hasErrors());
 	}
 
 	public void testClose3()
 	{
-		Closer clos1 = new Closer();
-		Closer clos2 = new Closer();
-
 		{
 			Thing1 t1 = new Thing1();
 			assertFalse(t1.closed);
-			clos1.close(t1);
+			Closer.close(t1);
 			assertTrue(t1.closed);
 		}
 
 		{
 			Thing2 u1 = new Thing2();
 			assertFalse(u1.closed);
-			clos2.close(u1);
+			Closer.close(u1);
 			assertTrue(u1.closed);
 		}
 
-		assertFalse(clos1.hasErrors());
-		assertFalse(clos2.hasErrors());
+		assertFalse(Closer.hasErrors());
 	}
 }
