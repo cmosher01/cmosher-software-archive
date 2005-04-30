cc5083883f5632ae6ef61d85119669c680c5af76
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 01:28:42 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8216 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/EnumIter.java
+++ core50/src/nu/mine/mosher/core/EnumIter.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 /**
  * Adapts an Enumeration so it can be used
  * as an Iterator.
+ * @param <T> 
  * 
  * @author Chris Mosher
  */
@@ -14,21 +15,34 @@ public class EnumIter<T> implements Iterator<T>
 {
     private final Enumeration<T> e;
 
+    /**
+     * @param e
+     */
     public EnumIter(final Enumeration<T> e)
     {
         this.e = e;
     }
 
+    /**
+     * @return
+     */
     public boolean hasNext()
     {
         return e.hasMoreElements();
     }
 
+    /**
+     * @return
+     * @throws NoSuchElementException
+     */
     public T next() throws NoSuchElementException
     {
         return e.nextElement();
     }
 
+    /**
+     * @throws UnsupportedOperationException
+     */
     public void remove() throws UnsupportedOperationException
     {
         throw new UnsupportedOperationException();
