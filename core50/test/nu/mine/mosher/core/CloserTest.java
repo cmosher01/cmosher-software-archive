a0459fb2d4e8ef4ab0b04d6b5fb099f2424a65db
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 00:11:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8158 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/test/nu/mine/mosher/core/CloserTest.java
+++ core50/test/nu/mine/mosher/core/CloserTest.java
@@ -24,12 +24,31 @@ public class CloserTest extends TestCase
 			closed = true;
 		}
 	}
-	public void testClose()
+	public void testClose1()
 	{
 		Thing1 t1 = new Thing1();
 		assertFalse(t1.closed);
 		Closer<Thing1> clos1 = new Closer<Thing1>();
 		clos1.close(t1);
 		assertTrue(t1.closed);
+	}
+
+	public void testClose1()
+	{
+		Closer<Thing1> clos1 = new Closer<Thing1>();
+
+		{
+			Thing1 t1 = new Thing1();
+			assertFalse(t1.closed);
+			clos1.close(t1);
+			assertTrue(t1.closed);
+		}
+
+		{
+			Thing1 u1 = new Thing1();
+			assertFalse(u1.closed);
+			clos1.close(u1);
+			assertTrue(u1.closed);
+		}
 	}
 }
