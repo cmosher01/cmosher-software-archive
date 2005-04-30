8124b70b8dc3268e04f9d90b879b062ef2f9002b
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-04-30 00:23:50 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8166 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Closer.java
+++ core50/src/nu/mine/mosher/core/Closer.java
@@ -12,103 +12,106 @@ import java.util.Map;
  *
  * @author Chris Mosher
  */
-public final class Closer<T>
+public final class Closer
 {
-//	private Closer()
-//	{
-//		assert false : "can't instantiate";
-//	}
-
-	private static final Object lock = new Object();
-	private static Method methodClose;
-	private static List<Throwable> rException = new ArrayList<Throwable>();
-
-	/**
-	 * @param object
-	 */
-	public void close(final T object)
+	private Closer()
 	{
-		try
-		{
-			tryClose(object);
-		}
-		catch (final Throwable ignore)
-		{
-			rException.add(ignore);
-		}
+		assert false : "can't instantiate";
 	}
 
-	/**
-	 * @return
-	 */
-	public boolean hasErrors()
-	{
-		return !rException.isEmpty();
-	}
-
-	/**
-	 * @param object
-	 * @throws SecurityException
-	 * @throws NoSuchMethodException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws InvocationTargetException
-	 */
-	private void tryClose(final T object) throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
-	{
-		synchronized (lock)
-		{
-			if (methodClose == null)
-			{
-				methodClose = object.getClass().getMethod("close");
-			}
-		}
-		methodClose.invoke(object);
-	}
-//	private static final Map<Class,Method> mClasses = new HashMap<Class,Method>();
+//	private static final Object lock = new Object();
+//	private static Method methodClose;
+//	private static List<Throwable> rException = new ArrayList<Throwable>();
 //
 //	/**
-//	 * Calls the given object's "close()" method, if it has one.
-//	 * Any exceptions are ignored.
-//	 * 
-//	 * For example:
-//	 * <pre>
-//	 * OutputStream os = null;
-//	 * try
-//	 * {
-//	 *     os = new FileOutputStream(new File("test.txt"));
-//	 *     os.write(65);
-//	 * }
-//	 * finally
-//	 * {
-//	 *     UniversalCloser.close(os);
-//	 * }
-//	 * </pre>
-//	 * 
-//	 * Performance considerations: on an Intel-based Windows PC
-//	 * with Java 1.4.2, where calling close on an object took
-//	 * about 4 nanoseconds, calling UniversalCloser2.close on
-//	 * that object took about 384 nanoseconds.
-//	 * 
-//	 * @param obj the Object whose close() method is to be called.
+//	 * @param object
 //	 */
-//	public static void close(final Object obj)
+//	public void close(final T object)
 //	{
 //		try
 //		{
-//			final Class cl = obj.getClass();
-//			Method methodClose = mClasses.get(cl);
-//			if (methodClose == null)
-//			{
-//				methodClose = cl.getMethod("close");
-//				mClasses.put(cl,methodClose);
-//			}
-//
-//			methodClose.invoke(obj);
+//			tryClose(object);
 //		}
 //		catch (final Throwable ignore)
 //		{
-//			ignore.printStackTrace();
+//			rException.add(ignore);
 //		}
 //	}
+//
+//	/**
+//	 * @return
+//	 */
+//	public boolean hasErrors()
+//	{
+//		return !rException.isEmpty();
+//	}
+//
+//	/**
+//	 * @param object
+//	 * @throws SecurityException
+//	 * @throws NoSuchMethodException
+//	 * @throws IllegalArgumentException
+//	 * @throws IllegalAccessException
+//	 * @throws InvocationTargetException
+//	 */
+//	private void tryClose(final T object) throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
+//	{
+//		synchronized (lock)
+//		{
+//			if (methodClose == null)
+//			{
+//				methodClose = object.getClass().getMethod("close");
+//			}
+//		}
+//		methodClose.invoke(object);
+//	}
+	private static final Map<Class,Method> mClasses = new HashMap<Class,Method>();
+
+	/**
+	 * Calls the given object's "close()" method, if it has one.
+	 * Any exceptions are ignored.
+	 * 
+	 * For example:
+	 * <pre>
+	 * OutputStream os = null;
+	 * try
+	 * {
+	 *     os = new FileOutputStream(new File("test.txt"));
+	 *     os.write(65);
+	 * }
+	 * finally
+	 * {
+	 *     UniversalCloser.close(os);
+	 * }
+	 * </pre>
+	 * 
+	 * Performance considerations: on an Intel-based Windows PC
+	 * with Java 1.4.2, where calling close on an object took
+	 * about 4 nanoseconds, calling UniversalCloser2.close on
+	 * that object took about 384 nanoseconds.
+	 * 
+	 * @param obj the Object whose close() method is to be called.
+	 */
+	public static void close(final Object obj)
+	{
+		try
+		{
+			final Class cl = obj.getClass();
+			Method methodClose;
+			synchronized (Closer.mClasses)
+			{
+				if (!Closer.mClasses.containsKey(cl))
+				{
+					Closer.mClasses.put(cl,cl.getMethod("close"));
+				}
+				methodClose = Closer.mClasses.get(cl);
+			}
+
+			methodClose.invoke(obj);
+		}
+		catch (final Throwable ignore)
+		{
+			ignore.printStackTrace();
+		}
+	}
 }
