fde260b309dc18b3050194b96178483fab21b170
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2005-07-13 02:40:33 +0000

    Recovered from Eclipse.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@8298 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ testNewProj/src/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Created on July 12, 2005
+ */
+
+/**
+ * Just a test of the new Eclipse 3.1.
+ *
+ * @author Chris Mosher
+ */
+public class Test
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+	}
+}
