59d47f161934cfc27bf5d4d02bed346ff10ea894
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2006-03-15 03:10:54 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8407 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grodb50/setup.bat
@@ -0,0 +1,6 @@
+echo on
+set Path=C:\java\jdk1.5.0_06\bin;%Path%
+set CLASSPATH=C:\eclipse_organize\installations\je\je-2.0.90\bin
+set DUMP=com.sleepycat.je.util.DbDump
+rem
+rem java %DUMP% -h dbdir -p -s dbname
