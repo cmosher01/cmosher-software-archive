66ded5dfaf77007a9b824007d01e00e5c1416950
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-09-10 21:35:22 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8656 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ja2/src/CPU6502.java
+++ ja2/src/CPU6502.java
@@ -5,6 +5,8 @@ import java.util.Random;
  */
 public class CPU6502 implements Clock.Timed
 {
+	private static final int BRK_VECTOR = 0xFFFE;
+
 	private int address;
 	private int data;
 
@@ -29,9 +31,13 @@ public class CPU6502 implements Clock.Timed
 	private int bal;
 	private int bah;
 	private int ial;
+	private int iah;
+	private int idx;
+	private int offset;
+	private boolean branch;
 
 	private Memory memory;
-	private int idx;
+
 
 	CPU6502(final Memory memory)
 	{
@@ -179,7 +185,7 @@ public class CPU6502 implements Clock.Timed
 						this.bah = this.data;
 					break;
 					case 3:
-						//TODO set idx
+						setIndex();
 						this.bal += this.idx;
 						this.bah += this.c;
 						this.address = ba();
@@ -214,7 +220,7 @@ public class CPU6502 implements Clock.Timed
 						read(); // discard
 					break;
 					case 3:
-						//TODO set idx
+						setIndex();
 						this.bal += this.idx;
 						this.address = ba();
 						read();
@@ -242,6 +248,7 @@ public class CPU6502 implements Clock.Timed
 						this.bah = this.data;
 					break;
 					case 4:
+						setIndex();
 						this.bal += this.idx;
 						this.bah += this.c;
 						this.address = ba();
@@ -262,13 +269,571 @@ public class CPU6502 implements Clock.Timed
 					break;
 				}
 			break;
+			case STORE_ZERO_PAGE:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+						execute();
+					break;
+					case 2:
+						this.adh = 0;
+						this.address = ad();
+						write();
+						done();
+					break;
+				}
+			break;
+			case STORE_ABSOLUTE:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+					break;
+					case 2:
+						this.address = this.pc++;
+						read();
+						this.adh = this.data;
+						execute();
+					break;
+					case 3:
+						this.address = ad();
+						write();
+						done();
+					break;
+				}
+			break;
+			case STORE_INDIRECT_X:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.bal = this.data;
+					break;
+					case 2:
+						this.address = this.bal;
+						read(); // discard
+					break;
+					case 3:
+						this.address += this.x;
+						read();
+						this.adl = this.data;
+					break;
+					case 4:
+						++this.address;
+						read();
+						this.adh = this.data;
+						execute();
+					break;
+					case 5:
+						this.address = ad();
+						write();
+						done();
+					break;
+				}
+			break;
+			case STORE_ABSOLUTE_XY:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.bal = this.data;
+					break;
+					case 2:
+						this.address = this.pc++;
+						read();
+						this.bah = this.data;
+					break;
+					case 3:
+						setIndex();
+						this.bal += this.idx;
+						this.bah += this.c; //  ???
+						this.address = ba();
+						read(); // discard
+						execute();
+					break;
+					case 4:
+						write();
+						done();
+					break;
+				}
+			break;
+			case STORE_ZERO_PAGE_XY:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.bal = this.data;
+					break;
+					case 2:
+						this.bah = 0;
+						this.address = ba();
+						read(); // discard
+						execute();
+					break;
+					case 3:
+						setIndex();
+						this.bal += this.idx;
+						this.address = ba();
+						write();
+						done();
+					break;
+				}
+			break;
+			case STORE_INDIRECT_Y:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.ial = this.data;
+					break;
+					case 2:
+						this.address = this.ial;
+						read();
+						this.bal = this.data;
+					break;
+					case 3:
+						++this.address;
+						read();
+						this.bah = this.data;
+					break;
+					case 4:
+						setIndex();
+						this.bal += this.idx;
+						this.bah += this.c; // ???
+						this.address = ba();
+						read(); // discard
+					break;
+					case 5:
+						execute();
+						write();
+						done();
+					break;
+				}
+			break;
+			case RMW_ZERO_PAGE:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+					break;
+					case 2:
+						this.adh = 0;
+						this.address = ad();
+						read();
+					break;
+					case 3:
+						write();
+						execute();
+					break;
+					case 4:
+						write();
+						done();
+					break;
+				}
+			break;
+			case RMW_ABSOLUTE:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+					break;
+					case 2:
+						this.address = this.pc++;
+						read();
+						this.adh = this.data;
+					break;
+					case 3:
+						this.address = ad();
+						read();
+					break;
+					case 4:
+						write();
+						execute();
+					break;
+					case 5:
+						write();
+						done();
+					break;
+				}
+			break;
+			case RMW_ZERO_PAGE_X:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.bal = this.data;
+					break;
+					case 2:
+						this.bah = 0;
+						this.address = ba();
+						read(); // discard
+					break;
+					case 3:
+						setIndex();
+						this.bal += this.idx;
+						this.address = ba();
+						read();
+					break;
+					case 4:
+						write();
+						execute();
+					break;
+					case 5:
+						write();
+						done();
+					break;
+				}
+			break;
+			case RMW_ABSOLUTE_X:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.bal = this.data;
+					break;
+					case 2:
+						this.address = this.pc++;
+						read();
+						this.bah = this.data;
+					break;
+					case 3:
+						setIndex();
+						this.bal += this.idx;
+						this.bah += this.c;
+						this.address = ba();
+						read(); // discard
+					break;
+					case 4:
+						read();
+					break;
+					case 5:
+						write();
+						execute();
+					break;
+					case 6:
+						write();
+						done();
+					break;
+				}
+			break;
+			case MISC_PUSH:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc;
+						read(); // discard
+						execute();
+					break;
+					case 2:
+						this.address = spPush();
+						write();
+						done();
+					break;
+				}
+			break;
+			case MISC_PULL:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc;
+						read(); // discard
+					break;
+					case 2:
+						this.address = sp();
+						read(); // discard
+					break;
+					case 3:
+						this.address = spPull();
+						read();
+						execute();
+						done();
+					break;
+				}
+			break;
+			case MISC_JSR:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+					break;
+					case 2:
+						this.address = spPush();
+						read(); // discard
+					break;
+					case 3:
+						this.data = pch();
+						write();
+						this.address = spPush();
+					break;
+					case 4:
+						this.data = pcl();
+						write();
+					break;
+					case 5:
+						this.address = this.pc;
+						read();
+						this.adh = this.data;
+						this.pc = ad();
+						done();
+					break;
+				}
+			break;
+			case MISC_BREAK:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc;
+						read(); // discard
+					break;
+					case 2:
+						this.address = spPush();
+						this.data = pch();
+						write();
+					break;
+					case 3:
+						this.address = spPush();
+						this.data = pcl();
+						write();
+					break;
+					case 4:
+						this.address = spPush();
+						this.data = p();
+						write();
+					break;
+					case 5:
+						this.address = BRK_VECTOR;
+						read();
+						this.adl = this.data;
+					break;
+					case 6:
+						this.address = BRK_VECTOR+1;
+						read();
+						this.adh = this.data;
+						this.pc = ad();
+						done();
+					break;
+				}
+			break;
+			case MISC_RTI:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc;
+						read(); // discard
+					break;
+					case 2:
+						this.address = sp();
+						read(); // discard
+					break;
+					case 3:
+						this.address = spPull();
+						read();
+						setP(this.data);
+					break;
+					case 4:
+						this.address = spPull();
+						read();
+						this.adl = this.data;
+					break;
+					case 5:
+						this.address = spPull();
+						read();
+						this.adh = this.data;
+						this.pc = ad();
+						done();
+					break;
+				}
+			break;
+			case JMP_ABSOLUTE:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.adl = this.data;
+					break;
+					case 2:
+						this.address = this.pc;
+						read();
+						this.adh = this.data;
+						this.pc = ad();
+						done();
+					break;
+				}
+			break;
+			case JMP_INDIRECT:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.ial = this.data;
+					break;
+					case 2:
+						this.address = this.pc;
+						read();
+						this.iah = this.data;
+					break;
+					case 3:
+						this.address = ia();
+						read();
+						this.adl = this.data;
+						++this.ial; // ???
+					break;
+					case 4:
+						this.address = ia();
+						read();
+						this.adh = this.data;
+						this.pc = ad();
+						done();
+					break;
+				}
+			break;
+			case RTS:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc;
+						read(); // discard
+					break;
+					case 2:
+						this.address = sp();
+						read(); // discard
+					break;
+					case 3:
+						this.address = spPull();
+						read();
+						execute();
+						done();
+					break;
+				}
+			break;
+			case BRANCH:
+				switch (this.t)
+				{
+					case 1:
+						this.address = this.pc++;
+						read();
+						this.offset = this.data;
+						execute();
+						if (!this.branch)
+						{
+							done();
+						}
+					break;
+					case 2:
+						if (this.offset >= 0x80)
+						{
+							this.offset -= 0x100;
+						}
+						int lo = pcl()+this.offset;
+						this.c = 0;
+						if (lo < 0)
+						{
+							lo += 0x100;
+							this.c = -1;
+						}
+						else if (lo >= 0x100)
+						{
+							lo -= 0x100;
+							this.c = 1;
+						}
+						this.pc = (this.pc & 0xFF00) | lo;
+						this.address = this.pc;
+						read();
+						if (this.c == 0)
+						{
+							done();
+						}
+					break;
+					case 3:
+						int hi = pch() + this.c;
+						this.pc = (hi << 8) | (this.pc & 0xFF);
+						read();
+						done();
+					break;
+				}
+			break;
 		}
 		++this.t;
 	}
 
+	private void setP(int data2)
+	{
+		// TODO
+	}
+
+	private int p()
+	{
+		return 0; // TODO
+	}
+
+	private int pch()
+	{
+		return (this.pc & 0xFF00) >> 8;
+	}
+
+	private int pcl()
+	{
+		return this.pc & 0xFF;
+	}
+
+	private int sp()
+	{
+		return 0x100+this.s;
+	}
+
+	private int spPush()
+	{
+		final int sp = sp();
+		--this.s;
+		if (this.s < 0)
+		{
+			this.s += 0x100;
+		}
+		return sp;
+	}
+
+	private int spPull()
+	{
+		++this.s;
+		if (this.s >= 0x100)
+		{
+			this.s -= 0x100;
+		}
+		return sp();
+	}
+
+	private void setIndex()
+	{
+		// TODO
+	}
+
 	private int ad()
 	{
-		return combine(this.adh,this.adl);
+		return combine(this.adl,this.adh);
+	}
+
+	private int ia()
+	{
+		return combine(this.ial,this.iah);
 	}
 
 	private static int combine(final int lo, final int hi)
@@ -278,7 +843,7 @@ public class CPU6502 implements Clock.Timed
 
 	private int ba()
 	{
-		return combine(this.bah,this.bal);
+		return combine(this.bal,this.bah);
 	}
 
 	private void done()
@@ -290,12 +855,13 @@ public class CPU6502 implements Clock.Timed
 	{
 		switch (this.opcode)
 		{
+			// TODO
 		}
 	}
 
 	private Addressing addressing(int op)
 	{
-		return Addressing.SINGLE;
+		return Addressing.SINGLE; // TODO
 	}
 
 	private void read()
--- ja2/src/ShowBits.java
+++ ja2/src/ShowBits.java
@@ -11,13 +11,15 @@ public class ShowBits
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		final InputStream rom = new FileInputStream("C:\\aaaws44\\Pom1\\bios\\apple1.vid");
+		final InputStream rom = new FileInputStream("C:\\aaaws44\\Pom1_KR\\bios\\apple1.vid");
 //		final InputStream rom = ShowBits.class.getResourceAsStream("charrom.bin");
+		int i = 0;
 		for (int c = rom.read(); c != -1; c = rom.read())
 		{
 			final StringBuilder sb = new StringBuilder(8);
 			bits((byte)c,sb);
-			System.out.println(sb.toString());
+			System.out.println(/*""+(i/8)+": "+Integer.toHexString(c)+": "+*/sb.toString());
+			++i;
 		}
 		rom.close();
 	}
@@ -32,7 +34,7 @@ public class ShowBits
 			}
 			else
 			{
-				sb.append(" ");
+				sb.append(".");
 			}
 			b = (byte)(b >> 1);
 		}
