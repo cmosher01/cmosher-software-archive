75f3aea6a90993d7acf4e3acf9eb8c4240125321
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-09-19 02:23:27 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8666 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ ja2/src/buttons/Buttons.java
@@ -0,0 +1,91 @@
+/*
+ * Created on Sep 18, 2007
+ */
+package buttons;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
+
+public class Buttons extends WindowAdapter implements Runnable
+{
+	private JFrame frame;
+    private Timer timer = new Timer();
+	private ButtonPanel panel;
+
+	/**
+	 * @param args
+	 * @throws InvocationTargetException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException, InvocationTargetException
+	{
+    	SwingUtilities.invokeAndWait(new Buttons());
+	}
+
+	public void run()
+	{
+    	try
+		{
+			tryRun();
+		}
+		catch (final Throwable e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void tryRun() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	{
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        this.frame = new JFrame();
+
+        this.frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        this.frame.addWindowListener(this);
+
+        this.frame.setTitle("Button Test");
+
+        // Create and set up the content pane.
+        this.panel = new ButtonPanel();
+        this.frame.setContentPane(panel);
+
+        // Set the window's size and position.
+        this.frame.pack();
+        this.frame.setLocationRelativeTo(null);
+
+        // Display the window.
+        this.frame.setVisible(true);
+
+        this.timer.schedule(new TimerTask()
+        {
+        	private boolean on;
+			@Override
+			public void run()
+			{
+				on = !on;
+				Buttons.this.panel.test(on);
+			} },5000,5000);
+		}
+
+	private static class TestThread implements Runnable
+	{
+		public void run()
+		{
+		}
+	}
+	public void windowClosing(WindowEvent e)
+	{
+		this.frame.dispose();
+	}
+
+}
