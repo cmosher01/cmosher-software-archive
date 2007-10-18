94af443f5f3d6ab96111063218e915820b805d3d
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-10-18 15:37:46 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8803 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- DosMasterToImage/src/CreateCatalog.java
+++ DosMasterToImage/src/CreateCatalog.java
@@ -14,7 +14,7 @@ public class CreateCatalog
 {
 	private static boolean parseOptions = true;
 	private static boolean runProgram = true;
-	private static int used = 0;
+	private static int used = 0x25;
 	private static int track = 0x11;
 	private static int version = 330;
 	private static int volume = 0xFE;
@@ -25,7 +25,6 @@ public class CreateCatalog
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		String outfile = "";
 		for (final String arg : args)
 		{
 			if (parseOptions && arg.startsWith("--"))
@@ -34,28 +33,15 @@ public class CreateCatalog
 			}
 			else
 			{
-				if (outfile.isEmpty())
-				{
-					outfile = arg;
-				}
-				else
-				{
-					throw new IllegalArgumentException(arg);
-				}
+				throw new IllegalArgumentException(arg);
 			}
 		}
-
-		final OutputStream outstream;
-		if (outfile.isEmpty())
+		if (!runProgram)
 		{
-			outstream = new FileOutputStream(FileDescriptor.out);
-		}
-		else
-		{
-			outstream = new FileOutputStream(new File(outfile));
+			return;
 		}
 
-		final BufferedOutputStream out = new BufferedOutputStream(outstream);
+		final BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(FileDescriptor.out));
 		writeCatalog(out);
 		out.flush();
 		out.close();
@@ -63,6 +49,8 @@ public class CreateCatalog
 
 
 	private static final int TPD = 0x23; // tracks per disk
+	private static final int BPS = 0x0100; // bytes per sector
+
 	private static void writeCatalog(final BufferedOutputStream out) throws IOException
 	{
 		final int SPT; // sectors per track
@@ -71,13 +59,11 @@ public class CreateCatalog
 		else
 			SPT = 16;
 
-		out.write(0);
 
-		out.write(track); // ptr to first catalog track/sector
-		if (version < 330)
-			out.write(SPT-1);
-		else
-			out.write(SPT-1);
+
+
+
+		linkout(SPT-1,out);
 
 		out.write(version / 10 % 10);
 
@@ -91,19 +77,15 @@ public class CreateCatalog
 
 		Util.nout(8,0,out);
 
-		if (track < TPD-1)
-			out.write(track+1);
-		else
-			out.write(track);
+		out.write(track);
 		out.write(1);
 
 		Util.nout(2,0,out);
 
 		out.write(TPD);
 		out.write(SPT);
-		Util.wordout(0x0100,out);
+		Util.wordout(BPS,out);
 
-		// TODO write free t/s map
 		for (int tr = 0; tr < TPD; ++tr)
 		{
 			int map;
@@ -132,14 +114,22 @@ public class CreateCatalog
 
 		Util.nout(60,0,out);
 
-		Util.nout(0x100,0,out);
-		for (int se = 1; se < SPT-1; ++se)
+
+
+
+
+		for (int se = 0; se < SPT-1; ++se)
 		{
-			out.write(0);
-			out.write(track);
-			out.write(se);
-			Util.nout(0x100-3,0,out);
+			linkout(se,out);
+			Util.nout(BPS-3,0,out);
 		}
+	}
+
+	private static void linkout(final int nextSector, final BufferedOutputStream out) throws IOException
+	{
+		out.write(0);
+		out.write(nextSector==0 ? 0 : track);
+		out.write(nextSector);
 	}
 
 	private static int getFreeTrack()
@@ -158,7 +148,7 @@ public class CreateCatalog
 
 	private static int getTrackUsedSectors(int nsect)
 	{
-		return getFreeTrack() << nsect;
+		return (getFreeTrack() << nsect) & 0xFFFF;
 	}
 
 	private static void parseArg(final String arg)
@@ -204,12 +194,13 @@ public class CreateCatalog
 
 	private static void help()
 	{
-		System.err.println("usage: java CreateCatalog [OPTIONS] [OUTFILE]");
+		System.err.println("usage: java CreateCatalog [OPTIONS]");
 		System.err.println("");
 		System.err.println("OPTIONS:");
 		System.err.println("--track=N    use track N (default: 0x11)");
-		System.err.println("--used=N     mark N sectors (from beginning of disk) as used");
-		System.err.println("--version=N  for DOS version N (310,320,330,331,332) (default: 330)");
+		System.err.println("--used=N     mark N sectors (from beginning of disk) as used (default: 0x25)");
+		System.err.println("             (for DOS disks use 0x25, 0x27, or 0x30");
+		System.err.println("--version=N  for DOS version N (300,310,320,321,330,331,332) (default: 332)");
 		System.err.println("--volume=N   disk volume (default: 0xFE)");
 		runProgram = false;
 	}
