33f8b5dff6b45ee68ba99b3fa8f1b2bdabfe955a
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-10-18 17:36:57 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8810 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- DosMasterToImage/test/test.bat
+++ DosMasterToImage/test/test.bat
@@ -1,7 +1,9 @@
-rem given dos 3.1 build exe image in file "dos", puts it into a clean nibble disk image "dos.dsk"
-java -cp bin dd --skip=0x1B00 dos >dos.d13
-java -cp bin dd --count=0x1B00 dos >>dos.d13
+rem given dos 3.1 build exe image in file "dos", puts it into a clean nibble disk image "dos.nib"
+
+java -cp bin dd --skip=0x1B00 <dos >dos.d13
+java -cp bin dd --count=0x1B00 <dos >>dos.d13
 java -cp bin dd --count=0xB800 --const=0 >>dos.d13
-java -cp bin CreateCatalog --version=310 --used=37 >>dos.d13
+java -cp bin CreateCatalog --version=310 >>dos.d13
 java -cp bin dd --count=0xDD00 --const=0 >>dos.d13
-java -cp bin DosMasterToImage dos.d13 dos.dsk
+
+java -cp bin ConvertD13toNibble <dos.d13 >dos.nib
