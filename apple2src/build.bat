c4d7ae93fc35af44136df86ef8b967abed78f5e4
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-10-18 19:21:07 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8817 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2src/build.bat
+++ apple2src/build.bat
@@ -5,7 +5,10 @@ SETLOCAL
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 
-SET CC65DIR=C:\apple2\windows_programs\assemblers\cc65\bin
+SET CC65DIR=C:\cc65\bin
+SET ACDTDIR=C:\aaaws44\DosMasterToImage
+
+SET A2CDT=%ACDTDIR%\a2cdt.jar
 
 IF EXIST build\NUL RMDIR /Q /S build
 MKDIR build
