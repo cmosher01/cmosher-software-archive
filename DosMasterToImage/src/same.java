e28ab8c6414e1c6d8a0c4bb3c06e4cf1d1db1181
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2007-10-23 01:26:17 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8839 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ DosMasterToImage/src/same.java
@@ -0,0 +1,45 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/*
+ * Created on 2007-10-22
+ */
+public class same
+{
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		if (args.length != 2)
+		{
+			throw new IllegalArgumentException("usage: java same file1 file2");
+		}
+		final BufferedInputStream in1 = new BufferedInputStream(new FileInputStream(new File(args[0])));
+		final BufferedInputStream in2 = new BufferedInputStream(new FileInputStream(new File(args[0])));
+
+		if (!same(in1,in2))
+		{
+			System.exit(1);
+		}
+	}
+
+	public static boolean same(final BufferedInputStream in1, final BufferedInputStream in2) throws IOException
+	{
+		int b1 = in1.read();
+		int b2 = in2.read();
+		while (b1 >= 0 && b2 >= 0)
+		{
+			if (b1 != b2)
+			{
+				return false;
+			}
+			b1 = in1.read();
+			b2 = in2.read();
+		}
+		return b1 == b2;
+	}
+}
---
+++ DosMasterToImage/src/sameTest.java
@@ -0,0 +1,67 @@
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import junit.framework.TestCase;
+
+/*
+ * Created on Oct 22, 2007
+ */
+/**
+ * TODO
+ *
+ * @author Chris Mosher
+ */
+public class sameTest extends TestCase
+{
+	/**
+	 * Test method for {@link same#same(java.io.BufferedInputStream, java.io.BufferedInputStream)}.
+	 */
+	public void testSame() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		assertTrue(same.same(in1,in2));
+	}
+
+	public void testOneLonger() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4}));
+		assertFalse(same.same(in1,in2));
+	}
+
+	public void testTwoLonger() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		assertFalse(same.same(in1,in2));
+	}
+
+	public void testDifferent() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,7,4,5}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		assertFalse(same.same(in1,in2));
+	}
+
+	public void testSameEmpty() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {}));
+		assertTrue(same.same(in1,in2));
+	}
+
+	public void testOneNonempty() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {}));
+		assertFalse(same.same(in1,in2));
+	}
+
+	public void testTwoNonempty() throws IOException
+	{
+		final BufferedInputStream in1 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {}));
+		final BufferedInputStream in2 = new BufferedInputStream(new ByteArrayInputStream(new byte[] {1,2,3,4,5}));
+		assertFalse(same.same(in1,in2));
+	}
+}
