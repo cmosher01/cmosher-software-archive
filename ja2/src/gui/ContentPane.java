3fa612eec842a2012bde62fa8d83abcca6fba152
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-01-04 21:53:39 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@8966 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ja2/src/gui/ContentPane.java
+++ ja2/src/gui/ContentPane.java
@@ -20,8 +20,9 @@ class ContentPane extends JPanel
 {
 	private final Screen screen;
 	private final Slots slots;
-	private DiskDrivePanel diskDrive1;
-	private DiskDrivePanel diskDrive2;
+	private DiskDriveControllerPanel diskController;
+//	private DiskDrivePanel diskDrive1;
+//	private DiskDrivePanel diskDrive2;
 	
 
 	public ContentPane(final Screen screen, final Slots slots, final GUI gui)
@@ -45,30 +46,33 @@ class ContentPane extends JPanel
 		Insets insets = getInsets();
 		this.screen.setBounds(insets.left,insets.top,szVideo.width,szVideo.height);
 
-		this.diskDrive1 = new DiskDrivePanel(drive1,gui);
-		add(this.diskDrive1);
-		Dimension szDisk = this.diskDrive1.getPreferredSize();
-		this.diskDrive1.setBounds(insets.left,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+		this.diskController = new DiskDriveControllerPanel(controller,gui);
+		this.diskController.setBounds(insets.left,szVideo.height+2,(int)this.diskController.getWidth(),(int)this.diskController.getHeight());
 
-		this.diskDrive2 = new DiskDrivePanel(drive2,gui);
-		add(this.diskDrive2);
-		this.diskDrive2.setBounds(insets.left+(int)szDisk.getWidth()+3,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+//		this.diskDrive1 = new DiskDrivePanel(drive1,gui);
+//		add(this.diskDrive1);
+//		Dimension szDisk = this.diskDrive1.getPreferredSize();
+//		this.diskDrive1.setBounds(insets.left,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+//
+//		this.diskDrive2 = new DiskDrivePanel(drive2,gui);
+//		add(this.diskDrive2);
+//		this.diskDrive2.setBounds(insets.left+(int)szDisk.getWidth()+3,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
 
 		setPreferredSize(new Dimension(szVideo.width+insets.left+insets.right,szVideo.height+szDisk.height+insets.top+insets.bottom));
 	}
 
-	private DiskDrivePanel getDrive(int drive)
-	{
-		if (drive == 0)
-		{
-			return this.diskDrive1;
-		}
-		if (drive == 1)
-		{
-			return this.diskDrive2;
-		}
-		throw new IllegalStateException();
-	}
+//	private DiskDrivePanel getDrive(int drive)
+//	{
+//		if (drive == 0)
+//		{
+//			return this.diskDrive1;
+//		}
+//		if (drive == 1)
+//		{
+//			return this.diskDrive2;
+//		}
+//		throw new IllegalStateException();
+//	}
 
 	public DropTargetListener getFirstDrivePanelDropListener()
 	{
@@ -80,51 +84,51 @@ class ContentPane extends JPanel
 		this.screen.plot(image);
 	}
 
-	public void updateDrives(boolean force)
-	{
-		final DiskDrivePanel drivePanelCurrent = getDrive(this.disk.getCurrentDriveNumber());
-		if (drivePanelCurrent == null)
-		{
-			return;
-		}
-
-		drivePanelCurrent.setCurrent(true);
-		drivePanelCurrent.setModified(this.disk.isModified());
-		drivePanelCurrent.setTrack(this.disk.getTrack());
-		drivePanelCurrent.setWriteProtected(this.disk.isWriteProtected());
-		if (this.disk.isMotorOn())
-		{
-			if (this.disk.isWriting())
-			{
-				drivePanelCurrent.setWriting(true);
-				drivePanelCurrent.setReading(false);
-			}
-			else
-			{
-				drivePanelCurrent.setWriting(false);
-				drivePanelCurrent.setReading(true);
-			}
-		}
-		else
-		{
-			drivePanelCurrent.setWriting(false);
-			drivePanelCurrent.setReading(false);
-		}
-		drivePanelCurrent.updateIf(force);
-
-		final DiskDrivePanel drivePanelOther = getDrive(this.disk.getOtherDriveNumber());
-		if (drivePanelOther == null)
-		{
-			return;
-		}
-		drivePanelOther.setCurrent(false);
-		drivePanelOther.setWriting(false);
-		drivePanelOther.setReading(false);
-
-		drivePanelOther.setModified(this.disk.isModifiedOther());
-
-		drivePanelOther.updateIf(force);
-	}
+//	public void updateDrives(boolean force)
+//	{
+//		final DiskDrivePanel drivePanelCurrent = getDrive(this.disk.getCurrentDriveNumber());
+//		if (drivePanelCurrent == null)
+//		{
+//			return;
+//		}
+//
+//		drivePanelCurrent.setCurrent(true);
+//		drivePanelCurrent.setModified(this.disk.isModified());
+//		drivePanelCurrent.setTrack(this.disk.getTrack());
+//		drivePanelCurrent.setWriteProtected(this.disk.isWriteProtected());
+//		if (this.disk.isMotorOn())
+//		{
+//			if (this.disk.isWriting())
+//			{
+//				drivePanelCurrent.setWriting(true);
+//				drivePanelCurrent.setReading(false);
+//			}
+//			else
+//			{
+//				drivePanelCurrent.setWriting(false);
+//				drivePanelCurrent.setReading(true);
+//			}
+//		}
+//		else
+//		{
+//			drivePanelCurrent.setWriting(false);
+//			drivePanelCurrent.setReading(false);
+//		}
+//		drivePanelCurrent.updateIf(force);
+//
+//		final DiskDrivePanel drivePanelOther = getDrive(this.disk.getOtherDriveNumber());
+//		if (drivePanelOther == null)
+//		{
+//			return;
+//		}
+//		drivePanelOther.setCurrent(false);
+//		drivePanelOther.setWriting(false);
+//		drivePanelOther.setReading(false);
+//
+//		drivePanelOther.setModified(this.disk.isModifiedOther());
+//
+//		drivePanelOther.updateIf(force);
+//	}
 
 	public boolean hasUnsavedChanges()
 	{
--- ja2/src/gui/DiskDriveControllerPanel.java
+++ ja2/src/gui/DiskDriveControllerPanel.java
@@ -6,21 +6,90 @@ package gui;
 import gui.buttons.DiskDrivePanel;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import disk.DiskController;
 
 public class DiskDriveControllerPanel extends JPanel
 {
+	final DiskController controller;
+	final GUI gui;
+
 	private DiskDrivePanel diskDrive1;
 	private DiskDrivePanel diskDrive2;
 
+	public DiskDriveControllerPanel(final DiskController controller, final GUI gui)
+	{
+		this.controller = controller;
+		this.gui = gui;
+	}
+
 	private void setUp()
 	{
-		this.diskDrive1 = new DiskDrivePanel(drive1,gui);
+		this.diskDrive1 = new DiskDrivePanel(this.controller.getDiskBytes(0),this.gui);
 		add(this.diskDrive1);
 		Dimension szDisk = this.diskDrive1.getPreferredSize();
-		this.diskDrive1.setBounds(insets.left,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+		this.diskDrive1.setBounds(0,2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
 
-		this.diskDrive2 = new DiskDrivePanel(drive2,gui);
+		this.diskDrive2 = new DiskDrivePanel(this.controller.getDiskBytes(1),this.gui);
 		add(this.diskDrive2);
-		this.diskDrive2.setBounds(insets.left+(int)szDisk.getWidth()+3,szVideo.height+2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+		this.diskDrive2.setBounds((int)szDisk.getWidth()+3,2,(int)szDisk.getWidth(),(int)szDisk.getHeight());
+	}
+
+	public void updateDrives(boolean force)
+	{
+		final DiskDrivePanel drivePanelCurrent = getDrive(this.controller.getCurrentDriveNumber());
+		if (drivePanelCurrent == null)
+		{
+			return;
+		}
+
+		drivePanelCurrent.setCurrent(true);
+		drivePanelCurrent.setModified(this.controller.isModified());
+		drivePanelCurrent.setTrack(this.controller.getTrack());
+		drivePanelCurrent.setWriteProtected(this.controller.isWriteProtected());
+		if (this.controller.isMotorOn())
+		{
+			if (this.controller.isWriting())
+			{
+				drivePanelCurrent.setWriting(true);
+				drivePanelCurrent.setReading(false);
+			}
+			else
+			{
+				drivePanelCurrent.setWriting(false);
+				drivePanelCurrent.setReading(true);
+			}
+		}
+		else
+		{
+			drivePanelCurrent.setWriting(false);
+			drivePanelCurrent.setReading(false);
+		}
+		drivePanelCurrent.updateIf(force);
+
+		final DiskDrivePanel drivePanelOther = getDrive(this.controller.getOtherDriveNumber());
+		if (drivePanelOther == null)
+		{
+			return;
+		}
+		drivePanelOther.setCurrent(false);
+		drivePanelOther.setWriting(false);
+		drivePanelOther.setReading(false);
+
+		drivePanelOther.setModified(this.controller.isModifiedOther());
+
+		drivePanelOther.updateIf(force);
+	}
+
+	private DiskDrivePanel getDrive(int drive)
+	{
+		if (drive == 0)
+		{
+			return this.diskDrive1;
+		}
+		if (drive == 1)
+		{
+			return this.diskDrive2;
+		}
+		throw new IllegalStateException();
 	}
 }
