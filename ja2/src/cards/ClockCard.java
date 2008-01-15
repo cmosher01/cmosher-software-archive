384a60a153aad007e1f3c449c8c872152d537278
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-01-15 22:28:25 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9029 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- ja2/src/cards/ClockCard.java
+++ ja2/src/cards/ClockCard.java
@@ -6,11 +6,12 @@ package cards;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Formatter;
+import java.util.TimeZone;
 
 public class ClockCard extends Card
 {
 	private byte latch;
-	private int pos = 0xd;
+	private int pos;
 	private String time;
 
 	@Override
@@ -21,16 +22,16 @@ public class ClockCard extends Card
 		{
 			if (this.latch >= 0)
 			{
-				if (this.pos == 0xd)
+				if (this.pos == 0)
 				{
 					getTime();
 				}
 				final char c = this.time.charAt(this.pos);
 				this.latch = (byte)(c | 0x80);
-				--this.pos;
-				if (this.pos < 0)
+				++this.pos;
+				if (this.pos >= this.time.length())
 				{
-					this.pos = 0xd;
+					this.pos = 0;
 				}
 			}
 		}
@@ -50,9 +51,16 @@ public class ClockCard extends Card
 		final int day = cal.get(Calendar.DAY_OF_MONTH);
 		final int hour = cal.get(Calendar.HOUR_OF_DAY);
 		final int minute = cal.get(Calendar.MINUTE);
+		final int second = cal.get(Calendar.SECOND);
+		final int millis = cal.get(Calendar.MILLISECOND);
+		final int year = cal.get(Calendar.YEAR);
+		final TimeZone zone = cal.getTimeZone();
+		final boolean dst = zone.inDaylightTime(cal.getTime());
+		final int zonemillis = zone.getRawOffset();
+
 		Formatter fmt = new Formatter();
 		// mo,da,dt,hr,mn
-		fmt.format("%02d,%02d,%02d,%02d,%02d",month,weekday,day,hour,minute);
+		fmt.format("%02d,%02d,%02d,%02d,%02d,%02d,%03d,%04d,%d08d,%01d\r",month,weekday,day,hour,minute,second,millis,year,zonemillis,(dst ? 1 : 0));
 		this.time = fmt.toString();
 	}
 
