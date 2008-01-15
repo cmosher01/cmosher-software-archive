b8c93e39ea9c5c8123885acc9e600e3cc7c30053
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-01-15 19:51:28 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9027 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ ja2/firmware/build.bat
@@ -0,0 +1,14 @@
+SET CC65_HOME=C:\cc65
+
+
+
+SET CC65DIR=%CC65_HOME%\bin
+
+"%CC65DIR%\ca65" -v -l -t apple2 -o clock.o65 clock.s65
+"%CC65DIR%\ld65" -v -C clock.cfg clock.o65
+
+"%CC65DIR%\ca65" -v -l -t apple2 -o stdin.o65 stdin.s65
+"%CC65DIR%\ld65" -v -C stdin.cfg stdin.o65
+
+"%CC65DIR%\ca65" -v -l -t apple2 -o stdout.o65 stdout.s65
+"%CC65DIR%\ld65" -v -C stdout.cfg stdout.o65
