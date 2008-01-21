02a3f344849ada88e7785a8e27bfca7a4eeabba1
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-01-21 23:09:50 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9052 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- tv/src/AnalogTV.java
+++ tv/src/AnalogTV.java
@@ -13,6 +13,37 @@ public class AnalogTV
 
 
 
+	private static class analogtv_yiq
+	{
+		private final double y;
+		private final double i;
+		private final double q;
+
+		public analogtv_yiq(final double y, final double i, final double q)
+		{
+			this.y = y;
+			this.i = i;
+			this.q = q;
+		}
+
+		public double getI()
+		{
+			return this.i;
+		}
+
+		public double getQ()
+		{
+			return this.q;
+		}
+
+		public double getY()
+		{
+			return this.y;
+		}
+	}
+
+
+
 	public void dump_signal()
 	{
 		int pi = 0;
