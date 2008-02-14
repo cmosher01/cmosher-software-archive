8921aa96ad1663c47e0d755c9938a6c7dd7e064c
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-02-14 22:09:20 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9133 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- epple2qt/src/timinggenerator.cpp
+++ epple2qt/src/timinggenerator.cpp
@@ -30,3 +30,53 @@ const int TimingGenerator::EXTRA_CRYSTAL_CYCLES_PER_CPU_LONG_CYCLE(2);
 const int TimingGenerator::HORIZ_CYCLES(65);
 const int TimingGenerator::AVG_CPU_HZ((int)((double)315000000*TimingGenerator::HORIZ_CYCLES)/(22*(TimingGenerator::CRYSTAL_CYCLES_PER_CPU_CYCLE*TimingGenerator::HORIZ_CYCLES+TimingGenerator::EXTRA_CRYSTAL_CYCLES_PER_CPU_LONG_CYCLE)));
 const int TimingGenerator::CPU_HZ(Util::divideRoundUp(TimingGenerator::CRYSTAL_HZ,TimingGenerator::CRYSTAL_CYCLES_PER_CPU_CYCLE));
+
+
+// This is just an example of how to use Boost threads mutex and condition
+/*
+class buffer
+{
+private:
+  int buf[BUF_SIZE];
+  unsigned int p, g, size;
+  boost::mutex mutex;
+  boost::condition cond;
+
+public:
+  buffer() : p(0), g(0), size(0) { }
+
+  void put(const int m)
+  {
+    boost::mutex::scoped_lock lock(mutex);
+
+    while (size >= BUF_SIZE)
+    {
+      cond.wait(lock);
+    }
+
+    buf[p++] = m;
+    p %= BUF_SIZE;
+
+    ++size;
+    cond.notify_all();
+  }
+
+  int get()
+  {
+    boost::mutex::scoped_lock lock(mutex);
+
+    while (size <= 0)
+    {
+      cond.wait(lock);
+    }
+
+    const int i = buf[g++];
+    g %= BUF_SIZE;
+
+    --size;
+    cond.notify_all();
+
+    return i;
+  }
+};
+*/
