a742f01ee5d70ce5b45fec5191dd864c466ded41
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-06-14 15:06:44 +0000

    Latest from Source Safe (V1.4 plus changes).
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9333 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ gro/obsolete/Password.h
@@ -0,0 +1,48 @@
+#if !defined(AFX_PASSWORD_H__969049A5_89A3_11D4_9E6A_0000F1112C95__INCLUDED_)
+#define AFX_PASSWORD_H__969049A5_89A3_11D4_9E6A_0000F1112C95__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// Password.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CPassword dialog
+
+class CPassword : public CDialog
+{
+// Construction
+public:
+	CPassword(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CPassword)
+	enum { IDD = IDD_PWD };
+	CString	m_strOldPwd;
+	CString	m_Pwd1;
+	CString	m_Pwd2;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPassword)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CPassword)
+		// NOTE: the ClassWizard will add member functions here
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_PASSWORD_H__969049A5_89A3_11D4_9E6A_0000F1112C95__INCLUDED_)
