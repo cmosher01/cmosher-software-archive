c4dccf35a04be7d2635fe54a6e1d16891ef4bfe2
Chris <Chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-08-04 22:37:41 +0000

    Apple 2 High Resolution Font generator. Initial checkin. Originals from Mark Foerster c. 1984.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9348 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ hrf/build.bat
@@ -0,0 +1,4 @@
+SET CC65_HOME=C:\apple2\windows_programs\assemblers\cc65
+SET CC65DIR=%CC65_HOME%\bin
+"%CC65DIR%\ca65" -v -l -t apple2 -o hrf040.o65 hrf040.s65
+"%CC65DIR%\ld65" -v -C hrf040.ld65config -m hrf040.map hrf040.o65
