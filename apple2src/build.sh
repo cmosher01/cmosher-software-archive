c23f8c976cdcbdc5aa841ac4cbf7ea3f990e91fc
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-09-25 21:01:04 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9391 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd dos
-./build.sh
-cd ..
-
-cd firmware
-./build.sh
-cd ..
--- apple2src/config.bat
+++
@@ -1,6 +0,0 @@
-SET CC65_HOME=C:\apple2\windows_programs\assemblers\cc65
-SET ACDT_HOME=C:\eclipse_organize\postsvn_workspace\a2cdt
-
-REM SET ECHO=OFF
-REM SET ECHO=ON
-SET ECHO=OFF
