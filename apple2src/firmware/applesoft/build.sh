168a3f3e397d8ed522c72e34dfb2d8f868303294
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-09-25 15:50:55 +0000

    clean up
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9378 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2src/firmware/applesoft/build.sh
+++
@@ -1,8 +0,0 @@
-INCDIR=../../../lib/macros
-
-asm()
-{
-	ca65 -v -t apple2 -I ../../applesoft -I $INCDIR -o $1.o65 ../../applesoft/$1.s65
-}
-asm applesoft
-ld65 -v -C ../../applesoft/as.cfg applesoft.o65
