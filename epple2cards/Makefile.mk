35483ca5fddb38ef5129fd163c4d434853e4a6b0
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2008-10-09 03:19:22 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9443 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- epple2cards/Makefile.mk
+++ epple2cards/Makefile.mk
@@ -1,4 +1,7 @@
 # Builds Epple ][ card ROMs: stdout, stdin, clock
+
+.SUFFIXES:
+
 .SUFFIXES: .s65 .o65 .ld65 .ex65
 
 .s65.o65:
@@ -22,17 +25,43 @@ clock.ex65: clock.o65 card.ld65
 
 
 
+DIST=epple2cards-1.0
+dist: card.ld65  clock.s65  configure  Makefile.mk  stdin.s65  stdout.s65
+	rm -fv $(DIST).tar.gz
+	rm -Rfv $(DIST)
+	mkdir -v $(DIST)
+	cp -v $^ $(DIST)
+	tar cvzf $(DIST).tar.gz $(DIST)
 
-install:
-	mkdir -p $(PREFIX)/lib/epple2/cards/
-	cp stdout.ex65 $(PREFIX)/lib/epple2/cards
-	cp stdin.ex65 $(PREFIX)/lib/epple2/cards
-	cp clock.ex65 $(PREFIX)/lib/epple2/cards
 
 
+install: all
+	mkdir -pv $(DESTDIR)$(PREFIX)/lib/epple2/cards/
+	cp -v stdout.ex65 $(DESTDIR)$(PREFIX)/lib/epple2/cards
+	cp -v stdin.ex65 $(DESTDIR)$(PREFIX)/lib/epple2/cards
+	cp -v clock.ex65 $(DESTDIR)$(PREFIX)/lib/epple2/cards
+
+
+
+uninstall:
+	rm -fv $(DESTDIR)$(PREFIX)/lib/epple2/cards/stdout.ex65
+	rm -fv $(DESTDIR)$(PREFIX)/lib/epple2/cards/stdin.ex65
+	rm -fv $(DESTDIR)$(PREFIX)/lib/epple2/cards/clock.ex65
+	rmdir -pv $(DESTDIR)$(PREFIX)/lib/epple2/cards/
 
 
 
 clean:
 	find . -name "*.ex65" | xargs rm -fv
 	find . -name "*.o65" | xargs rm -fv
+	rm -fv $(DIST).tar.gz
+	rm -Rfv $(DIST)
+
+
+
+mostlyclean: clean
+
+
+
+distclean: clean
+	rm -fv Makefile
