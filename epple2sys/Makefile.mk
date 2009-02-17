f850fc9ba9439513cc6289a82284c5ffb1297fe0
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2009-02-17 05:14:27 +0000

    Initial development.
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9501 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ epple2sys/Makefile.mk
@@ -0,0 +1,76 @@
+# Builds Epple ][ System ROM code
+
+NAME = epple2sys
+VERSION = 1.0
+
+DIST = $(NAME)-$(VERSION)
+
+.SUFFIXES:
+
+.SUFFIXES: .s65 .o65 .ld65 .ex65
+
+.s65.o65:
+	$(CA65) -v -t apple2 -I $(dir $<) -o $@ $<
+
+.o65.ex65:
+	$(LD65) -v -C $(filter %.ld65,$^) -o $@ $(filter %.o65,$^)
+
+
+
+
+
+.PHONY: all install clean
+
+
+
+e2system.ex65: e2system.o65 e2system.ld65
+
+
+
+dist: e2system.ld65  e2system.s65  configure  Makefile.mk
+	rm -fv $(DIST).tar.gz
+	rm -Rfv $(DIST)
+	mkdir -v $(DIST)
+	cp -v $^ $(DIST)
+	tar cvzf $(DIST).tar.gz $(DIST)
+	rm -Rf $(DIST)
+
+
+
+install: all
+	mkdir -pv $(DESTDIR)$(PREFIX)/lib/epple2/system/
+	cp -v e2system.ex65 $(DESTDIR)$(PREFIX)/lib/epple2/system
+
+
+
+uninstall:
+	rm -fv $(DESTDIR)$(PREFIX)/lib/epple2/system/e2system.ex65
+	rmdir -pv $(DESTDIR)$(PREFIX)/lib/epple2/system/
+
+
+
+clean:
+	find . -name "*.ex65" | xargs rm -fv
+	find . -name "*.o65" | xargs rm -fv
+	rm -fv $(DIST).tar.gz
+	rm -Rfv $(DIST)
+
+
+
+mostlyclean: clean
+
+
+
+distclean: clean
+	rm -fv Makefile
+
+RPM = /usr/src/rpm
+TMP = /tmp/rpm
+
+ARCH = noarch
+
+package: $(NAME).spec
+	mkdir -p $(TMP)
+	cp $(NAME)-$(VERSION).tar.gz $(RPM)/SOURCES
+	rpmbuild -ba --clean --buildroot $(TMP) $<
+	fakeroot alien $(RPM)/RPMS/$(ARCH)/$(NAME)-$(VERSION)-1.$(ARCH).rpm
---
+++ epple2sys/e2system.spec
@@ -0,0 +1,30 @@
+Summary: EPPLE ][ System ROM
+Name: epple2system
+Version: 1.0
+Release: 1
+Source: %{name}-%{version}.tar.gz
+License: GPL
+Group: System/Emulators/Other
+BuildArch: noarch
+
+%description
+Demo free ROMs for EPPLE ][ emulator.
+Not compatible with any Apple ][ software.
+Just some free (GPLv3) ROM code to demo the emulator.
+
+%prep
+%setup -q
+
+%build
+./configure -p %{_prefix}
+make
+
+%install
+rm -Rf %{buildroot}/*
+make DESTDIR=%{buildroot} install
+
+%clean
+rm -Rf %{buildroot}/*
+
+%files
+%dir %{_libdir}/epple2/system
