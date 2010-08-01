7e3292604c79032b136534e1fbf177e5b3cc9a8d
chris <chris@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2010-08-01 15:48:39 +0000

    make fixes
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9638 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2src/dos3x/Makefile.mk
+++ apple2src/dos3x/Makefile.mk
@@ -7,12 +7,15 @@ ARCH = noarch
 DIST = $(NAME)-$(VERSION)
 
 .s65.o65:
+	mkdir -p $(@D)
 	$(CA65) -v -t apple2 -o $@ -I $(<D) -I $(INCDIR) $(CA65DEFS) $<
 
 .o65.ex65:
+	mkdir -p $(@D)
 	$(LD65) -v -C $(filter %.ld65,$^) -o $@ $(filter %.o65,$^)
 
 .ex65.d13:
+	mkdir -p $(@D)
 	$(CDT) dd --skip=0x1B00 <$< >$@
 	$(CDT) dd --count=0x1B00 <$< >>$@
 	$(CDT) dd --count=0xB800 --const=0 >>$@
@@ -20,9 +23,11 @@ DIST = $(NAME)-$(VERSION)
 	$(CDT) dd --count=0xDD00 --const=0 >>$@
 
 .d13.nib:
+	mkdir -p $(@D)
 	$(CDT) ConvertD13toNibble <$< >$@
 
 .ex65.do:
+	mkdir -p $(@D)
 	$(CDT) dd --skip=0x1B00 <$< >$@
 	$(CDT) dd --count=0x1B00 <$< >>$@
 	$(CDT) dd --count=0xEB00 --const=0 >>$@
@@ -30,33 +35,12 @@ DIST = $(NAME)-$(VERSION)
 	$(CDT) dd --count=0x11000 --const=0 >>$@
 
 .do.nib:
+	mkdir -p $(@D)
 	$(CDT) ConvertD16toNibble <$< >$@
 
 .SUFFIXES: .s65 .o65 .ld65 .ex65 .d13 .do .nib .wxs .wixobj .msi
 
-SUBDIRS = \
-	13sector \
-	13sector/controller \
-	13sector/software \
-	13sector/software/dos310 \
-	13sector/software/dos320 \
-	13sector/software/dos321 \
-	13sector/disks \
-	13sector/disks/dos310 \
-	13sector/disks/dos320 \
-	13sector/disks/dos321 \
-	16sector \
-	16sector/controller \
-	16sector/software \
-	16sector/software/dos330 \
-	16sector/software/dos331 \
-	16sector/software/dos332 \
-	16sector/disks \
-	16sector/disks/dos330 \
-	16sector/disks/dos331 \
-	16sector/disks/dos332
-
-.PHONY: all subdirs $(SUBDIRS) install clean
+.PHONY: all install clean
 
 
 ifdef WINDOWS
@@ -77,7 +61,6 @@ endif
 
 
 all: \
-	subdirs \
 	13sector/controller/disk2.ex65 \
 	13sector/software/dos310/dos.ex65 \
 	13sector/software/dos320/dos.ex65 \
@@ -94,11 +77,6 @@ all: \
 	16sector/disks/dos332/clean332.nib
 
 
-# TODO: remove this bad subdirs thing
-subdirs: $(SUBDIRS)
-	mkdir -p $^
-
-
 .PRECIOUS: %.d13 %.do
 
 MODS = dos reloc main filemgr boot1 boot2 rwts rwtsapi
@@ -110,6 +88,7 @@ MODS = dos reloc main filemgr boot1 boot2 rwts rwtsapi
 13sector/controller/disk2.ex65: CA65DEFS = -D VERSION=13 -D NODELAY
 13sector/controller/disk2.ex65: 13sector/controller/disk2.o65 controller/disk2.ld65
 13sector/controller/disk2.s65: controller/disk2.s65
+	mkdir -p $(@D)
 	cp $< $@
 
 
@@ -119,11 +98,13 @@ OBJS_310 = $(foreach mod,$(MODS),13sector/software/dos310/$(mod).o65)
 13sector/software/dos310/dos.ex65: CA65DEFS = -D VERSION=310 -D NODELAY
 13sector/software/dos310/dos.ex65: $(OBJS_310) software/dos.ld65
 13sector/software/dos310/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 13sector/disks/dos310/clean310.ex65
 13sector/disks/dos310/clean310.nib: VERSION = 310
 13sector/disks/dos310/clean310.ex65: 13sector/software/dos310/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 13sector/disks/dos320/clean320.nib: 13sector/disks/dos320/clean320.d13
@@ -131,11 +112,13 @@ OBJS_320 = $(foreach mod,$(MODS),13sector/software/dos320/$(mod).o65)
 13sector/software/dos320/dos.ex65: CA65DEFS = -D VERSION=320 -D NODELAY
 13sector/software/dos320/dos.ex65: $(OBJS_320) software/dos.ld65
 13sector/software/dos320/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 13sector/disks/dos320/clean320.ex65
 13sector/disks/dos320/clean320.nib: VERSION = 320
 13sector/disks/dos320/clean320.ex65: 13sector/software/dos320/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 13sector/disks/dos321/clean321.nib: 13sector/disks/dos321/clean321.d13
@@ -143,11 +126,13 @@ OBJS_321 = $(foreach mod,$(MODS),13sector/software/dos321/$(mod).o65)
 13sector/software/dos321/dos.ex65: CA65DEFS = -D VERSION=321 -D NODELAY
 13sector/software/dos321/dos.ex65: $(OBJS_321) software/dos.ld65
 13sector/software/dos321/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 13sector/disks/dos321/clean321.ex65
 13sector/disks/dos321/clean321.nib: VERSION = 321
 13sector/disks/dos321/clean321.ex65: 13sector/software/dos321/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 
@@ -156,6 +141,7 @@ OBJS_321 = $(foreach mod,$(MODS),13sector/software/dos321/$(mod).o65)
 16sector/controller/disk2.ex65: CA65DEFS = -D VERSION=16 -D NODELAY
 16sector/controller/disk2.ex65: 16sector/controller/disk2.o65 controller/disk2.ld65
 16sector/controller/disk2.s65: controller/disk2.s65
+	mkdir -p $(@D)
 	cp $< $@
 
 16sector/disks/dos330/clean330.nib: 16sector/disks/dos330/clean330.do
@@ -163,11 +149,13 @@ OBJS_330 = $(foreach mod,$(MODS),16sector/software/dos330/$(mod).o65)
 16sector/software/dos330/dos.ex65: CA65DEFS = -D VERSION=330 -D NODELAY
 16sector/software/dos330/dos.ex65: $(OBJS_330) software/dos.ld65
 16sector/software/dos330/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 16sector/disks/dos330/clean330.ex65
 16sector/disks/dos330/clean330.nib: VERSION = 330
 16sector/disks/dos330/clean330.ex65: 16sector/software/dos330/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 16sector/disks/dos331/clean331.nib: 16sector/disks/dos331/clean331.do
@@ -175,11 +163,13 @@ OBJS_331 = $(foreach mod,$(MODS),16sector/software/dos331/$(mod).o65)
 16sector/software/dos331/dos.ex65: CA65DEFS = -D VERSION=331 -D NODELAY
 16sector/software/dos331/dos.ex65: $(OBJS_331) software/dos.ld65
 16sector/software/dos331/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 16sector/disks/dos331/clean331.ex65
 16sector/disks/dos331/clean331.nib: VERSION = 331
 16sector/disks/dos331/clean331.ex65: 16sector/software/dos331/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 16sector/disks/dos332/clean332.nib: 16sector/disks/dos332/clean332.do
@@ -187,11 +177,13 @@ OBJS_332 = $(foreach mod,$(MODS),16sector/software/dos332/$(mod).o65)
 16sector/software/dos332/dos.ex65: CA65DEFS = -D VERSION=332 -D NODELAY
 16sector/software/dos332/dos.ex65: $(OBJS_332) software/dos.ld65
 16sector/software/dos332/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
+	mkdir -p $(@D)
 	cp $? $(@D)
 
 .INTERMEDIATE: 16sector/disks/dos332/clean332.ex65
 16sector/disks/dos332/clean332.nib: VERSION = 332
 16sector/disks/dos332/clean332.ex65: 16sector/software/dos332/dos.ex65
+	mkdir -p $(@D)
 	cp $? $@
 
 
@@ -363,7 +355,13 @@ dist:	\
 RPM = $(abspath rpm)
 
 package: $(NAME).spec dist
-	mkdir -p $(RPM)/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS,VPATH}
+	mkdir -p $(RPM)/BUILD
+	mkdir -p $(RPM)/BUILDROOT
+	mkdir -p $(RPM)/RPMS
+	mkdir -p $(RPM)/SOURCES
+	mkdir -p $(RPM)/SPECS
+	mkdir -p $(RPM)/SRPMS
+	mkdir -p $(RPM)/VPATH
 	cp $(NAME)-$(VERSION).tar.gz $(RPM)/SOURCES
 	touch .rpmmacros
 	echo "%_prefix $(PREFIX)" >>$(RPM)/.rpmmacros
