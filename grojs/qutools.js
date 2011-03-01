c3fd52cc9d4cdbc354b5e8d0a174887d7222d9d1
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-01 13:01:29 +0000

    QUint tools
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9677 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ grojs/qutools.js
@@ -0,0 +1,7 @@
+$(function() {
+	QUnit.log = function(details) {
+		if (window.console && window.console.log) {
+		   window.console.log(details.result+": expected "+details.expected+", was actually "+details.actual);
+		}
+	}
+});
