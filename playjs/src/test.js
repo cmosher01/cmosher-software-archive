9d8cf88cc80e079d96e0bc6fc9f8cb8ffe9f271e
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-03 22:04:41 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9698 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- playjs/src/test.js
+++ playjs/src/test.js
@@ -105,32 +105,32 @@ var stdout = new DomBodyOutputLister(this.document);
 
 //Define the constructor.
 //Note how it initializes the object referred to by "this".
-function Rectangle(width, height)
-{
-	if (this === window) {
-		throw new Error("must use new to construct an object.");
-	}
-	this.width = width;
-	this.height = height;
-	if (this.width === undefined || this.width === null) {
-		throw new Error("must define width");
-	}
-	if (this.height === undefined || this.height === null) {
-		throw new Error("must define height");
-	}
-}
+//function Rectangle(width, height)
+//{
+//	if (this === window) {
+//		throw new Error("must use new to construct an object.");
+//	}
+//	this.width = width;
+//	this.height = height;
+//	if (this.width === undefined || this.width === null) {
+//		throw new Error("must define width");
+//	}
+//	if (this.height === undefined || this.height === null) {
+//		throw new Error("must define height");
+//	}
+//}
 //Invoke the constructor to create two Rectangle objects.
 //We pass the width and height to the constructor,
 //so it can initialize each new object appropriately.
-var rect1 = new Rectangle(2, 4);
-stdout.addLine(rect1.width);
-stdout.addLine(rect1.height);
-
-var rect3 = new Rectangle(5,6);
-stdout.addLine("here");
-
-stdout.addLine(rect3.width);
-stdout.addLine(rect3.height);
+//var rect1 = new Rectangle(2, 4);
+//stdout.addLine(rect1.width);
+//stdout.addLine(rect1.height);
+//
+//var rect3 = new Rectangle(5,6);
+//stdout.addLine("here");
+//
+//stdout.addLine(rect3.width);
+//stdout.addLine(rect3.height);
 
 //Rectangle(1,1);
 /*
@@ -143,3 +143,95 @@ try {
 
 /*stdout.addLine(window.java);*/
 
+
+
+
+function TypeDef() {
+}
+
+TypeDef.getName = function(x) {
+  var n;
+
+  if (x === undefined) {
+    return "undefined";
+  }
+  if (x === null) {
+    return "null";
+  }
+
+  n = Object.prototype.toString.apply(x);
+  n = /\[object\s*(.*)\s*\]/.exec(n)[1];
+
+  if (n !== "Object") {
+    return n;
+  }
+
+  n = x.constructor;
+  if (n === undefined || n === null) {
+    return "Object";
+  }
+
+  n = n.name;
+  if (n === undefined || n === null) {
+    return "Object";
+  }
+
+  return n;
+};
+
+TypeDef.define = function(clsName,obj) {
+	if (obj.hasOwnProperty("typeName")) {
+    throw new TypeError("Object is already a "+obj.typeName);
+	}
+  if (TypeDef.getName(obj) !== clsName) {
+    throw new TypeError("Object must be of class "+clsName);
+  }
+	obj.typeName = clsName;
+};
+
+TypeDef.isStaticCall = function(obj) {
+	return !obj.hasOwnProperty("typeName");
+};
+
+
+
+
+
+
+function Thing() {
+  TypeDef.define("Thing",this);
+  stdout.addLine("Thing ctor: this is "+Object.prototype.toString.apply(this)+"; "+this.constructor.name+"; typeName="+this.typeName);
+  stdout.addLine("Thing ctor: isStaticCall()-->"+TypeDef.isStaticCall(this));
+};
+Thing.prototype.fn = function() {
+  stdout.addLine("Thing fn: this is "+Object.prototype.toString.apply(this)+"; "+this.constructor.name+"; typeName="+this.typeName);
+  stdout.addLine("Thing fn: isStaticCall()-->"+TypeDef.isStaticCall(this));
+};
+Thing.sfn = function() {
+  stdout.addLine("Thing sfn: this is "+Object.prototype.toString.apply(this)+"; "+this.constructor.name+"; name="+this.name+"; typeName="+this.typeName);
+  stdout.addLine("Thing sfn: isStaticCall()-->"+TypeDef.isStaticCall(this));
+};
+var fff = Thing.prototype.fn;
+
+stdout.addLine("-----------------------   x = new Thing();");
+var x = new Thing();
+stdout.addLine("-----------------------   xx = Thing();");
+try {
+	var xx = Thing();
+} catch (e) {
+	stdout.addLine(e);
+}
+stdout.addLine("-----------------------   x.fn();");
+x.fn();
+stdout.addLine("-----------------------   Thing.prototype.fn();       ????????????");
+Thing.prototype.fn();
+stdout.addLine("-----------------------   new Thing().fn();");
+new Thing().fn();
+stdout.addLine("-----------------------   fff();");
+fff();
+stdout.addLine("-----------------------   Thing.prototype.fn.apply();");
+Thing.prototype.fn.apply();
+stdout.addLine("-----------------------   Thing.prototype.fn.call();");
+Thing.prototype.fn.call();
+stdout.addLine("-----------------------   Thing.sfn();");
+Thing.sfn();
