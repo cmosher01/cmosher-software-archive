9473ad356894801d616ae14c3068b54af3e6d7c0
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-12 21:31:14 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9779 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/test/nu/mine/mosher/core/CloserTest.java
+++ core50/test/nu/mine/mosher/core/CloserTest.java
@@ -3,75 +3,87 @@
  */
 package nu.mine.mosher.core;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * TODO
  *
  * @author Chris Mosher
  */
-public class CloserTest extends TestCase
+public class CloserTest
 {
 	private static final class Thing1
 	{
 		public boolean closed;
+		@SuppressWarnings("unused")
 		public void close() { this.closed = true; }
 	}
 
 	private static final class Thing2
 	{
 		public boolean closed;
+		@SuppressWarnings("unused")
 		public void close() { this.closed = true; }
 	}
 
 
 
-
+	@Test
 	public void testClose1()
 	{
+		@SuppressWarnings("synthetic-access")
 		Thing1 t1 = new Thing1();
 		assertFalse(t1.closed);
-		Closer.close(t1);
+		final Throwable hadError = Closer.close(t1);
 		assertTrue(t1.closed);
 
-		assertFalse(Closer.hasErrors());
+		assertTrue(hadError == null);
 	}
 
+	@Test
 	public void testClose2()
 	{
 		{
+			@SuppressWarnings("synthetic-access")
 			Thing1 t1 = new Thing1();
 			assertFalse(t1.closed);
-			Closer.close(t1);
+			final Throwable hadError = Closer.close(t1);
 			assertTrue(t1.closed);
+			assertTrue(hadError == null);
 		}
 
 		{
+			@SuppressWarnings("synthetic-access")
 			Thing1 u1 = new Thing1();
 			assertFalse(u1.closed);
-			Closer.close(u1);
+			final Throwable hadError = Closer.close(u1);
 			assertTrue(u1.closed);
+			assertTrue(hadError == null);
 		}
-
-		assertFalse(Closer.hasErrors());
 	}
 
+	@Test
 	public void testClose3()
 	{
 		{
+			@SuppressWarnings("synthetic-access")
 			Thing1 t1 = new Thing1();
 			assertFalse(t1.closed);
-			Closer.close(t1);
+			final Throwable hadError = Closer.close(t1);
 			assertTrue(t1.closed);
+			assertTrue(hadError == null);
 		}
 
 		{
+			@SuppressWarnings("synthetic-access")
 			Thing2 u1 = new Thing2();
 			assertFalse(u1.closed);
-			Closer.close(u1);
+			final Throwable hadError = Closer.close(u1);
 			assertTrue(u1.closed);
+			assertTrue(hadError == null);
 		}
-
-		assertFalse(Closer.hasErrors());
 	}
 }
