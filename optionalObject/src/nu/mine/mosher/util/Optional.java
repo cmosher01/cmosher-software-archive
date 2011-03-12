d130a9da5e9499b828f83f042c95a464b32d7c83
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-12 02:47:18 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9748 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- optionalObject/src/nu/mine/mosher/util/Optional.java
+++ optionalObject/src/nu/mine/mosher/util/Optional.java
@@ -1,13 +1,51 @@
 package nu.mine.mosher.util;
-/*
- * Created on Mar 17, 2006
- */
+
+
 
 /**
- * A safe container for an optional object.
- * 
- * @author Chris Mosher
+ * A safe container for an optional object. Avoids the need to use
+ * null references, and therefore avoids <code>NullPointerException</code>s.
+ * Typical use is to declare a parameter or a return of type <code>X</code> that could
+ * be null as <code>Optional&lt;X&gt;</code>.
+ * <p>
+ * For example, to return an optional <code>String</code>, declare the return type
+ * as <code>Optional&lt;String&gt;</code>. Then, return either new <code>Optional&lt;String&gt;(result)</code>
+ * if the string exists, or new <code>Optional&lt;String&gt;(null)</code> if the string does
+ * not exist (instead of returning just <code>null</code>).
+ * <blockquote>
+ * <pre>
+ * public Optional&lt;String&gt; getName()
+ * {
+ *     if (isPrivate())
+ *         return new Optional&lt;String&gt;(null);
+ *     return new Optional&lt;String&gt;(name);
+ * }
+ * </pre>
+ * </blockquote>
+ * </p>
+ * <p>
+ * The consumer of such an object can check the existance by using
+ * the <code>exists</code> method:
+ * <blockquote>
+ * <pre>
+ * Optional&lt;String&gt; s = x.getName();
+ * String name;
+ * if (s.exists())
+ *     name = s.get();
+ * else
+ *     name = "[private]";
+ * </pre>
+ * </blockquote>
+ * </p>
+ * <p>
+ * If the consumer accidentally calls the <code>get</code> method on an object that does not
+ * exist, <code>get</code> will throw an <code>Optional.FieldDoesNotExist</code> exception, which
+ * will contain as its cause an <code>IllegalStateException</code> with a stack trace of where
+ * the object was assigned <code>null</code>.
+ * </p>
+ * @author mosherc
  * @param <T> data-type of the wrapped object
+ * @see Nothing
  */
 public class Optional<T>
 {
@@ -15,40 +53,33 @@ public class Optional<T>
 
     private final boolean exists;
     private final boolean considerTwoNullsEqual;
-    private final String name;
     private final Throwable whereSet;
 
-
     /**
-     * @param objectOrNull the object to wrap, or
-     * <code>null</code> if the object doesn't exist
-     * @param name a readable name for the wrapped object (for error messages)
+     * Initializes an optional wrapped object. Also,
+     * null wrapped objects will be considered equal.
+     * @param objectOrNull
+     *            the object to wrap, or <code>null</code> if the object does not exist
      */
-    public Optional(final T objectOrNull, final String name)
+    public Optional(final T objectOrNull)
     {
-        this(objectOrNull,name,true);
+        this(objectOrNull,true);
     }
 
     /**
-     * @param objectOrNull the object to wrap, or
-     * <code>null</code> if the object doesn't exist
-     * @param name a readable name for the wrapped object (for error messages)
-     * @param considerTwoNullsEqual 
+     * Initializes an optional wrapped object. You can define if two null wrapped
+     * objects should be considered equal of not.
+     * @param objectOrNull
+     *            the object to wrap, or <code>null</code> if the object does not exist
+     * @param considerTwoNullsEqual
+     *            <code>true</code> if the <code>equals</code> method should consider
+     *            two null (wrapped) references as equal
      */
-    public Optional(final T objectOrNull, final String name, final boolean considerTwoNullsEqual)
+    public Optional(final T objectOrNull, final boolean considerTwoNullsEqual)
     {
         this.t = objectOrNull;
         this.exists = (this.t != null);
         this.considerTwoNullsEqual = considerTwoNullsEqual;
-
-        if (name != null)
-        {
-            this.name = name;
-        }
-        else
-        {
-            this.name = "[unnamed field]";
-        }
 
         if (this.exists)
         {
@@ -56,18 +87,14 @@ public class Optional<T>
         }
         else
         {
-            this.whereSet = new IllegalStateException(this.name+" set to null.");
+            /*
+             * If the object we are wrapping doesn't exist, then get the
+             * current stack trace. We will use it later if the user calls
+             * the "get" method.
+             */
+            this.whereSet = new IllegalStateException("Object set to null.");
             this.whereSet.fillInStackTrace();
         }
-    }
-
-    /**
-     * The name (passed into the constructor).
-     * @return the name
-     */
-    public String getName()
-    {
-        return this.name;
     }
 
     /**
@@ -79,30 +106,37 @@ public class Optional<T>
     }
 
     /**
-     * Gets the wrapped object (passed into the constructor),
-     * as long as it exists.
+     * Gets the wrapped object (passed into the constructor), as long as it exists.
+     * 
      * @return the wrapped object (never <code>null</code>)
-     * @throws FieldDoesNotExist if the wrapped object does not exist
+     * @throws FieldDoesNotExist
+     *             if the wrapped object does not exist
      */
     public T get() throws FieldDoesNotExist
     {
         if (!this.exists)
         {
-            throw new FieldDoesNotExist(this.name,this.whereSet);
+            throw new FieldDoesNotExist(this.whereSet);
         }
         return this.t;
     }
 
-    private static class FieldDoesNotExist extends RuntimeException
+    public static class FieldDoesNotExist extends RuntimeException
     {
-        FieldDoesNotExist(final String name, final Throwable cause)
+        FieldDoesNotExist(final Throwable cause)
         {
-            super("Field does not exist: "+name,cause);
+            super("Field does not exist.",cause);
         }
     }
 
-
-
+    /**
+     * Indicates whether the given object's wrapped object is
+     * equal to this object's wrapped object. If both objects
+     * are wrapping a null object, then this method returns
+     * true or false depending on the considerTwoNullsEqual
+     * parameter passed into the constructor of this object.
+     * @param other
+     */
     @Override
     public boolean equals(final Object other)
     {
@@ -120,6 +154,10 @@ public class Optional<T>
         return this.t.equals(that.t);
     }
 
+    /**
+     * Returns the hash code of the wrapped object, or zero if
+     * the wrapped object is null.
+     */
     @Override
     public int hashCode()
     {
@@ -131,12 +169,16 @@ public class Optional<T>
         return this.t.hashCode();
     }
 
+    /**
+     * Returns the string representation of the wrapped object, or "[null]" if
+     * the wrapped object is null.
+     */
     @Override
     public String toString()
     {
         if (!this.exists)
         {
-            return "["+this.name+" missing]";
+            return "[null]";
         }
         return this.t.toString();
     }
