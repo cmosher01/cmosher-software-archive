7fbeac7215b8567c666b9fff51b14474f02c7271
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-12 04:39:47 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9764 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- core50/src/nu/mine/mosher/core/Closer.java
+++ core50/src/nu/mine/mosher/core/Closer.java
@@ -1,9 +1,7 @@
 package nu.mine.mosher.core;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,52 +16,6 @@ public final class Closer
 		assert false : "can't instantiate";
 	}
 
-//	private static final Object lock = new Object();
-//	private static Method methodClose;
-	private static List<Throwable> rException = new ArrayList<Throwable>();
-//
-//	/**
-//	 * @param object
-//	 */
-//	public void close(final T object)
-//	{
-//		try
-//		{
-//			tryClose(object);
-//		}
-//		catch (final Throwable ignore)
-//		{
-//			rException.add(ignore);
-//		}
-//	}
-//
-	/**
-	 * @return <code>true</code> if any errors have occurred calling <code>close</code> for this class
-	 */
-	public static boolean hasErrors()
-	{
-		return !rException.isEmpty();
-	}
-//
-//	/**
-//	 * @param object
-//	 * @throws SecurityException
-//	 * @throws NoSuchMethodException
-//	 * @throws IllegalArgumentException
-//	 * @throws IllegalAccessException
-//	 * @throws InvocationTargetException
-//	 */
-//	private void tryClose(final T object) throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException
-//	{
-//		synchronized (lock)
-//		{
-//			if (methodClose == null)
-//			{
-//				methodClose = object.getClass().getMethod("close");
-//			}
-//		}
-//		methodClose.invoke(object);
-//	}
 	private static final Map<Class<?>,Method> mClasses = new HashMap<Class<?>,Method>();
 
 	/**
@@ -84,24 +36,20 @@ public final class Closer
 	 * }
 	 * </pre>
 	 * 
-	 * Performance considerations: on an Intel-based Windows PC
-	 * with Java 1.4.2, where calling close on an object took
-	 * about 4 nanoseconds, calling UniversalCloser2.close on
-	 * that object took about 384 nanoseconds.
 	 * @param <T> 
 	 * 
 	 * @param object the Object whose close() method is to be called.
 	 */
-	public static<T> void close(final T object)
+	public static<T> Throwable close(final T object)
 	{
 		try
 		{
 			getCloseMethod(object.getClass()).invoke(object);
+			return null;
 		}
-		catch (final Throwable ignore)
+		catch (final Throwable e)
 		{
-			rException.add(ignore);
-//			ignore.printStackTrace();
+			return e;
 		}
 	}
 
