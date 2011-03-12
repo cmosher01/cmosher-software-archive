d71c84e1a98ad32cc42847833e71d395022d6c9c
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-03-12 03:48:14 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@9759 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

---
+++ optionalObject/src/nu/mine/mosher/util/Nothing.java
@@ -0,0 +1,34 @@
+package nu.mine.mosher.util;
+
+/**
+ * <p>
+ * An {@link Optional} that contains <code>null</code> (that is,
+ * one whose {@link Optional#exists() exists} method returns <code>false</code>).
+ * </p>
+ * <p>
+ * For example:
+ * <blockquote>
+ * <code>
+ * new Nothing&lt;String&gt;()
+ * </code>
+ * </blockquote>
+ * is equivalent to:
+ * <blockquote>
+ * <code>
+ * new Optional&lt;String&gt;(null)
+ * </code>
+ * </blockquote>
+ * </p>
+ * @author mosherc
+ * @param <T> data-type of the wrapped object (which does not exist)
+ */
+public class Nothing<T> extends Optional<T>
+{
+    /**
+     * Initializes this optional object to one that is empty.
+     */
+    public Nothing()
+    {
+        super(null);
+    }
+}
