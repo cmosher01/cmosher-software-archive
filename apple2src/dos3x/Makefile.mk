8c42a79a26cdfed782ab7aea3f0fb6581af32530
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-06-15 20:48:25 +0000

    remove NODELAY from builds
    git-svn-id: https://subversion.assembla.com/svn/cmosher@9985 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- apple2src/dos3x/Makefile.mk
+++ apple2src/dos3x/Makefile.mk
@@ -83,7 +83,7 @@ MODS = dos reloc main filemgr boot1 boot2 rwts rwtsapi
 
 
 .INTERMEDIATE: 13sector/controller/disk2.s65
-13sector/controller/disk2.ex65: CA65DEFS = -D VERSION=13 -D NODELAY
+13sector/controller/disk2.ex65: CA65DEFS = -D VERSION=13
 13sector/controller/disk2.ex65: 13sector/controller/disk2.o65 controller/disk2.ld65
 13sector/controller/disk2.s65: controller/disk2.s65
 	mkdir -p $(@D)
@@ -93,7 +93,7 @@ MODS = dos reloc main filemgr boot1 boot2 rwts rwtsapi
 
 13sector/disks/dos310/clean310.nib: 13sector/disks/dos310/clean310.d13
 OBJS_310 = $(foreach mod,$(MODS),13sector/software/dos310/$(mod).o65)
-13sector/software/dos310/dos.ex65: CA65DEFS = -D VERSION=310 -D NODELAY
+13sector/software/dos310/dos.ex65: CA65DEFS = -D VERSION=310
 13sector/software/dos310/dos.ex65: $(OBJS_310) software/dos.ld65
 13sector/software/dos310/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
@@ -107,7 +107,7 @@ OBJS_310 = $(foreach mod,$(MODS),13sector/software/dos310/$(mod).o65)
 
 13sector/disks/dos320/clean320.nib: 13sector/disks/dos320/clean320.d13
 OBJS_320 = $(foreach mod,$(MODS),13sector/software/dos320/$(mod).o65)
-13sector/software/dos320/dos.ex65: CA65DEFS = -D VERSION=320 -D NODELAY
+13sector/software/dos320/dos.ex65: CA65DEFS = -D VERSION=320
 13sector/software/dos320/dos.ex65: $(OBJS_320) software/dos.ld65
 13sector/software/dos320/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
@@ -121,7 +121,7 @@ OBJS_320 = $(foreach mod,$(MODS),13sector/software/dos320/$(mod).o65)
 
 13sector/disks/dos321/clean321.nib: 13sector/disks/dos321/clean321.d13
 OBJS_321 = $(foreach mod,$(MODS),13sector/software/dos321/$(mod).o65)
-13sector/software/dos321/dos.ex65: CA65DEFS = -D VERSION=321 -D NODELAY
+13sector/software/dos321/dos.ex65: CA65DEFS = -D VERSION=321
 13sector/software/dos321/dos.ex65: $(OBJS_321) software/dos.ld65
 13sector/software/dos321/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
@@ -136,7 +136,7 @@ OBJS_321 = $(foreach mod,$(MODS),13sector/software/dos321/$(mod).o65)
 
 
 .INTERMEDIATE: 16sector/controller/disk2.s65
-16sector/controller/disk2.ex65: CA65DEFS = -D VERSION=16 -D NODELAY
+16sector/controller/disk2.ex65: CA65DEFS = -D VERSION=16
 16sector/controller/disk2.ex65: 16sector/controller/disk2.o65 controller/disk2.ld65
 16sector/controller/disk2.s65: controller/disk2.s65
 	mkdir -p $(@D)
@@ -144,7 +144,7 @@ OBJS_321 = $(foreach mod,$(MODS),13sector/software/dos321/$(mod).o65)
 
 16sector/disks/dos330/clean330.nib: 16sector/disks/dos330/clean330.do
 OBJS_330 = $(foreach mod,$(MODS),16sector/software/dos330/$(mod).o65)
-16sector/software/dos330/dos.ex65: CA65DEFS = -D VERSION=330 -D NODELAY
+16sector/software/dos330/dos.ex65: CA65DEFS = -D VERSION=330
 16sector/software/dos330/dos.ex65: $(OBJS_330) software/dos.ld65
 16sector/software/dos330/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
@@ -158,7 +158,7 @@ OBJS_330 = $(foreach mod,$(MODS),16sector/software/dos330/$(mod).o65)
 
 16sector/disks/dos331/clean331.nib: 16sector/disks/dos331/clean331.do
 OBJS_331 = $(foreach mod,$(MODS),16sector/software/dos331/$(mod).o65)
-16sector/software/dos331/dos.ex65: CA65DEFS = -D VERSION=331 -D NODELAY
+16sector/software/dos331/dos.ex65: CA65DEFS = -D VERSION=331
 16sector/software/dos331/dos.ex65: $(OBJS_331) software/dos.ld65
 16sector/software/dos331/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
@@ -172,7 +172,7 @@ OBJS_331 = $(foreach mod,$(MODS),16sector/software/dos331/$(mod).o65)
 
 16sector/disks/dos332/clean332.nib: 16sector/disks/dos332/clean332.do
 OBJS_332 = $(foreach mod,$(MODS),16sector/software/dos332/$(mod).o65)
-16sector/software/dos332/dos.ex65: CA65DEFS = -D VERSION=332 -D NODELAY
+16sector/software/dos332/dos.ex65: CA65DEFS = -D VERSION=332
 16sector/software/dos332/dos.ex65: $(OBJS_332) software/dos.ld65
 16sector/software/dos332/%.s65: software/%.s65 software/zpabs.s65 software/symbols.s65
 	mkdir -p $(@D)
