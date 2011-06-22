473e59023a26c79075b49407f3ddc783da59beef
cmosher <cmosher@884d43a4-ffbe-47e9-90ca-fd8c37d7eb32>
2011-06-22 13:20:52 +0000

    git-svn-id: https://subversion.assembla.com/svn/cmosher@10080 884d43a4-ffbe-47e9-90ca-fd8c37d7eb32

--- TestServletApp/docroot/hello.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-This is my first ServletApp test <%= new java.util.Date() %>
-</body>
-</html>
--- TestServletApp/src/HelloJSP.java
+++
@@ -1,9 +0,0 @@
-import servletapp.ServletApp;
-
-public class HelloJSP
-{
-    public static void main(String[] rArg) throws Throwable
-    {
-    	ServletApp.startBrowser("/hello.jsp");
-    }
-}
